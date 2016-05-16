2cc0154b4f15a392426bdc4055ef034242514e76
Paul Nakata <paul@pmn.org>
2016-05-16 13:52:17 -0700

    Better looking icons for the task bar

--- lib/browser/tray.ts
+++ lib/browser/tray.ts
@@ -2,7 +2,19 @@ import {Menu, Tray} from 'electron'
 
 export default class TrayIcon {
   public load() {
-      let appIcon = new Tray(`${__dirname}/../../static/GitHub-Mark-16px.png`);
+      let platform = require('os').platform();
+      let trayImage = "";
+      let imageFolder = __dirname + '/../../static';
+
+      // Determine appropriate icon for platform
+      if (platform == 'darwin') {
+          trayImage = imageFolder + '/iconDarkTemplate.png';
+      }
+      else if (platform == 'win32') {
+          trayImage = imageFolder + '/icon-light.ico';
+      }
+      let appIcon = new Tray(trayImage);
+
       const contextMenu = Menu.buildFromTemplate([
         {label: 'About GitHub Client', checked: false},
         {type: 'separator'},
