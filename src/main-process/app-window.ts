33cbef3840faea210b52f8d49d54416059b1ae67
Markus Olsson <j.markus.olsson@gmail.com>
2016-05-26 18:26:10 +0200

    Add explicit window background color to fix subpixel aliasing on Windows

--- src/main-process/app-window.ts
+++ src/main-process/app-window.ts
@@ -13,7 +13,10 @@ export default class AppWindow {
       width: 800,
       height: 600,
       show: false,
-      titleBarStyle: 'hidden'
+      titleBarStyle: 'hidden',
+      // This fixes subpixel aliasing on Windows
+      // See https://github.com/atom/atom/commit/683bef5b9d133cb194b476938c77cc07fd05b972
+      backgroundColor: "#fff"
   })
 
     this.stats = stats
