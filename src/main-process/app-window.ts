45b4aa8f99065441b0718d0f86cb3d10d60c2b68
Markus Olsson <j.markus.olsson@gmail.com>
2016-05-26 18:49:58 +0200

    Reindent

--- src/main-process/app-window.ts
+++ src/main-process/app-window.ts
@@ -17,7 +17,7 @@ export default class AppWindow {
       // This fixes subpixel aliasing on Windows
       // See https://github.com/atom/atom/commit/683bef5b9d133cb194b476938c77cc07fd05b972
       backgroundColor: "#fff"
-  })
+    })
 
     this.stats = stats
   }
