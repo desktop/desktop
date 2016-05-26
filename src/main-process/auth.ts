1ef567ab8cf43e7aa274eb2a026328540d984d1f
joshaber <joshaber@gmail.com>
2016-05-26 17:01:25 -0400

    Dummy out some OAuth stuff

---
+++ src/main-process/auth.ts
@@ -0,0 +1,48 @@
+import {shell} from 'electron'
+
+const ClientID = 'de0e3c7e9973e1c4dd77'
+const ClientSecret = '4b35aab1581a32e23af0d930f2a294ae3bb84960'
+
+const DefaultHeaders: {[key: string]: string} = {
+  'Accept': 'application/vnd.github.v3+json, application/json',
+  'Content-Type': 'application/json',
+  // TODO: We should get these from package.json at compile time.
+  'User-Agent': 'GitHubClient/0.0.1'
+}
+
+let authState: string = null
+
+export function requestToken(code: string): Promise<string> {
+  // TODO: This should take a server URL.
+  return fetch('https://github.com/login/oauth/access_token', {
+      method: 'post',
+      headers: DefaultHeaders,
+      body: JSON.stringify({
+          'client_id': ClientID,
+          'client_secret': ClientSecret,
+          'code': code,
+          'state': authState
+      })
+    })
+    .then(response => response.json())
+    .then(response => response.access_token)
+}
+
+function getOAuthURL(state: string): string {
+  // TODO: This should take a server URL.
+  return 'https://github.com/login/oauth/authorize?client_id=' + ClientID + '&scope=repo&state=' + state
+}
+
+function guid(): string {
+  function s4(): string {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1)
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
+}
+
+export function authenticate() {
+  authState = guid()
+  shell.openExternal(getOAuthURL(authState))
+}
--- src/main-process/main.ts
+++ src/main-process/main.ts
@@ -1,11 +1,15 @@
-import {app} from 'electron'
+import * as URL from 'url'
+import {app, ipcMain} from 'electron'
 
 import AppWindow from './app-window'
 import Stats from './stats'
+import {requestToken, authenticate} from './auth'
 
 const stats = new Stats()
 
 let mainWindow: AppWindow = null
+
+type URLAction = 'oauth' | 'unknown'
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -13,10 +17,26 @@ app.on('window-all-closed', () => {
   }
 })
 
+app.on('will-finish-launching', () => {
+  app.on('open-url', (event, url) => {
+    const parsedURL = URL.parse(url, true)
+    const action = parseURLAction(parsedURL)
+    if (action === 'oauth') {
+      requestToken(parsedURL.query.code)
+    } else {
+      console.error(`I dunno how to handle this URL: ${url}`)
+    }
+
+    event.preventDefault()
+  })
+})
+
 app.on('ready', () => {
   stats.readyTime = Date.now()
 
   app.setAsDefaultProtocolClient('x-github-client')
+  ipcMain.on('request-auth', authenticate)
+
   mainWindow = new AppWindow(stats)
   mainWindow.onClose(() => {
     mainWindow = null
@@ -24,3 +44,12 @@ app.on('ready', () => {
 
   mainWindow.load()
 })
+
+function parseURLAction(parsedURL: URL.Url): URLAction {
+  const actionName = parsedURL.hostname
+  if (actionName === 'oauth') {
+    return 'oauth'
+  } else {
+    return 'unknown'
+  }
+}
