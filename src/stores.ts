fa0fa3cb81895ed2faf40579ce701dbc5c251c3f
joshaber <joshaber@gmail.com>
2016-05-31 15:17:34 -0400

    Secure second

--- src/stores.ts
+++ src/stores.ts
@@ -1,9 +1,10 @@
-export interface SecureStore {
-  setItem(key: string, login: string, value: string): void
-  getItem(key: string, login: string): string
-}
-
 export interface DataStore {
   setItem(key: string, value: string): void
   getItem(key: string): string
+}
+
+
+export interface SecureStore {
+  setItem(key: string, login: string, value: string): void
+  getItem(key: string, login: string): string
 }
