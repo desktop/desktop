07201fbb48dfecd35d2bca1bbf1befea177e15fb
Markus Olsson <j.markus.olsson@gmail.com>
2016-06-13 20:41:53 +0200

    Use the new OcticonSymbol type for the component

--- src/octicon.tsx
+++ src/octicon.tsx
@@ -1,23 +1,28 @@
 import * as React from 'react'
+import {OcticonSymbol} from './octicons.generated'
+
+export {OcticonSymbol as OcticonSymbol};
 
 interface OcticonProps {
   width?: number,
   height?: number,
-  symbol: string
+  symbol: OcticonSymbol
 }
 
-export default class Octicon extends React.Component<OcticonProps, void> {
+export class Octicon extends React.Component<OcticonProps, void> {
 
-  public static defaultProps: OcticonProps = { width: 16, height: 16, symbol: "" };
+  public static defaultProps: OcticonProps = { width: 16, height: 16, symbol: OcticonSymbol.mark_github };
 
   public constructor(props: OcticonProps) {
     super(props)
   }
 
   public render() {
+    const symbol = this.props.symbol;
+    const viewBox = `0 0 ${symbol.w} ${symbol.h}`
     return (
-      <svg aria-hidden="true" class="octicon" width="12" height="16" role="img" version="1.1" viewBox="0 0 12 16">
-        <path d="M12 9H7v5H5V9H0V7h5V2h2v5h5v2z"></path>
+      <svg aria-hidden="true" className="octicon" width={this.props.width} height={this.props.height} role="img" version="1.1" viewBox={viewBox}>
+        <path d={symbol.d}></path>
       </svg>
     )
   }
