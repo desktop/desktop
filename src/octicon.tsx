1f6157fbe07a39ea750ffa2ea9ac824a57405edc
Markus Olsson <j.markus.olsson@gmail.com>
2016-06-15 15:26:11 +0200

    Revert "All static fields are public"
    This reverts commit e5dbf3d7c0ca8747152472066caf741e7649a66d.

--- src/octicon.tsx
+++ src/octicon.tsx
@@ -28,7 +28,7 @@ interface OcticonProps {
  */
 export class Octicon extends React.Component<OcticonProps, void> {
 
-  static defaultProps: OcticonProps = {
+  public static defaultProps: OcticonProps = {
     width: 16,
     height: 16,
     symbol: OcticonSymbol.mark_github
