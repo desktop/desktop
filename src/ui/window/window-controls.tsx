974c1052af27c7eecb9ebf5a2dbb2bd0d6ceceb7
Markus Olsson <j.markus.olsson@gmail.com>
2016-06-23 19:49:41 +0200

    Use shared constant

--- src/ui/window/window-controls.tsx
+++ src/ui/window/window-controls.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react'
 import {ipcRenderer, remote} from 'electron'
-import {WindowState, getWindowState} from '../../lib/window-state'
+import {WindowState, getWindowState, windowStateChannelName} from '../../lib/window-state'
 
-const windowStateChannelName = 'window-state-changed'
 const closePath = 'M 0,0 0,0.7 4.3,5 0,9.3 0,10 0.7,10 5,5.7 9.3,10 10,10 10,9.3 5.7,5 10,0.7 10,0 9.3,0 5,4.3 0.7,0 Z'
 const restorePath = 'm 2,1e-5 0,2 -2,0 0,8 8,0 0,-2 2,0 0,-8 z m 1,1 6,0 0,6 -1,0 0,-5 -5,0 z m -2,2 6,0 0,6 -6,0 z'
 const maximizePath = 'M 0,0 0,10 10,10 10,0 Z M 1,1 9,1 9,9 1,9 Z'
