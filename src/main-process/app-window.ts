dfe4fd5d547f7e26ccd76444ab6ce51195345321
Markus Olsson <j.markus.olsson@gmail.com>
2016-06-23 23:08:59 +0200

    Cool backtick bruh

--- src/main-process/app-window.ts
+++ src/main-process/app-window.ts
@@ -20,7 +20,7 @@ export default class AppWindow {
       width: 800,
       height: 600,
       show: false,
-      // This fixes subpixel aliasing on Windows`
+      // This fixes subpixel aliasing on Windows
       // See https://github.com/atom/atom/commit/683bef5b9d133cb194b476938c77cc07fd05b972
       backgroundColor: '#fff'
     }
