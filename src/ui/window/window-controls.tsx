4d0f5f1c4ba10a7a75c0b62296c2f6736a954ae9
Markus Olsson <j.markus.olsson@gmail.com>
2016-06-23 16:15:03 +0200

    Omg looks at these artisanal handcrafted win10 replica paths

--- src/ui/window/window-controls.tsx
+++ src/ui/window/window-controls.tsx
@@ -3,6 +3,10 @@ import {ipcRenderer, remote} from 'electron'
 import {WindowState, getWindowState} from '../../lib/window-state'
 
 const windowStateChannelName = 'window-state-changed'
+const closePath = 'M 0,0 0,0.7 4.3,5 0,9.3 0,10 0.7,10 5,5.7 9.3,10 10,10 10,9.3 5.7,5 10,0.7 10,0 9.3,0 5,4.3 0.7,0 Z'
+const restorePath = 'm 2,1e-5 0,2 -2,0 0,8 8,0 0,-2 2,0 0,-8 z m 1,1 6,0 0,6 -1,0 0,-5 -5,0 z m -2,2 6,0 0,6 -6,0 z'
+const maximizePath = 'M 0,0 0,10 10,10 10,0 Z M 1,1 9,1 9,9 1,9 Z'
+const minimizePath = 'M 0,5 10,5 10,6 0,6 Z'
 
 interface WindowControlState {
   windowState: WindowState
@@ -24,11 +28,15 @@ export class WindowControls extends React.Component<void, WindowControlState> {
     this.setState({ windowState: args as WindowState })
   }
 
-  private renderButton(name: string, onClick: React.EventHandler<React.MouseEvent>) {
+  private renderButton(name: string, onClick: React.EventHandler<React.MouseEvent>, path: string) {
     const className = `window-control ${name}`
+    const title = name[0].toUpperCase() + name.substring(1)
+
     return (
-      <button tabIndex='-1' className={className} onClick={onClick}>
-        {name}
+      <button area-label={name} title={title} tabIndex='-1' className={className} onClick={onClick}>
+        <svg aria-hidden='true' role='img' version='1.1' width='10' height='10'>
+          <path d={path}></path>
+        </svg>
       </button>)
   }
 
@@ -55,11 +63,11 @@ export class WindowControls extends React.Component<void, WindowControlState> {
       return null
     }
 
-    const min = this.renderButton('minimize', this.onMinimize)
+    const min = this.renderButton('minimize', this.onMinimize, minimizePath)
     const maximizeOrRestore = this.state.windowState === 'maximized'
-      ? this.renderButton('restore', this.onRestore)
-      : this.renderButton('maximize', this.onMaximize)
-    const close = this.renderButton('close', this.onClose)
+      ? this.renderButton('restore', this.onRestore, restorePath)
+      : this.renderButton('maximize', this.onMaximize, maximizePath)
+    const close = this.renderButton('close', this.onClose, closePath)
 
     return (
       <div className='window-controls'>
