2568c1182425997065181077b512db709f63a472
joshaber <joshaber@gmail.com>
2016-07-01 11:33:14 -0400

    Split out the communication functions

--- src/shared-process/index.ts
+++ src/shared-process/index.ts
@@ -1,5 +1,4 @@
-import {ipcRenderer, remote} from 'electron'
-import {Message} from './message'
+import {ipcRenderer} from 'electron'
 import tokenStore from './token-store'
 import UsersStore from './users-store'
 import {requestToken, askUserToAuth, getDotComEndpoint} from './auth'
@@ -8,13 +7,9 @@ import {URLActionType, isOAuthAction} from '../lib/parse-url'
 import Database from './database'
 import RepositoriesStore from './repositories-store'
 import Repository, {IRepository} from '../models/repository'
-
-const {BrowserWindow} = remote
+import {dispatch, register, broadcastUpdate} from './communication'
 
 const Octokat = require('octokat')
-
-type SharedProcessFunction = (args: any) => Promise<any>
-const registeredFunctions: {[key: string]: SharedProcessFunction} = {}
 
 const usersStore = new UsersStore(localStorage, tokenStore)
 usersStore.loadFromStore()
@@ -46,7 +41,7 @@ register('add-repositories', async ({repositories}: {repositories: IRepository[]
     await repositoriesStore.addRepository(repo)
   }
 
-  broadcastUpdate()
+  broadcastUpdate(usersStore, repositoriesStore)
 })
 
 register('get-repositories', () => {
@@ -55,8 +50,15 @@ register('get-repositories', () => {
 
 register('url-action', async ({action}: {action: URLActionType}) => {
   if (isOAuthAction(action)) {
-    await addUserWithCode(action.args.code)
-    broadcastUpdate()
+    try {
+      const token = await requestToken(action.args.code)
+      const octo = new Octokat({token})
+      const user = await octo.user.fetch()
+      usersStore.addUser(new User(user.login, getDotComEndpoint(), token))
+    } catch (e) {
+      console.error(`Error adding user: ${e}`)
+    }
+    broadcastUpdate(usersStore, repositoriesStore)
   }
 })
 
@@ -68,58 +70,3 @@ register('request-oauth', () => {
 ipcRenderer.on('shared/request', (event, args) => {
   dispatch(args[0])
 })
-
-/**
- * Dispatch the received message to the appropriate function and respond with
- * the return value.
- */
-function dispatch(message: Message) {
-  const name = message.name
-  if (!name) {
-    console.error('Unnamed message sent to shared process:')
-    console.error(message)
-    return
-  }
-
-  const fn = registeredFunctions[name]
-  if (!fn) {
-    console.error('No handler found for message:')
-    console.error(message)
-    return
-  }
-
-  const guid = message.guid
-  const args = message.args
-  const promise = fn(args)
-  promise.then(result => {
-    BrowserWindow.getAllWindows().forEach(window => {
-      window.webContents.send(`shared/response/${guid}`, [result])
-    })
-  })
-}
-
-/** Register a function to respond to requests with the given name. */
-function register(name: string, fn: SharedProcessFunction) {
-  registeredFunctions[name] = fn
-}
-
-/** Tell all the windows that something was updated. */
-function broadcastUpdate() {
-  BrowserWindow.getAllWindows().forEach(async (window) => {
-    const repositories = await repositoriesStore.getRepositories()
-    const state = {users: usersStore.getUsers(), repositories}
-    window.webContents.send('shared/did-update', [{state}])
-  })
-}
-
-/** Request a token, given an OAuth code, and add the user. */
-async function addUserWithCode(code: string) {
-  try {
-    const token = await requestToken(code)
-    const octo = new Octokat({token})
-    const user = await octo.user.fetch()
-    usersStore.addUser(new User(user.login, getDotComEndpoint(), token))
-  } catch (e) {
-    console.error(`Error adding user: ${e}`)
-  }
-}
