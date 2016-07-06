08ae4a94b2114707e264701cd131448523abed77
Markus Olsson <j.markus.olsson@gmail.com>
2016-07-06 10:30:00 +0200

    Persist window state

--- src/main-process/app-window.ts
+++ src/main-process/app-window.ts
@@ -5,15 +5,27 @@ import SharedProcess from '../shared-process/shared-process'
 import {WindowState, windowStateChannelName} from '../lib/window-state'
 import {buildDefaultMenu} from './menu'
 
+const windowStateKeeper:
+  (opts: ElectronWindowState.WindowStateKeeperOptions) => ElectronWindowState.WindowState =
+  require('electron-window-state')
+
 export default class AppWindow {
   private window: Electron.BrowserWindow
   private sharedProcess: SharedProcess
   private stats: Stats
 
   public constructor(stats: Stats, sharedProcess: SharedProcess) {
+
+    const savedWindowState: ElectronWindowState.WindowState = windowStateKeeper({
+      defaultWidth: 800,
+      defaultHeight: 600
+    })
+
     const windowOptions: Electron.BrowserWindowOptions = {
-      width: 800,
-      height: 600,
+      x: savedWindowState.x,
+      y: savedWindowState.y,
+      width: savedWindowState.width,
+      height: savedWindowState.height,
       show: false,
       // This fixes subpixel aliasing on Windows
       // See https://github.com/atom/atom/commit/683bef5b9d133cb194b476938c77cc07fd05b972
@@ -27,6 +39,7 @@ export default class AppWindow {
     }
 
     this.window = new BrowserWindow(windowOptions)
+    savedWindowState.manage(this.window)
 
     this.stats = stats
 
