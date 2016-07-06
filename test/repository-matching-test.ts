b3a383e709b2020f896d720ad7084586c9a19b08
joshaber <joshaber@gmail.com>
2016-07-06 14:10:24 -0400

    Test repository matching

---
+++ test/repository-matching-test.ts
@@ -0,0 +1,34 @@
+import * as chai from 'chai'
+const expect = chai.expect
+
+import {matchGitHubRepository} from '../src/lib/repository-matching'
+import User from '../src/models/user'
+
+describe('Repository matching', () => {
+  it('matches HTTPS URLs', () => {
+    const users = [new User('alovelace', 'https://api.github.com', '')]
+    const repo = matchGitHubRepository(users, 'https://github.com/someuser/somerepo.git')
+    expect(repo.getName()).to.equal('somerepo')
+    expect(repo.getOwner().getLogin()).to.equal('someuser')
+  })
+
+  it('matches git URLs', () => {
+    const users = [new User('alovelace', 'https://api.github.com', '')]
+    const repo = matchGitHubRepository(users, 'git:github.com/someuser/somerepo.git')
+    expect(repo.getName()).to.equal('somerepo')
+    expect(repo.getOwner().getLogin()).to.equal('someuser')
+  })
+
+  it('matches SSH URLs', () => {
+    const users = [new User('alovelace', 'https://api.github.com', '')]
+    const repo = matchGitHubRepository(users, 'git@github.com:someuser/somerepo.git')
+    expect(repo.getName()).to.equal('somerepo')
+    expect(repo.getOwner().getLogin()).to.equal('someuser')
+  })
+
+  it(`doesn't match if there aren't any users with that endpoint`, () => {
+    const users = [new User('alovelace', 'https://github.babbageinc.com', '')]
+    const repo = matchGitHubRepository(users, 'https://github.com/someuser/somerepo.git')
+    expect(repo).to.equal(null)
+  })
+})
