a56046a270506594e1f478a3529f9c254bc24cdb
joshaber <joshaber@gmail.com>
2016-07-07 13:15:59 -0400

    Stub out history too

--- src/history/index.tsx
+++ src/history/index.tsx
@@ -1,7 +1,16 @@
 import * as React from 'react'
+import CommitList from './commit-list'
+import CommitSummary from './commit-summary'
+import FileDiff from '../file-diff'
 
 export default class History extends React.Component<void, void> {
   public render() {
-    return <div id='history'>History goes here</div>
+    return (
+      <div id='history'>
+        <CommitList/>
+        <CommitSummary/>
+        <FileDiff/>
+      </div>
+    )
   }
 }
