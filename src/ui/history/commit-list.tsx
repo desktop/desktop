677eb16779793d712e44809cf0353741e1c8d3e4
joshaber <joshaber@gmail.com>
2016-07-13 19:47:25 -0700

    Do the history fetching in History

--- src/ui/history/commit-list.tsx
+++ src/ui/history/commit-list.tsx
@@ -1,48 +1,28 @@
 import * as React from 'react'
-import Repository from '../../models/repository'
-import {Commit, LocalGitOperations} from '../../lib/local-git-operations'
+import {Commit} from '../../lib/local-git-operations'
 import {default as CommitView} from './commit'
 import List from '../list'
 
 interface ICommitListProps {
-  repository: Repository
-}
-
-interface ICommitListState {
+  onSelectionChanged: (row: number) => void
   commits: Commit[]
+  selectedRow: number
 }
 
-export default class CommitList extends React.Component<ICommitListProps, ICommitListState> {
-  public constructor(props: ICommitListProps) {
-    super(props)
-
-    this.state = {
-      commits: [],
-    }
-  }
-
-  public async componentDidMount() {
-    const commits = await LocalGitOperations.getHistory(this.props.repository)
-    this.setState(Object.assign({}, this.state, {commits}))
-  }
-
+export default class CommitList extends React.Component<ICommitListProps, void> {
   private renderCommit(row: number) {
-    const commit = this.state.commits[row]
+    const commit = this.props.commits[row]
     return <CommitView commit={commit} key={commit.getSHA()}/>
-  }
-
-  private selectionChanged(row: number) {
-    this.setState(Object.assign({}, this.state, {selectedRow: row}))
   }
 
   public render() {
     return (
       <div id='commit-list'>
-        <List itemCount={this.state.commits.length}
+        <List itemCount={this.props.commits.length}
               itemHeight={44}
-              selectedRow={-1}
+              selectedRow={this.props.selectedRow}
               renderItem={row => this.renderCommit(row)}
-              onSelectionChanged={row => this.selectionChanged}/>
+              onSelectionChanged={row => this.props.onSelectionChanged}/>
       </div>
     )
   }
--- src/ui/history/index.tsx
+++ src/ui/history/index.tsx
@@ -3,16 +3,39 @@ import CommitList from './commit-list'
 import CommitSummary from './commit-summary'
 import FileDiff from '../file-diff'
 import Repository from '../../models/repository'
+import {Commit, LocalGitOperations} from '../../lib/local-git-operations'
 
 interface IHistoryProps {
   repository: Repository
 }
 
-export default class History extends React.Component<IHistoryProps, void> {
+interface IHistoryState {
+  commits: Commit[]
+  selectedRow: number
+}
+
+export default class History extends React.Component<IHistoryProps, IHistoryState> {
+  public constructor(props: IHistoryProps) {
+    super(props)
+
+    this.state = {commits: [], selectedRow: -1}
+  }
+
+  public async componentDidMount() {
+    const commits = await LocalGitOperations.getHistory(this.props.repository)
+    this.setState(Object.assign({}, this.state, {commits}))
+  }
+
+  private selectionChanged(row: number) {
+    this.setState(Object.assign({}, this.state, {selectedRow: row}))
+  }
+
   public render() {
     return (
       <div id='history'>
-        <CommitList {...this.props}/>
+        <CommitList commits={this.state.commits}
+                    selectedRow={this.state.selectedRow}
+                    onSelectionChanged={row => this.selectionChanged(row)}/>
         <CommitSummary/>
         <FileDiff/>
       </div>
