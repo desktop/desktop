6e7c6f999056878df621a01a685b3475effaa2f9
Brendan Forster <brendan@github.com>
2016-07-15 13:35:10 -0700

    move refreshing the working directory up a level

--- src/ui/changes/changes-list.tsx
+++ src/ui/changes/changes-list.tsx
@@ -4,7 +4,7 @@ import { ChangedFile } from './changed-file'
 import List from '../list'
 
 import Repository from '../../models/repository'
-import { WorkingDirectoryStatus, WorkingDirectoryFileChange} from '../../models/status'
+import { WorkingDirectoryStatus } from '../../models/status'
 
 import { LocalGitOperations } from '../../lib/local-git-operations'
 
@@ -12,89 +12,25 @@ const RowHeight = 20
 
 interface ChangesListProps {
   repository: Repository,
+  workingDirectory: WorkingDirectoryStatus,
   readonly selectedRow: number
   readonly onSelectionChanged: (row: number) => void
+  readonly onIncludeChanged: (row: number, include: boolean) => void
+  readonly onSelectAll: (selectAll: boolean) => void
 }
 
-interface ChangesListState {
-  workingDirectory: WorkingDirectoryStatus
-}
-
-export class ChangesList extends React.Component<ChangesListProps, ChangesListState> {
+export class ChangesList extends React.Component<ChangesListProps, void> {
 
   public constructor(props: ChangesListProps) {
     super(props)
-
-    this.state = {
-      workingDirectory: new WorkingDirectoryStatus()
-    }
   }
 
   private refresh(repository: Repository) {
-    LocalGitOperations.getStatus(repository)
-      .then(result => this.setState({
-        workingDirectory: result.workingDirectory
-      }))
-      .catch(rejected => {
-        console.error(rejected)
-        this.setState({
-         workingDirectory: new WorkingDirectoryStatus()
-       })
-    })
+
   }
-
-  public componentWillReceiveProps(nextProps: ChangesListProps) {
-    this.refresh(nextProps.repository)
-  }
-
-  private onIncludeChanged(file: WorkingDirectoryFileChange, include: boolean) {
-
-    const workingDirectory = this.state.workingDirectory
-
-    const foundFile = workingDirectory.files.find((f, index, array) => {
-      return f.path === file.path
-    })
-
-    if (!foundFile) {
-      console.error('unable to find working directory path to apply included change: ' + file.path)
-      return
-    }
-
-    foundFile.include = include
-
-    const allSelected = workingDirectory.files.every((f, index, array) => {
-      return f.include
-    })
-
-    const noneSelected = workingDirectory.files.every((f, index, array) => {
-      return !f.include
-    })
-
-    if (allSelected && !noneSelected) {
-      workingDirectory.includeAll = true
-    } else if (!allSelected && noneSelected) {
-      workingDirectory.includeAll = false
-    } else {
-      workingDirectory.includeAll = null
-    }
-
-    this.setState({ workingDirectory: workingDirectory })
-  }
-
-  private handleSelectAll(event: React.FormEvent) {
-    const include = (event.target as any).checked
-
-    const workingDirectory = this.state.workingDirectory
-
-    workingDirectory.includeAll = include
-    workingDirectory.includeAllFiles(include)
-
-    this.setState({ workingDirectory: workingDirectory })
-  }
-
 
   private async onCreateCommit(title: string) {
-    const files = this.state.workingDirectory.files.filter(function(file, index, array) {
+    const files = this.props.workingDirectory.files.filter(function(file, index, array) {
       return file.include === true
     })
 
@@ -103,8 +39,13 @@ export class ChangesList extends React.Component<ChangesListProps, ChangesListSt
     await this.refresh(this.props.repository)
   }
 
+  private handleOnChangeEvent(event: React.FormEvent) {
+    const include = (event.target as any).checked
+    this.props.onSelectAll(include)
+  }
+
   private renderRow(row: number): JSX.Element {
-    const file = this.state.workingDirectory.files[row]
+    const file = this.props.workingDirectory.files[row]
     const path = file.path
 
     return (
@@ -112,13 +53,13 @@ export class ChangesList extends React.Component<ChangesListProps, ChangesListSt
                    status={file.status}
                    include={file.include}
                    key={path}
-                   onIncludeChanged={include => this.onIncludeChanged(file, include)}/>
+                   onIncludeChanged={include => this.props.onIncludeChanged(row, include)}/>
     )
   }
 
   public render() {
 
-    const includeAll = this.state.workingDirectory.includeAll
+    const includeAll = this.props.workingDirectory.includeAll
 
     return (
       <div id='changes-list'>
@@ -126,7 +67,7 @@ export class ChangesList extends React.Component<ChangesListProps, ChangesListSt
           <input
             type='checkbox'
             checked={includeAll}
-            onChange={event => this.handleSelectAll(event)}
+            onChange={event => this.handleOnChangeEvent(event) }
             ref={function(input) {
               if (input != null) {
                 input.indeterminate = (includeAll === null)
@@ -135,7 +76,7 @@ export class ChangesList extends React.Component<ChangesListProps, ChangesListSt
         </div>
 
         <List id='changes-list-list'
-              itemCount={this.state.workingDirectory.files.length}
+              itemCount={this.props.workingDirectory.files.length}
               itemHeight={RowHeight}
               renderItem={row => this.renderRow(row)}
               selectedRow={this.props.selectedRow}
--- src/ui/changes/index.tsx
+++ src/ui/changes/index.tsx
@@ -3,13 +3,19 @@ import { ChangesList } from './changes-list'
 import FileDiff from '../file-diff'
 
 import Repository from '../../models/repository'
+import { WorkingDirectoryStatus } from '../../models/status'
+
+
+import { LocalGitOperations } from '../../lib/local-git-operations'
 
 interface ChangesProps {
-  selectedRepo: Repository
+  selectedRepo: Repository,
+
 }
 
 interface ChangesState {
-  selectedRow: number
+  selectedRow: number,
+  workingDirectory: WorkingDirectoryStatus
 }
 
 /** TODO: handle "repository not found" scenario */
@@ -20,14 +26,69 @@ export class Changes extends React.Component<ChangesProps, ChangesState> {
     super(props)
 
     this.state = {
-      selectedRow: -1
+      selectedRow: -1,
+      workingDirectory: new WorkingDirectoryStatus()
     }
+  }
+
+  public componentWillReceiveProps(nextProps: ChangesProps) {
+    this.refreshWorkingDirectory(nextProps.selectedRepo)
+  }
+
+  private refreshWorkingDirectory(repository: Repository) {
+    LocalGitOperations.getStatus(repository)
+      .then(result => this.setState(Object.assign({}, this.state, {workingDirectory: result.workingDirectory})))
+      .catch(rejected => {
+        console.error(rejected)
+        this.setState(Object.assign({}, this.state, {workingDirectory: new WorkingDirectoryStatus()}))
+    })
   }
 
   private handleSelectionChanged(row: number) {
     this.setState(Object.assign({}, this.state, {selectedRow: row}))
 
     // TODO: show file diff for selected item
+  }
+
+  private handleIncludeChanged(row: number, include: boolean) {
+
+    const workingDirectory = this.state.workingDirectory
+
+    const foundFile = workingDirectory.files[row]
+
+    if (!foundFile) {
+      console.error('unable to find working directory path to apply included change: ' + row)
+      return
+    }
+
+    foundFile.include = include
+
+    const allSelected = workingDirectory.files.every((f, index, array) => {
+      return f.include
+    })
+
+    const noneSelected = workingDirectory.files.every((f, index, array) => {
+      return !f.include
+    })
+
+    if (allSelected && !noneSelected) {
+      workingDirectory.includeAll = true
+    } else if (!allSelected && noneSelected) {
+      workingDirectory.includeAll = false
+    } else {
+      workingDirectory.includeAll = null
+    }
+
+    this.setState(Object.assign({}, this.state, { workingDirectory: workingDirectory }))
+  }
+
+  private handleSelectAll(selectAll: boolean) {
+    const workingDirectory = this.state.workingDirectory
+
+    workingDirectory.includeAll = selectAll
+    workingDirectory.includeAllFiles(selectAll)
+
+    this.setState(Object.assign({}, this.state, { workingDirectory: workingDirectory }))
   }
 
   private renderNoSelection() {
@@ -48,8 +109,11 @@ export class Changes extends React.Component<ChangesProps, ChangesState> {
     return (
       <div id='changes'>
         <ChangesList repository={this.props.selectedRepo}
+                     workingDirectory={this.state.workingDirectory}
                      selectedRow={this.state.selectedRow}
-                     onSelectionChanged={event => this.handleSelectionChanged(event)}/>
+                     onSelectionChanged={event => this.handleSelectionChanged(event)}
+                     onIncludeChanged={(row, include) => this.handleIncludeChanged(row, include) }
+                     onSelectAll={selectAll => this.handleSelectAll(selectAll) }/>
         <FileDiff/>
       </div>
     )
