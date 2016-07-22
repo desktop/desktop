30ee916f5d0cf1fe35671b99ad46a103ef1aff72
joshaber <joshaber@gmail.com>
2016-07-22 15:30:33 -0400

    Don't need this declaration anymore.

--- app/src/lib/globals.d.ts
+++ app/src/lib/globals.d.ts
@@ -1,4 +1,2 @@
 /** Is the app running in dev mode? */
 declare const __DEV__: boolean
-
-declare function requestIdleCallback(fn: (timeRemaining: number, didTimeout: boolean) => void, options?: {timeout: number}): number
