a0a824f9e061e934032e960cce8af0bc2e41556f
joshaber <joshaber@gmail.com>
2016-07-21 20:23:41 -0400

    Type declaration for requestIdleCallback.

--- app/src/lib/globals.d.ts
+++ app/src/lib/globals.d.ts
@@ -1,2 +1,4 @@
 /** Is the app running in dev mode? */
 declare const __DEV__: boolean
+
+declare function requestIdleCallback(fn: (timeRemaining: number, didTimeout: boolean) => void, options?: {timeout: number}): number
