d8319a4c19fa26613a684423ccd867ad3c83e268
joshaber <joshaber@gmail.com>
2016-07-25 12:05:28 -0400

    More tests

--- app/test/local-git-operations-test.ts
+++ app/test/local-git-operations-test.ts
@@ -8,6 +8,7 @@ const temp = require('temp').track()
 
 import Repository from '../src/models/repository'
 import { LocalGitOperations } from '../src/lib/local-git-operations'
+import { FileStatus } from '../src/models/status'
 
 describe('LocalGitOperations', () => {
   let repository: Repository | null = null
@@ -28,21 +29,70 @@ describe('LocalGitOperations', () => {
       fs.writeFileSync(path.join(repository!.path, 'README.md'), 'Hi world\n')
 
       const status = await LocalGitOperations.getStatus(repository!)
-      expect(status.workingDirectory.files.length).to.equal(1)
+      const files = status.workingDirectory.files
+      expect(files.length).to.equal(1)
+
+      const file = files[0]
+      expect(file.path).to.equal('README.md')
+      expect(file.status).to.equal(FileStatus.Modified)
+      console.log(repository!.path)
+    })
+
+    it('returns an empty array when there are no changes', async () => {
+      const status = await LocalGitOperations.getStatus(repository!)
+      console.log(repository!.path)
+      const files = status.workingDirectory.files
+      expect(files.length).to.equal(0)
     })
   })
 
   describe('committing', () => {
+    it('commits the given files', async () => {
+      fs.writeFileSync(path.join(repository!.path, 'README.md'), 'Hi world\n')
 
+      let status = await LocalGitOperations.getStatus(repository!)
+      let files = status.workingDirectory.files
+      expect(files.length).to.equal(1)
+
+      await LocalGitOperations.createCommit(repository!, 'Special commit', files)
+
+      status = await LocalGitOperations.getStatus(repository!)
+      files = status.workingDirectory.files
+      expect(files.length).to.equal(0)
+
+      const commits = await LocalGitOperations.getHistory(repository!)
+      expect(commits.length).to.equal(2)
+      expect(commits[0].summary).to.equal('Special commit')
+    })
   })
 
   describe('history', () => {
-    describe('changed files', () => {
+    it('loads history', async () => {
+      const commits = await LocalGitOperations.getHistory(repository!)
+      expect(commits.length).to.equal(1)
+      expect(commits[0].summary).to.equal('first')
+      expect(commits[0].sha).to.equal('7cd6640e5b6ca8dbfd0b33d0281ebe702127079c')
+    })
 
+    describe('changed files', () => {
+      it('loads the files changed in the commit', async () => {
+        const files = await LocalGitOperations.getChangedFiles(repository!, '7cd6640e5b6ca8dbfd0b33d0281ebe702127079c')
+        expect(files.length).to.equal(1)
+        expect(files[0].path).to.equal('README.md')
+        expect(files[0].status).to.equal(FileStatus.New)
+      })
     })
   })
 
   describe('config', () => {
+    it('looks up config values', async () => {
+      const bare = await LocalGitOperations.getConfigValue(repository!, 'core.bare')
+      expect(bare).to.equal('false')
+    })
 
+    it('returns null for undefined values', async () => {
+      const value = await LocalGitOperations.getConfigValue(repository!, 'core.the-meaning-of-life')
+      expect(value).to.equal(null)
+    })
   })
 })
