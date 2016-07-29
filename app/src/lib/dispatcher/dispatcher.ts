e867e1045003af872b0be95ae1bc85e98fd70216
joshaber <joshaber@gmail.com>
2016-07-29 15:03:07 -0400

    Added refreshRepository

--- app/src/lib/dispatcher/dispatcher.ts
+++ app/src/lib/dispatcher/dispatcher.ts
@@ -173,4 +173,11 @@ export class Dispatcher {
   public changeIncludeAllFiles(repository: Repository, includeAll: boolean): Promise<void> {
     return this.store._changeIncludeAllFiles(repository, includeAll)
   }
+
+  /**
+   * Refresh the repository. This would be used, e.g., when the app gains focus.
+   */
+  public refreshRepository(repository: Repository): Promise<void> {
+    return this.store._refreshRepository(repository)
+  }
 }
