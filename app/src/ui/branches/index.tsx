f41b105d3ac6a4eb4ae85b056e1a12a865d36047
joshaber <joshaber@gmail.com>
2016-08-11 17:10:25 -0400

    Split groupedBranches into its own file

--- app/src/ui/branches/index.tsx
+++ app/src/ui/branches/index.tsx
@@ -3,6 +3,7 @@ import List from '../list'
 import { Dispatcher } from '../../lib/dispatcher'
 import Repository from '../../models/repository'
 import { Branch } from '../../lib/local-git-operations'
+import { groupedBranches, BranchListItem } from './grouped-branches'
 
 const RowHeight = 22
 
@@ -51,34 +52,4 @@ export default class Branches extends React.Component<IBranchesProps, void> {
       </div>
     )
   }
-}
-
-type BranchListItem = { kind: 'branch', branch: Branch } | { kind: 'label', label: string }
-
-function groupedBranches(defaultBranch: Branch | null, currentBranch: Branch | null, allBranches: ReadonlyArray<Branch>, recentBranches: ReadonlyArray<Branch>): ReadonlyArray<BranchListItem> {
-  const items = new Array<BranchListItem>()
-
-  if (defaultBranch) {
-    items.push({ kind: 'label', label: 'Default Branch' })
-    items.push({ kind: 'branch', branch: defaultBranch })
-  }
-
-  items.push({ kind: 'label', label: 'Recent Branches' })
-  const recentBranchNames = new Set<string>()
-  const defaultBranchName = defaultBranch ? defaultBranch.name : null
-  recentBranches.forEach(branch => {
-    if (branch.name !== defaultBranchName) {
-      items.push({ kind: 'branch', branch: branch })
-    }
-    recentBranchNames.add(branch.name)
-  })
-
-  items.push({ kind: 'label', label: 'Other Branches' })
-  allBranches.forEach(branch => {
-    if (!recentBranchNames.has(branch.name)) {
-      items.push({ kind: 'branch', branch: branch })
-    }
-  })
-
-  return items
 }
