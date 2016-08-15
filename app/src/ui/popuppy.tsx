26f8bb64cf767e64c872f87e36c6be8e1cad870c
Don Okuda <don.okuda@gmail.com>
2016-08-15 13:17:53 -0700

    Move react styles to stylesheet

--- app/src/ui/popuppy.tsx
+++ app/src/ui/popuppy.tsx
@@ -6,22 +6,8 @@ import * as React from 'react'
  */
 export default class Popuppy extends React.Component<any, any> {
   public render() {
-    const style: React.CSSProperties = {
-      display: 'flex',
-      flexDirection: 'column',
-      zIndex: 99,
-      maxWidth: '400px',
-      width: '50%',
-      height: '50%',
-      margin: 'auto',
-      backgroundColor: '#f9f9f9',
-      position: 'absolute',
-      boxShadow: '0 0 7px rgba(0,0,0,0.08)',
-      border: '1px solid #E2E6E8',
-      borderRadius: '3px'
-    }
     return (
-      <div className='popup' style={style}>
+      <div className='popup'>
         <div className='panel popup-content'>
           {this.props.children}
         </div>
