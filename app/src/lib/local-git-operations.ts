6cc122d090ef5b11a1e1e33a8cb12f4e423d6b39
Brendan Forster <brendan@github.com>
2016-08-23 16:58:40 +1000

    extract the diff parser for better testability

--- app/src/lib/local-git-operations.ts
+++ app/src/lib/local-git-operations.ts
@@ -1,11 +1,12 @@
 import * as Path from 'path'
 
 import { WorkingDirectoryStatus, WorkingDirectoryFileChange, FileChange, FileStatus } from '../models/status'
-import { DiffSelectionType, DiffSelection, Diff, DiffSection, DiffSectionRange } from '../models/diff'
+import { DiffSelectionType, DiffSelection, Diff } from '../models/diff'
 import Repository from '../models/repository'
 
 import { GitProcess, GitError, GitErrorCode } from './git-process'
 import { createPatchesForModifiedFile, createPatchForNewFile } from './patch-formatter'
+import { parseRawDiff } from './diff-parser'
 
 /** The encapsulation of the result from 'git status' */
 export class StatusResult {
@@ -279,98 +280,7 @@ export class LocalGitOperations {
     }
 
     return GitProcess.execWithOutput(args, repository.path)
-      .then(result => {
-        const lines = result.split('\0')
-
-        const sectionRegex = /^@@ -(\d+)(,+(\d+))? \+(\d+)(,(\d+))? @@ ?(.*)$/m
-        const regexGroups = { oldFileStart: 1, oldFileEnd: 3, newFileStart: 4, newFileEnd: 6 }
-
-        const diffText = lines[lines.length - 1]
-
-        const diffSections = new Array<DiffSection>()
-
-        // track the remaining text in the raw diff to parse
-        let diffTextBuffer = diffText
-        // each diff section starts with these two characters
-        let sectionPrefixIndex = diffTextBuffer.indexOf('@@')
-        // continue to iterate while these sections exist
-        let prefixFound = sectionPrefixIndex > -1
-
-        let pointer: number = 0
-
-        while (prefixFound) {
-
-          // trim any preceding text
-          diffTextBuffer = diffTextBuffer.substr(sectionPrefixIndex)
-
-          // extract the diff section numbers
-          const match = sectionRegex.exec(diffTextBuffer)
-
-          let oldStartLine: number = -1
-          let oldEndLine: number = -1
-          let newStartLine: number = -1
-          let newEndLine: number = -1
-
-          if (match) {
-            const first = match[regexGroups.oldFileStart]
-            oldStartLine = parseInt(first, 10)
-            const second = match[regexGroups.oldFileEnd]
-            oldEndLine = parseInt(second, 10)
-            const third = match[regexGroups.newFileStart]
-            newStartLine = parseInt(third, 10)
-            const fourth = match[regexGroups.newFileEnd]
-            newEndLine = parseInt(fourth, 10)
-          }
-
-          const range = new DiffSectionRange(oldStartLine, oldEndLine, newStartLine, newEndLine)
-
-          // re-evaluate whether other sections exist to parse
-          const endOfThisLine = diffTextBuffer.indexOf('\n')
-          sectionPrefixIndex = diffTextBuffer.indexOf('@@', endOfThisLine + 1)
-          prefixFound = sectionPrefixIndex > -1
-
-          // add new section based on the remaining text in the raw diff
-          if (prefixFound) {
-            const diffBody = diffTextBuffer.substr(0, sectionPrefixIndex)
-
-            let startDiffSection: number = 0
-            let endDiffSection: number = 0
-
-            const diffLines = diffBody.split('\n')
-
-            if (diffSections.length === 0) {
-              startDiffSection = 0
-              endDiffSection = diffLines.length
-            } else {
-              startDiffSection = pointer + 1
-              endDiffSection = startDiffSection + diffLines.length
-            }
-
-            pointer += diffLines.length
-
-            diffSections.push(new DiffSection(range, diffLines, startDiffSection, endDiffSection))
-          } else {
-            const diffBody = diffTextBuffer
-
-            let startDiffSection: number = 0
-            let endDiffSection: number = 0
-
-            const diffLines = diffBody.split('\n')
-
-            if (diffSections.length === 0) {
-              startDiffSection = 0
-              endDiffSection = diffLines.length
-            } else {
-              startDiffSection = pointer
-              endDiffSection = startDiffSection + diffLines.length
-            }
-
-            diffSections.push(new DiffSection(range, diffLines, startDiffSection, endDiffSection))
-          }
-        }
-
-        return new Diff(diffSections)
-      })
+      .then(result => parseRawDiff(result.split('\0')))
   }
 
   /**
