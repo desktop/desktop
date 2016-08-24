f7cd289185cc4078e911387133e32a8e23784809
joshaber <joshaber@gmail.com>
2016-08-24 17:26:55 -0400

    CloningRepositoriesStore

---
+++ app/src/lib/dispatcher/cloning-repositories-store.ts
@@ -0,0 +1,69 @@
+import { Emitter, Disposable } from 'event-kit'
+import * as FS from 'fs'
+
+import { LocalGitOperations } from '../local-git-operations'
+import { Dispatcher } from './dispatcher'
+
+export class CloningRepository {
+  public readonly path: string
+
+  public constructor(path: string) {
+    this.path = path
+  }
+}
+
+export class CloningRepositoriesStore {
+  private readonly emitter = new Emitter()
+  private readonly dispatcher: Dispatcher
+
+  private readonly repositoryProgress = new Map<CloningRepository, number>()
+
+  private emitQueued = false
+
+  public constructor(dispatcher: Dispatcher) {
+    this.dispatcher = dispatcher
+  }
+
+  private emitUpdate() {
+    if (this.emitQueued) { return }
+
+    this.emitQueued = true
+
+    window.requestAnimationFrame(() => {
+      this.emitter.emit('did-update', {})
+      this.emitQueued = false
+    })
+  }
+
+  /** Register a function to be called when the store updates. */
+  public onDidUpdate(fn: () => void): Disposable {
+    return this.emitter.on('did-update', fn)
+  }
+
+  public async clone(url: string, path: string) {
+    FS.mkdir(path, async () => {
+      const cloningRepository = new CloningRepository(path)
+
+      this.repositoryProgress.set(cloningRepository, 0)
+      this.emitUpdate()
+
+      await LocalGitOperations.clone(url, path, progress => {
+        this.repositoryProgress.set(cloningRepository, progress)
+        this.emitUpdate()
+      })
+
+      this.dispatcher.addRepositories([ path ])
+      this.repositoryProgress.delete(cloningRepository)
+      this.emitUpdate()
+    })
+  }
+
+  public get repositories(): ReadonlyArray<CloningRepository> {
+    return Array.from(this.repositoryProgress.keys())
+  }
+
+  public getProgress(repository: CloningRepository): number | null {
+    const progress = this.repositoryProgress.get(repository)
+    return progress ? progress : null
+  }
+}
