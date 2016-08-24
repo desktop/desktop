712bba092f4d7bc3c89ad950756bd9cc34bedcae
joshaber <joshaber@gmail.com>
2016-08-24 15:27:26 -0400

    Parse the open repository action

--- app/src/lib/parse-url.ts
+++ app/src/lib/parse-url.ts
@@ -14,19 +14,32 @@ export interface IOAuthAction extends IURLAction<IOAuthActionArgs> {
   readonly args: IOAuthActionArgs
 }
 
+export interface IOpenRepositoryAction extends IURLAction<string> {
+  readonly name: 'open-repository'
+  readonly args: string
+}
+
 export interface IUnknownAction extends IURLAction<{}> {
   readonly name: 'unknown'
   readonly args: {}
 }
 
-export type URLActionType = IOAuthAction | IUnknownAction
+export type URLActionType = IOAuthAction | IOpenRepositoryAction | IUnknownAction
 
 export default function parseURL(url: string): URLActionType {
   const parsedURL = URL.parse(url, true)
-  const actionName = parsedURL.hostname
+  const hostname = parsedURL.hostname
+  const unknown: IUnknownAction = { name: 'unknown', args: {} }
+  if (!hostname) { return unknown }
+
+  const actionName = hostname.toLowerCase()
   if (actionName === 'oauth') {
     return { name: 'oauth', args: { code: parsedURL.query.code } }
+  } else if (actionName === 'openrepo') {
+    // The `path` will be: /https://github.com/user/repo, so we need to take a
+    // substring from the first character on.
+    return { name: 'open-repository', args: parsedURL.path!.substr(1) }
   } else {
-    return { name: 'unknown', args: {} }
+    return unknown
   }
 }
