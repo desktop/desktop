201ae8bbb3b1911caeae5a1646a5762390467475
joshaber <joshaber@gmail.com>
2016-08-24 16:24:35 -0400

    Select the repository if it's already been added

--- app/src/ui/index.tsx
+++ app/src/ui/index.tsx
@@ -7,6 +7,7 @@ import App from './app'
 import { WindowState, getWindowState } from '../lib/window-state'
 import { Dispatcher, AppStore, GitUserStore, GitUserDatabase } from '../lib/dispatcher'
 import { URLActionType } from '../lib/parse-url'
+import { find } from '../lib/find'
 
 if (!process.env.TEST_ENV) {
   /* This is the magic trigger for webpack to go compile
@@ -43,7 +44,19 @@ ipcRenderer.on('url-action', async (event: Electron.IpcRendererEvent, { action }
   const handled = await dispatcher.handleURLAction(action)
   if (handled) { return }
 
+  if (action.name === 'open-repository') {
+    const repositories = appStore.getState().repositories
+    const repositoryUrl = action.args
+    const existingRepository = find(repositories, r => {
+      const gitHubRepository = r.gitHubRepository
+      if (!gitHubRepository) { return false }
+      return gitHubRepository.htmlURL === repositoryUrl
+    })
 
+    if (existingRepository) {
+      dispatcher.selectRepository(existingRepository)
+    }
+  }
 })
 
 ReactDOM.render(<App dispatcher={dispatcher} appStore={appStore} gitUserStore={gitUserStore}/>, document.getElementById('desktop-app-container')!)
