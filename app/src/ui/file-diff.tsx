b016bfd53cc2ec356223f867af29e0359bb590a2
Don Okuda <don.okuda@gmail.com>
2016-08-26 10:18:13 -0700

    This is silly

--- app/src/ui/file-diff.tsx
+++ app/src/ui/file-diff.tsx
@@ -177,8 +177,9 @@ export default class FileDiff extends React.Component<IFileDiffProps, IFileDiffS
   }
 
   private editableSidebar(diff: DiffLine, rowIndex: number) {
-    const baseClassName = 'diff-line-column ' + this.getClassName(diff.type)
-    const className = diff.selected ? baseClassName + '-selected' : baseClassName
+    const baseClassName = 'diff-line-column'
+    const typeClassName = this.getClassName(diff.type)
+    const className = diff.selected ? baseClassName + ' ' + baseClassName + '-selected' + ' ' + typeClassName : baseClassName + ' ' + typeClassName
 
     // TODO: depending on cursor position, highlight hunk rather than line
 
