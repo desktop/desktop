4e5d8e6f8e658bb35380b5dfbd6cef3dd015fc25
joshaber <joshaber@gmail.com>
2016-09-01 22:58:32 -0400

    Fix the test

--- app/test/app-test.tsx
+++ app/test/app-test.tsx
@@ -6,13 +6,12 @@ import * as ReactDOM from 'react-dom'
 import * as TestUtils from 'react-addons-test-utils'
 
 import App from '../src/ui/app'
-import { Dispatcher, AppStore, GitHubUserStore, CloningRepositoriesStore } from '../src/lib/dispatcher'
+import { Dispatcher, AppStore, CloningRepositoriesStore } from '../src/lib/dispatcher'
 import InMemoryDispatcher from './in-memory-dispatcher'
 import TestGitHubUserDatabase from './test-github-user-database'
 
 describe('App', () => {
   let appStore: AppStore | null = null
-  let gitHubUserStore: GitHubUserStore | null = null
   let dispatcher: Dispatcher | null = null
 
   beforeEach(async () => {
@@ -21,13 +20,12 @@ describe('App', () => {
     const db = new TestGitHubUserDatabase()
     await db.reset()
 
-    gitHubUserStore = new GitHubUserStore(db)
-    dispatcher = new InMemoryDispatcher(appStore, gitHubUserStore, new CloningRepositoriesStore())
+    dispatcher = new InMemoryDispatcher(appStore, new CloningRepositoriesStore())
   })
 
   it('renders', () => {
     const app = TestUtils.renderIntoDocument(
-      <App dispatcher={dispatcher!} appStore={appStore!} gitHubUserStore={gitHubUserStore!} cloningRepositoriesStore={new CloningRepositoriesStore()}/>
+      <App dispatcher={dispatcher!} appStore={appStore!} cloningRepositoriesStore={new CloningRepositoriesStore()}/>
     ) as React.Component<any, any>
     const node = ReactDOM.findDOMNode(app)
     expect(node).not.to.equal(null)
