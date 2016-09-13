ca9610049026757e83542583779be8fe7e4e8be4
joshaber <joshaber@gmail.com>
2016-09-13 16:18:23 -0400

    Docs

--- app/src/ui/autocompletion/index.tsx
+++ app/src/ui/autocompletion/index.tsx
@@ -1,7 +1,23 @@
 export { default as AutocompletingTextArea } from './autocompleting-text-area'
 
+/** An interface which defines the protocol for an autocompletion provider. */
 export interface IAutocompletionProvider<T> {
+  /**
+   * Get the regex which it used to capture text for the provider. The captured
+   * text will then be passed to `getAutocompletionItems` to get the
+   * autocompletions.
+   */
   getRegExp(): RegExp
+
+  /**
+   * Get the autocompletion results for the given text. The text is whatever was
+   * captured by the regex returned from `getRegExp`.
+   */
   getAutocompletionItems(text: string): ReadonlyArray<T>
+
+  /**
+   * Render the autocompletion item. The item will be one which the provider
+   * returned from `getAutocompletionItems`.
+   */
   renderItem(item: T): JSX.Element
 }
