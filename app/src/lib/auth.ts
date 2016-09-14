98c4f5f0c888a4e0b5678db7523a00299b709daa
joshaber <joshaber@gmail.com>
2016-09-14 15:23:07 -0400

    Look up the token for the user and host

---
+++ app/src/lib/auth.ts
@@ -0,0 +1,11 @@
+import User from '../models/user'
+
+/** Get the auth key for the user. */
+export function getKeyForUser(user: User): string {
+  return getKeyForEndpoint(user.endpoint)
+}
+
+/** Get the auth key for the endpoint. */
+export function getKeyForEndpoint(endpoint: string): string {
+  return `GitHub - ${endpoint}`
+}
--- app/src/main-process/ask-pass.ts
+++ app/src/main-process/ask-pass.ts
@@ -1,9 +1,16 @@
+import { getKeyForEndpoint } from '../lib/auth'
+import tokenStore from '../shared-process/token-store'
+
 /** Parse the GIT_ASKPASS prompt and determine the appropriate response. */
 export function responseForPrompt(prompt: string): string | null {
+  const username = process.env.DESKTOP_USERNAME
   if (prompt.startsWith('Username')) {
-    return process.env.DESKTOP_USERNAME
+    return username
   } else if (prompt.startsWith('Password')) {
-    return process.env.DESKTOP_PASSWORD
+    const endpoint = process.env.DESKTOP_ENDPOINT
+    const key = getKeyForEndpoint(endpoint)
+    const token = tokenStore.getItem(key, username)
+    return token
   }
 
   return null
