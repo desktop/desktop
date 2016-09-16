e6ea93534ce660417fc1debef9bfd66bd5a81af9
Markus Olsson <j.markus.olsson@gmail.com>
2016-09-16 21:23:16 +0200

    Compose emoji store from gemoji db

--- app/src/lib/dispatcher/emoji-store.ts
+++ app/src/lib/dispatcher/emoji-store.ts
@@ -1,43 +1,75 @@
 import * as Fs from 'fs'
 import * as Path from 'path'
-import * as Url from 'url'
 
-const snakeCase = require('snake-case')
+type IGemojiDb = [ IGemojiDefinition ]
+
+interface IGemojiDefinition {
+  emoji?: string,
+  aliases: [ string ]
+  description?: string
+}
 
 export default class EmojiStore {
   /** Map from shorcut (e.g., :+1:) to on disk URL. */
   public readonly emoji = new Map<string, string>()
 
+  private getEmojiImageUrlFromRelativePath(relativePath: string) {
+    return `file://${Path.join(__dirname, 'emoji', relativePath)}`
+  }
+
+  private getHexCodePoint(cp: number) {
+    const str = cp.toString(16)
+    return str.length >= 4
+      ? str
+      : ('0000' + str).substring(str.length)
+  }
+
+  private getUrlFromUnicodeEmoji(emoji: string) {
+
+    const codePoint = emoji.codePointAt(0)
+
+    if (!codePoint) {
+      return null
+    }
+
+    let filename = this.getHexCodePoint(codePoint)
+
+    if (emoji.length > 2) {
+      const combiningCodePoint = emoji.codePointAt(2)
+
+      if (combiningCodePoint && combiningCodePoint !== 0xfe0f) {
+        filename = `${filename}-${this.getHexCodePoint(combiningCodePoint)}`
+      }
+    }
+
+    return this.getEmojiImageUrlFromRelativePath(`unicode/${filename}.png`)
+  }
+
   public read(): Promise<void> {
     return new Promise((resolve, reject) => {
       const basePath = process.env.TEST_ENV ? Path.join(__dirname, '..', '..', '..', 'static') : __dirname
       Fs.readFile(Path.join(basePath, 'emoji.json'), 'utf8', (err, data) => {
-        const json = JSON.parse(data)
-        for (const key in json) {
-          const serverURL = json[key]
-          const localPath = serverURLToLocalPath(serverURL)
+        const db: IGemojiDb = JSON.parse(data)
+        console.log(db)
+        db.forEach(emoji => {
 
-          // For whatever reason, the mapping we get from the API is in camel
-          // case, but it really should be in snake case. So convert it.
-          this.emoji.set(`:${snakeCase(key)}:`, localPath)
-        }
+          const url = emoji.emoji
+            ? this.getUrlFromUnicodeEmoji(emoji.emoji)
+            : this.getEmojiImageUrlFromRelativePath(`${emoji.aliases[0]}.png`)
+
+          if (!url) {
+            console.error('Emoji crisis')
+            return
+          }
+
+          emoji.aliases.forEach(alias => {
+            console.log(`:${alias}: ${url}`)
+            this.emoji.set(`:${alias}:`, url)
+          })
+        })
 
         resolve()
       })
     })
   }
-}
-
-function serverURLToLocalPath(url: string): string {
-  // url = https://assets-cdn.github.com/images/icons/emoji/unicode/1f44e.png?v6
-
-  const parsedURL = Url.parse(url)
-
-  const path = parsedURL.pathname!
-  // path = /images/icons/emoji/unicode/1f44e.png
-
-  const relativePath = path.replace('/images/icons', '')
-  // relativePath = /emoji/unicode/1f44e.png
-
-  return `file://${Path.join(__dirname, relativePath)}`
 }
