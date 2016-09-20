2528c03effbb07a9fc0298b9fc5feda573d34dba
Markus Olsson <j.markus.olsson@gmail.com>
2016-09-20 11:36:24 +0200

    I see dead code

--- app/src/ui/lib/escape-regex.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Converts the input string by escaping characters that have special meaning in
- * regular expressions.
- *
- * From https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions
- */
-export function escapeRegExp(str: string) {
-  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
-}
