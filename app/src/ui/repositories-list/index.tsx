3adde685df6726faca2881cd2cb5cc1ff52ae72d
Markus Olsson <j.markus.olsson@gmail.com>
2016-09-22 14:13:29 +0200

    Rename RepositoryListItem to RepositoryListItemModel

--- app/src/ui/repositories-list/index.tsx
+++ app/src/ui/repositories-list/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { List } from '../list'
 import RepositoryListItem from './repository-list-item'
 import { Repository } from '../../models/repository'
-import { groupRepositories, RepositoryListItem as RepositoryListItemModel, Repositoryish } from './group-repositories'
+import { groupRepositories, RepositoryListItemModel, Repositoryish } from './group-repositories'
 import { Dispatcher, CloningRepository } from '../../lib/dispatcher'
 
 interface IRepositoriesListProps {
