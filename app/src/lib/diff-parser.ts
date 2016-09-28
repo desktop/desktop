0db1fb45dcdf2d105b56ad0113618c9c2413e508
Markus Olsson <j.markus.olsson@gmail.com>
2016-09-28 10:21:19 +0200

    Refactor hacky parser into class

--- app/src/lib/diff-parser.ts
+++ app/src/lib/diff-parser.ts
@@ -18,94 +18,154 @@ function numberFromGroup(m: RegExpMatchArray, group: number): number {
 // the number of lines the change hunk applies to for each respective file.
 const diffHeaderRe = /^@@ -(\d+)(?:,(\d+))? \+(\d+)(?:,(\d+))? @@/
 
-function parseDiffHeader(line: string): DiffSectionRange {
-  const m = diffHeaderRe.exec(line)
-  if (!m) { throw new Error('') }
+export class DiffParser {
+  private ls: number
+  private le: number
 
-  const oldStartLine = numberFromGroup(m, 1)
-  const oldEndLine = numberFromGroup(m, 2)
-  const newStartLine = numberFromGroup(m, 3)
-  const newEndLine = numberFromGroup(m, 4)
+  private text: string
+  private lineBuffer = new Array<string>()
 
-  return new DiffSectionRange(oldStartLine, oldEndLine, newStartLine, newEndLine)
+  public constructor() {
+    this.reset()
+  }
+
+  private nextLine(): boolean {
+    this.ls = this.le + 1
+
+    if (this.ls >= this.text.length) {
+      return false
+    }
+
+    this.le = this.text.indexOf('\n', this.ls)
+
+    if (this.le === -1) {
+      this.le = this.text.length // -1?
+    }
+
+    return this.ls !== this.le
+  }
+
+  private readLine(): string | null {
+    return this.nextLine()
+      ? this.text.substring(this.ls, this.le)
+      : null
+  }
+
+  private lineStartsWith(searchString: string): boolean {
+    return this.text.startsWith(searchString, this.ls)
+  }
+
+  private peek(): string | null {
+    const p = this.le + 1
+    return p < this.text.length ? this.text[p] : null
+  }
+
+  private reset() {
+    this.ls = 0
+    this.le = 0
+    this.text = ''
+    this.lineBuffer = new Array<string>()
+  }
+
+  private parseDiffPreText() {
+
+    // Scan past the diff header for now.
+    // TODO: There's information in here that we might want to
+    // capture, such as mode changes
+    while (this.nextLine()) {
+      if (this.lineStartsWith('+++')) {
+        return
+      }
+    }
+
+    throw new Error('Could not find end of diff header')
+  }
+
+  private parseHunkHeader(line: string): DiffSectionRange {
+    const m = diffHeaderRe.exec(line)
+    if (!m) { throw new Error(`Invalid hunk header format: '${line}'`) }
+
+    const oldStartLine = numberFromGroup(m, 1)
+    const oldEndLine = numberFromGroup(m, 2)
+    const newStartLine = numberFromGroup(m, 3)
+    const newEndLine = numberFromGroup(m, 4)
+
+    return new DiffSectionRange(oldStartLine, oldEndLine, newStartLine, newEndLine)
+  }
+
+  private parseHunk(linesConsumed: number): DiffSection {
+
+    const headerLine = this.readLine()
+
+    if (!headerLine) {
+      throw new Error('Expected hunk header but reached end of diff')
+    }
+
+    const header = this.parseHunkHeader(headerLine)
+    const lines = new Array<string>()
+    lines.push(headerLine)
+
+    let c: string | null
+
+    while ((c = this.peek()) && (c === '+' || c === '-' || c === ' ')) {
+      const line = this.readLine()
+
+      if (!line) {
+        throw new Error('Expected unified diff line but reached end of diff')
+      }
+
+      lines.push(line)
+    }
+
+    // Just the header
+    if (lines.length === 1) {
+      throw new Error('Malformed diff, empty hunk')
+    }
+
+    return new DiffSection(header, lines, linesConsumed, linesConsumed + lines.length - 1)
+  }
+
+  private consumeNewlineWarningAndAssertEndOfDiff() {
+    const newLineWarning = this.readLine()
+    if (!newLineWarning || newLineWarning !== '\\ No newline at end of file') {
+      throw new Error('')
+    }
+
+    if (this.nextLine()) {
+      throw new Error('Expected end of diff after newline warning')
+    }
+  }
+
+  public parse(text: string): Diff {
+
+    this.text = text
+
+    this.parseDiffPreText()
+
+    const hunkBuf = new Array<DiffSection>()
+    let linesConsumed = 0
+    let noNewlineAtEndOfFile = false
+    let c: string | null = null
+
+    do {
+      const hunk = this.parseHunk(linesConsumed)
+      hunkBuf.push(hunk)
+      linesConsumed += hunk.lines.length
+
+    } while ((c = this.peek()) && c !== '\\')
+
+    if (c === '\\') {
+      this.consumeNewlineWarningAndAssertEndOfDiff()
+      noNewlineAtEndOfFile = true
+    }
+
+    this.reset()
+
+    return new Diff(hunkBuf, noNewlineAtEndOfFile)
+  }
 }
 
 export function parseRawDiff(diffText: string): Diff {
-
-    let ls = 0
-    let le = diffText.indexOf('\n')
-    let hasSeenDiffPreText = false
-    let diffSectionHeader: DiffSectionRange | null = null
-    let numberOfUnifiedDiffLines = 0
-
-    const diffSections = new Array<DiffSection>()
-    let diffLines = new Array<string>()
-
-    while (le !== -1 && ls < diffText.length) {
-
-      if (!hasSeenDiffPreText) {
-        if (diffText.startsWith('+++', ls)) {
-          hasSeenDiffPreText = true
-        }
-      } else {
-        // is inhunk
-        if (diffSectionHeader == null) {
-          const line = diffText.substring(ls, le)
-          diffSectionHeader = parseDiffHeader(line)
-          diffLines.push(line)
-        } else {
-          const c = diffText[ls]
-          if (c === '+' || c === '-' || c === ' ') {
-            diffLines.push(diffText.substring(ls, le))
-          } else {
-            if (diffText.startsWith('@@', ls)) {
-
-              // add new section based on the remaining text in the raw diff
-              let startDiffSection: number = 0
-              let endDiffSection: number = 0
-
-              if (diffSections.length === 0) {
-                startDiffSection = 0
-                endDiffSection = diffLines.length - 1
-              } else {
-                startDiffSection = numberOfUnifiedDiffLines
-                endDiffSection = startDiffSection + diffLines.length - 1
-              }
-
-              numberOfUnifiedDiffLines += diffLines.length
-
-              diffSections.push(new DiffSection(diffSectionHeader, diffLines, startDiffSection, endDiffSection))
-
-              diffSectionHeader = null
-              diffLines = new Array<string>()
-              continue
-            } else {
-              throw new Error('')
-            }
-          }
-        }
-      }
-
-      ls = le + 1
-      le = diffText.indexOf('\n', ls)
-    }
-
-    if (diffLines.length > 0 && diffSectionHeader != null) {
-
-      // add new section based on the remaining text in the raw diff
-      let startDiffSection: number = 0
-      let endDiffSection: number = 0
-
-      if (diffSections.length === 0) {
-        startDiffSection = 0
-        endDiffSection = diffLines.length - 1
-      } else {
-        startDiffSection = numberOfUnifiedDiffLines
-        endDiffSection = startDiffSection + diffLines.length - 1
-      }
-
-      diffSections.push(new DiffSection(diffSectionHeader, diffLines, startDiffSection, endDiffSection))
-    }
-
-    return new Diff(diffSections)
+    const parser = new DiffParser()
+    return parser.parse(diffText)
 }
