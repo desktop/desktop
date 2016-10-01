986bc171b9924286c1259f233f6fdb59c177cb28
Markus Olsson <j.markus.olsson@gmail.com>
2016-10-01 16:42:59 +0200

    Maybe document stuff tho

--- app/src/ui/diff/index.tsx
+++ app/src/ui/diff/index.tsx
@@ -283,6 +283,7 @@ export class Diff extends React.Component<IDiffProps, IDiffState> {
       showCursorWhenSelecting: false,
       cursorBlinkRate: -1,
       lineWrapping: localStorage.getItem('soft-wrap-is-best-wrap') ? true : false,
+      // Make sure CodeMirror doesn't capture Tab and thus destroy tab navigation
       extraKeys: { Tab: false },
     }
 
