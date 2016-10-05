b19c27cae17b2149b8e164601eb5138bf5ef98c6
joshaber <joshaber@gmail.com>
2016-10-05 15:19:38 -0400

    Actually post the exception

--- app/src/lib/exception-reporting.ts
+++ app/src/lib/exception-reporting.ts
@@ -1,7 +1,13 @@
+import * as http from 'http'
 import * as app from '../ui/lib/app'
 
 export function reportError(error: Error) {
   debugger
+  const options = {
+    host: 'https://central.github.com',
+    path: '/api/desktop/exception',
+    method: 'POST',
+  }
 
   const payload = {
     name: error.name,
@@ -10,4 +16,18 @@ export function reportError(error: Error) {
     version: app.getVersion(),
   }
   console.log(payload)
+
+  const req = http.request(options, response => {
+    const result = ''
+
+    response.on('data', chunk => {
+      result += chunk
+    })
+
+    response.on('end', () => {
+      console.log(`Exception reported. Response: ${result}`)
+    })
+  })
+  req.write(JSON.stringify(payload))
+  req.end()
 }
