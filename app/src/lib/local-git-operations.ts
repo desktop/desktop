0425af46052edfceb37996f1d8bad49b2a5b9acd
Markus Olsson <j.markus.olsson@gmail.com>
2016-10-05 14:58:18 +0200

    Deal with user.useconfigonly

--- app/src/lib/local-git-operations.ts
+++ app/src/lib/local-git-operations.ts
@@ -374,9 +374,20 @@ export class LocalGitOperations {
    * email configured. If no email or name is configured Git will
    * attempt to come up with a suitable replacement using the
    * signed-in system user and hostname.
+   *
+   * A null return value means that no name/and or email was set
+   * and the user.useconfigonly setting prevented Git from making
+   * up a user ident string. If this returns null any subsequent
+   * commits can be expected to fail as well.
    */
   public static async getAuthorIdentity(repository: Repository): Promise<CommitIdentity | null> {
-    const result = await git([ 'var', 'GIT_AUTHOR_IDENT' ], repository.path)
+    const result = await git([ 'var', 'GIT_AUTHOR_IDENT' ], repository.path, undefined, new Set([ 0, 128 ]))
+
+    // If user.user.useconfigonly is set and no user.name or user.email
+    if (result.exitCode === 128) {
+      return null
+    }
+
     return CommitIdentity.parseIdent(result.stdout)
   }
 
