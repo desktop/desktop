6295dc548a6bb5f3b30cfbc683e50821517be05d
Markus Olsson <j.markus.olsson@gmail.com>
2016-10-07 01:05:38 +0200

    Extract parsing logic to allow testing

---
+++ app/src/lib/status-parser.ts
@@ -0,0 +1,27 @@
+export interface IStatusEntry {
+  readonly path: string,
+  readonly statusCode: string
+  readonly oldPath?: string
+}
+
+/** Parses output from git status --porcelain -z */
+export function parsePorcelainStatus(output: string): ReadonlyArray<IStatusEntry> {
+  const entries = new Array<IStatusEntry>()
+  const fields = output.split('\0')
+  let field: string | undefined
+
+  while ((field = fields.shift())) {
+    const statusCode = field.substr(0, 3)
+    const path = field.substr(3)
+
+    let oldPath: string | undefined = undefined
+
+    if (statusCode.startsWith('R')) {
+      oldPath = fields.shift()
+    }
+
+    entries.push({ path, statusCode, oldPath })
+  }
+
+  return entries
+}
