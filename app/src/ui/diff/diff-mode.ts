481b88604b5c3814852eaf022a683d27f0830698
joshaber <joshaber@gmail.com>
2016-10-07 14:30:37 -0400

    Separate more things

--- app/src/ui/diff/diff-mode.ts
+++ app/src/ui/diff/diff-mode.ts
@@ -1,8 +1,19 @@
 import * as CodeMirror from 'codemirror'
 
+/** The name of our diff mode. */
 const DiffModeName = 'github/diff'
 
+/** The token to be used if no other tokens match. */
+const DefaultToken = 'diff-context'
+
+/** Has the diff mode been defined yet? It should happen only once. */
 let diffModeDefined = false
+
+const TokenNames: any = {
+  '+': 'diff-add',
+  '-': 'diff-delete',
+  '@': 'diff-hunk',
+}
 
 export function getDiffMode(): string {
   if (diffModeDefined) {
@@ -12,23 +23,18 @@ export function getDiffMode(): string {
   diffModeDefined = true
 
   CodeMirror.defineMode(DiffModeName, function(config: CodeMirror.EditorConfiguration, modeOptions?: any) {
-    const TOKEN_NAMES = {
-      '+': 'diff-add',
-      '-': 'diff-delete',
-      '@': 'diff-hunk',
-    }
-
     return {
-      token: function(stream) {
-        const token: any = (TOKEN_NAMES as any)[stream.peek()] || 'diff-context'
-
-        stream.skipToEnd()
-
-        // Use the token to style both the line background and the line content.
-        return `line-background-${token} ${token}`
-      },
+      token: parseToken,
     }
   })
 
   return DiffModeName
+}
+
+function parseToken(stream: CodeMirror.StringStream): string {
+  const token = TokenNames[stream.peek()] || DefaultToken
+  stream.skipToEnd()
+
+  // Use the token to style both the line background and the line content.
+  return `line-background-${token} ${token}`
 }
