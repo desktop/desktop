71f2fe0e007de4cf480f3fdaa12bab0da8131f7c
Brendan Forster <brendan@github.com>
2016-10-07 12:05:15 +1100

    removed classic render function

--- app/test/render-utils.ts
+++ app/test/render-utils.ts
@@ -24,28 +24,6 @@ export function getParentNode(stylesheet?: string): HTMLElement {
 }
 
 /**
- * Helper method for testing components that may use Portal and thus require cleanup.
- * This helper method renders components to a transient node that is destroyed after the test completes.
- * Note that rendering twice within the same test method will update the same element (rather than recreate it).
- */
-export function render(markup: any, stylesheet?: string): Element {
-  const mountNode = document.createElement('div')
-
-  if (stylesheet) {
-      const sheet = document.createElement('style')
-      sheet.innerHTML = stylesheet
-      document.body.appendChild(sheet)
-  }
-
-  // Unless we attach the mount-node to body, getBoundingClientRect() won't work
-  document.body.appendChild(mountNode)
-
-  afterEach(() => unmount(mountNode))
-
-  return ReactDOM.render(markup, mountNode)
-}
-
-/**
  * The render() method auto-unmounts components after each test has completed.
  * Use this method manually to test the componentWillUnmount() lifecycle method.
  */
