8188c5dde2e1b0846c7802b7b5caf0ac234259cd
Markus Olsson <j.markus.olsson@gmail.com>
2016-10-08 15:01:07 +0200

    Annotate the return values

--- app/src/lib/compare.ts
+++ app/src/lib/compare.ts
@@ -1,10 +1,10 @@
-export function compare<T>(x: T, y: T) {
+export function compare<T>(x: T, y: T): number {
   if (x < y) { return -1 }
   if (x > y) { return 1 }
 
   return 0
 }
 
-export function caseInsenstiveCompare(x: string, y: string) {
+export function caseInsenstiveCompare(x: string, y: string): number {
   return compare(x.toLowerCase(), y.toLocaleLowerCase())
 }
