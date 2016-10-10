669ef9e4cf92099caf8359c9c3309e7f34400c98
Markus Olsson <j.markus.olsson@gmail.com>
2016-10-09 18:54:45 -0700

    Fix partial committing of renamed files

--- app/src/lib/local-git-operations.ts
+++ app/src/lib/local-git-operations.ts
@@ -211,16 +211,41 @@ export class LocalGitOperations {
   private static async addFileToIndex(repository: Repository, file: WorkingDirectoryFileChange): Promise<void> {
 
     if (file.status === FileStatus.New) {
-      await git([ 'add', file.path ], repository.path)
+      await git([ 'add', '--', file.path ], repository.path)
     } else if (file.status === FileStatus.Renamed && file.oldPath) {
-      await git([ 'add', file.path ], repository.path)
-      await git([ 'add', '-u', file.oldPath ], repository.path)
+      await git([ 'add', '--', file.path ], repository.path)
+      await git([ 'add', '-u', '--', file.oldPath ], repository.path)
     } else {
-      await git([ 'add', '-u', file.path ], repository.path)
+      await git([ 'add', '-u', '--', file.path ], repository.path)
     }
   }
 
   private static async applyPatchToIndex(repository: Repository, file: WorkingDirectoryFileChange): Promise<void> {
+
+    // If the file was a rename we have to recreate that rename since we've
+    // just blown away the index. This of this block of weird looking commands
+    // as running `git mv`.
+    if (file.status === FileStatus.Renamed && file.oldPath) {
+      // Make sure the index knows of the removed file. We could use
+      // update-index --force-remove here but we're not since it's theoretically
+      // possible that someone staged a rename and then recreated the
+      // original file and we don't have any guarantees for in which order
+      // partial stages vs full-file stages happen. By using git add the
+      // worst that could happen is that we re-stage a file already staged
+      // by addFileToIndex
+      await git([ 'add', '--u', '--', file.oldPath ], repository.path)
+
+      // Figure out the blob oid of the removed file
+      // <mode> SP <type> SP <object> TAB <file>
+      const oldFile = await git([ 'ls-tree', 'HEAD', '--', file.oldPath ], repository.path)
+
+      const [ info ] = oldFile.stdout.split('\t', 1)
+      const [ mode, , oid ] = info.split(' ', 3)
+
+      // Add the old file blob to the index under the new name
+      await git([ 'update-index', '--add', '--cacheinfo', mode, oid, file.path ], repository.path)
+    }
+
     const applyArgs: string[] = [ 'apply', '--cached', '--unidiff-zero', '--whitespace=nowarn', '-' ]
 
     const diff = await LocalGitOperations.getWorkingDirectoryDiff(repository, file)
@@ -744,6 +769,8 @@ async function git(args: string[], path: string, options?: IGitExecutionOptions)
     }
   }
 
+  console.log(`$ git ${args.join(' ')}`)
+
   const exitCode = result.exitCode
 
   if (!opts.successExitCodes!.has(exitCode)) {
@@ -768,5 +795,6 @@ async function git(args: string[], path: string, options?: IGitExecutionOptions)
     throw new GitError(result, args, gitError)
   }
 
+  console.log(result.stdout)
   return result
 }
