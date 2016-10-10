b0a70abbeb8437537b95a1d635226bbf45985cbe
joshaber <joshaber@gmail.com>
2016-10-10 18:45:50 -0400

    Wait until the window is visible to continue

--- app/test/integration/launch-test.ts
+++ app/test/integration/launch-test.ts
@@ -36,10 +36,10 @@ describe('App', function (this: any) {
 
   it('opens a window on launch', function () {
     return app.client.waitUntilWindowLoaded()
+      .browserWindow.isVisible().should.eventually.be.true
       .getWindowCount().should.eventually.equal(2)
       .browserWindow.isMinimized().should.eventually.be.false
       .browserWindow.isDevToolsOpened().should.eventually.be.false
-      .browserWindow.isVisible().should.eventually.be.true
       .browserWindow.isFocused().should.eventually.be.true
       .browserWindow.getBounds().should.eventually.have.property('width').and.be.above(0)
       .browserWindow.getBounds().should.eventually.have.property('height').and.be.above(0)
