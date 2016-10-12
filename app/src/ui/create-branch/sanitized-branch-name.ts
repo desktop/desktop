63d4d79bcb5579e2e777ce9d968ba062393b1a2e
Phil Haack <haacked@gmail.com>
2016-10-12 13:53:49 -0700

    Improve branch name sanitization
    Also added some unit tests pulled from the GitHub Desktop for Windows
    code.
    
    Fixes #291

--- app/src/ui/create-branch/sanitized-branch-name.ts
+++ app/src/ui/create-branch/sanitized-branch-name.ts
@@ -1,4 +1,6 @@
 /** Sanitize a proposed branch name by replacing illegal characters. */
 export function sanitizedBranchName(name: string): string {
-  return name.replace(/ /g, '-')
+  return name.replace(/[\x00-\x20\x7F~^:?*\[\\|""<>]|@{|\.\.+|^\.|\.$|\.lock$|\/$/g, '-')
+    .replace(/--+/g, '-')
+    .replace(/^-/g, '')
 }
