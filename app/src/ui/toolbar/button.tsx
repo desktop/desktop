1026a2c6ad246b536ac3f50ecae8ad204f33ffb3
Markus Olsson <j.markus.olsson@gmail.com>
2016-11-01 14:02:57 +0100

    Outline for toolbar button

---
+++ app/src/ui/toolbar/button.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { Octicon, OcticonSymbol } from '../octicons'
+
+interface IToolbarButtonProps {
+  title: string,
+  description?: string,
+  icon?: OcticonSymbol
+}
+
+/**
+ * A general purpose toolbar button
+ */
+export class ToolbarButton extends React.Component<IToolbarButtonProps, void> {
+  public render() {
+    const icon = this.props.icon
+      ? <Octicon symbol={this.props.icon} />
+      : null
+
+    const description = this.props.description
+      ? <div className='label'>{this.props.description}</div>
+      : null
+
+    return (
+      <button className='toolbar-button'>
+        {icon}
+        <div className='text'>
+          <div className='title'>{this.props.title}</div>
+          {description}
+        </div>
+        {this.props.children}
+      </button>
+    )
+  }
+}
