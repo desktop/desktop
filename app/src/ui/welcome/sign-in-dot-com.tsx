e4052b00db44346d165863d032882e3e453a7aae
joshaber <joshaber@gmail.com>
2016-11-03 16:05:25 -0400

    First pass on .com sign in

--- app/src/ui/welcome/sign-in-dot-com.tsx
+++ app/src/ui/welcome/sign-in-dot-com.tsx
@@ -11,33 +11,56 @@ interface ISignInDotComProps {
   readonly cancel: () => void
 }
 
-export class SignInDotCom extends React.Component<ISignInDotComProps, void> {
+interface ISignInDotComState {
+  readonly username: string
+  readonly password: string
+}
+
+export class SignInDotCom extends React.Component<ISignInDotComProps, ISignInDotComState> {
+  public constructor(props: ISignInDotComProps) {
+    super(props)
+
+    this.state = { username: '', password: '' }
+  }
+
   public render() {
+    // Always disabled as we don't support sign in in-app yet.
+    const signInDisabled = Boolean(!this.state.username.length || !this.state.password.length) || true
     return (
-      <div>
+      <div id='sign-in-dot-com'>
         <h1>Sign in to GitHub.com</h1>
         <div>Get started by signing into GitHub.com</div>
 
-        <label>Username or email address
-          <input/>
-        </label>
+        <form id='sign-in-form' onSubmit={() => this.signIn()}>
+          <label>Username or email address
+            <input onChange={e => this.onUsernameChange(e)}/>
+          </label>
 
-        <label>Password
-          <input/>
-        </label>
+          <label>Password
+            <input type='password' onChange={e => this.onPasswordChange(e)}/>
+          </label>
 
-        <a onClick={e => this.forgotPassword(e)}>Forgot password?</a>
+          <a href='' onClick={e => this.forgotPassword(e)}>Forgot password?</a>
 
-        <div>
-          <button onClick={() => this.signIn()}>Sign in</button>
-          <button onClick={() => this.advance()}>Cancel</button>
-        </div>
+          <div className='actions'>
+            <button type='submit' disabled={signInDisabled} onClick={() => this.signIn()}>Sign in</button>
+            <button onClick={() => this.advance()}>Cancel</button>
+          </div>
 
-        <div>or</div>
+          <div>or</div>
 
-        <a onClick={e => this.signInWithBrowser(e)}>Sign in using your browser</a>
+          <a href='' onClick={e => this.signInWithBrowser(e)}>Sign in using your browser</a>
+        </form>
       </div>
     )
+  }
+
+  private onUsernameChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({ username: event.currentTarget.value, password: this.state.password })
+  }
+
+  private onPasswordChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({ username: this.state.username, password: event.currentTarget.value })
   }
 
   private forgotPassword(event: React.MouseEvent<HTMLAnchorElement>) {
