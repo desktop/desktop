230f0c422bb111168a90e38236d93f1a43417968
joshaber <joshaber@gmail.com>
2016-11-03 16:53:11 -0400

    Load and set name and email

--- app/src/ui/welcome/configure-git.tsx
+++ app/src/ui/welcome/configure-git.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { WelcomeStep } from './welcome'
 import { Dispatcher } from '../../lib/dispatcher'
+import { LocalGitOperations } from '../../lib/local-git-operations'
 
 interface IConfigureGitProps {
   readonly dispatcher: Dispatcher
@@ -8,37 +9,74 @@ interface IConfigureGitProps {
   readonly cancel: () => void
 }
 
-export class ConfigureGit extends React.Component<IConfigureGitProps, void> {
+interface IConfigureGitState {
+  readonly name: string
+  readonly email: string
+}
+
+export class ConfigureGit extends React.Component<IConfigureGitProps, IConfigureGitState> {
+  public constructor(props: IConfigureGitProps) {
+    super(props)
+
+    this.state = { name: '', email: '' }
+  }
+
+  public async componentWillMount() {
+    const name = await LocalGitOperations.getGlobalConfigValue('user.name')
+    const email = await LocalGitOperations.getGlobalConfigValue('user.email')
+    this.setState({ name: name || '', email: email || '' })
+  }
+
   public render() {
     return (
-      <div>
+      <div id='configure-git'>
         <h1>Configure Git</h1>
-        <div>This is used to identify the commits you create. Anyone will be able to see this information if you publish commits.</div>
+        <div className='description'>This is used to identify the commits you create. Anyone will be able to see this information if you publish commits.</div>
 
-        <label>
-          Name
-          <input/>
-        </label>
+        <form onSubmit={e => this.continue(e)}>
+          <label>
+            Name
+            <input placeholder='Hubot' value={this.state.name} onChange={e => this.onNameChange(e)}/>
+          </label>
 
-        <label>
-          Email
-          <input/>
-        </label>
+          <label>
+            Email
+            <input placeholder='hubot@github.com' value={this.state.email} onChange={e => this.onEmailChange(e)}/>
+          </label>
 
-        <div>
-          <button onClick={() => this.continue()}>Continue</button>
-          <button onClick={() => this.cancel()}>Cancel</button>
-        </div>
+          <div className='actions'>
+            <button type='submit'>Continue</button>
+            <button onClick={() => this.cancel()}>Cancel</button>
+          </div>
+        </form>
 
         <div>PLACEHOLDER</div>
       </div>
     )
   }
 
-  private continue() {
-    // TODO: Actually set the git config
+  private onNameChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({ name: event.currentTarget.value, email: this.state.email })
+  }
+
+  private onEmailChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({ name: this.state.name, email: event.currentTarget.value })
+  }
+
+  private async continue(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
 
     this.props.cancel()
+
+    const name = this.state.name
+    if (name.length) {
+      await LocalGitOperations.setGlobalConfigValue('user.name', name)
+    }
+
+    const email = this.state.email
+    if (email.length) {
+      await LocalGitOperations.setGlobalConfigValue('user.email', email)
+    }
   }
 
   private cancel() {
