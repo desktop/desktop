29d58ac17197e1bded96509504df156b852a692e
joshaber <joshaber@gmail.com>
2016-11-08 14:34:44 -0500

    Pull the whole welcome uri into a constant

--- app/src/ui/welcome/start.tsx
+++ app/src/ui/welcome/start.tsx
@@ -5,7 +5,7 @@ import { Dispatcher } from '../../lib/dispatcher'
 
 const CreateAccountURL = 'https://github.com/join?source=github-desktop'
 
-const WelcomeImagePath = 'static/space.png'
+const WelcomeImageUri = 'file:///${__dirname}/static/space.png'
 const WelcomeImageDimensions = { width: 552, height: 307 }
 
 interface IStartProps {
@@ -17,10 +17,9 @@ interface IStartProps {
 /** The first step of the Welcome flow. */
 export class Start extends React.Component<IStartProps, void> {
   public render() {
-    const spacePath = `file:///${__dirname}/${WelcomeImagePath}`
     return (
       <div id='start'>
-        <img src={spacePath} style={WelcomeImageDimensions}/>
+        <img src={WelcomeImageUri} style={WelcomeImageDimensions}/>
 
         <h1>Welcome to GitHub Desktop</h1>
         <div>Get started by signing into GitHub.com or your GitHub Enterprise server.</div>
