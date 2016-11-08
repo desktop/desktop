ebf629f6528514105164fdc0dfc1de84c448e4fa
Markus Olsson <j.markus.olsson@gmail.com>
2016-11-08 18:33:49 +0100

    :book:

--- app/src/lib/format-commit-message.ts
+++ app/src/lib/format-commit-message.ts
@@ -1,5 +1,12 @@
 import { ICommitMessage } from './dispatcher/git-store'
 
+/**
+ * Formats a summary and a description into a git-friendly
+ * commit message where the summary and (optional) description
+ * is separated by a blank line.
+ *
+ * See https://git-scm.com/docs/git-commit#_discussion
+ */
 export function formatCommitMessage(message: ICommitMessage) {
   let msg = message.summary
   if (message.description) {
