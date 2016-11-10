b7aaac1de9c470028085cf22e4733e98a3a9af86
joshaber <joshaber@gmail.com>
2016-11-10 16:43:11 -0500

    Pull the common sign in stuff into its own component

---
+++ app/src/ui/lib/sign-in-dot-com.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react'
+import { LinkButton } from '../lib/link-button'
+import { Button } from '../lib/button'
+
+const ForgotPasswordURL = 'https://github.com/password_reset'
+
+interface ISignInDotComProps {
+  readonly onSignInWithBrowser: () => void
+
+  readonly additionalButtons?: ReadonlyArray<JSX.Element>
+}
+
+interface ISignInDotComState {
+  readonly username: string
+  readonly password: string
+}
+
+/** The GitHub.com sign in component. */
+export class SignInDotCom extends React.Component<ISignInDotComProps, ISignInDotComState> {
+  public constructor(props: ISignInDotComProps) {
+    super(props)
+
+    this.state = { username: '', password: '' }
+  }
+
+  public render() {
+    const signInDisabled = Boolean(!this.state.username.length || !this.state.password.length)
+    return (
+      <form id='sign-in-form' onSubmit={e => this.signIn(e)}>
+        <label>Username or email address
+          <input onChange={e => this.onUsernameChange(e)}/>
+        </label>
+
+        <label>Password
+          <input type='password' onChange={e => this.onPasswordChange(e)}/>
+        </label>
+
+        <LinkButton uri={ForgotPasswordURL}>Forgot password?</LinkButton>
+
+        <div className='actions'>
+          <Button type='submit' disabled={signInDisabled}>Sign in</Button>
+          {this.props.additionalButtons}
+        </div>
+
+        <div>or</div>
+
+        <LinkButton onClick={() => this.props.onSignInWithBrowser()}>Sign in using your browser</LinkButton>
+      </form>
+    )
+  }
+
+  private onUsernameChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({
+      username: event.currentTarget.value,
+      password: this.state.password,
+    })
+  }
+
+  private onPasswordChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({
+      username: this.state.username,
+      password: event.currentTarget.value,
+    })
+  }
+
+  private signIn(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    // TODO: Actually sign in lolololol
+  }
+}
