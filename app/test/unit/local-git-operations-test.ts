382dab866a9b2d9ee8b68a2c27b9c843fbd56c56
Markus Olsson <j.markus.olsson@gmail.com>
2016-11-14 21:00:57 +0100

    Move status tests to dedicated file

--- app/test/unit/local-git-operations-test.ts
+++ app/test/unit/local-git-operations-test.ts
@@ -28,45 +28,6 @@ describe('LocalGitOperations', () => {
     temp.cleanupSync()
   })
 
-  describe('status', () => {
-    it('parses changed files', async () => {
-      fs.writeFileSync(path.join(repository!.path, 'README.md'), 'Hi world\n')
-
-      const status = await getStatus(repository!)
-      const files = status.workingDirectory.files
-      expect(files.length).to.equal(1)
-
-      const file = files[0]
-      expect(file.path).to.equal('README.md')
-      expect(file.status).to.equal(FileStatus.Modified)
-    })
-
-    it('returns an empty array when there are no changes', async () => {
-      const status = await getStatus(repository!)
-      const files = status.workingDirectory.files
-      expect(files.length).to.equal(0)
-    })
-
-    it('reflects renames', async () => {
-
-      const repo = await setupEmptyRepository()
-
-      fs.writeFileSync(path.join(repo.path, 'foo'), 'foo\n')
-
-      await GitProcess.exec([ 'add', 'foo' ], repo.path)
-      await GitProcess.exec([ 'commit', '-m', 'Initial commit' ], repo.path)
-      await GitProcess.exec([ 'mv', 'foo', 'bar' ], repo.path)
-
-      const status = await getStatus(repo)
-      const files = status.workingDirectory.files
-
-      expect(files.length).to.equal(1)
-      expect(files[0].status).to.equal(FileStatus.Renamed)
-      expect(files[0].oldPath).to.equal('foo')
-      expect(files[0].path).to.equal('bar')
-    })
-  })
-
   describe('committing', () => {
     it('commits the given files', async () => {
       fs.writeFileSync(path.join(repository!.path, 'README.md'), 'Hi world\n')
