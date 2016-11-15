5d66d2c1c714e901ce57d00f1a797d9b3e658087
Markus Olsson <j.markus.olsson@gmail.com>
2016-11-15 11:35:09 +0100

    Move for-each-ref based tests into git folder

--- app/test/unit/git/for-each-ref-test.ts
+++ app/test/unit/git/for-each-ref-test.ts
@@ -1,6 +1,6 @@
 import { expect, use as chaiUse } from 'chai'
 import { Repository } from '../../../src/models/repository'
-import { setupFixtureRepository } from '../../fixture-helper'
+import { setupFixtureRepository, setupEmptyRepository } from '../../fixture-helper'
 import { getBranches, getCurrentBranch } from '../../../src/lib/git/for-each-ref'
 import { BranchType } from '../../../src/models/branch'
 
@@ -43,6 +43,21 @@ describe('git/for-each-ref', () => {
       expect(master.tip.summary).to.equal('stubbed a README')
       expect(master.tip.parentSHAs.length).to.equal(1)
     })
+
+    it('should list all branches', async () => {
+      const branches = await getBranches(repository!, 'refs/heads', BranchType.Local)
+      expect(branches.length).to.equal(1)
+      expect(branches[0].name).to.equal('master')
+      expect(branches[0].upstream).to.equal(null)
+      expect(branches[0].type).to.equal(BranchType.Local)
+      expect(branches[0].tip.sha).to.equal('04c7629c588c74659f03dda5e5fb3dd8d6862dfa')
+    })
+
+    it('should return empty list for empty repo', async () => {
+      const repo = await setupEmptyRepository()
+      const branches = await getBranches(repo, '', BranchType.Local)
+      expect(branches.length).to.equal(0)
+    })
   })
 
   describe('getCurrentBranch', () => {
@@ -60,6 +75,21 @@ describe('git/for-each-ref', () => {
       expect(date).to.equalDate(new Date('Tue Oct 18 16:23:42 2016 +1100'))
 
       expect(currentBranch!.tip.parentSHAs.length).to.equal(1)
+    })
+
+    it('should get the current branch', async () => {
+      const branch = await getCurrentBranch(repository!)
+      expect(branch!.name).to.equal('master')
+      expect(branch!.upstream).to.equal(null)
+      expect(branch!.tip).to.be.not.null
+      expect(branch!.tip.sha).to.equal('04c7629c588c74659f03dda5e5fb3dd8d6862dfa')
+      expect(branch!.type).to.equal(BranchType.Local)
+    })
+
+    it('should return null for empty repo', async () => {
+      const repo = await setupEmptyRepository()
+      const branch = await getCurrentBranch(repo)
+      expect(branch).to.be.null
     })
   })
 })
