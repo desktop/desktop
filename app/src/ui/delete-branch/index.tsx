874839e1b9cb2485f05739baabf0bebd3fec4b5d
Markus Olsson <j.markus.olsson@gmail.com>
2016-11-15 15:44:28 +0100

    Bind event handlers in delete-branch

--- app/src/ui/delete-branch/index.tsx
+++ app/src/ui/delete-branch/index.tsx
@@ -1,5 +1,3 @@
-/* tslint:disable:react-this-binding-issue */
-
 import * as React from 'react'
 
 import { Dispatcher } from '../../lib/dispatcher'
@@ -15,23 +13,23 @@ interface IDeleteBranchProps {
 export class DeleteBranch extends React.Component<IDeleteBranchProps, void> {
   public render() {
     return (
-      <form className='panel' onSubmit={event => this.cancel(event)}>
+      <form className='panel' onSubmit={this.cancel}>
         <div>Delete branch "{this.props.branch.name}"?</div>
         <div>This cannot be undone.</div>
 
         <button type='submit'>Cancel</button>
-        <button onClick={() => this.deleteBranch()}>Delete</button>
+        <button onClick={this.deleteBranch}>Delete</button>
       </form>
     )
   }
 
-  private cancel(event: React.FormEvent<HTMLFormElement>) {
+  private cancel = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
     this.props.dispatcher.closePopup()
   }
 
-  private deleteBranch() {
+  private deleteBranch = () => {
     this.props.dispatcher.deleteBranch(this.props.repository, this.props.branch)
     this.props.dispatcher.closePopup()
   }
