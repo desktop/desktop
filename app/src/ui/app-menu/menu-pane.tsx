46b2d613df22b1d56e68ee52186d3deb8cf1bf73
Markus Olsson <j.markus.olsson@gmail.com>
2016-11-25 23:43:11 +0100

    Selection events from panes

--- app/src/ui/app-menu/menu-pane.tsx
+++ app/src/ui/app-menu/menu-pane.tsx
@@ -3,7 +3,11 @@ import { List } from '../list'
 import { Octicon, OcticonSymbol } from '../octicons'
 
 interface IMenuPaneProps {
+  readonly depth: number,
   readonly items: ReadonlyArray<Electron.MenuItem>
+  readonly selectedItem?: Electron.MenuItem
+  readonly onItemClicked: (depth: number, item: Electron.MenuItem) => void
+  readonly onSelectionChanged: (depth: number, item: Electron.MenuItem) => void
 }
 
 const RowHeight = 30
@@ -29,14 +33,38 @@ export class MenuPane extends React.Component<IMenuPaneProps, void> {
     )
   }
 
+  private onRowSelected = (row: number) => {
+    this.props.onItemClicked(this.props.depth, this.props.items[row])
+  }
+
+  private onSelectionChanged = (row: number) => {
+    this.props.onSelectionChanged(this.props.depth, this.props.items[row])
+  }
+
+  private onRowKeyDown = (row: number, event: React.KeyboardEvent<any>) => {
+    const item = this.props.items[row]
+
+    if (event.key === 'Enter' || (item.type === 'submenu' && event.key === 'ArrowRight')) {
+      this.props.onItemClicked(this.props.depth, item)
+    }
+  }
+
   public render(): JSX.Element {
+
+    const selectedRow = this.props.selectedItem
+      ? this.props.items.indexOf(this.props.selectedItem)
+      : -1
+
     return (
       <div className='menu-pane'>
         <List
           rowCount={this.props.items.length}
           rowHeight={RowHeight}
           rowRenderer={this.renderMenuItem}
-          selectedRow={-1}
+          selectedRow={selectedRow}
+          onRowSelected={this.onRowSelected}
+          onSelectionChanged={this.onSelectionChanged}
+          onRowKeyDown={this.onRowKeyDown}
         />
       </div>
     )
