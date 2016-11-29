facf2915ef5f00fa4d2c450508854b0d68561381
Markus Olsson <j.markus.olsson@gmail.com>
2016-11-29 15:56:26 +0100

    Render accelerator keys

--- app/src/ui/app-menu/menu-list-item.tsx
+++ app/src/ui/app-menu/menu-list-item.tsx
@@ -9,7 +9,51 @@ export interface IMenuListItemProps {
   readonly separatorBelow?: boolean
 }
 
+/**
+ * Converts Electron accelerator modifiers to their platform specific
+ * name or symbol.
+ *
+ * Example: CommandOrControl becomes either '⌘' or 'Ctrl' depending on platform.
+ *
+ * See https://github.com/electron/electron/blob/fb74f55/docs/api/accelerator.md
+ */
+function getPlatformSpecificNameOrSymbolForModifier(modifier: string): string {
+  switch (modifier.toLowerCase()) {
+    case 'cmdorctrl':
+    case 'commandorcontrol': return __DARWIN__ ? '⌘' : 'Ctrl'
+
+    case 'ctrl':
+    case 'control': return __DARWIN__ ? '⌃' : 'Ctrl'
+
+    case 'shift': return __DARWIN__ ? '⇧' : 'Shift'
+    case 'alt': return __DARWIN__ ? '⌥' : 'Alt'
+
+    // Mac only
+    case 'cmd':
+    case 'command': return '⌘'
+    case 'option': return '⌥'
+
+    // Special case space because no one would be able to see it
+    case ' ': return 'Space'
+  }
+
+  // Not a known modifier, likely a normal key
+  return modifier
+}
+
+/**
+ * Returns a platform specific human readable version of an Electron
+ * accelerator string. See getPlatformSpecificNameOrSymbolForModifier
+ * for more information.
+ */
+export function friendlyAcceleratorText(accelerator: string): string {
+  return accelerator.split('+')
+    .map(getPlatformSpecificNameOrSymbolForModifier)
+    .join(__DARWIN__ ? '' : '+')
+}
+
 export class MenuListItem extends React.Component<IMenuListItemProps, void> {
+
 
   public render() {
     const item = this.props.item
@@ -22,6 +66,10 @@ export class MenuListItem extends React.Component<IMenuListItemProps, void> {
       ? <Octicon className='submenu-arrow' symbol={OcticonSymbol.triangleRight} />
       : null
 
+    const accelerator = item.type !== 'submenuItem' && item.accelerator
+      ? <div className='accelerator'>{friendlyAcceleratorText(item.accelerator)}</div>
+      : null
+
     const className = classNames(
       'menu-item',
       { 'separator' : this.props.separatorBelow }
@@ -30,6 +78,7 @@ export class MenuListItem extends React.Component<IMenuListItemProps, void> {
     return (
       <div className={className}>
         <div className='label'>{item.label}</div>
+        {accelerator}
         {arrow}
       </div>
     )
