e9d558c2227e616d036fc44ef7b19330d0daade2
joshaber <joshaber@gmail.com>
2016-12-01 16:21:42 -0500

    Select component

---
+++ app/src/ui/lib/select.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+
+interface ISelectProps {
+  readonly label?: string
+  readonly onChange?: (event: React.FormEvent<HTMLSelectElement>) => void
+  readonly defaultValue?: string
+  readonly children?: ReadonlyArray<JSX.Element>
+}
+
+export class Select extends React.Component<ISelectProps, void> {
+  public render() {
+    return (
+      <label className='select-component'>
+        {this.props.label}
+
+        <select
+          onChange={this.props.onChange}
+          defaultValue={this.props.defaultValue}>
+          {this.props.children}
+        </select>
+      </label>
+    )
+  }
+}
