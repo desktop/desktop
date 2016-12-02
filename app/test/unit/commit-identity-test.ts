4b18946ca75f00a897a23274ff72a5a89a048169
Markus Olsson <j.markus.olsson@gmail.com>
2016-12-02 10:36:35 +0100

    Add test for date parsing

--- app/test/unit/commit-identity-test.ts
+++ app/test/unit/commit-identity-test.ts
@@ -1,5 +1,6 @@
-import * as chai from 'chai'
-const expect = chai.expect
+import { expect, use as chaiUse } from 'chai'
+
+chaiUse(require('chai-datetime'))
 
 import { CommitIdentity } from '../../src/models/commit-identity'
 
@@ -11,6 +12,8 @@ describe('CommitIdentity', () => {
 
       expect(identity!.name).to.equal('Markus Olsson')
       expect(identity!.email).to.equal('markus@github.com')
+      expect(identity!.date).to.equalTime(new Date('2016-10-05T12:29:40.000Z'))
+    })
     })
 
     it('parses even if the email address isn\'t a normal email', () => {
