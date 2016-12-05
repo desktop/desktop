eacd324e23d26a342ea9b5409eb448087f585fa0
Brendan Forster <brendan@github.com>
2016-12-05 11:24:59 +1100

    resolve bad merge

--- app/src/ui/main-process-proxy.ts
+++ app/src/ui/main-process-proxy.ts
@@ -4,11 +4,6 @@ import { v4 as guid } from 'node-uuid'
 import { IHTTPRequest, IHTTPResponse } from '../lib/http'
 import { ExecutableMenuItem } from '../models/app-menu'
 
-/** Show the app menu as a popup. */
-export function showPopupAppMenu() {
-  ipcRenderer.send('show-popup-app-menu')
-}
-
 /** Set the menu item's enabledness. */
 export function setMenuEnabled(id: MenuIDs, enabled: boolean) {
   ipcRenderer.send('set-menu-enabled', { id, enabled })
