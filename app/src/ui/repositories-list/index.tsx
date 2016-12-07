80a700a29d6d89255985cec40f24150d2d90e679
joshaber <joshaber@gmail.com>
2016-12-07 11:24:24 -0500

    Don't make assumptions about the first and last selectable row

--- app/src/ui/repositories-list/index.tsx
+++ app/src/ui/repositories-list/index.tsx
@@ -91,10 +91,15 @@ export class RepositoriesList extends React.Component<IRepositoriesListProps, IR
   }
 
   private onRowKeyDown = (row: number, event: React.KeyboardEvent<any>) => {
+    const list = this.list
+    if (!list) { return }
+
     let focusInput = false
-    if (event.key === 'ArrowUp' && row === 1) {
+    const firstSelectableRow = list.nextSelectableRow('down', 0)
+    const lastSelectableRow = list.nextSelectableRow('up', 0)
+    if (event.key === 'ArrowUp' && row === firstSelectableRow) {
       focusInput = true
-    } else if (event.key === 'ArrowDown' && row === this.state.listItems.length - 1) {
+    } else if (event.key === 'ArrowDown' && row === lastSelectableRow) {
       focusInput = true
     }
 
