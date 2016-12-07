692aa236c6b66653f8848131d1a8c5aa65312215
joshaber <joshaber@gmail.com>
2016-12-07 10:49:36 -0500

    Add a menu item for the repos list

--- app/src/main-process/menu.ts
+++ app/src/main-process/menu.ts
@@ -5,7 +5,7 @@ export type MenuEvent = 'push' | 'pull' | 'select-changes' | 'select-history' |
                         'add-local-repository' | 'create-branch' |
                         'show-branches' | 'remove-repository' | 'add-repository' |
                         'rename-branch' | 'delete-branch' | 'check-for-updates' |
-                        'quit-and-install-update' | 'show-preferences'
+                        'quit-and-install-update' | 'show-preferences' | 'choose-repository'
 
 export type MenuIDs = 'rename-branch' | 'delete-branch' | 'check-for-updates' |
                       'checking-for-updates' | 'downloading-update' | 'quit-and-install-update' |
@@ -16,6 +16,13 @@ export function buildDefaultMenu(sharedProcess: SharedProcess): Electron.Menu {
     {
       label: 'File',
       submenu: [
+        {
+          label: 'Choose Repository…',
+          accelerator: 'CmdOrCtrl+L',
+          click (item: any, focusedWindow: Electron.BrowserWindow) {
+            emitMenuEvent('choose-repository')
+          },
+        },
         {
           label: 'New Branch…',
           accelerator: 'CmdOrCtrl+Shift+N',
