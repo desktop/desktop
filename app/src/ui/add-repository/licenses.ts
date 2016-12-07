3a14f229213133b4a8142ace427189292d473d29
joshaber <joshaber@gmail.com>
2016-12-07 17:28:21 -0500

    Load some licenses

---
+++ app/src/ui/add-repository/licenses.ts
@@ -0,0 +1,74 @@
+import * as Path from 'path'
+import * as Fs from 'fs'
+
+interface IFrontMatterResult<T> {
+  readonly attributes: T
+  readonly body: string
+}
+
+const frontMatter: <T>(path: string) => IFrontMatterResult<T> = require('front-matter')
+
+interface IChooseALicense {
+  readonly title: string
+  readonly featured?: boolean
+}
+
+export interface ILicense {
+  /** The human-readable title. */
+  readonly title: string
+
+  /** Is the license featured? */
+  readonly featured: boolean
+
+  /** The actual text of the license. */
+  readonly body: string
+}
+
+const root = Path.join(__dirname, 'static', 'choosealicense.com', '_licenses')
+
+let cachedLicenses: ReadonlyArray<ILicense> | null = null
+
+function readFileAsync(path: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    Fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+/** Get the available licenses. */
+export function getLicenses(): Promise<ReadonlyArray<ILicense>> {
+  if (cachedLicenses) {
+    return Promise.resolve(cachedLicenses)
+  } else {
+    return new Promise((resolve, reject) => {
+      Fs.readdir(root, async (err, files) => {
+        if (err) {
+          reject(err)
+        } else {
+          const licenses = new Array<ILicense>()
+          for (const file of files) {
+            const fullPath = Path.join(root, file)
+            const contents = await readFileAsync(fullPath)
+            const result = frontMatter<IChooseALicense>(contents)
+            const license: ILicense = {
+              title: result.attributes.title,
+              featured: result.attributes.featured || false,
+              body: result.body,
+            }
+
+            licenses.push(license)
+          }
+
+          cachedLicenses = licenses
+
+          resolve(cachedLicenses)
+        }
+      })
+    })
+  }
+}
