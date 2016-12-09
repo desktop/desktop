f578041eae3746c095408e9caf4bbe6a702431e8
joshaber <joshaber@gmail.com>
2016-12-09 09:25:12 -0500

    Explicitly import these

--- app/test/globals.ts
+++ app/test/globals.ts
@@ -4,3 +4,6 @@ const g: any = global
 g['__WIN32__'] = process.platform === 'win32'
 g['__DARWIN__'] = process.platform === 'darwin'
 g['__RELEASE_ENV__'] = 'development'
+
+import 'mocha'
+import 'chai-datetime'
