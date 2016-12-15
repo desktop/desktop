b25eb4290e29bfa843c5c80330883a5e26271c0d
joshaber <joshaber@gmail.com>
2016-12-15 10:20:02 -0500

    Test error handling

---
+++ app/test/unit/git/core-test.ts
@@ -0,0 +1,83 @@
+import { expect } from 'chai'
+import { GitError } from 'git-kitchen-sink'
+
+import { Repository } from '../../../src/models/repository'
+import { git } from '../../../src/lib/git'
+import { setupFixtureRepository } from '../../fixture-helper'
+
+const temp = require('temp').track()
+
+describe.only('git/core', () => {
+  let repository: Repository | null = null
+
+  beforeEach(() => {
+    const testRepoPath = setupFixtureRepository('test-repo')
+    repository = new Repository(testRepoPath, -1, null)
+  })
+
+  after(() => {
+    temp.cleanupSync()
+  })
+
+  describe('error handling', () => {
+    it('does not throw for errors that were expected', async () => {
+      const args = [ 'rev-list', '--left-right', '--count', 'some-ref', '--' ]
+
+      let threw = false
+      try {
+        const result = await git(args, repository!.path, 'test', {
+          expectedErrors: new Set([ GitError.BadRevision ]),
+        })
+        expect(result.gitError).to.equal(GitError.BadRevision)
+      } catch (e) {
+        threw = true
+      }
+
+      expect(threw).to.equal(false)
+    })
+
+    it('throws for errors that were not expected', async () => {
+      const args = [ 'rev-list', '--left-right', '--count', 'some-ref', '--' ]
+
+      let threw = false
+      try {
+        await git(args, repository!.path, 'test')
+      } catch (e) {
+        threw = true
+      }
+
+      expect(threw).to.equal(true)
+    })
+  })
+
+  describe('exit code handling', () => {
+    it('does not throw for exit codes that were expected', async () => {
+      const args = [ 'rev-list', '--left-right', '--count', 'some-ref', '--' ]
+
+      let threw = false
+      try {
+        const result = await git(args, repository!.path, 'test', {
+          successExitCodes: new Set([ 128 ]),
+        })
+        expect(result.exitCode).to.equal(128)
+      } catch (e) {
+        threw = true
+      }
+
+      expect(threw).to.equal(false)
+    })
+
+    it('throws for exit codes that were not expected', async () => {
+      const args = [ 'rev-list', '--left-right', '--count', 'some-ref', '--' ]
+
+      let threw = false
+      try {
+        await git(args, repository!.path, 'test')
+      } catch (e) {
+        threw = true
+      }
+
+      expect(threw).to.equal(true)
+    })
+  })
+})
