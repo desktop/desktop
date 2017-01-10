c2f96c9909480d52aca3f652f15a6cb27cfc1b5b
joshaber <joshaber@gmail.com>
2017-01-10 13:39:53 -0500

    I bless the rains

--- app/src/ui/lib/default-dir.ts
+++ app/src/ui/lib/default-dir.ts
@@ -2,5 +2,6 @@ import * as OS from 'os'
 
 /** The path to the default directory. */
 export function getDefaultDir(): string {
+  // TODO: This should be saved and loaded from local storage.
   return OS.homedir()
 }
