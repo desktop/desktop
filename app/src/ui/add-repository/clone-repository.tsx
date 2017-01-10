b581a09eaf6555f3cbf15965e7edf958214299e5
joshaber <joshaber@gmail.com>
2017-01-10 13:22:55 -0500

    Display a form

--- app/src/ui/add-repository/clone-repository.tsx
+++ app/src/ui/add-repository/clone-repository.tsx
@@ -1,13 +1,39 @@
 import * as React from 'react'
+import { Form } from '../lib/form'
+import { TextBox } from '../lib/text-box'
+import { Button } from '../lib/button'
 
 interface ICloneRepositoryProps {
 }
 
-/** The component for cloning an existing GitHub repository. */
-export class CloneRepository extends React.Component<ICloneRepositoryProps, void> {
+interface ICloneRepositoryState {
+  readonly url: string
+}
+
+/** The component for cloning a repository. */
+export class CloneRepository extends React.Component<ICloneRepositoryProps, ICloneRepositoryState> {
+  public constructor(props: ICloneRepositoryProps) {
+    super(props)
+
+    this.state = { url: '' }
+  }
+
   public render() {
+    const disabled = this.state.url.length === 0
     return (
-      <div>Clone repository goes here</div>
+      <Form className='clone-repository' onSubmit={this.clone}>
+        <div>Enter a repository URL or GitHub username and repository (e.g., <pre>hubot/cool-repo</pre>)</div>
+        <TextBox placeholder='URL or username/repository' value={this.state.url} onChange={this.onURLChange}/>
+        <Button disabled={disabled} type='submit'>Clone</Button>
+      </Form>
     )
+  }
+
+  private onURLChange = (event: React.FormEvent<HTMLInputElement>) => {
+    this.setState({ url: event.currentTarget.value })
+  }
+
+  private clone = () => {
+
   }
 }
