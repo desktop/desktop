8ee4ac5239519218a1c1f47bca8fe91474c894d6
joshaber <joshaber@gmail.com>
2017-01-12 14:41:45 -0800

    Document why we're doing this

--- app/test/globals.ts
+++ app/test/globals.ts
@@ -1,3 +1,5 @@
+// This shouldn't be necessary, but without this CI fails on Windows. Seems to
+// be a bug in TS itself or ts-node.
 /// <reference path="../../node_modules/@types/node/index.d.ts" />
 
 import 'mocha'
