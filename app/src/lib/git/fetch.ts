205f5307edfb7d3973e7629c379bc4ba906b938d
Brendan Forster <brendan@github.com>
2017-01-12 13:33:50 +1100

    handle and rethrow auth issue when fetching

--- app/src/lib/git/fetch.ts
+++ app/src/lib/git/fetch.ts
@@ -2,7 +2,21 @@ import { git, envForAuthentication } from './core'
 import { Repository } from '../../models/repository'
 import { User } from '../../models/user'
 
+import { GitError } from 'git-kitchen-sink'
+
+
 /** Fetch from the given remote. */
 export async function fetch(repository: Repository, user: User | null, remote: string): Promise<void> {
-  await git([ 'fetch', '--prune', remote ], repository.path, 'fetch', { env: envForAuthentication(user) })
+  const options = {
+    env: envForAuthentication(user),
+    expectedErrors: new Set([ GitError.HTTPSAuthenticationFailed, GitError.SSHAuthenticationFailed ])
+  }
+
+  const result = await git([ 'fetch', '--prune', remote ], repository.path, 'fetch', options)
+
+  if (result.gitErrorDescription) {
+    return Promise.reject(new Error(result.gitErrorDescription))
+  }
+
+  return Promise.resolve()
 }
