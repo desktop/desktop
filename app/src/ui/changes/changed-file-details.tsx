1016e47b01d75f1684ac99c0bfe009d83dd3ec03
iAmWillShepherd <williamrshepherd@gmail.com>
2017-01-17 15:56:56 -0800

    Choose better variable name

--- app/src/ui/changes/changed-file-details.tsx
+++ app/src/ui/changes/changed-file-details.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react'
 
 interface IChangedFileDetailsProps {
-  readonly fileName: string | null
+  readonly filePath: string | null
 }
 
 export class ChangedFileDetails extends React.Component<IChangedFileDetailsProps, void> {
   public render() {
-    const fullFileName = this.props.fileName ? this.props.fileName : undefined
+    const filePath = this.props.filePath ? this.props.filePath : undefined
 
     return (
       <div id='changed-file-details'>
-        {fullFileName}
+        {filePath}
       </div>
     )
   }
