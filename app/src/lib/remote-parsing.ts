f4b768472bbf740f4057b49f97957f514617217c
joshaber <joshaber@gmail.com>
2017-01-17 16:04:04 -0800

    :book:

--- app/src/lib/remote-parsing.ts
+++ app/src/lib/remote-parsing.ts
@@ -1,6 +1,17 @@
 interface IGitRemoteURL {
+  /** The hostname of the remote. */
   readonly hostname: string
+
+  /**
+   * The owner of the GitHub repository. This will be null if the URL doesn't
+   * take the form of a GitHub repository URL (e.g., owner/name).
+   */
   readonly owner: string | null
+
+  /**
+   * The name of the GitHub repository. This will be null if the URL doesn't
+   * take the form of a GitHub repository URL (e.g., owner/name).
+   */
   readonly name: string | null
 }
 
