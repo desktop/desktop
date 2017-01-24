5d992a197b26d829cc2723e8d9028a489ac79e0a
iAmWillShepherd <williamrshepherd@gmail.com>
2017-01-24 16:50:45 -0600

    Passing shell to function

--- app/src/lib/open-terminal.ts
+++ app/src/lib/open-terminal.ts
@@ -6,19 +6,19 @@ class Command {
   public args: string[]
 }
 
-export function openTerminal(fullPath: string) {
+export function openTerminal(fullPath: string, shell?: string) {
   const currentPlatform = platform()
-  const command = new Command()
+  const command = new Command
 
   switch (currentPlatform) {
     case 'darwin': {
       command.name = 'open'
-      command.args = [ '-a', 'Terminal', fullPath ]
+      command.args = [ '-a', shell || 'Terminal', fullPath ]
       break
     }
     case 'win32': {
       command.name = 'start'
-      command.args = [ '/D', '"%cd%"', 'cmd', fullPath ]
+      command.args = [ '/D', '"%cd%"', shell || 'cmd', fullPath ]
       break
     }
   }
