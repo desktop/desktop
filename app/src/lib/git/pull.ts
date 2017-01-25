90c21b5efa85f37b054ed7b6ffbb92a525dd648c
Brendan Forster <brendan@github.com>
2017-01-25 15:20:22 +0100

    implement the same pattern for pull

--- app/src/lib/git/pull.ts
+++ app/src/lib/git/pull.ts
@@ -2,7 +2,26 @@ import { git, envForAuthentication } from './core'
 import { Repository } from '../../models/repository'
 import { User } from '../../models/user'
 
+import { GitError } from 'git-kitchen-sink'
+
 /** Pull from the remote to the branch. */
 export async function pull(repository: Repository, user: User | null, remote: string, branch: string): Promise<void> {
-  await git([ 'pull', remote, branch ], repository.path, 'pull', { env: envForAuthentication(user) })
+
+  const options = {
+    env: envForAuthentication(user),
+    expectedErrors: new Set([
+      GitError.HTTPSAuthenticationFailed,
+      GitError.SSHAuthenticationFailed,
+      GitError.HTTPSRepositoryNotFound,
+      GitError.SSHRepositoryNotFound
+    ]),
+  }
+
+  const result = await git([ 'pull', remote, branch ], repository.path, 'pull', options)
+
+  if (result.gitErrorDescription) {
+    return Promise.reject(new Error(result.gitErrorDescription))
+  }
+
+  return Promise.resolve()
 }
