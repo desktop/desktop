57202bb2e2f6bca9124f15c29839a82d600383e0
joshaber <joshaber@gmail.com>
2017-01-30 16:50:37 -0500

    Docs

--- app/static/darwin/ask-pass-trampoline.sh
+++ app/static/darwin/ask-pass-trampoline.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# We set HOME to an empty string in lib/git/core.ts to avoid picking up the
+# user's git config, but our AskPass script needs it in order to find the user's
+# keychain.
 env -u HOME ELECTRON_RUN_AS_NODE=1 "$DESKTOP_PATH" "$DESKTOP_ASKPASS_SCRIPT" "$@"
