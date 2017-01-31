ca0dffe7c5e7a4beb75ca37049dcdafe28c510e8
joshaber <joshaber@gmail.com>
2017-01-31 14:54:46 -0500

    Add `rm` wrapper

---
+++ app/src/lib/git/rm.ts
@@ -0,0 +1,7 @@
+import { git } from './core'
+import { Repository } from '../../models/repository'
+
+/** Remove the given path from the index. */
+export async function removeFromIndex(repository: Repository, path: string): Promise<void> {
+  await git([ 'rm', '--cached', '--', path ], repository.path, 'rmFromIndex', { successExitCodes: new Set([ 0 ]) })
+}
