736a7fc7c45387fb81fb87c35eb43a6946deed0b
Brendan Forster <brendan@github.com>
2017-01-31 17:43:00 +0100

    s/input/textarea/

--- app/src/ui/lib/text-area.tsx
+++ app/src/ui/lib/text-area.tsx
@@ -2,40 +2,40 @@ import * as React from 'react'
 import * as classNames from 'classnames'
 
 interface ITextAreaProps {
-  /** The label for the input field. */
+  /** The label for the textarea field. */
   readonly label?: string
 
   /** The class name for the label. */
   readonly labelClassName?: string
 
-  /** The class name for the input field. */
-  readonly inputClassName?: string
+  /** The class name for the textarea field. */
+  readonly textareaClassName?: string
 
-  /** The placeholder for the input field. */
+  /** The placeholder for the textarea field. */
   readonly placeholder?: string
 
   readonly rows?: number
 
-  /** The current value of the input field. */
+  /** The current value of the textarea field. */
   readonly value?: string
 
-  /** Whether the input field should auto focus when mounted. */
+  /** Whether the textarea field should auto focus when mounted. */
   readonly autoFocus?: boolean
 
-  /** Whether the input field is disabled. */
+  /** Whether the textarea field is disabled. */
   readonly disabled?: boolean
 
-  /** Called when the user changes the value in the input field. */
+  /** Called when the user changes the value in the textarea field. */
   readonly onChange?: (event: React.FormEvent<HTMLTextAreaElement>) => void
 
   /** Called on key down. */
   readonly onKeyDown?: (event: React.KeyboardEvent<HTMLTextAreaElement>) => void
 
-  /** A callback to receive the underlying `input` instance. */
-  readonly onInputRef?: (instance: HTMLTextAreaElement) => void
+  /** A callback to receive the underlying `textarea` instance. */
+  readonly onTextAreaRef?: (instance: HTMLTextAreaElement) => void
 }
 
-/** An input element with app-standard styles. */
+/** A textarea element with app-standard styles. */
 export class TextArea extends React.Component<ITextAreaProps, void> {
   public render() {
     const className = classNames('text-area-component', this.props.labelClassName)
@@ -45,14 +45,14 @@ export class TextArea extends React.Component<ITextAreaProps, void> {
 
         <textarea
           autoFocus={this.props.autoFocus}
-          className={this.props.inputClassName}
+          className={this.props.textareaClassName}
           disabled={this.props.disabled}
           rows={this.props.rows || 3}
           placeholder={this.props.placeholder}
           value={this.props.value}
           onChange={this.props.onChange}
           onKeyDown={this.props.onKeyDown}
-          ref={this.props.onInputRef}/>
+          ref={this.props.onTextAreaRef}/>
       </label>
     )
   }
