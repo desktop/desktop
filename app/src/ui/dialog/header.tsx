c17d5bbbb409ff5770b772142c69488bd3e0a240
Markus Olsson <j.markus.olsson@gmail.com>
2017-02-02 17:39:49 +0100

    :book: DialogHeader

--- app/src/ui/dialog/header.tsx
+++ app/src/ui/dialog/header.tsx
@@ -3,12 +3,40 @@ import { Octicon, OcticonSymbol } from '../octicons'
 import { assertNever } from '../../lib/fatal-error'
 
 interface IDialogHeaderProps {
+  /**
+   * The dialog title text. Will be rendered top and center in a dialog.
+   */
   readonly title: string
+
+  /**
+   * Whether or not the implementing dialog is dismissable. This controls
+   * whether or not the dialog header renders a close button or not.
+   */
   readonly dismissable: boolean
+
+  /**
+   * Event triggered when the dialog is dismissed by the user in the
+   * ways described in the dismissable prop.
+   */
   readonly onDismissed?: () => void
+
+  /**
+   * An optional type of dialog header. If the type is error or warning
+   * an applicable icon will be rendered top left in the dialog.
+   * 
+   * Defaults to 'normal' if omitted.
+   */
   readonly type?: 'normal' | 'warning' | 'error'
 }
 
+/**
+ * A high-level component for Dialog headers.
+ * 
+ * This component should typically not be used by consumers as the title prop
+ * of the Dialog component should suffice. There are, however, cases where
+ * custom content needs to be rendered in a dialog and in that scenario it
+ * might be necessary to use this component directly.
+ */
 export class DialogHeader extends React.Component<IDialogHeaderProps, void> {
 
   private onCloseButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
