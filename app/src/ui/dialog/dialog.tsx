49bdc5f6a2b5b598c0f8ef32efb8dc317880e8e1
Markus Olsson <j.markus.olsson@gmail.com>
2017-02-02 17:31:28 +0100

    :book: dialog component

--- app/src/ui/dialog/dialog.tsx
+++ app/src/ui/dialog/dialog.tsx
@@ -3,16 +3,87 @@ import * as classNames from 'classnames'
 import { DialogHeader } from './header'
 
 interface IDialogProps {
+  /** 
+   * An optional dialog title. Most, if not all dialogs should have
+   * this. When present the Dialog renders a DialogHeader element
+   * containing an icon (if the type prop warrants it), the title itself
+   * and a close button (if the dialog is dismissable).
+   * 
+   * By omitting this consumers may use their own custom DialogHeader
+   * for when the default component doesn't cut it.
+   */
   readonly title?: string
+
+  /**
+   * Whether or not the dialog should be dismissable. A dismissable dialog
+   * can be dismissed either by clicking on the backdrop or by clicking
+   * the close button in the header (if a header was specified). Dismissal
+   * will trigger the onDismissed event which callers must handle and pass
+   * on to the dispatcher in order to close the dialog.
+   * 
+   * A non-dismissable dialog can only be closed by means of the component
+   * implementing a dialog. An example would be a critical error or warning
+   * that requires explicit user action by for example clicking on a button.
+   * 
+   * Defaults to true if omitted.
+   */
   readonly dismissable?: boolean
+
+  /**
+   * Event triggered when the dialog is dismissed by the user in the
+   * ways described in the dismissable prop.
+   */
   readonly onDismissed?: () => void
+
+  /**
+   * An optional id for the rendered dialog element.
+   */
   readonly id?: string
+
+  /**
+   * An optional dialog type. A warning or error dialog type triggers custom
+   * styling of the dialog, see _dialog.scss for more detail.
+   * 
+   * Defaults to 'normal' if omitted
+   */
   readonly type?: 'normal' | 'warning' | 'error'
+
+  /**
+   * An event triggered when the dialog form is submitted. All dialogs contain
+   * a top-level form element which can be triggered through a submit button.
+   * 
+   * Consumers should handle this rather than subscribing to the onClick event
+   * on the button itself since there may be other ways of submitting a specific
+   * form (such as Ctrl+Enter).
+   */
   readonly onSubmit?: () => void
+
+  /**
+   * An optional className to be applied to the rendered dialog element.
+   */
   readonly className?: string
+
+  /**
+   * Whether or not the dialog should be disabled. All dialogs wrap their
+   * content in a <fieldset> element which, when disabled, causes all descendant
+   * form elements and buttons to also become disabled. This is useful for
+   * consumers implementing a typical save dialog where the save action isn't
+   * instantaneous (such as a sign in dialog) and they need to ensure that the
+   * user doesn't continue mutating the form state or click buttons while the
+   * save/submit action is in progress. Note that this does not prevent the
+   * dialog from being dismissed.
+   */
   readonly disabled?: boolean
 }
 
+/**
+ * A general purpose, versatile, dialog component which utilizes the new
+ * <dialog> element. See https://demo.agektmr.com/dialog/
+ * 
+ * A dialog is opened as a modal that prevents keyboard or pointer access to
+ * underlying elements. It's not possible to use the tab key to move focus
+ * out of the dialog without first dismissing it.
+ */
 export class Dialog extends React.Component<IDialogProps, void> {
 
   private dialogElement?: HTMLElement
@@ -22,6 +93,8 @@ export class Dialog extends React.Component<IDialogProps, void> {
   }
 
   public componentDidMount() {
+    // This cast to any is necessary since React doesn't know about the
+    // dialog element yet.
     (this.dialogElement as any).showModal()
   }
 
@@ -36,6 +109,7 @@ export class Dialog extends React.Component<IDialogProps, void> {
       return
     }
 
+    // Figure out if the user clicked on the backdrop or in the dialog itself.
     const rect = e.currentTarget.getBoundingClientRect()
 
     // http://stackoverflow.com/a/26984690/2114
@@ -52,6 +126,9 @@ export class Dialog extends React.Component<IDialogProps, void> {
   }
 
   private onDialogRef = (e: HTMLElement | undefined) => {
+    // We need to explicitly subscribe to and unsubscribe from the dialog
+    // element as react doesn't yet understand the element and which events
+    // it has.
     if (!e) {
       if (this.dialogElement) {
         this.dialogElement.removeEventListener('cancel', this.onDialogCancel)
