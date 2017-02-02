58d7c947a312dd3f001c0e81e6f3b60a1e6a09eb
Markus Olsson <j.markus.olsson@gmail.com>
2017-02-02 17:33:38 +0100

    :book: DialogContent

--- app/src/ui/dialog/content.tsx
+++ app/src/ui/dialog/content.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react'
 
-interface IDialogContentProps {
-}
-
-export class DialogContent extends React.Component<IDialogContentProps, any> {
+/**
+ * A container element for content (ie non-header, non-footer) in a Dialog.
+ * This component should only be used once in any given dialog.
+ * 
+ * If a dialog implements a tabbed interface where each tab is a child component
+ * the child components _should_ render the DialogContent component themselves
+ * to avoid excessive nesting and to ensure that styles applying to phrasing
+ * content in the dialog get applied consistently.
+ */
+export class DialogContent extends React.Component<void, void> {
 
   public render() {
     return (
