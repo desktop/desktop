7617ce08b6f19a0299eff0bfa49325706742a044
joshaber <joshaber@gmail.com>
2017-02-02 11:05:57 -0500

    It's OK if the file's not in the index.

--- app/src/lib/git/rm.ts
+++ app/src/lib/git/rm.ts
@@ -3,5 +3,7 @@ import { Repository } from '../../models/repository'
 
 /** Remove the given path from the index. */
 export async function removeFromIndex(repository: Repository, path: string): Promise<void> {
-  await git([ 'rm', '--cached', '--', path ], repository.path, 'removeFromIndex', { successExitCodes: new Set([ 0 ]) })
+  // Exit code of 128 means the file wasn't in the index to being with. That's
+  // OK.
+  await git([ 'rm', '--cached', '--', path ], repository.path, 'removeFromIndex', { successExitCodes: new Set([ 0, 128 ]) })
 }
