17bbed90f19ff2c47f2aa01d95fe20532a158a4a
joshaber <joshaber@gmail.com>
2017-02-02 11:36:53 -0500

    Generalize Avatar to only require the properties we need

--- app/src/ui/lib/avatar.tsx
+++ app/src/ui/lib/avatar.tsx
@@ -1,29 +1,44 @@
 import * as React from 'react'
-import { IGitHubUser } from '../../lib/dispatcher'
-
-interface IAvatarProps {
-  readonly gitHubUser: IGitHubUser | null
-  readonly title: string | null
-}
 
 const DefaultAvatarURL = 'https://github.com/hubot.png'
 
+/** The minimum properties we need in order to display a user's avatar. */
+export interface IAvatarUser {
+  /** The user's email. */
+  readonly email: string
+
+  /** The user's avatar URL. */
+  readonly avatarURL: string
+}
+
+interface IAvatarProps {
+  /** The user whose avatar should be displayed. */
+  readonly user?: IAvatarUser
+
+  /** The title of the avatar. Defaults to `email` if provided. */
+  readonly title?: string
+}
+
+/** A component for displaying a user avatar. */
 export class Avatar extends React.Component<IAvatarProps, void> {
-   private getTitle(user: IGitHubUser | null): string {
-    if (user === null) {
-      return this.props.title || 'Unknown User'
+  private getTitle(): string {
+    if (this.props.title) {
+      return this.props.title
     }
 
-    return this.props.title || user.email
-   }
+    if (this.props.user) {
+      return this.props.user.email
+    }
+
+    return 'Unknown user'
+  }
 
   public render() {
-    const gitHubUser = this.props.gitHubUser
-    const avatarURL = (gitHubUser ? gitHubUser.avatarURL : null) || DefaultAvatarURL
-    const avatarTitle = this.getTitle(gitHubUser)
+    const url = this.props.user ? this.props.user.avatarURL : DefaultAvatarURL
+    const title = this.getTitle()
 
     return (
-      <img className='avatar' title={avatarTitle} src={avatarURL} alt={avatarTitle}/>
+      <img className='avatar' title={title} src={url} alt={title}/>
     )
   }
 }
