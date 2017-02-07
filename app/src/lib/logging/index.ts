5c4bae07709d70c58d605288315d3f2613ac9bfd
Brendan Forster <brendan@github.com>
2017-02-07 12:38:13 +0100

    cleanup and differentiate between main process and renderer

--- app/src/lib/logging/index.ts
+++ app/src/lib/logging/index.ts
@@ -1,15 +1,29 @@
 import * as winston from 'winston'
 require('winston-daily-rotate-file')
 
+import * as Path from 'path'
+
 import { ElectronConsole } from './electron-console'
 
-import { getUserDataPath } from '../../ui/lib/app-proxy'
+import { getUserDataPath as getUserDataPathRenderer } from '../../ui/lib/app-proxy'
 
-function getLogFilePath(): string {
-  return `${getUserDataPath()}\\desktop.${process.env.NODE_ENV || 'production'}.log`
+function getUserDataPathMain() {
+  const { app } = require('electron')
+  return app.getPath('userData')
+}
+
+function getLogFilePath(mainProcess: boolean): string {
+  const path = mainProcess
+    ? getUserDataPathMain()
+    : getUserDataPathRenderer()
+
+  const environment = process.env.NODE_ENV || 'production'
+  const fileName = `desktop.${environment}.log`
+  return Path.join(path, fileName)
 }
 
 interface ILogger {
+  filename: string,
   debug: (message: string) => void,
   info: (message: string) => void,
   error: (message: string, error?: Error) => void
@@ -17,35 +31,33 @@ interface ILogger {
 
 let logger: ILogger | null = null
 
-function create() {
-  if (process.env.NODE_ENV === 'development') {
-    winston.configure({
-      transports: [
-        new ElectronConsole(),
-      ],
-    })
-  } else {
-    winston.configure({
-      transports: [
-        // only log errors to the console
-        new ElectronConsole({
-          level: 'error',
-        }),
-        new winston.transports.DailyRotateFile({
-          filename: getLogFilePath(),
-          humanReadableUnhandledException: true,
-          handleExceptions: true,
-          json: false,
-          datePattern: 'yyyy-MM-dd.',
-          prepend: true,
-          // log everything interesting (info and up)
-          level: 'info',
-        }),
-      ],
-    })
-  }
+function create(mainProcess: boolean = false) {
+  const filename = getLogFilePath(mainProcess)
+  const fileLogger = new winston.transports.DailyRotateFile({
+    filename,
+    humanReadableUnhandledException: true,
+    handleExceptions: true,
+    json: false,
+    datePattern: 'yyyy-MM-dd.',
+    prepend: true,
+    // log everything interesting (info and up)
+    level: 'info',
+  })
+
+  const level = process.env.NODE_ENV === 'development' ? 'debug' : 'error'
+  const consoleLogger = new ElectronConsole({
+    level,
+  })
+
+  winston.configure({
+    transports: [
+      consoleLogger,
+      fileLogger,
+    ],
+  })
 
   return {
+    filename,
     debug: (message: string) => winston.debug(message),
     info: (message: string) => winston.info(message),
     error: (message: string, error?: Error) => {
@@ -64,3 +76,11 @@ export function getLogger(): ILogger {
   }
   return logger
 }
+
+export function getMainProcessLogger(): ILogger {
+  if (!logger) {
+    logger = create(true)
+  }
+  return logger
+}
+
