06d831b9a98459523c0165e1d0c607448e18c223
Brendan Forster <brendan@github.com>
2017-02-08 13:24:47 +0100

    ugh, typo

--- app/src/lib/git/add.ts
+++ app/src/lib/git/add.ts
@@ -10,9 +10,9 @@ export async function addFileToIndex(repository: Repository, file: WorkingDirect
     // NOTE:
     // copied files are considered untracked content which are already staged,
     // and this only occurs when the old file is modified and also staged in
-    // the index. however the user may not want to commit the modifed old file,
-    // which means it won't be seen in the commit as a copy - just a new file
-    // - and that's okay...
+    // the index. however the user may not want to commit the modified old
+    // file, which means it won't be seen in the commit as a copy - just a new
+    // file - and that's okay...
     await git([ 'add', '--', file.path ], repository.path, 'addFileToIndex')
   } else if (file.status === FileStatus.Renamed && file.oldPath) {
     await git([ 'add', '--', file.path ], repository.path, 'addFileToIndex')
