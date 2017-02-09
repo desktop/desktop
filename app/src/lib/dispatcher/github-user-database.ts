41e554429e20d91536767a2accb6b2a7dbadbfb3
joshaber <joshaber@gmail.com>
2017-02-09 11:34:52 -0500

    Store mentionable associations in the DB

--- app/src/lib/dispatcher/github-user-database.ts
+++ app/src/lib/dispatcher/github-user-database.ts
@@ -1,7 +1,7 @@
 import Dexie from 'dexie'
 
 // NB: This _must_ be incremented whenever the DB key scheme changes.
-const DatabaseVersion = 1
+const DatabaseVersion = 2
 
 export interface IGitHubUser {
   readonly id?: number
@@ -11,14 +11,24 @@ export interface IGitHubUser {
   readonly avatarURL: string
 }
 
+export interface IMentionableAssociation {
+  readonly userID: number
+  readonly repositoryID: number
+}
+
 export class GitHubUserDatabase extends Dexie {
   public users: Dexie.Table<IGitHubUser, number>
+  public mentionables: Dexie.Table<IMentionableAssociation, number>
 
   public constructor(name: string) {
     super(name)
 
-    this.version(DatabaseVersion).stores({
+    this.version(1).stores({
       users: '++id, &[endpoint+email]',
+    })
+
+    this.version(DatabaseVersion).stores({
+      mentionables: '++, repositoryID',
     })
   }
 }
