6b8ea473b187b674b904a237902acb111cd56a24
joshaber <joshaber@gmail.com>
2017-02-10 11:36:26 -0500

    First pass for user autocompletion provider

---
+++ app/src/ui/autocompletion/user-autocompletion-provider.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+
+import { IAutocompletionProvider } from './index'
+import { Dispatcher, GitHubUserStore } from '../../lib/dispatcher'
+import { GitHubRepository } from '../../models/github-repository'
+
+/** An autocompletion hit for a user. */
+export interface IUserHit {
+  /** The username. */
+  readonly username: string
+
+  /** The user's name. */
+  readonly name: string
+}
+
+/** The autocompletion provider for user mentions in a GitHub repository. */
+export class UserAutocompletionProvider implements IAutocompletionProvider<IUserHit> {
+  private readonly gitHubUserStore: GitHubUserStore
+  private readonly repository: GitHubRepository
+  private readonly dispatcher: Dispatcher
+
+  public constructor(gitHubUserStore: GitHubUserStore, repository: GitHubRepository, dispatcher: Dispatcher) {
+    this.gitHubUserStore = gitHubUserStore
+    this.repository = repository
+    this.dispatcher = dispatcher
+  }
+
+  public getRegExp(): RegExp {
+    return /(?:^|\n| )(?:@)([a-z0-9\\+\\-][a-z0-9_]*)?/g
+  }
+
+  public async getAutocompletionItems(text: string): Promise<ReadonlyArray<IUserHit>> {
+    const users = await this.gitHubUserStore.getMentionableUsers(this.repository)
+    const filtered = users.filter(u => {
+      const login = u.login
+      // TODO: should also match on `name`
+      return login && login.startsWith(text)
+    })
+    return filtered.map(u => ({ username: u.login!, name: u.login! }))
+  }
+
+  public renderItem(item: IUserHit): JSX.Element {
+    return (
+      <div className='user' key={item.username}>
+        <span className='username'>{item.username}</span>
+        <span className='name'>{item.name}</span>
+      </div>
+    )
+  }
+
+  public getCompletionText(item: IUserHit): string {
+    return `@${item.username}`
+  }
+}
