25a26efef3a3b1ff32bdf6cd9c02a6e9430d2c71
Brendan Forster <brendan@github.com>
2017-02-10 10:06:27 +0100

    drop empty constructor

--- app/test/test-app-shell.ts
+++ app/test/test-app-shell.ts
@@ -3,10 +3,6 @@ import { AppShell } from '../src/lib/dispatcher/app-shell'
 import * as Fs from 'fs'
 
 export class TestAppShell extends AppShell {
-  public constructor() {
-    super()
-  }
-
   public moveItemToTrash(path: string): boolean {
     Fs.unlinkSync(path)
     return true
