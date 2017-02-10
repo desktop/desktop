6200d1c3c496af4e6bf04bb483d18e742ab11f35
Markus Olsson <j.markus.olsson@gmail.com>
2017-02-10 17:05:21 +0100

    Start using types from store

--- app/src/ui/lib/sign-in.tsx
+++ app/src/ui/lib/sign-in.tsx
@@ -4,84 +4,26 @@ import { User } from '../../models/user'
 import { assertNever, fatalError } from '../../lib/fatal-error'
 import { TwoFactorAuthentication } from '../lib/two-factor-authentication'
 import { EnterpriseServerEntry, AuthenticationMethods } from '../lib/enterprise-server-entry'
+import { SignInStep, Step } from '../../lib/dispatcher/sign-in-store'
 
 interface ISignInProps {
-  /**
-   * The endpoint against which the user is authenticating. If omitted, the
-   * component will prompt for endpoint entry before moving on to the sign in
-   * flow.
-   */
-  readonly endpoint?: string
-
-  /**
-   * The set of authentication methods supported by the endpoint. This is only
-   * used if `endpoint` is defined in the props.
-   */
-  readonly authenticationMethods?: Set<AuthenticationMethods>
-
-  /** Called after the user has signed in. */
-  readonly onDidSignIn: (user: User) => void
+  readonly currentStep: SignInStep
 
   /** An array of additional buttons to render after the "Sign In" button. */
   readonly children?: ReadonlyArray<JSX.Element>
 }
 
-enum SignInStep {
-  EndpointEntry,
-  Authentication,
-  TwoFactorAuthentication,
-}
-
-/** The default set of authentication methods. */
-export const DefaultAuthMethods = new Set([
-  AuthenticationMethods.BasicAuth,
-  AuthenticationMethods.OAuth,
-])
-
-type Step = { kind: SignInStep.EndpointEntry } |
-            { kind: SignInStep.Authentication, endpoint: string, authMethods: Set<AuthenticationMethods> } |
-            { kind: SignInStep.TwoFactorAuthentication, endpoint: string, login: string, password: string }
-
-interface ISignInState {
-  readonly step: Step
-}
-
 /** The sign in flow for GitHub. */
-export class SignIn extends React.Component<ISignInProps, ISignInState> {
-  public constructor(props: ISignInProps) {
-    super(props)
-
-    this.state = { step: this.stepForProps(props) }
-  }
-
-  public componentWillReceiveProps(nextProps: ISignInProps) {
-    if (nextProps.endpoint !== this.props.endpoint) {
-      this.setState({ step: this.stepForProps(nextProps) })
-    }
-  }
-
-  private stepForProps(props: ISignInProps): Step {
-    if (props.endpoint) {
-      return {
-        kind: SignInStep.Authentication,
-        endpoint: props.endpoint,
-        authMethods: props.authenticationMethods || DefaultAuthMethods,
-      }
-    } else {
-      return {
-        kind: SignInStep.EndpointEntry,
-      }
-    }
-  }
+export class SignIn extends React.Component<ISignInProps, void> {
 
   public render() {
-    const step = this.state.step
-    if (step.kind === SignInStep.EndpointEntry) {
+    const step = this.props.currentStep
+    if (step.kind === Step.EndpointEntry) {
       return <EnterpriseServerEntry
         onContinue={this.onContinue}
         additionalButtons={this.props.children}
       />
-    } else if (step.kind === SignInStep.Authentication) {
+    } else if (step.kind === Step.Authentication) {
       const supportsBasicAuth = step.authMethods.has(AuthenticationMethods.BasicAuth)
       return <AuthenticationForm
         endpoint={step.endpoint}
@@ -89,10 +31,10 @@ export class SignIn extends React.Component<ISignInProps, ISignInState> {
         additionalButtons={this.props.children}
         onDidSignIn={this.onDidSignIn}
         onNeeds2FA={this.onNeeds2FA}/>
-    } else if (step.kind === SignInStep.TwoFactorAuthentication) {
+    } else if (step.kind === Step.TwoFactorAuthentication) {
       return <TwoFactorAuthentication
         endpoint={step.endpoint}
-        login={step.login}
+        login={step.username}
         password={step.password}
         onDidSignIn={this.onDidSignIn}/>
     } else {
