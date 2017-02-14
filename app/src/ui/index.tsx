d11d9a1b54efd98a3b09960b2d41899e83faf900
joshaber <joshaber@gmail.com>
2017-02-13 22:52:02 -0500

    Expose Perf on the window.
    Apparently this is required for the React Perf extension to work.

--- app/src/ui/index.tsx
+++ app/src/ui/index.tsx
@@ -25,6 +25,12 @@ if (__DEV__) {
   const g: any = global
   // Expose GitPerf as a global so it can be started.
   g.GitPerf = require('./lib/git-perf')
+
+  // Expose Perf on the window so that the React Perf dev tool extension can
+  // find it.
+  const w: any = window
+  const Perf = require('react-addons-perf')
+  w.Perf = Perf
 }
 
 const startTime = Date.now()
