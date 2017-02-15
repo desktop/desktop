41d0bec5c5c5f97c41bdbd35f8be72aef7778b76
joshaber <joshaber@gmail.com>
2017-02-15 10:09:06 -0500

    Wait for presentError to complete

--- app/src/lib/dispatcher/error-handlers.ts
+++ app/src/lib/dispatcher/error-handlers.ts
@@ -2,8 +2,8 @@ import { IAppState } from '../app-state'
 import { Dispatcher } from './index'
 
 /** Handle errors by presenting them. */
-export function defaultErrorHandler(error: Error, appState: IAppState, dispatcher: Dispatcher): Promise<Error | null> {
-  dispatcher.presentError(error)
+export async function defaultErrorHandler(error: Error, appState: IAppState, dispatcher: Dispatcher): Promise<Error | null> {
+  await dispatcher.presentError(error)
 
-  return Promise.resolve(null)
+  return null
 }
