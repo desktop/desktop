86914a9ba962ff92a7b8c87d05fb1f283e2d8f75
Brendan Forster <brendan@github.com>
2017-02-17 08:52:10 +1100

    refine test

--- app/test/unit/components/rich-text-test.tsx
+++ app/test/unit/components/rich-text-test.tsx
@@ -2,8 +2,7 @@ import * as chai from 'chai'
 const expect = chai.expect
 
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import * as TestUtils from 'react-addons-test-utils'
+import { shallow } from 'enzyme'
 
 import { RichText } from '../../../src/ui/lib/rich-text'
 
@@ -13,12 +12,11 @@ describe('RichText', () => {
     const children = 'fixed based on suggestion from @shiftkey'
     const linkClicked = () => { }
 
-    const text = TestUtils.renderIntoDocument(
+    const wrapper = shallow(
       <RichText emoji={emoji} children={children} linkClicked={linkClicked} />
-    ) as React.Component<any, any>
-    const node = ReactDOM.findDOMNode(text)
-    expect(node).not.to.equal(null)
+    )
 
-    // TODO: find the user element inside the child nodes
+    const links = wrapper.find('.username')
+    expect(links.length).to.equal(1)
   })
 })
