529e3b7c5843cb243b2d52dc2ba0ca0cc098af1b
Brendan Forster <brendan@github.com>
2017-02-17 11:51:47 +1100

    test for different repository scenarios

--- app/test/unit/components/rich-text-test.tsx
+++ app/test/unit/components/rich-text-test.tsx
@@ -7,16 +7,33 @@ import { shallow } from 'enzyme'
 import { RichText } from '../../../src/ui/lib/rich-text'
 
 describe('RichText', () => {
-  it('renders a hyperlink when the mention is found', () => {
-    const emoji = new Map<string, string>()
-    const children = 'fixed based on suggestion from @shiftkey'
-    const linkClicked = () => { }
 
-    const wrapper = shallow(
-      <RichText emoji={emoji} children={children} linkClicked={linkClicked} />
-    )
+  const emoji = new Map<string, string>()
 
-    const links = wrapper.find('.username')
-    expect(links.length).to.equal(1)
+  describe('with GitHub repository', () => {
+    it('renders hyperlink when a mention is found', () => {
+      const linkClicked = () => { }
+      const children = 'fixed based on suggestion from @shiftkey'
+
+      const wrapper = shallow(
+        <RichText emoji={emoji} children={children} linkClicked={linkClicked} />
+      )
+
+      const links = wrapper.find('.username')
+      expect(links.length).to.equal(1)
+    })
+  })
+
+  describe('with non-GitHub repository', () => {
+    it('does not render hyperlinks', () => {
+      const children = 'fixed based on suggestion from @shiftkey'
+
+      const wrapper = shallow(
+        <RichText emoji={emoji} children={children} />
+      )
+
+      const links = wrapper.find('.username')
+      expect(links.length).to.equal(0)
+    })
   })
 })
