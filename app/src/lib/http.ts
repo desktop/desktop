c96ebfedfb9d795b2ae9af0c8d2ded49c8e4c340
Brendan Forster <brendan@github.com>
2017-02-20 22:05:32 +1100

    introduce generic HTTP abstractions for common operations

--- app/src/lib/http.ts
+++ app/src/lib/http.ts
@@ -1,3 +1,4 @@
+import * as URL from 'url'
 import * as appProxy from '../ui/lib/app-proxy'
 
 import { proxyRequest } from '../ui/main-process-proxy'
@@ -29,6 +30,12 @@ export interface IHTTPRequest {
 /** The HTTP methods available. */
 export type HTTPMethod = 'GET' | 'POST' | 'PUT' | 'HEAD'
 
+export interface IGitHubAPIOptions {
+  readonly params?: Object
+  readonly endpoint: string
+  readonly token: string
+}
+
 /** Resolve a given header on the HTTP response */
 export function getHeader(response: IHTTPResponse, key: string): string | null {
   if (!response.headers) {
@@ -56,6 +63,68 @@ export function getHeader(response: IHTTPResponse, key: string): string | null {
 
   return null
 }
+
+function getLinkHeaders(response: IHTTPResponse): { next?: URL.Url } {
+  const linkHeader = getHeader(response, 'link')
+  if (linkHeader) {
+    const matches = /\<([a-z0-9\=\_\&\?\/\.\:]*)\>; rel="([a-z]*)"/.exec(linkHeader)
+    if (matches) {
+      const pairs = matches.slice(1)
+      for (let i = 0; i < pairs.length; i += 2) {
+        const url = pairs[i]
+        const type = pairs[i + 1]
+        if (type === 'next') {
+          const result = URL.parse(url)
+          return { next: result }
+        }
+      }
+    }
+  }
+  return {}
+}
+
+/**
+ * Convert a JSON object to a URI-encoded querystring
+ *
+ * Note: does not handle arrays or nested objects
+ */
+function toQueryString(json: any): string {
+  // citation: http://stackoverflow.com/a/30707423/1363815
+  return '?' +
+    Object.keys(json).map(function (key) {
+      // timestamps in the GitHub API should not be URI-encoded
+      if (key === 'since') {
+        return `${key}=${json[key]}`
+      }
+      return encodeURIComponent(key) + '=' +
+        encodeURIComponent(json[key])
+    }).join('&')
+}
+
+/**
+ * Lookup the link headers on the HTTP response to see whether there are
+ * further resources to retrieve.
+ *
+ * Removes the host information, as the API client tracks this information.
+ */
+function resolveNextPath(response: IHTTPResponse): string | null {
+  const linkHeader = getLinkHeaders(response)
+  if (!linkHeader.next) {
+    return null
+  }
+
+  const nextPath = linkHeader.next.path
+  if (nextPath) {
+    if (nextPath.startsWith('/')) {
+      // URL builder will specify this, let's drop it here
+      return nextPath.substr(1)
+    }
+    return nextPath
+  }
+
+  return null
+}
+
 
 /**
  * Deserialize the HTTP response body into an expected object shape
@@ -129,7 +198,6 @@ export function getEncoding(response: IHTTPResponse): string | null {
     }
   }
 
-
   // while we expect JSON endpoints to return a charset, we might
   // have some scenarios where this is not the case
   // see http://www.iana.org/assignments/media-types/application/json
@@ -177,4 +245,60 @@ export function request(endpoint: string, authorization: string | null, method: 
   }
 
   return proxyRequest(options)
+}
+
+function isSuccess(statusCode: number | undefined): boolean {
+  return statusCode >= 200 && statusCode <= 299
+}
+
+export async function post<T>(path: string, body: Object, options: IGitHubAPIOptions): Promise<T | null> {
+  const response = await request(options.endpoint, `token ${options.token}`, 'POST', path, body)
+
+  if (isSuccess(response.statusCode)) {
+    return deserialize<T>(response.body)
+  } else {
+    return Promise.reject(`Could not complete request successfully. Status code ${response.statusCode} received.`)
+  }
+}
+
+export async function get<T>(path: string, options: IGitHubAPIOptions): Promise<T | null> {
+  let currentPath = path
+  if (options.params) {
+    currentPath = `${path}${toQueryString(options.params)}`
+  }
+
+  const response = await request(options.endpoint, `token ${options.token}`, 'GET', currentPath)
+
+  if (isSuccess(response.statusCode)) {
+    return deserialize<T>(response.body)
+  } else {
+    return Promise.reject(`Could not complete request successfully. Status code ${response.statusCode} received.`)
+  }
+}
+
+export async function getAllPages<T>(path: string, options: IGitHubAPIOptions): Promise<ReadonlyArray<T>> {
+  const allItems: Array<T> = []
+
+  const params = Object.assign({ per_page: '100' }, options.params)
+
+  let currentPath: string | null = `${path}${toQueryString(params)}`
+
+  do {
+    const response = await request(options.endpoint, `token ${options.token}`, 'GET', currentPath)
+
+    if (!isSuccess(response.statusCode)) {
+      currentPath = null
+      break
+    }
+
+    const issues = deserialize<T[]>(response.body)
+
+    if (issues) {
+      allItems.push(...issues)
+    }
+
+    currentPath = resolveNextPath(response)
+  } while (currentPath !== null)
+
+  return allItems
 }
