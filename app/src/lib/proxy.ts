b0397b8f0cd7bda2478d9eea5eb44306d200c1c2
Brendan Forster <brendan@github.com>
2017-02-20 16:23:52 +1100

    flesh out the proxy flow a bit better

--- app/src/lib/proxy.ts
+++ app/src/lib/proxy.ts
@@ -1,3 +1,4 @@
+import * as keytar from 'keytar'
 
 export type AuthInfo = {
  isProxy: boolean,
@@ -17,10 +18,29 @@ export function getProxyInfo(authInfo: AuthInfo): ProxyCredential {
   // TODO: if no cached credentials found, prompt user for credentials
   // TODO: user may bypass, which means they're gonna have a bad time
 
-  if (authInfo.realm === 'FiddlerProxy (user: 1, pass: 1)') {
-    // this is the proxy credentials that fiddler expects
-    return { username: '1', password: '1' }
+  if (authInfo.isProxy) {
+    if (authInfo.realm === 'FiddlerProxy (user: 1, pass: 1)') {
+      // this is the proxy credentials that fiddler expects
+      return { username: '1', password: '1' }
+    } else {
+      // TODO: lookup in cache for credentials for the given host (and realm?)
+      // HACK: where should i store this?
+      const username = window.localStorage.getItem('proxy-account-name')
+      if (username) {
+        const service = `Proxy Server - ${authInfo.host} - ${authInfo.realm}`
+        const password = keytar.getPassword(service, username)
+        if (password) {
+          return { username, password }
+        } else {
+          // TODO: prompt user for credentials
+        }
+      } else {
+
+      }
+    }
   }
+
+  // TODO: user may bypass, which means they're gonna have a bad time
 
   return { }
 }
