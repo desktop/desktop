94a9ed08cbdac5a3c1ed1eea6b775f7e7285dc41
Brendan Forster <brendan@github.com>
2017-02-21 15:34:38 +1100

    centralize the component creation

--- app/test/unit/components/rich-text-test.tsx
+++ app/test/unit/components/rich-text-test.tsx
@@ -7,8 +7,13 @@ import { shallow } from 'enzyme'
 import { RichText } from '../../../src/ui/lib/rich-text'
 
 describe('RichText', () => {
-
   const emoji = new Map<string, string>([ [ ':shipit:', '/some/path.png' ] ])
+
+  function createComponent(children: string, linkClicked?: () => void) {
+    return shallow(
+      <RichText emoji={emoji} children={children} linkClicked={linkClicked} />
+    )
+  }
 
   describe('with GitHub repository', () => {
 
@@ -16,55 +21,35 @@ describe('RichText', () => {
 
     it('renders emoji when matched', () => {
       const children = 'releasing the thing :shipit:'
-
-      const wrapper = shallow(
-        <RichText emoji={emoji} children={children} linkClicked={linkClicked} />
-      )
-
+      const wrapper = createComponent(children, linkClicked)
       const links = wrapper.find('.emoji')
       expect(links.length).to.equal(1)
     })
 
     it('skips emoji when no match exists', () => {
       const children = 'releasing the thing :unknown:'
-
-      const wrapper = shallow(
-        <RichText emoji={emoji} children={children} linkClicked={linkClicked} />
-      )
-
+      const wrapper = createComponent(children, linkClicked)
       const links = wrapper.find('.emoji')
       expect(links.length).to.equal(0)
     })
 
     it('does not render hyperlink when email address found', () => {
       const children = 'the email address support@github.com should be ignored'
-
-      const wrapper = shallow(
-        <RichText emoji={emoji} children={children} linkClicked={linkClicked} />
-      )
-
+      const wrapper = createComponent(children, linkClicked)
       const links = wrapper.find('.username')
       expect(links.length).to.equal(0)
     })
 
     it('renders hyperlink when a mention is found', () => {
       const children = 'fixed based on suggestion from @shiftkey'
-
-      const wrapper = shallow(
-        <RichText emoji={emoji} children={children} linkClicked={linkClicked} />
-      )
-
+      const wrapper = createComponent(children, linkClicked)
       const links = wrapper.find('.username')
       expect(links.length).to.equal(1)
     })
 
     it('renders hyperlink when an issue reference is found', () => {
       const children = 'Merge pull request #955 from desktop/computering-icons-for-all'
-
-      const wrapper = shallow(
-        <RichText emoji={emoji} children={children} linkClicked={linkClicked} />
-      )
-
+      const wrapper = createComponent(children, linkClicked)
       const links = wrapper.find('.issue')
       expect(links.length).to.equal(1)
     })
@@ -73,44 +58,28 @@ describe('RichText', () => {
   describe('with non-GitHub repository', () => {
     it('renders emoji when matched', () => {
       const children = 'releasing the thing :shipit:'
-
-      const wrapper = shallow(
-        <RichText emoji={emoji} children={children} />
-      )
-
+      const wrapper = createComponent(children)
       const links = wrapper.find('.emoji')
       expect(links.length).to.equal(1)
     })
 
     it('skips emoji when no match exists', () => {
       const children = 'releasing the thing :unknown:'
-
-      const wrapper = shallow(
-        <RichText emoji={emoji} children={children} />
-      )
-
+      const wrapper = createComponent(children)
       const links = wrapper.find('.emoji')
       expect(links.length).to.equal(0)
     })
 
     it('does not render hyperlink for mention', () => {
       const children = 'fixed based on suggestion from @shiftkey'
-
-      const wrapper = shallow(
-        <RichText emoji={emoji} children={children} />
-      )
-
+      const wrapper = createComponent(children)
       const links = wrapper.find('.username')
       expect(links.length).to.equal(0)
     })
 
     it('does not render hyperlink for issue reference', () => {
       const children = 'Merge pull request #955 from desktop/computering-icons-for-all'
-
-      const wrapper = shallow(
-        <RichText emoji={emoji} children={children} />
-      )
-
+      const wrapper = createComponent(children)
       const links = wrapper.find('.issue')
       expect(links.length).to.equal(0)
     })
