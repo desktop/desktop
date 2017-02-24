25ad2cc0b7b019611a29f1c73ab6323b15ddf448
Markus Olsson <j.markus.olsson@gmail.com>
2017-02-24 14:04:43 +0100

    Add an optional label link prop

--- app/src/ui/lib/text-box.tsx
+++ app/src/ui/lib/text-box.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as classNames from 'classnames'
 import { v4 as guid } from 'uuid'
+import { LinkButton } from './link-button'
 
 interface ITextBoxProps {
   /** The label for the input field. */
@@ -47,6 +48,10 @@ interface ITextBoxProps {
 
   /** A callback to receive the underlying `input` instance. */
   readonly onInputRef?: (instance: HTMLInputElement) => void
+
+  readonly labelLinkText?: string
+  readonly labelLinkUri?: string
+  readonly onLabelLinkClick?: () => void
 }
 
 interface ITextBoxState {
@@ -70,18 +75,42 @@ export class TextBox extends React.Component<ITextBoxProps, ITextBoxState> {
     }
   }
 
+  private renderLabelLink() {
+    if (!this.props.labelLinkText) {
+      return null
+    }
+
+    return (
+      <LinkButton
+        uri={this.props.labelLinkUri}
+        onClick={this.props.onLabelLinkClick}
+      >
+          {this.props.labelLinkText}
+      </LinkButton>
+    )
+  }
+
+  private renderLabel() {
+    if (!this.props.label) {
+      return null
+    }
+
+    return (
+      <div className='label'>
+        <label htmlFor={this.state.inputId}>{this.props.label}</label>
+        {this.renderLabelLink()}
+      </div>
+    )
+  }
+
   public render() {
     const className = classNames('text-box-component', this.props.className)
 
     const inputId = this.props.label ? this.state.inputId : undefined
 
-    const label = this.props.label
-      ? <label htmlFor={inputId}>{this.props.label}</label>
-      : null
-
     return (
       <div className={className}>
-        {label}
+        {this.renderLabel()}
 
         <input
           id={inputId}
