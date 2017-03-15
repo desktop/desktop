3bd11917f928ffb6b99d7f696cbc1779513bb52c
iAmWillShepherd <williamrshepherd@gmail.com>
2017-03-14 23:51:08 -0500

    Remove unused error handler

--- app/src/lib/dispatcher/error-handlers.ts
+++ app/src/lib/dispatcher/error-handlers.ts
@@ -1,27 +1,8 @@
 import { Dispatcher } from './index'
-import { GitError } from '../git/core'
-import { GitError as GitErrorType } from 'git-kitchen-sink'
-import { PopupType  as popupType } from '../app-state'
 
 /** Handle errors by presenting them. */
 export async function defaultErrorHandler(error: Error, dispatcher: Dispatcher): Promise<Error | null> {
   await dispatcher.presentError(error)
 
   return null
-}
-
-//** Handle errors by giving user actions to complete. */
-export async function performGitActionErrorHandler(error: Error, dispatcher: Dispatcher): Promise<Error | null> {
-  if (error instanceof GitError) {
-    switch (error.result.gitError)  {
-      case GitErrorType.HTTPSAuthenticationFailed: {
-        await dispatcher.showPopup({ type: popupType.Preferences })
-        break
-      }
-    }
-
-    return null
-  }
-
-  return error
 }
