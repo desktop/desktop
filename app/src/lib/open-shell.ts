7e44a2acbbda918cf1717c836b99d901d751784b
Brendan Forster <brendan@github.com>
2017-03-16 11:25:09 +1030

    macOS needs to always show a new Terminal

--- app/src/lib/open-shell.ts
+++ app/src/lib/open-shell.ts
@@ -3,26 +3,22 @@ import { fatalError } from './fatal-error'
 
 /** Opens a shell setting the working directory to fullpath. If a shell is not specified, OS defaults are used. */
 export function openShell(fullPath: string, shell?: string) {
-  let commandName: string = ''
-  let commandArgs: ReadonlyArray<string>
-
   if ( __DARWIN__) {
-    commandName = 'open'
-    commandArgs = [ '-a', shell || 'Terminal' ]
-  } else if (__WIN32__) {
-    commandName = 'START'
-    if (shell) {
-      // not sure what other sorts of arguments we expect here
-      // so for now let's just try and launch this other shell
-      commandArgs = [ shell ]
-    } else {
-      // '/K' to run the subseqent command and keep the prompt visible
-      // 'TITLE {value}' sets the Command Prompt title to {value}
-      commandArgs = [ 'cmd', '/K', 'TITLE GitHub Desktop' ]
-    }
-  } else {
-    return fatalError('Unsupported OS')
+    // fullPath argument ensures a new terminal is always shown
+    const commandArgs = [ '-a', shell || 'Terminal', fullPath ]
+    return spawn('open', commandArgs, { 'shell': true })
   }
 
-  return spawn(commandName, Array.from(commandArgs), { 'shell': true, cwd: fullPath })
+  if (__WIN32__) {
+    const commandArgs = shell
+      // not sure what other sorts of arguments we expect here
+      // so for now let's just try and launch this other shell
+      ? [ shell ]
+      // '/K' to run the subseqent command and keep the prompt visible
+      // 'TITLE {value}' sets the Command Prompt title to {value}
+      : [ 'cmd', '/K', 'TITLE GitHub Desktop' ]
+    return spawn('START', commandArgs, { 'shell': true, cwd: fullPath })
+  }
+
+  return fatalError('Unsupported OS')
 }
