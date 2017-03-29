75f256bd20ec2917baeb00bc33fd4b74a6eb3004
Brendan Forster <brendan@github.com>
2017-03-30 08:11:59 +1100

    document the methods

--- app/src/lib/welcome.ts
+++ app/src/lib/welcome.ts
@@ -1,11 +1,17 @@
 /** The `localStorage` key for whether we've shown the Welcome flow yet. */
 const HasShownWelcomeFlowKey = 'has-shown-welcome-flow'
 
+/**
+ * Check if the current user has completed the welcome flow.
+ */
 export function hasShownWelcomeFlow(): boolean {
   const hasShownWelcomeFlow = localStorage.getItem(HasShownWelcomeFlowKey)
   return !hasShownWelcomeFlow || !parseInt(hasShownWelcomeFlow, 10)
 }
 
+/**
+ * Update local storage to indicate the welcome flow has been completed.
+ */
 export function markWelcomeFlowComplete() {
   localStorage.setItem(HasShownWelcomeFlowKey, '1')
 }
