9d427783f001dd33199b5310fe40eece14af89b1
Markus Olsson <j.markus.olsson@gmail.com>
2017-03-30 12:14:21 +0200

    Add a link to the release notes

--- app/src/ui/about/about.tsx
+++ app/src/ui/about/about.tsx
@@ -5,16 +5,39 @@ import { Button } from '../lib/button'
 import { ButtonGroup } from '../lib/button-group'
 import { Dialog, DialogContent, DialogFooter } from '../dialog'
 import { Octicon, OcticonSymbol } from '../octicons'
+import { LinkButton} from '../lib/link-button'
 
 interface IAboutProps {
   readonly onDismissed: () => void
   readonly version: string
 }
 
+const releaseNotesUri = 'https://desktop.github.com/release-notes/tng/'
+
 /** The Create Branch component. */
 export class About extends React.Component<IAboutProps, void> {
 
+  private closeButton: Button | null = null
+
+  private onCloseButtonRef = (button: Button | null) => {
+    this.closeButton = button
+  }
+
+  public componentDidMount() {
+    // A modal dialog autofocuses the first element that can receive
+    // focus (and our dialog even uses the autofocus attribute on its
+    // fieldset). In our case that's the release notes link button and
+    // we don't want that to have focus so we'll move it over to the
+    // close button instead.
+    if (this.closeButton) {
+      this.closeButton.focus()
+    }
+  }
+
   public render() {
+
+    const version = this.props.version
+    const releaseNotesLink = <LinkButton uri={releaseNotesUri}>release notes</LinkButton>
 
     return (
       <Dialog
@@ -26,12 +49,14 @@ export class About extends React.Component<IAboutProps, void> {
             <Octicon symbol={OcticonSymbol.markGithub} />
           </Row>
           <h2>GitHub Desktop</h2>
-          <p>Installed version {this.props.version}</p>
+          <p>
+            Version {version} ({releaseNotesLink})
+          </p>
         </DialogContent>
 
         <DialogFooter>
           <ButtonGroup>
-            <Button type='submit'>Close</Button>
+            <Button type='submit' ref={this.onCloseButtonRef}>Close</Button>
           </ButtonGroup>
         </DialogFooter>
       </Dialog>
