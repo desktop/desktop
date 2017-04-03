fc1c2c785232376ceef74127c3cdda3620f6e529
Brendan Forster <brendan@github.com>
2017-04-03 11:54:08 +1000

    drop the check-ref-format guard

--- app/src/lib/git/checkout.ts
+++ app/src/lib/git/checkout.ts
@@ -3,16 +3,6 @@ import { Repository } from '../../models/repository'
 
 /** Check out the given branch. */
 export async function checkoutBranch(repository: Repository, name: string): Promise<void> {
-  const options = {
-    successExitCodes: new Set([ 0, 1 ]),
-  }
-
-  const checkRef = await git([ 'check-ref-format', '--normalize', `refs/heads/${name}` ], repository.path, 'checkoutBranch', options)
-
-  if (checkRef.exitCode === 1) {
-    throw new Error(`The provided name '${name}' is not a valid ref.`)
-  }
-
   await git([ 'checkout', name, '--' ], repository.path, 'checkoutBranch')
 }
 
