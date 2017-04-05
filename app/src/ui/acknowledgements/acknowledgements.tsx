8d5ea40b434a3d47c5b43274b9df514e2ba76395
joshaber <joshaber@gmail.com>
2017-04-05 16:46:41 -0400

    Flesh out the Acknowledgements component

---
+++ app/src/ui/acknowledgements/acknowledgements.tsx
@@ -0,0 +1,65 @@
+import * as Path from 'path'
+import * as Fs from 'fs'
+import * as React from 'react'
+import { getAppPath } from '../lib/app-proxy'
+import { Loading } from '../lib/loading'
+import { Button } from '../lib/button'
+import { ButtonGroup } from '../lib/button-group'
+import { Dialog, DialogContent, DialogFooter } from '../dialog'
+
+interface IAcknowledgementsProps {
+  readonly onDismissed: () => void
+}
+
+interface ILicense {
+  readonly repository: string
+  readonly licenseName: string
+}
+
+interface IAcknowledgementsState {
+  readonly licenses: Map<string, ILicense> | null
+}
+
+/** The component which displays the licenses for packages used in the app. */
+export class Acknowledgements extends React.Component<IAcknowledgementsProps, IAcknowledgementsState> {
+  public constructor(props: IAcknowledgementsProps) {
+    super(props)
+
+    this.state = { licenses: null }
+  }
+
+  public componentDidMount() {
+    const path = Path.join(getAppPath(), 'static', 'licenses.json')
+    Fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        return
+      }
+
+      const parsed = JSON.parse(data)
+      if (!parsed) {
+        return
+      }
+
+      this.setState({ licenses: parsed })
+    })
+  }
+
+  public render() {
+    const licenses = this.state.licenses
+    return (
+      <Dialog
+        onSubmit={this.props.onDismissed}
+        onDismissed={this.props.onDismissed}>
+        <DialogContent>
+          {licenses ? licenses.size : <Loading/>}
+        </DialogContent>
+
+        <DialogFooter>
+          <ButtonGroup>
+            <Button type='submit'>Close</Button>
+          </ButtonGroup>
+        </DialogFooter>
+      </Dialog>
+    )
+  }
+}
