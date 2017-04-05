f4bd2d2ce6a01ce8187d098daf217f53f580be96
Brendan Forster <brendan@github.com>
2017-04-05 15:16:22 +1000

    document the fields

--- app/src/models/email.ts
+++ app/src/models/email.ts
@@ -1,8 +1,22 @@
 /** The data-only interface for Email for transport across IPC. */
 export interface IEmail {
+
   readonly email: string
+  /**
+   * Represents whether GitHub has confirmed the user has access to this
+   * email address. New users require a verified email address before
+   * they can sign into GitHub Desktop.
+   */
   readonly verified: boolean
+  /**
+   * Flag for the user's preferred email address. Other email addresses
+   * are provided for associating commit authors with the one GitHub account.
+   */
   readonly primary: boolean
+  /**
+   * Defines the privacy settings for an email address provided by the user.
+   * If 'private' is found, we should not use this email address anywhere.
+   */
   readonly visibility: 'public' | 'private'
 }
 
@@ -14,7 +28,6 @@ export class Email implements IEmail {
   public readonly verified: boolean
   public readonly primary: boolean
   public readonly visibility: 'public' | 'private'
-
 
   /** Create a new Email from some JSON. */
   public static fromJSON(obj: IEmail): Email {
