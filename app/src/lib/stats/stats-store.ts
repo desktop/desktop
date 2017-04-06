5686b527171a41275aaddc6bb8d3e4548b71a3b2
joshaber <joshaber@gmail.com>
2017-04-06 08:59:57 -0400

    Only parse the UA on Mac

--- app/src/lib/stats/stats-store.ts
+++ app/src/lib/stats/stats-store.ts
@@ -1,3 +1,4 @@
+import * as OS from 'os'
 import { UAParser } from 'ua-parser-js'
 import { StatsDatabase, ILaunchStats, IDailyMeasures } from './stats-database'
 import { getVersion } from '../../ui/lib/app-proxy'
@@ -106,16 +107,27 @@ export class StatsStore {
     const launchStats = await this.getAverageLaunchStats()
     const dailyMeasures = await this.getDailyMeasures()
 
-    const parser = new UAParser()
-    const os = parser.getOS()
-    const osVersion = `${os.name} ${os.version}`
-
     return {
       version: getVersion(),
-      osVersion,
+      osVersion: this.getOS(),
       platform: process.platform,
       ...launchStats,
       ...dailyMeasures,
+    }
+  }
+
+  private getOS() {
+    if (__DARWIN__) {
+      // On macOS, OS.release() gives us the kernel version which isn't terribly
+      // meaningful to any human being, so we'll parse the User Agent instead.
+      // See https://github.com/desktop/desktop/issues/1130.
+      const parser = new UAParser()
+      const os = parser.getOS()
+      return `${os.name} ${os.version}`
+    } else if (__WIN32__) {
+      return `Windows ${OS.release()}`
+    } else {
+      return `${OS.type()} ${OS.release()}`
     }
   }
 
