f6c50231e9dc23d680c92a5e1aab05d477d5ff79
Brendan Forster <brendan@github.com>
2017-04-07 13:52:31 +1000

    actually this can all :fire:

--- app/src/models/user.ts
+++ app/src/models/user.ts
@@ -1,27 +1,15 @@
-/** The data-only interface for User for transport across IPC. */
-export interface IUser {
-  readonly login: string
-  readonly avatarURL: string
-  readonly name: string
-}
-
 /**
  * A GitHub user, representing an entity that has interacted with a repository used by GitHub Desktop.
  *
  * These are read-only entities, and are typically associated with commits or mentions.
  */
-export class User implements IUser {
+export class User {
   /** The account name associated with the user */
   public readonly login: string
   /** The profile URL to render for this user */
   public readonly avatarURL: string
   /** The friendly name associated with the user */
   public readonly name: string
-
-  /** Create a new User from some JSON. */
-  public static fromJSON(obj: IUser): User {
-    return new User(obj.login, obj.avatarURL, obj.name)
-  }
 
   public constructor(login: string, avatarURL: string, name: string) {
     this.login = login
