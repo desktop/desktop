766955a6d678433d1a71d9b93b42077ab3d5bd65
Brendan Forster <brendan@github.com>
2017-04-07 13:30:57 +1000

    we no longer need to track this array of email addresses

--- app/src/models/user.ts
+++ app/src/models/user.ts
@@ -2,7 +2,6 @@
 export interface IUser {
   readonly login: string
   readonly endpoint: string
-  readonly emails: ReadonlyArray<string>
   readonly avatarURL: string
   readonly id: number
   readonly name: string
@@ -16,20 +15,18 @@ export interface IUser {
 export class User implements IUser {
   public readonly login: string
   public readonly endpoint: string
-  public readonly emails: ReadonlyArray<string>
   public readonly avatarURL: string
   public readonly id: number
   public readonly name: string
 
   /** Create a new User from some JSON. */
   public static fromJSON(obj: IUser): User {
-    return new User(obj.login, obj.endpoint, obj.emails, obj.avatarURL, obj.id, obj.name)
+    return new User(obj.login, obj.endpoint, obj.avatarURL, obj.id, obj.name)
   }
 
-  public constructor(login: string, endpoint: string, emails: ReadonlyArray<string>, avatarURL: string, id: number, name: string) {
+  public constructor(login: string, endpoint: string, avatarURL: string, id: number, name: string) {
     this.login = login
     this.endpoint = endpoint
-    this.emails = emails
     this.avatarURL = avatarURL
     this.id = id
     this.name = name
