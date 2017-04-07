0b13bbbba5bccb31f711478109e7abbec3661c6f
joshaber <joshaber@gmail.com>
2017-04-07 11:42:04 -0400

    Link to help

--- app/src/ui/repository-settings/no-remote.tsx
+++ app/src/ui/repository-settings/no-remote.tsx
@@ -2,6 +2,9 @@ import * as React from 'react'
 import { DialogContent } from '../dialog'
 import { Button } from '../lib/button'
 import { Row } from '../lib/row'
+import { LinkButton } from '../lib/link-button'
+
+const HelpURL = 'https://help.github.com/articles/about-remote-repositories/'
 
 interface INoRemoteProps {
   /** The function to call when the users chooses to publish. */
@@ -14,7 +17,7 @@ export class NoRemote extends React.Component<INoRemoteProps, void> {
     return (
       <DialogContent>
         <Row className='no-remote'>
-          <div>Publish your repository to GitHub.</div>
+          <div>Publish your repository to GitHub. Need help? <LinkButton uri={HelpURL}>Learn more</LinkButton> about remote repositories.</div>
           <Button type='submit' onClick={this.onPublish}>Publish</Button>
         </Row>
       </DialogContent>
