f7f09115bd49172f9869e2910eff055b544b63aa
iAmWillShepherd <williamrshepherd@gmail.com>
2017-04-10 14:07:15 -0500

    Make label text make sense

--- app/src/ui/preferences/advanced.tsx
+++ app/src/ui/preferences/advanced.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Dispatcher } from '../../lib/dispatcher'
 import { DialogContent } from '../dialog'
 import { Checkbox, CheckboxValue } from '../lib/checkbox'
+import { LinkButton } from '../lib/link-button'
 
 interface IAdvancedPreferencesProps {
   readonly dispatcher: Dispatcher
@@ -10,6 +11,8 @@ interface IAdvancedPreferencesProps {
 interface IAdvancedPreferencesState {
   readonly reportingOptOut: boolean,
 }
+
+const SamplesURL = 'https://desktop.github.com/samples/'
 
 export class Advanced extends React.Component<IAdvancedPreferencesProps, IAdvancedPreferencesState> {
   public constructor(props: IAdvancedPreferencesProps) {
@@ -41,8 +44,12 @@ export class Advanced extends React.Component<IAdvancedPreferencesProps, IAdvanc
   public render() {
     return (
       <DialogContent>
+        <div>
+          Would you like to help us improve GitHub Desktop by periodically submitting <LinkButton uri={SamplesURL}>anonymous usage data</LinkButton>?
+        </div>
+
         <Checkbox
-          label='Opt-out of usage reporting'
+          label='Yes, submit anonymized usage data'
           value={this.state.reportingOptOut ? CheckboxValue.Off : CheckboxValue.On}
           onChange={this.onChange} />
       </DialogContent>
