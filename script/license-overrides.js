db7e93158b1b5d5e9243d8f76519965c6caf9df4
joshaber <joshaber@gmail.com>
2017-04-10 10:26:28 -0400

    Add the license overrides

---
+++ script/license-overrides.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'cycle@1.0.3': {
+    repository: 'git+ssh://git@github.com/dscape/cycle',
+    license: 'Public Domain',
+    source: 'cycle.js header comment at https://github.com/dscape/cycle/blob/ab67ce90e8fa2efdb8f21074661366ec56f6a724/cycle.js',
+    sourceText: 'Public Domain.\n\nNO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.'
+  },
+
+  'json-schema@0.2.3': {
+    repositor: 'git+ssh://git@github.com/kriszyp/json-schema',
+    license: 'BSD',
+    source: 'README.md at https://github.com/kriszyp/json-schema/tree/24c4ed1b2359ab457a00e90606a777c2962ecd3b',
+    sourceText: 'Code is licensed under the AFL or BSD 3-Clause license as part of the Persevere project which is administered under the Dojo foundation, and all contributions require a Dojo CLA.'
+  }
+}
