94aa715e3667047149f3c9627e8d0fc7227f3ad5
joshaber <joshaber@gmail.com>
2017-04-10 16:00:33 -0400

    First pass at a call to action component

---
+++ app/src/ui/lib/call-to-action.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import { Row } from './row'
+import { Button } from './button'
+
+interface ICallToActionProps {
+  /**
+   * The call-to-action message. If not provided, the component's children are
+   * used instead.
+   */
+  readonly message?: string
+
+  /** The action title. */
+  readonly actionTitle: string
+
+  /** The function to call when the user clicks the action button. */
+  readonly onAction: () => void
+}
+
+/** A call-to-action component which displays a message and a button. */
+export class CallToAction extends React.Component<ICallToActionProps, void> {
+  public render() {
+    return (
+      <Row className='call-to-action'>
+        {this.props.message || this.props.children}
+        <Button className='action-button' type='submit' onClick={this.onClick}>{this.props.actionTitle}</Button>
+      </Row>
+    )
+  }
+
+  private onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+
+    this.props.onAction()
+  }
+}
