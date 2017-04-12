87b060da1716ef9f6647ce1b70dc27cf7761f18a
Brendan Forster <brendan@github.com>
2017-04-13 09:20:54 +1000

    added failing test

--- app/test/unit/git-store-test.ts
+++ app/test/unit/git-store-test.ts
@@ -57,4 +57,35 @@ describe('GitStore', () => {
     expect(files[0].path).to.equal('README.md')
     expect(files[0].status).to.equal(FileStatus.Modified)
   })
+
+  it('can discard a renamed file', async () => {
+
+    const repo = await setupEmptyRepository()
+    const gitStore = new GitStore(repo, shell)
+
+    const file = 'README.md'
+    const renamedFile = 'NEW-README.md'
+    const filePath = Path.join(repo.path, file)
+
+    Fs.writeFileSync(filePath, 'SOME WORDS GO HERE\n')
+
+    // commit the file
+    await GitProcess.exec([ 'add', file ], repo.path)
+    await GitProcess.exec([ 'commit', '-m', 'added file' ], repo.path)
+
+    Fs.writeFileSync(filePath, 'WRITING SOME NEW WORDS\n')
+
+    await GitProcess.exec([ 'mv', file, renamedFile ], repo.path)
+
+    const statusBeforeDiscard = await getStatus(repo)
+    const filesToDiscard = statusBeforeDiscard.workingDirectory.files
+
+    // discard the renamed file
+    await gitStore.discardChanges(filesToDiscard)
+
+    const status = await getStatus(repo)
+    const files = status.workingDirectory.files
+
+    expect(files.length).to.equal(0)
+  })
 })
