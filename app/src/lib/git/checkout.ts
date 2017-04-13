8db41bf33ac5c2713cd148eeaa68d5b1123c11b9
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-13 17:48:58 +0200

    Allow passing along a progress callback to checkout branch

--- app/src/lib/git/checkout.ts
+++ app/src/lib/git/checkout.ts
@@ -1,9 +1,32 @@
 import { git } from './core'
 import { Repository } from '../../models/repository'
+import { ChildProcess } from 'child_process'
+
+const byline = require('byline')
+
+type ProcessCallback = (process: ChildProcess) => void
+export type ProgressCallback = (line: string) => void
 
 /** Check out the given branch. */
-export async function checkoutBranch(repository: Repository, name: string): Promise<void> {
-  await git([ 'checkout', name, '--' ], repository.path, 'checkoutBranch')
+export async function checkoutBranch(repository: Repository, name: string, progressCallback?: ProgressCallback): Promise<void> {
+
+  let processCallback: ProcessCallback | undefined = undefined
+
+  if (progressCallback) {
+    processCallback = (process: ChildProcess) => {
+      byline(process.stderr).on('data', (chunk: string) => {
+        progressCallback(chunk)
+      })
+    }
+  }
+
+  const args = processCallback
+    ? [ 'checkout', '--progress', name, '--' ]
+    : [ 'checkout', name, '--' ]
+
+  await git(args, repository.path, 'checkoutBranch', {
+    processCallback,
+  })
 }
 
 /** Check out the paths at HEAD. */
