ec30bde46a3615da8f578786a2b37872c7c988c4
joshaber <joshaber@gmail.com>
2017-04-14 11:44:03 -0400

    Check if we can access the repository before returning the account

--- app/src/lib/find-account.ts
+++ app/src/lib/find-account.ts
@@ -70,14 +70,23 @@ export async function findAccountForRemote(url: string, accounts: ReadonlyArray<
     // air because we truly don't care.
     const parsedURL = parseRemote(url)
     if (parsedURL) {
-      const matchingAccount = allAccounts.find(a => {
+      const account = allAccounts.find(a => {
         const htmlURL = getHTMLURL(a.endpoint)
         const parsedEndpoint = URL.parse(htmlURL)
         return parsedURL.hostname === parsedEndpoint.hostname
       }) || null
 
-      if (matchingAccount) {
-        return matchingAccount
+      if (account) {
+        const owner = parsedURL.owner
+        const name = parsedURL.name
+        if (owner && name) {
+          const canAccess = await canAccessRepository(account, owner, name)
+          if (canAccess) {
+            return account
+          }
+        } else {
+          return account
+        }
       }
     }
 
