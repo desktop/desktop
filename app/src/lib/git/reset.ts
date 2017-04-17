e4d8768878a61ec6f02c3d77501a8f414b25ec9d
joshaber <joshaber@gmail.com>
2017-04-17 14:02:26 -0400

    Add unstage

--- app/src/lib/git/reset.ts
+++ app/src/lib/git/reset.ts
@@ -1,6 +1,7 @@
 import { git } from './core'
 import { Repository } from '../../models/repository'
 import { assertNever } from '../fatal-error'
+import { WorkingDirectoryFileChange } from '../../models/status'
 
 /** The reset modes which are supported. */
 export const enum GitResetMode {
@@ -22,5 +23,12 @@ function resetModeToFlag(mode: GitResetMode): string {
 export async function reset(repository: Repository, mode: GitResetMode, ref: string): Promise<true> {
   const modeFlag = resetModeToFlag(mode)
   await git([ 'reset', modeFlag, ref, '--' ], repository.path, 'reset')
+  return true
+}
+
+/** Unstage the given files. */
+export async function unstage(repository: Repository, files: ReadonlyArray<WorkingDirectoryFileChange>): Promise<true> {
+  const paths = files.map(f => f.path)
+  await git([ 'reset', '--', ...paths ], repository.path, 'unstage')
   return true
 }
