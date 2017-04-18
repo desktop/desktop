ee0c17ba48dafc2d6abd2ffd512ff02019cda215
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-18 19:46:21 +0200

    :book: ICheckoutProgress

--- app/src/lib/app-state.ts
+++ app/src/lib/app-state.ts
@@ -176,9 +176,23 @@ export enum RepositorySection {
   History
 }
 
+/** 
+ * An object describing the progression of a branch checkout operation
+ */
 export interface ICheckoutProgress {
+  /** The branch that's currently being checked out */
   readonly targetBranch: string
+
+  /** 
+   * The overall progress of the operation, represented as a fraction between
+   * 0 and 1.
+   */
   readonly progressValue: number
+
+  /**
+   * An informative text for user consumption indicating the current operation
+   * state.
+   */
   readonly progressText: string
 }
 
