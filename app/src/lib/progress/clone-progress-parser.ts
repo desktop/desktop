05952a59e910e143d5e2536086cbeec94e66e836
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-19 22:59:07 +0200

    Use StepProgressParser inside CloneProgressParser

--- app/src/lib/progress/clone-progress-parser.ts
+++ app/src/lib/progress/clone-progress-parser.ts
@@ -1,11 +1,13 @@
+import { StepProgressParser } from './step-progress'
+
 /* Highly approximate (some would say outright innacurate) division
  * of the individual progress reporting steps in a clone operation
  */
-const costsByStep = [
-  { expr: /remote: Compressing objects:\s+(\d+)%/, cost: 0.1 },
-  { expr: /Receiving objects:\s+(\d+)%/, cost: 0.7 },
-  { expr: /Resolving deltas:\s+(\d+)%/, cost: 0.1 },
-  { expr: /Checking out files:\s+(\d+)%/, cost: 0.09 },
+const steps = [
+  { title: 'remote: Compressing objects', weight: 0.1 },
+  { title: 'Receiving objects', weight: 0.7 },
+  { title: 'Resolving deltas', weight: 0.1 },
+  { title: 'Checking out files', weight: 0.09 },
 ]
 
 /**
@@ -14,18 +16,12 @@ const costsByStep = [
  * of the clone.
  */
 export class CloneProgressParser {
-  /* The steps listed in costsByStep always occur in order but some
-   * might not happen at all (like remote compression of objects) so
-   * we keep track of the "highest" seen step so that we can fill in
-   * progress with the assumption that we've already seen the previous
-   * steps. Null means that we haven't seen anything matching our
-   * regular expressions yet.
-   */
-  private highestSeenStep: number | null = null
 
-  /* The last progress value we returned from parse. Null means
-   * we haven't parsed a line we understand yet, ie indeterminate */
-  private lastProgress: number | null = null
+  private readonly parser: StepProgressParser
+
+  public constructor() {
+    this.parser = new StepProgressParser(steps)
+  }
 
   /**
    * Parses a single line of output from 'git clone --progress'.
@@ -34,28 +30,7 @@ export class CloneProgressParser {
    * indeterminate.
    */
   public parse(line: string): number | null {
-    /* Iterate over the steps we haven't seen yet and try to find
-     * one that matches. */
-    for (let i = this.highestSeenStep || 0; i < costsByStep.length; i++) {
-      const step = costsByStep[i]
-      const match = step.expr.exec(line)
-
-      if (match != null) {
-        this.highestSeenStep = i
-        let progressValue = (parseInt(match[1], 10) / 100) * step.cost
-
-        /* Sum up the full progress of previous steps whether we've
-         * actually seen them or not */
-        for (let i = 0; i < this.highestSeenStep; i++) {
-          progressValue += costsByStep[i].cost
-        }
-
-        this.lastProgress = progressValue
-        return progressValue
-      }
-    }
-
-    /* No match, return whatever we've got stored */
-    return this.lastProgress
+    const progress = this.parser.parse(line)
+    return progress ? progress.percent : null
   }
 }
