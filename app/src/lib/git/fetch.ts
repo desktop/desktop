9da2b74c601fb5f13729703ca9a3b030ddfe3fde
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-20 00:34:53 +0200

    Expose progress from fetch

--- app/src/lib/git/fetch.ts
+++ app/src/lib/git/fetch.ts
@@ -1,15 +1,33 @@
-import { git, envForAuthentication } from './core'
+import { git, envForAuthentication, IGitExecutionOptions } from './core'
 import { Repository } from '../../models/repository'
 import { Account } from '../../models/account'
+import { ChildProcess } from 'child_process'
+
+const byline = require('byline')
 
 /** Fetch from the given remote. */
-export async function fetch(repository: Repository, account: Account | null, remote: string): Promise<void> {
-  const options = {
+export async function fetch(repository: Repository, account: Account | null, remote: string, progressCallback?: (line: string) => void): Promise<void> {
+  let options: IGitExecutionOptions = {
     successExitCodes: new Set([ 0 ]),
     env: envForAuthentication(account),
   }
 
-  await git([ 'fetch', '--prune', remote ], repository.path, 'fetch', options)
+  if (progressCallback) {
+    options = {
+      ...options,
+      processCallback: (process: ChildProcess) => {
+        byline(process.stderr).on('data', (chunk: string) => {
+          progressCallback(chunk)
+        })
+      },
+    }
+  }
+
+  const args = progressCallback
+    ? [ 'fetch', '--progress', '--prune', remote ]
+    : [ 'fetch', '--prune', remote ]
+
+  await git(args, repository.path, 'fetch', options)
 }
 
 /** Fetch a given refspec from the given remote. */
