bce42496361375240cf43e67f6ae337e920a1ed0
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-20 00:11:42 +0200

    Expose progress callback for push

--- app/src/lib/git/push.ts
+++ app/src/lib/git/push.ts
@@ -1,17 +1,33 @@
-import { git, envForAuthentication, expectedAuthenticationErrors } from './core'
+import { git, envForAuthentication, expectedAuthenticationErrors, IGitExecutionOptions } from './core'
 import { Repository } from '../../models/repository'
 import { Account } from '../../models/account'
+import { ChildProcess } from 'child_process'
+
+const byline = require('byline')
 
 /** Push from the remote to the branch, optionally setting the upstream. */
-export async function push(repository: Repository, account: Account | null, remote: string, branch: string, setUpstream: boolean): Promise<void> {
+export async function push(repository: Repository, account: Account | null, remote: string, branch: string, setUpstream: boolean, progressCallback?: (line: string) => void): Promise<void> {
   const args = [ 'push', remote, branch ]
   if (setUpstream) {
     args.push('--set-upstream')
   }
 
-  const options = {
+  let options: IGitExecutionOptions = {
     env: envForAuthentication(account),
     expectedErrors: expectedAuthenticationErrors(),
+  }
+
+  if (progressCallback) {
+    args.push('--progress')
+
+    options = {
+      ...options,
+      processCallback: (process: ChildProcess) => {
+        byline(process.stderr).on('data', (chunk: string) => {
+          progressCallback(chunk)
+        })
+      },
+    }
   }
 
   const result = await git(args, repository.path, 'push', options)
