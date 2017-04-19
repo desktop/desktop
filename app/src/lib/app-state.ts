9d3eec687fa8494259cd91f17f2e1d20e821113d
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-19 10:55:29 +0200

    Extract checkout progress interface

--- app/src/lib/app-state.ts
+++ app/src/lib/app-state.ts
@@ -12,6 +12,7 @@ import { ICommitMessage } from './dispatcher/git-store'
 import { IMenu } from '../models/app-menu'
 import { IRemote } from '../models/remote'
 import { WindowState } from './window-state'
+import { ICheckoutProgress } from './progress'
 
 export { ICloningRepositoryState }
 export { ICommitMessage }
@@ -176,26 +177,6 @@ export type Foldout =
 export enum RepositorySection {
   Changes,
   History
-}
-
-/** 
- * An object describing the progression of a branch checkout operation
- */
-export interface ICheckoutProgress {
-  /** The branch that's currently being checked out */
-  readonly targetBranch: string
-
-  /** 
-   * The overall progress of the operation, represented as a fraction between
-   * 0 and 1.
-   */
-  readonly progressValue: number
-
-  /**
-   * An informative text for user consumption indicating the current operation
-   * state.
-   */
-  readonly progressText: string
 }
 
 export interface IRepositoryState {
