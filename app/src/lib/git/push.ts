db05b15123b2bd9ffd324d0bf5a70c5220b79567
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-20 00:13:36 +0200

    :book:

--- app/src/lib/git/push.ts
+++ app/src/lib/git/push.ts
@@ -5,7 +5,26 @@ import { ChildProcess } from 'child_process'
 
 const byline = require('byline')
 
-/** Push from the remote to the branch, optionally setting the upstream. */
+/**
+ * Push from the remote to the branch, optionally setting the upstream.
+ * 
+ * @param repository - The repository in which the branch checkout should
+ *                     take place
+ * 
+ * @param account - The account to use when authenticating with the remote
+ *
+ * @param remote - The remote to push the specified branch to
+ *
+ * @param branch - The branch to push
+ *
+ * @param setUpstream - Whether or not to update the tracking information
+ *                      of the specified branch to point to the remote.
+ * 
+ * @param progressCallback - An optional function which will be invoked
+ *                           once per each line of output from Git. When
+ *                           provided this also enables the '--progress'
+ *                           command line flag for 'git push'.
+ */
 export async function push(repository: Repository, account: Account | null, remote: string, branch: string, setUpstream: boolean, progressCallback?: (line: string) => void): Promise<void> {
   const args = [ 'push', remote, branch ]
   if (setUpstream) {
