cdc06ec8c611156a879deee78768ce3b1ec93d37
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-20 00:45:53 +0200

    Add a fetch progress parser

---
+++ app/src/lib/progress/fetch-progress-parser.ts
@@ -0,0 +1,22 @@
+import { StepProgressParser } from './step-progress'
+
+/**
+ * Highly approximate (some would say outright inaccurate) division
+ * of the individual progress reporting steps in a fetch operation
+ */
+const steps = [
+  { title: 'remote: Compressing objects', weight: 0.1 },
+  { title: 'Receiving objects', weight: 0.7 },
+  { title: 'Resolving deltas', weight: 0.2 },
+]
+
+/**
+ * A utility class for interpreting the output from `git push --progress`
+ * and turning that into a percentage value estimating the overall progress
+ * of the clone.
+ */
+export class PushProgressParser extends StepProgressParser {
+  public constructor() {
+    super(steps)
+  }
+}
