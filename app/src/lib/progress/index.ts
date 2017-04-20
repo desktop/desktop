8290e837e90f071690d32da99488495ce5b3f1b5
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-20 16:39:12 +0200

    Move parsing into git.ts

--- app/src/lib/progress/index.ts
+++ app/src/lib/progress/index.ts
@@ -1,6 +1,14 @@
 export * from './checkout'
-export * from './parser'
-export * from './git'
 export * from './clone'
 export * from './push'
 export * from './fetch'
+
+// We export everything except the parse function from ./git,
+// the parse function is only interesting from tests so we can
+// import it directly there instead.
+export {
+  IGitProgress,
+  ICombinedProgress,
+  IContextOutput,
+  GitProgressParser
+} from './git'
--- app/test/unit/progress/git-test.ts
+++ app/test/unit/progress/git-test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 
-import { GitProgressParser, ICombinedProgress } from '../../../src/lib/progress'
+import { GitProgressParser, ICombinedProgress, IGitProgress } from '../../../src/lib/progress'
+import { parse } from '../../../src/lib/progress/git'
 
 describe('GitProgressParser', () => {
 
@@ -60,5 +61,90 @@ describe('GitProgressParser', () => {
     result = parser.parse('remote: Compressing objects:  72% (16/22)')
 
     expect(result.kind).to.equal('context')
+  })
+})
+
+describe('progress parser', () => {
+  it('parses progress with no total', () => {
+    const result = parse('remote: Counting objects: 167587')
+
+    expect(result).to.deep.equal(<IGitProgress>{
+      title: 'remote: Counting objects',
+      text: 'remote: Counting objects: 167587',
+      value: 167587,
+      done: false,
+      percent: undefined,
+      total: undefined,
+    })
+  })
+
+  it('parses final progress with no total', () => {
+    const result = parse('remote: Counting objects: 167587, done.')
+
+    expect(result).to.deep.equal(<IGitProgress>{
+      title: 'remote: Counting objects',
+      text: 'remote: Counting objects: 167587, done.',
+      value: 167587,
+      done: true,
+      percent: undefined,
+      total: undefined,
+    })
+  })
+
+  it('parses progress with total', () => {
+    const result = parse('remote: Compressing objects:  72% (16/22)')
+
+    expect(result).to.deep.equal(<IGitProgress>{
+      title: 'remote: Compressing objects',
+      text: 'remote: Compressing objects:  72% (16/22)',
+      value: 16,
+      done: false,
+      percent: 72,
+      total: 22,
+    })
+  })
+
+  it('parses final with total', () => {
+    const result = parse('remote: Compressing objects: 100% (22/22), done.')
+
+    expect(result).to.deep.equal(<IGitProgress>{
+      title: 'remote: Compressing objects',
+      text: 'remote: Compressing objects: 100% (22/22), done.',
+      value: 22,
+      done: true,
+      percent: 100,
+      total: 22,
+    })
+  })
+
+  it('parses with total and throughput', () => {
+    const result = parse('Receiving objects:  99% (166741/167587), 267.24 MiB | 2.40 MiB/s')
+
+    expect(result).to.deep.equal(<IGitProgress>{
+      title: 'Receiving objects',
+      text: 'Receiving objects:  99% (166741/167587), 267.24 MiB | 2.40 MiB/s',
+      value: 166741,
+      done: false,
+      percent: 99,
+      total: 167587,
+    })
+  })
+
+  it('parses final with total and throughput', () => {
+    const result = parse('Receiving objects: 100% (167587/167587), 279.67 MiB | 2.43 MiB/s, done.')
+
+    expect(result).to.deep.equal(<IGitProgress>{
+      title: 'Receiving objects',
+      text: 'Receiving objects: 100% (167587/167587), 279.67 MiB | 2.43 MiB/s, done.',
+      value: 167587,
+      done: true,
+      percent: 100,
+      total: 167587,
+    })
+  })
+
+  it('does not parse things that aren\'t progress', () => {
+    const result = parse('remote: Total 167587 (delta 19), reused 11 (delta 11), pack-reused 167554         ')
+    expect(result).to.be.null
   })
 })
