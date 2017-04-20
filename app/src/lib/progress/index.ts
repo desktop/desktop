0605960bbd69bcd775943b78bd35bacfcb4a103f
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-20 23:50:36 +0200

    Export pull parser

--- app/src/lib/progress/index.ts
+++ app/src/lib/progress/index.ts
@@ -2,13 +2,5 @@ export * from './checkout'
 export * from './clone'
 export * from './push'
 export * from './fetch'
-
-// We export everything except the parse function from ./git,
-// the parse function is only interesting from tests so we can
-// import it directly there instead.
-export {
-  IGitProgress,
-  ICombinedProgress,
-  IContextOutput,
-  GitProgressParser
-} from './git'
+export * from './git'
+export * from './pull'
