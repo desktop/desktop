113911f9c347f303978183a8f8a81b2a8c79995d
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-21 14:48:52 +0200

    :book: clone

--- app/src/lib/git/clone.ts
+++ app/src/lib/git/clone.ts
@@ -11,7 +11,26 @@ export type CloneOptions = {
   readonly branch?: string
 }
 
-/** Clone the repository to the path. */
+/** 
+ * Clones a repository from a given url into to the specified path.
+ * 
+ * @param url     - The remote repository URL to clone from
+ * 
+ * @param path    - The destination path for the cloned repository. If the
+ *                  path does not exist it will be created. Cloning into an
+ *                  existing directory is only allowed if the directory is
+ *                  empty.
+ *
+ * @param options  - Options specific to the clone operation, see the
+ *                   documentation for CloneOptions for more details.
+ *
+ * @param progressCallback - An optional function which will be invoked
+ *                           with information about the current progress
+ *                           of the clone operation. When provided this enables
+ *                           the '--progress' command line flag for
+ *                           'git clone'.
+ * 
+ */
 export async function clone(url: string, path: string, options: CloneOptions, progressCallback?: (progress: ICloneProgress) => void): Promise<void> {
   const env = envForAuthentication(options.account)
 
