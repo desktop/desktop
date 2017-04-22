fb754c59636953930dfeada38e4f843632157274
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-22 15:49:21 +0200

    Fix the clone progress tests

--- app/test/unit/progress/clone-test.ts
+++ app/test/unit/progress/clone-test.ts
@@ -25,29 +25,33 @@ describe('CloneProgressParser', () => {
     })
 
     it('understands relative weights', () => {
-      expect(parser.parse('remote: Compressing objects:  45% (10/22)'))
-        .to.be.closeTo(10 / 22 * 0.1, 0.01)
+      const compressing = parser.parse('remote: Compressing objects:  45% (10/22)')
+      expect(compressing.kind).to.equal('progress')
+      expect(compressing.percent).to.be.closeTo(10 / 22 * 0.1, 0.01)
 
-      expect(parser.parse('Receiving objects:  17% (4808/28282), 3.30 MiB | 1.29 MiB/s'))
-        .to.be.closeTo(0.1 + (4808 / 28282 * 0.6), 0.01)
+      const receiving = parser.parse('Receiving objects:  17% (4808/28282), 3.30 MiB | 1.29 MiB/s')
+      expect(receiving.kind).to.equal('progress')
+      expect(receiving).to.be.closeTo(0.1 + (4808 / 28282 * 0.6), 0.01)
 
-      expect(parser.parse('Resolving deltas:  89% (18063/20263)'))
-        .to.be.closeTo(0.7 + (18063 / 20263 * 0.1), 0.01)
+      const resolving = parser.parse('Resolving deltas:  89% (18063/20263)')
+      expect(resolving.kind).to.equal('progress')
+      expect(resolving.percent).to.be.closeTo(0.7 + (18063 / 20263 * 0.1), 0.01)
 
-      expect(parser.parse('Checking out files: 100% (579/579)'))
-        .to.be.closeTo(0.8 + (579 / 579 * 0.2), 0.01)
+      const checkingOut = parser.parse('Checking out files: 100% (579/579)')
+      expect(checkingOut.kind).to.equal('progress')
+      expect(checkingOut.percent).to.be.closeTo(0.8 + (579 / 579 * 0.2), 0.01)
     })
 
     it('ignores wrong order', () => {
       const finalProgress = parser.parse('Checking out files: 100% (579/579)')
       const earlyProgress = parser.parse('Receiving objects:   1% (283/28282)')
 
-      expect(earlyProgress).to.be.null
-      expect(finalProgress).to.not.be.null
+      expect(earlyProgress.kind).to.be('context')
+      expect(finalProgress).to.be('progress')
     })
 
     it('ignores lines it doesn\'t understand', () => {
-      expect(parser.parse('Counting objects: 28282, done.')).to.be.null
+      expect(parser.parse('Counting objects: 28282, done.').kind).to.be('context')
     })
   })
 })
