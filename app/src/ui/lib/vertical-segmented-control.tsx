21f963059c4d5c11bd89f2b55eefb5b23c564eec
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-24 20:18:13 +0200

    Sketch out a segmented control component

---
+++ app/src/ui/lib/vertical-segmented-control.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react'
+
+interface ISegmentedItem {
+  readonly title: string
+  readonly description?: string
+}
+
+interface IVerticalSegmentedControlProps {
+  readonly items: ReadonlyArray<ISegmentedItem>
+  readonly selectedIndex: number
+  readonly onSelectionChanged: (selectedIndex: number) => void
+}
+
+/** The two-factor authentication component. */
+export class IVerticalSegmentedControl extends React.Component<IVerticalSegmentedControlProps, void> {
+  private renderItem(item: ISegmentedItem, selected: boolean) {
+
+    const description = item.description
+      ? <p>{item.description}</p>
+      : undefined
+
+    const className = selected ? 'selected' : undefined
+    const tabIndex = selected ? 0 : -1
+
+    return (
+      <li className={className} role='button' tabIndex={tabIndex}>
+        <div className='title'>{item.title}</div>
+        {description}
+      </li>
+    )
+  }
+
+  public render() {
+
+    if (!this.props.items.length) {
+      return null
+    }
+
+    const selectedIndex = this.props.selectedIndex
+
+    return (
+      <ul className='vertical-segmented-control'>
+        {this.props.items.map((item, index) =>
+          this.renderItem(item, index === selectedIndex))}
+      </ul>
+    )
+  }
+}
