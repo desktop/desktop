56fbd87fa0df28acf175ce30202a229a8db21a22
joshaber <joshaber@gmail.com>
2017-04-24 14:48:03 -0400

    Don't use all of octokat's plugins

--- app/src/lib/api.ts
+++ app/src/lib/api.ts
@@ -28,6 +28,28 @@ const Scopes = [
 const NoteURL = 'https://desktop.github.com/'
 
 /**
+ * The plugins we'll use with Octokat.
+ *
+ * Most notably, this doesn't include:
+ *   - hypermedia
+ *   - camel-case
+ * Both take a _lot_ of time in post-processing and are unnecessary.
+ */
+const OctokatPlugins = [
+  require('octokat/dist/node/plugins/object-chainer'),
+  require('octokat/dist/node/plugins/path-validator'),
+  require('octokat/dist/node/plugins/authorization'),
+  require('octokat/dist/node/plugins/preview-apis'),
+  require('octokat/dist/node/plugins/use-post-instead-of-patch'),
+
+  require('octokat/dist/node/plugins/simple-verbs'),
+  require('octokat/dist/node/plugins/fetch-all'),
+
+  require('octokat/dist/node/plugins/read-binary'),
+  require('octokat/dist/node/plugins/pagination'),
+]
+
+/**
  * Information about a repository as returned by the GitHub API.
  */
 export interface IAPIRepository {
@@ -160,7 +182,11 @@ export class API {
 
   public constructor(account: Account) {
     this.account = account
-    this.client = new Octokat({ token: account.token, rootURL: account.endpoint })
+    this.client = new Octokat({
+      token: account.token,
+      rootURL: account.endpoint,
+      plugins: OctokatPlugins,
+    })
   }
 
   /**
