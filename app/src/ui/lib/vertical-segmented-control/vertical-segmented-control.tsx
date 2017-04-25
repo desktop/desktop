7605a20c94a0ec598642a2e6a2280d4b3186eb98
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-25 14:11:33 +0200

    Update import to be relative

--- app/src/ui/lib/vertical-segmented-control/vertical-segmented-control.tsx
+++ app/src/ui/lib/vertical-segmented-control/vertical-segmented-control.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { createUniqueId, releaseUniqueId } from './id-pool'
+import { createUniqueId, releaseUniqueId } from '../id-pool'
 
 interface ISegmentedItem {
   readonly title: string
