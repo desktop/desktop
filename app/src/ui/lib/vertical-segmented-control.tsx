f07495c5e8bd612cd1aa8ae0eaa5b30231e2d5ab
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-25 12:25:05 +0200

    Use a field set so that we can attribute properly

--- app/src/ui/lib/vertical-segmented-control.tsx
+++ app/src/ui/lib/vertical-segmented-control.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { createUniqueId, releaseUniqueId } from './id-pool'
 
 interface ISegmentedItem {
   readonly title: string
@@ -12,7 +13,52 @@ interface IVerticalSegmentedControlProps {
   readonly onSelectionChanged: (selectedIndex: number) => void
 }
 
-export class VerticalSegmentedControl extends React.Component<IVerticalSegmentedControlProps, void> {
+interface IVerticalSegmentedControlState {
+  /**
+   * An automatically generated id for the list element used to reference
+   * it from the label element. This is generated once via the id pool when the
+   * component is mounted and then released once the component unmounts.
+   */
+  readonly listId?: string
+}
+
+export class VerticalSegmentedControl extends React.Component<IVerticalSegmentedControlProps, IVerticalSegmentedControlState> {
+  private listRef: HTMLUListElement | null = null
+
+  public constructor(props: IVerticalSegmentedControlProps) {
+    super(props)
+    this.state = { }
+  }
+
+  private updateListId() {
+    if (this.state.listId) {
+      releaseUniqueId(this.state.listId)
+      this.setState({ listId: undefined })
+    }
+
+    if (this.props.label) {
+      this.setState({
+        listId: createUniqueId(`VerticalSegmentedControl_${this.props.label}`)
+      })
+    }
+  }
+
+  public componentWillMount() {
+    this.updateListId()
+  }
+
+  public componentWillUnmount() {
+    if (this.state.listId) {
+      releaseUniqueId(this.state.listId)
+    }
+  }
+
+  public componentWillReceiveProps(props: IVerticalSegmentedControlProps) {
+    if (this.props.label !== props.label) {
+      this.updateListId()
+    }
+  }
+
   private onItemClick = (index: number) => {
     if (index !== this.props.selectedIndex) {
       this.props.onSelectionChanged(index)
@@ -46,6 +92,16 @@ export class VerticalSegmentedControl extends React.Component<IVerticalSegmented
     }
   }
 
+  private onListRef = (ref: HTMLUListElement | null) => {
+    this.listRef = ref
+  }
+
+  private onLegendClick = () => {
+    if (this.listRef) {
+      this.listRef.focus()
+    }
+  }
+
   public render() {
 
     if (!this.props.items.length) {
@@ -54,17 +110,26 @@ export class VerticalSegmentedControl extends React.Component<IVerticalSegmented
 
     const selectedIndex = this.props.selectedIndex
     const label = this.props.label
-      ? <label>{this.props.label}</label>
+      ? <legend onClick={this.onLegendClick}>{this.props.label}</legend>
       : undefined
 
+    // Using a fieldset with a legend seems to be the way to go here since
+    // we can't use a label to point to a list (https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Content_categories#Form_labelable).
+    // See http://stackoverflow.com/a/13273907/2114
     return (
-      <div className='vertical-segmented-control'>
+      <fieldset className='vertical-segmented-control'>
         {label}
-        <ul className='vertical-segmented-control' tabIndex={0} onKeyDown={this.onKeyDown}>
+        <ul
+          ref={this.onListRef}
+          id={this.state.listId}
+          className='vertical-segmented-control'
+          tabIndex={0}
+          onKeyDown={this.onKeyDown}
+        >
           {this.props.items.map((item, index) =>
             this.renderItem(item, index, index === selectedIndex))}
         </ul>
-      </div>
+      </fieldset>
     )
   }
 }
