843b051a9064e7afdbcea3091fde245f2fd2fb21
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-25 11:39:51 +0200

    Store start point instead of branch instance

--- app/src/ui/create-branch/index.tsx
+++ app/src/ui/create-branch/index.tsx
@@ -7,6 +7,7 @@ import { Branch } from '../../models/branch'
 import { TextBox } from '../lib/text-box'
 import { Row } from '../lib/row'
 import { Button } from '../lib/button'
+import { LinkButton } from '../lib/link-button'
 import { ButtonGroup } from '../lib/button-group'
 import { Dialog, DialogError, DialogContent, DialogFooter } from '../dialog'
 import { Octicon, OcticonSymbol } from '../octicons'
@@ -23,12 +24,36 @@ interface ICreateBranchProps {
   readonly allBranches: ReadonlyArray<Branch>
 }
 
+type StartPoint = 'current-branch' | 'default-branch' | 'HEAD'
+
 interface ICreateBranchState {
   readonly currentError: Error | null
   readonly proposedName: string
   readonly sanitizedName: string
-  readonly baseBranch: Branch | null
+  readonly startPoint: StartPoint
   readonly loading: boolean
+}
+
+function getStartPoint(props: ICreateBranchProps, preferred: StartPoint): StartPoint {
+  if (preferred === 'default-branch' && props.defaultBranch) {
+    return preferred
+  }
+
+  if (preferred === 'current-branch' && props.tip.kind === TipState.Valid) {
+    return preferred
+  }
+
+  if (preferred === 'HEAD') {
+    return preferred
+  }
+
+  if (props.defaultBranch) {
+    return 'default-branch'
+  } else if (props.tip.kind === TipState.Valid) {
+    return 'current-branch'
+  } else {
+    return 'HEAD'
+  }
 }
 
 /** The Create Branch component. */
@@ -40,9 +65,15 @@ export class CreateBranch extends React.Component<ICreateBranchProps, ICreateBra
       currentError: null,
       proposedName: '',
       sanitizedName: '',
-      baseBranch: this.props.defaultBranch,
+      startPoint: getStartPoint(props, 'default-branch'),
       loading: false,
     }
+  }
+
+  public componentWillReceiveProps(props: ICreateBranchProps) {
+    this.setState({
+      startPoint: getStartPoint(props, this.state.startPoint)
+    })
   }
 
   private renderBranchSelection() {
@@ -60,8 +91,8 @@ export class CreateBranch extends React.Component<ICreateBranchProps, ICreateBra
     } else if (tip.kind === TipState.Unborn) {
       return (
         <p>
-          Your current branch is unborn (does not yet contain any commits).
-          Creating a new branch now rename the current branch.
+          Your current branch is unborn (does not contain any commits).
+          Creating a new branch will rename the current branch.
         </p>
       )
     } else if (tip.kind === TipState.Valid) {
@@ -90,12 +121,15 @@ export class CreateBranch extends React.Component<ICreateBranchProps, ICreateBra
           },
         ]
 
+        const startPoint = this.state.startPoint
+        const selectedIndex = startPoint === 'default-branch' ? 0 : 1
+
         return (
           <Row>
             <VerticalSegmentedControl
               label='Create branch based on...'
               items={items}
-              selectedIndex={0}
+              selectedIndex={selectedIndex}
               onSelectionChanged={this.onBaseBranchChanged}
             />
           </Row>
@@ -108,6 +142,11 @@ export class CreateBranch extends React.Component<ICreateBranchProps, ICreateBra
   }
 
   private onBaseBranchChanged = (selectedIndex: number) => {
+    if (selectedIndex === 0) {
+      this.setState({ startPoint: 'default-branch' })
+    } else if (selectedIndex === 1) {
+      this.setState({ startPoint: 'current-branch' })
+    }
   }
 
   private renderSanitizedName() {
@@ -169,20 +208,28 @@ export class CreateBranch extends React.Component<ICreateBranchProps, ICreateBra
       currentError = new Error(`A branch named ${sanitizedName} already exists`)
     }
 
-    this.setState({
-      currentError,
-      proposedName: str,
-      baseBranch: this.state.baseBranch,
-      sanitizedName,
-    })
+    this.setState({ proposedName: str, sanitizedName })
   }
 
   private createBranch = async () => {
     const name = this.state.sanitizedName
-    const baseBranch = this.state.baseBranch
-    if (name.length > 0 && baseBranch) {
+
+    let startPoint = undefined
+
+    if (this.state.startPoint === 'default-branch') {
+      // This really shouldn't happen, we take all kinds of precautions
+      // to make sure the startPoint state is valid given the current props.
+      if (!this.props.defaultBranch) {
+        this.setState({ currentError: new Error('Could not determine the default branch') })
+        return
+      }
+
+      startPoint = this.props.defaultBranch.name
+    }
+
+    if (name.length > 0) {
       this.setState({ loading: true })
-      await this.props.dispatcher.createBranch(this.props.repository, name, baseBranch.name)
+      await this.props.dispatcher.createBranch(this.props.repository, name, startPoint)
       this.props.onDismissed()
     }
   }
