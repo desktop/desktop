4a4b952bc01d428441c382d1baabc29195bf85ea
Don Okuda <don.okuda@gmail.com>
2017-04-25 21:38:37 -0700

    Move text out of form component
    and fix more spacing issues

--- app/src/ui/welcome/usage-opt-out.tsx
+++ app/src/ui/welcome/usage-opt-out.tsx
@@ -20,24 +20,26 @@ interface IUsageOptOutProps {
 export class UsageOptOut extends React.Component<IUsageOptOutProps, void> {
   public render() {
     return (
-      <Form className='usage-opt-out' onSubmit={this.finish}>
+      <div className='usage-opt-out'>
         <h1 className='welcome-title'>Help make GitHub Desktop better!</h1>
 
         <p>
           Would you like to help us improve GitHub Desktop by periodically submitting <LinkButton uri={SamplesURL}>anonymous usage data</LinkButton>?
         </p>
 
-        <Checkbox
-          label='Yes, submit anonymized usage data'
-          value={this.props.optOut ? CheckboxValue.Off : CheckboxValue.On}
-          onChange={this.onChange}
-        />
+        <Form  onSubmit={this.finish}>
+          <Checkbox
+            label='Yes, submit anonymized usage data'
+            value={this.props.optOut ? CheckboxValue.Off : CheckboxValue.On}
+            onChange={this.onChange}
+          />
 
-        <Row className='actions'>
-          <Button type='submit'>Finish</Button>
-          <Button onClick={this.cancel}>Cancel</Button>
-        </Row>
-      </Form>
+          <Row className='actions'>
+            <Button type='submit'>Finish</Button>
+            <Button onClick={this.cancel}>Cancel</Button>
+          </Row>
+        </Form>
+      </div>
     )
   }
 
