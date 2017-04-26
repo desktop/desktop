a5ce04aece126fd88069b28f9dc9808a61bcf532
Brendan Forster <brendan@github.com>
2017-04-26 14:06:12 +1000

    Revert "extract shared function for resolving the OS version"
    This reverts commit d4d4306c6f23b9ca68de326316c15f82b96330ed.

--- app/src/lib/stats/stats-store.ts
+++ app/src/lib/stats/stats-store.ts
@@ -1,4 +1,5 @@
-import { getOperatingSystem } from '../operating-system'
+import * as OS from 'os'
+import { UAParser } from 'ua-parser-js'
 import { StatsDatabase, ILaunchStats, IDailyMeasures } from './stats-database'
 import { getDotComAPIEndpoint } from '../api'
 import { getVersion } from '../../ui/lib/app-proxy'
@@ -127,12 +128,27 @@ export class StatsStore {
 
     return {
       version: getVersion(),
-      osVersion: getOperatingSystem(),
+      osVersion: this.getOS(),
       platform: process.platform,
       ...launchStats,
       ...dailyMeasures,
       ...userType,
       guid: this.guid,
+    }
+  }
+
+  private getOS() {
+    if (__DARWIN__) {
+      // On macOS, OS.release() gives us the kernel version which isn't terribly
+      // meaningful to any human being, so we'll parse the User Agent instead.
+      // See https://github.com/desktop/desktop/issues/1130.
+      const parser = new UAParser()
+      const os = parser.getOS()
+      return `${os.name} ${os.version}`
+    } else if (__WIN32__) {
+      return `Windows ${OS.release()}`
+    } else {
+      return `${OS.type()} ${OS.release()}`
     }
   }
 
