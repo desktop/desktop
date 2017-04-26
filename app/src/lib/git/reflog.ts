3e4025b99107a38aa9aa61fa666d612ddb9a09ae
Markus Olsson <j.markus.olsson@gmail.com>
2017-04-26 17:19:51 +0200

    Turns out this makes it go twice as fast (o_O)

--- app/src/lib/git/reflog.ts
+++ app/src/lib/git/reflog.ts
@@ -10,7 +10,7 @@ export async function getRecentBranches(repository: Repository, branches: Readon
   // but by using log we can give it a max number which should prevent us from balling out
   // of control when there's ginormous reflogs around (as in e.g. github/github).
   const regex = new RegExp(/.*? checkout: moving from .*? to (.*?)$/i)
-  const result = await git([ 'log', '-g', '--abbrev-commit', '--pretty=oneline', 'HEAD', '-n', '2500', '--' ], repository.path, 'getRecentBranches', { successExitCodes: new Set([ 0, 128 ]) })
+  const result = await git([ 'log', '-g', '--no-abbrev-commit', '--pretty=oneline', 'HEAD', '-n', '2500', '--' ], repository.path, 'getRecentBranches', { successExitCodes: new Set([ 0, 128 ]) })
 
   if (result.exitCode === 128) {
     // error code 128 is returned if the branch is unborn
