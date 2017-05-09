d3f4eb4e7fefb0e5b8ce1a3278f00e1d4db95225
Brendan Forster <brendan@github.com>
2017-05-09 11:46:20 +1000

    and added at test to ensure we don't trash commentary by default

--- app/test/unit/git/commit-test.ts
+++ app/test/unit/git/commit-test.ts
@@ -58,6 +58,25 @@ describe('git/commit', () => {
       expect(commits[0].summary).to.equal('Special commit')
     })
 
+    it('commit does not strip commentary by default', async () => {
+      fs.writeFileSync(path.join(repository!.path, 'README.md'), 'Hi world\n')
+
+      const status = await getStatus(repository!)
+      const files = status.workingDirectory.files
+      expect(files.length).to.equal(1)
+
+      const message = `Special commit
+
+# this is a comment`
+
+      await createCommit(repository!, message, files)
+
+      const commits = await getCommits(repository!, 'HEAD', 1)
+      expect(commits.length).to.equal(1)
+      expect(commits[0].summary).to.equal('Special commit')
+      expect(commits[0].body).to.equal('# this is a comment\n')
+    })
+
     it('can commit for empty repository', async () => {
 
       const repo = await setupEmptyRepository()
