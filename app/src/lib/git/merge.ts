0584e2edaa4a9df53d89fc4867698b97126bbff9
Brendan Forster <brendan@github.com>
2017-05-09 11:34:38 +1000

    added afunction to check for the existence of MERGE_HEAD

--- app/src/lib/git/merge.ts
+++ app/src/lib/git/merge.ts
@@ -1,7 +1,23 @@
+import * as Path from 'path'
+import * as Fs from 'fs'
+
 import { git } from './core'
 import { Repository } from '../../models/repository'
 
 /** Merge the named branch into the current branch. */
 export async function merge(repository: Repository, branch: string): Promise<void> {
   await git([ 'merge', branch ], repository.path, 'merge')
+}
+
+/**
+ * Check that the repository is in the middle of a merge, using the presence of
+ * the MERGE_HEAD file to validate this.
+ */
+export async function mergeExists(repository: Repository): Promise<boolean> {
+  return new Promise<boolean>((resolve, reject) => {
+    const mergeHead = Path.join(repository.path, '.git', 'MERGE_HEAD')
+    Fs.exists(mergeHead, (exists) => {
+      resolve(exists)
+    })
+  })
 }
