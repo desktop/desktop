2823a0ae07588094593aa30ba7d8cbb358673e3a
joshaber <joshaber@gmail.com>
2017-05-11 09:45:21 -0400

    Set zoom limits
    Fixes #1431

--- app/src/main-process/app-window.ts
+++ app/src/main-process/app-window.ts
@@ -111,6 +111,8 @@ export class AppWindow {
       const now = Date.now()
       this._loadTime = now - startLoad
 
+      this.window.webContents.setVisualZoomLevelLimits(1, 1)
+
       this.maybeEmitDidLoad()
     })
 
