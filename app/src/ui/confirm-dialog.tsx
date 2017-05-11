3ccf5afba6637a4df3237176581b3757c0a7545a
iAmWillShepherd <williamrshepherd@gmail.com>
2017-05-11 11:34:58 -0500

    Remove dependency on dispatcher in favor of event handler

--- app/src/ui/confirm-dialog.tsx
+++ app/src/ui/confirm-dialog.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react'
-import { Dispatcher } from '../lib/dispatcher'
 import { ButtonGroup } from '../ui/lib/button-group'
 import { Button } from '../ui/lib/button'
 import { Dialog, DialogContent, DialogFooter } from '../ui/dialog'
 
 interface IConfirmDialogProps {
-  readonly dispatcher: Dispatcher
   readonly title: string
   readonly message: string
   readonly onConfirmation: () => void
+  readonly onDismissed: () => void
 }
 
 export class ConfirmDialog extends React.Component<IConfirmDialogProps, void> {
@@ -17,12 +16,12 @@ export class ConfirmDialog extends React.Component<IConfirmDialogProps, void> {
   }
 
   private cancel = () => {
-    this.props.dispatcher.closePopup()
+    this.props.onDismissed()
   }
 
   private onConfirmed = () => {
     this.props.onConfirmation()
-    this.props.dispatcher.closePopup()
+    this.props.onDismissed()
   }
 
   public render() {
