50a57755cf673054b62177ff67aa339b96474cad
joshaber <joshaber@gmail.com>
2017-05-12 08:05:38 -0400

    Terms updates

--- app/src/ui/terms-and-conditions/terms-and-conditions.tsx
+++ app/src/ui/terms-and-conditions/terms-and-conditions.tsx
@@ -21,7 +21,7 @@ export class TermsAndConditions extends React.Component<ITermsAndConditionsProps
           <div id='text'>
             <h1>GitHub Open Source Applications Terms and Conditions</h1>
 
-            <p>These GitHub Open Source Applications Terms and Conditions ("Application Terms") are a legal agreement between you (either as an individual or on behalf of an entity) and GitHub, Inc. regarding your use of GitHub's applications, such as GitHub DesktopTM and associated documentation ("Software"). These Application Terms apply to the executable code version of the Software. Source code for the Software is available separately and free of charge under open source software license agreements. If you do not agree to all of the terms in these Application Terms, do not download, install, use, or copy the Software.</p>
+            <p>These GitHub Open Source Applications Terms and Conditions ("Application Terms") are a legal agreement between you (either as an individual or on behalf of an entity) and GitHub, Inc. regarding your use of GitHub's applications, such as GitHub Desktop™ and associated documentation ("Software"). These Application Terms apply to the executable code version of the Software. Source code for the Software is available separately and free of charge under open source software license agreements. If you do not agree to all of the terms in these Application Terms, do not download, install, use, or copy the Software.</p>
 
             <h2>Connecting to GitHub</h2>
 
@@ -45,7 +45,7 @@ export class TermsAndConditions extends React.Component<ITermsAndConditionsProps
 
             <h2>Privacy</h2>
 
-            <p>The Software may collect personal information. You may control what information the Software collects in the Settings panel. If the Software does collect personal information on GitHub's behalf, GitHub will process that information in accordance with the <LinkButton uri='https://help.github.com/articles/github-privacy-statement/'>GitHub Privacy Statement</LinkButton>.</p>
+            <p>The Software may collect personal information. You may control what information the Software collects in the settings panel. If the Software does collect personal information on GitHub's behalf, GitHub will process that information in accordance with the <LinkButton uri='https://help.github.com/articles/github-privacy-statement/'>GitHub Privacy Statement</LinkButton>.</p>
 
             <h2>Additional Services</h2>
 
