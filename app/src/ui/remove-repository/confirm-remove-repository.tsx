e5becdb8ff1645bb46d0f8422fef04dde10a7224
Markus Olsson <j.markus.olsson@gmail.com>
2017-05-16 11:06:16 +0200

    Add a description

--- app/src/ui/remove-repository/confirm-remove-repository.tsx
+++ app/src/ui/remove-repository/confirm-remove-repository.tsx
@@ -37,6 +37,9 @@ export class ConfirmRemoveRepository extends React.Component<IConfirmRemoveRepos
       >
         <DialogContent>
           <p>Are you sure you want to remove the repository "{this.props.repository.name}"?</p>
+          <p className='description'>
+            The repository will be removed from GitHub Desktop but will remain on disk.
+          </p>
         </DialogContent>
         <DialogFooter>
           <ButtonGroup>
