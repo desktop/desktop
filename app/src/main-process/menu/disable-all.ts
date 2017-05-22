6f95878335675fac28716a1e81133be8f6d87682
Markus Olsson <j.markus.olsson@gmail.com>
2017-05-22 14:03:52 +0200

    Try disabling everything that doesn't have a role

---
+++ app/src/main-process/menu/disable-all.ts
@@ -0,0 +1,32 @@
+import { Menu } from 'electron'
+
+export function disableAll() {
+  const menu = Menu.getApplicationMenu()
+
+  if (!menu) {
+    return
+  }
+
+  for (const topLevelItem of menu.items) {
+    disable(topLevelItem)
+  }
+}
+
+function disable(item: Electron.MenuItem) {
+  let anyEnabled = false
+
+  if (item.submenu instanceof Electron.Menu) {
+    for (const submenuItem of item.submenu.items) {
+      if (disable(submenuItem)) {
+        anyEnabled = true
+      }
+    }
+  }
+
+  if (anyEnabled || item.role) {
+    return true
+  }
+
+  item.enabled = false
+  return false
+}
