e54e1ea8f545e584bed98c7a36ffc053106125fe
Markus Olsson <j.markus.olsson@gmail.com>
2017-05-25 20:14:31 +0200

    This seems appropriate

--- app/test/globals.ts
+++ app/test/globals.ts
@@ -10,5 +10,5 @@ import 'chai-datetime'
 const g: any = global
 g['__WIN32__'] = process.platform === 'win32'
 g['__DARWIN__'] = process.platform === 'darwin'
-g['__RELEASE_ENV__'] = 'development'
+g['__RELEASE_ENV__'] = 'test'
 g['__DEV__'] = false
