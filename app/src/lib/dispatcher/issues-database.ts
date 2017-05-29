ec5c2469c042e6eca20d70faae514b341138e7c6
Markus Olsson <j.markus.olsson@gmail.com>
2017-05-29 16:42:03 +0200

    explain yo self

--- app/src/lib/dispatcher/issues-database.ts
+++ app/src/lib/dispatcher/issues-database.ts
@@ -31,6 +31,18 @@ export class IssuesDatabase extends Dexie {
         .filter(key => /^IssuesStore\/\d+\/lastFetch$/.test(key))
         .forEach(key => localStorage.removeItem(key))
 
+      // Unfortunately we have to clear the issues in order to maintain
+      // data consistency in the database. The issues table is only supposed
+      // to store 'open' issues and if we kept the existing issues (which)
+      // don't have an updated_at field around the initial query for
+      // max(updated_at) would return null, causing us to fetch all _open_
+      // issues which in turn means we wouldn't be able to detect if we
+      // have any issues in the database that have been closed since the
+      // last time we fetched. Not only that, these closed issues wouldn't
+      // be updated to include the updated_at field unless they were actually
+      // modified at a later date.
+      //
+      // TL;DR; This is the safest approach
       return t.table('issues').clear()
     })
   }
