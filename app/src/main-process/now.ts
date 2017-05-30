584fbf904d4d8744c057c20d0a47109de85a4757
joshaber <joshaber@gmail.com>
2017-05-30 13:39:31 -0400

    Doc why this is a thing

--- app/src/main-process/now.ts
+++ app/src/main-process/now.ts
@@ -1,6 +1,9 @@
 /**
  * Get the time from some arbitrary fixed starting point. The time will not be
  * based on clock time.
+ *
+ * Ideally we'd just use `performance.now` but that's a browser API and not
+ * available in our Plain Old Node main process environment.
  */
 export function now(): number {
   const time = process.hrtime()
