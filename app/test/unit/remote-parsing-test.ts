d5918da84f44769e7d6424e3d5ba14bd7654838d
joshaber <joshaber@gmail.com>
2017-05-31 11:29:23 -0400

    Fix parsing user names
    Fixes #1826

--- app/test/unit/remote-parsing-test.ts
+++ app/test/unit/remote-parsing-test.ts
@@ -19,6 +19,14 @@ describe('URL remote parsing', () => {
     expect(remote!.name).to.equal('repo')
   })
 
+  it('parses HTTPS URLs which include a username', () => {
+    const remote = parseRemote('https://monalisa@github.com/hubot/repo.git')
+    expect(remote).not.to.equal(null)
+    expect(remote!.hostname).to.equal('github.com')
+    expect(remote!.owner).to.equal('hubot')
+    expect(remote!.name).to.equal('repo')
+  })
+
   it('parses SSH URLs', () => {
     const remote = parseRemote('git@github.com:hubot/repo.git')
     expect(remote).not.to.equal(null)
