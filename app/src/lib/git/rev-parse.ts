afbceb189aa3c5963cadb0e0ec2c3239e12e8598
Markus Olsson <j.markus.olsson@gmail.com>
2017-06-02 22:31:15 +0200

    Remove getGitDir in favor of getTopLevelWorkingDirectory

--- app/src/lib/git/rev-parse.ts
+++ app/src/lib/git/rev-parse.ts
@@ -3,20 +3,6 @@ import * as Path from 'path'
 import { git } from './core'
 import { Repository } from '../../models/repository'
 
-/** Get the git dir of the path. */
-export async function getGitDir(path: string): Promise<string | null> {
-  const result = await git([ 'rev-parse', '--git-dir' ], path, 'getGitDir', { successExitCodes: new Set([ 0, 128 ]) })
-  // Exit code 128 means it was run in a directory that's not a git
-  // repository.
-  if (result.exitCode === 128) {
-    return null
-  }
-
-  const gitDir = result.stdout
-  const trimmedDir = gitDir.trim()
-  return Path.join(path, trimmedDir)
-}
-
 /**
  * Get the absolute path to the top level working directory.
  *
@@ -55,6 +41,5 @@ export async function resolveHEAD(repository: Repository): Promise<string | null
 
 /** Is the path a git repository? */
 export async function isGitRepository(path: string): Promise<boolean> {
-  const result = await getGitDir(path)
-  return !!result
+  return await getTopLevelWorkingDirectory(path) !== null
 }
--- app/test/unit/git/rev-parse-test.ts
+++ app/test/unit/git/rev-parse-test.ts
@@ -6,7 +6,7 @@ import * as os from 'os'
 import { expect } from 'chai'
 
 import { Repository } from '../../../src/models/repository'
-import { getGitDir, isGitRepository, getTopLevelWorkingDirectory } from '../../../src/lib/git/rev-parse'
+import { isGitRepository, getTopLevelWorkingDirectory } from '../../../src/lib/git/rev-parse'
 import { setupFixtureRepository } from '../../fixture-helper'
 
 const temp = require('temp').track()
@@ -22,18 +22,6 @@ describe('git/rev-parse', () => {
 
   after(() => {
     temp.cleanupSync()
-  })
-
-  describe('getGitDir', () => {
-    it('should return the git dir path for a repository', async () => {
-      const result = await getGitDir(repository!.path)
-      expect(result).to.equal(path.join(repository!.path, '.git'))
-    })
-
-    it('should return null for a directory', async () => {
-      const result = await getGitDir(path.dirname(repository!.path))
-      expect(result).to.equal(null)
-    })
   })
 
   describe('isGitRepository', () => {
