7a1a13a4985d8a99645cc9628f6850a8dff989d4
Markus Olsson <j.markus.olsson@gmail.com>
2017-06-05 18:00:37 +0200

    :book:

--- app/src/lib/git/diff-index.ts
+++ app/src/lib/git/diff-index.ts
@@ -1,6 +1,12 @@
 import { git } from './core'
 import { Repository } from '../../models/repository'
 
+/**
+ * Get a list of relative paths of files which have recorded changes in the
+ * index as compared to HEAD.
+ *
+ * @param repository The repository for which to retrieve the index changes.
+ */
 export async function getChangedPathsInIndex(repository: Repository): Promise<string[]> {
 
   const args = [ 'diff-index', '--cached', '--name-only', '--no-renames', '-z' ]
