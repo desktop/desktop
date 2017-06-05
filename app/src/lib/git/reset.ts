547e1c18d03ec2fc5adfbf57c9107e2d9e3be189
Markus Olsson <j.markus.olsson@gmail.com>
2017-06-05 16:09:04 +0200

    :book: GitResetMode

--- app/src/lib/git/reset.ts
+++ app/src/lib/git/reset.ts
@@ -4,8 +4,23 @@ import { assertNever } from '../fatal-error'
 
 /** The reset modes which are supported. */
 export const enum GitResetMode {
+  /**
+   * Resets the index and working tree. Any changes to tracked files in the
+   * working tree since <commit> are discarded.
+   */
   Hard = 0,
+  /**
+   * Does not touch the index file or the working tree at all (but resets the
+   * head to <commit>, just like all modes do). This leaves all your changed
+   * files "Changes to be committed", as git status would put it.
+   */
   Soft,
+
+  /**
+   * Resets the index but not the working tree (i.e., the changed files are
+   * preserved but not marked for commit) and reports what has not been updated.
+   * This is the default action for git reset.
+   */
   Mixed,
 }
 
