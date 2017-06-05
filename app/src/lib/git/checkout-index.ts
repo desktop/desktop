5160c2bbc604b5472bef1ebfc03d2909d599a65b
Markus Olsson <j.markus.olsson@gmail.com>
2017-06-05 16:16:12 +0200

    :book: checkoutIndex

--- app/src/lib/git/checkout-index.ts
+++ app/src/lib/git/checkout-index.ts
@@ -1,6 +1,20 @@
 import { git } from './core'
 import { Repository } from '../../models/repository'
 
+/**
+ * Forcefully updates the working directory with information from the index
+ * for a given set of files.
+ *
+ * This method is essentially the same as running `git checkout -- files`
+ * except by using `checkout-index` we can pass the files we want updated
+ * on stdin, avoiding all issues with too long arguments.
+ *
+ * @param repository The repository in which to update the working directory
+ *                   with information from the index
+ *
+ * @param paths      The relative paths in the working directory to update
+ *                   with information from the index.
+ */
 export async function checkoutIndex(repository: Repository, paths: ReadonlyArray<string>) {
   if (!paths.length) {
     return
