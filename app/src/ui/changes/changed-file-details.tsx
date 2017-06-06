a8da3518804b999e6190603624336b3f4ec2fd2f
Brendan Forster <brendan@github.com>
2017-06-06 12:44:53 -0300

    display a message in the file header when a line endings change found

--- app/src/ui/changes/changed-file-details.tsx
+++ app/src/ui/changes/changed-file-details.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import { PathLabel } from '../lib/path-label'
 import { FileStatus, mapStatus, iconForStatus } from '../../models/status'
-import { Octicon } from '../octicons'
+import { IDiff, DiffType } from '../../models/diff'
+import { Octicon, OcticonSymbol } from '../octicons'
 
 interface IChangedFileDetailsProps {
   readonly path: string
   readonly oldPath?: string
   readonly status: FileStatus
+  readonly diff: IDiff
 }
 
 /** Displays information about a file */
@@ -16,12 +18,25 @@ export class ChangedFileDetails extends React.Component<IChangedFileDetailsProps
     const status = this.props.status
     const fileStatus = mapStatus(status)
 
+    let metadataElement: JSX.Element | undefined
+    const diff = this.props.diff
+    if (diff.kind === DiffType.Text) {
+      if (diff.lineEndingsChange) {
+        const message = `Warning: line endings change from '${diff.lineEndingsChange.from}' to '${diff.lineEndingsChange.to}'.`
+        metadataElement = <Octicon symbol={OcticonSymbol.alert}
+                            className={'line-endings'}
+                            title={message} />
+      }
+    }
+
     return (
       <div className='header'>
         <PathLabel
           path={this.props.path}
           oldPath={this.props.oldPath}
           status={this.props.status} />
+        { metadataElement }
+
         <Octicon symbol={iconForStatus(status)}
             className={'status status-' + fileStatus.toLowerCase()}
             title={fileStatus} />
