4fe429806436120b7641fefb49928b6b7ce2feee
joshaber <joshaber@gmail.com>
2017-06-07 15:15:39 -0400

    Handle uninstalling

--- app/src/main-process/squirrel-updater.ts
+++ app/src/main-process/squirrel-updater.ts
@@ -1,4 +1,3 @@
-import { app } from 'electron'
 import * as ChildProcess from 'child_process'
 import * as Path from 'path'
 import * as Fs from 'fs'
@@ -15,12 +14,10 @@ export function handleSquirrelEvent(eventName: string): boolean {
       return true
 
     case '--squirrel-uninstall':
-      removeShortcut()
-      setTimeout(app.quit, 1000)
+      handleUninstall()
       return true
 
     case '--squirrel-obsolete':
-      app.quit()
       return true
   }
 
@@ -30,16 +27,18 @@ export function handleSquirrelEvent(eventName: string): boolean {
 async function handleUpdate(): Promise<void> {
   await updateShortcut()
 
+  const binPath = await writeCLITrampoline()
+
   const paths = await getPathSegments()
-
-  await writeCLITrampoline()
-
-  const binPath = getBinPath()
   if (paths.indexOf(binPath) > -1) { return }
 
   return setPathSegments([ ...paths, binPath ])
 }
 
+/**
+ * Get the path for the `bin` directory which exists in our `AppData` but
+ * outside path which includes the installed app version.
+ */
 function getBinPath(): string {
   const appFolder = Path.resolve(process.execPath, '..')
   const rootAppDir = Path.resolve(appFolder, '..')
@@ -47,31 +46,34 @@ function getBinPath(): string {
 }
 
 /**
- * Here's the problem: our path contains our version number. So each time we
- * update, the path to our app changes. So it's Real Hard to add our path
- * directly to `Path` and remove stale entries.
+ * Here's the problem: our app's path contains its version number. So each time
+ * we update, the path to our app changes. So it's Real Hard to add our path
+ * directly to `Path`. We'd have to detect and remove stale entries, etc.
  *
  * So instead, we write a trampoline out to a fixed path, still inside our
- * `AppData` directory but outside the part that includes the version. Then we
- * update the trampoline every time we update to launch the _real_ `github`
- * tool.
+ * `AppData` directory but outside the version-specific path. That trampoline
+ * just launches the current version's CLI tool. Then, whenever we update, we
+ * rewrite the trampoline to point to the new, version-specific path. Bingo
+ * bango Bob's your uncle.
  */
-async function writeCLITrampoline(): Promise<void> {
+async function writeCLITrampoline(): Promise<string> {
+  const binPath = getBinPath()
   const appFolder = Path.resolve(process.execPath, '..')
-  const versionedPath = Path.relative(getBinPath(), Path.join(appFolder, 'resources', 'app', 'static', 'github.bat'))
+  const versionedPath = Path.relative(binPath, Path.join(appFolder, 'resources', 'app', 'static', 'github.bat'))
   const trampline = `@echo off\n"%~dp0\\${versionedPath}" %*`
-  const trampolinePath = Path.join(getBinPath(), 'github.bat')
-  return new Promise<void>((resolve, reject) => {
+  const trampolinePath = Path.join(binPath, 'github.bat')
+  return new Promise<string>((resolve, reject) => {
     Fs.writeFile(trampolinePath, trampline, err => {
       if (err) {
         reject(err)
       } else {
-        resolve()
+        resolve(binPath)
       }
     })
   })
 }
 
+/** Spawn the Squirrel.Windows `Update.exe` with a command. */
 async function spawnSquirrelUpdate(command: string): Promise<void> {
   const appFolder = Path.resolve(process.execPath, '..')
   const rootAppDir = Path.resolve(appFolder, '..')
@@ -83,6 +85,15 @@ async function spawnSquirrelUpdate(command: string): Promise<void> {
 
 function createShortcut(): Promise<void> {
   return spawnSquirrelUpdate('--createShortcut')
+}
+
+async function handleUninstall(): Promise<void> {
+  await removeShortcut()
+
+  const paths = await getPathSegments()
+  const binPath = getBinPath()
+  const pathsWithoutBinPath = paths.filter(p => p !== binPath)
+  return setPathSegments(pathsWithoutBinPath)
 }
 
 function removeShortcut(): Promise<void> {
@@ -108,6 +119,7 @@ function updateShortcut(): Promise<void> {
   }
 }
 
+/** Get the path segments in the user's `Path`. */
 async function getPathSegments(): Promise<ReadonlyArray<string>> {
   let powershellPath: string
   const systemRoot = process.env['SystemRoot']
@@ -141,6 +153,20 @@ async function getPathSegments(): Promise<ReadonlyArray<string>> {
     .filter(segment => segment.length)
 }
 
+/** Set the user's `Path`. */
+async function setPathSegments(paths: ReadonlyArray<string>): Promise<void> {
+  let setxPath: string
+  const systemRoot = process.env['SystemRoot']
+  if (systemRoot) {
+    const system32Path = Path.join(systemRoot, 'System32')
+    setxPath = Path.join(system32Path, 'setx.exe')
+  } else {
+    setxPath = 'setx.exe'
+  }
+
+  await spawn(setxPath, [ 'Path', paths.join(';') ])
+}
+
 function spawn(command: string, args: ReadonlyArray<string>): Promise<string> {
   try {
     const child = ChildProcess.spawn(command, args as string[])
@@ -170,17 +196,4 @@ function spawn(command: string, args: ReadonlyArray<string>): Promise<string> {
   } catch (error) {
     return Promise.reject(error)
   }
-}
-
-async function setPathSegments(paths: ReadonlyArray<string>): Promise<void> {
-  let setxPath: string
-  const systemRoot = process.env['SystemRoot']
-  if (systemRoot) {
-    const system32Path = Path.join(systemRoot, 'System32')
-    setxPath = Path.join(system32Path, 'setx.exe')
-  } else {
-    setxPath = 'setx.exe'
-  }
-
-  await spawn(setxPath, [ 'Path', paths.join(';') ])
 }
