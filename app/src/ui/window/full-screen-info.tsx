820dcf032ebb1e70fa5f0d4b7960728546db1d97
Don Okuda <don.okuda@gmail.com>
2017-06-07 15:20:31 -0700

    Start a basic component

---
+++ app/src/ui/window/full-screen-info.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export class FullScreenInfo extends React.Component<any, any> {
+  public render () {
+    return (
+      <div>FULL SCREEN AHEAD</div>
+    )
+  }
+}
