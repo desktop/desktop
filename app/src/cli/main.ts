eb305cc18a39c0ada86522e0e5512c2cd29d898e
joshaber <joshaber@gmail.com>
2017-06-07 11:31:39 -0400

    Invoke the URL protocol

--- app/src/cli/main.ts
+++ app/src/cli/main.ts
@@ -1,1 +1,15 @@
-console.log('hi everyone')
+import * as ChildProcess from 'child_process'
+import * as Path from 'path'
+
+const args = process.argv.slice(2)
+
+// At some point we may have other command line options, but for now we assume
+// the first arg is the path to open.
+const pathArg = args.length > 0 ? args[0] : ''
+const repositoryPath = Path.resolve(process.cwd(), pathArg)
+const url = `x-github-client://openLocalRepo/${encodeURIComponent(repositoryPath)}`
+
+const command = __DARWIN__ ? 'open' : 'start'
+ChildProcess.exec(`${command} ${url}`)
+
+process.exit(0)
