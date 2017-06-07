9820f7f988e04ea4b268f1462f6dbe6bb6d8c473
joshaber <joshaber@gmail.com>
2017-06-07 14:56:27 -0400

    Cargo-cult something awful from atom

--- app/src/main-process/squirrel-updater.ts
+++ app/src/main-process/squirrel-updater.ts
@@ -11,7 +11,7 @@ export function handleSquirrelEvent(eventName: string): boolean {
       return true
 
     case '--squirrel-updated':
-      updateShortcut()
+      handleUpdate()
       return true
 
     case '--squirrel-uninstall':
@@ -27,22 +27,58 @@ export function handleSquirrelEvent(eventName: string): boolean {
   return false
 }
 
-function spawnSquirrelUpdate(command: string): Promise<void> {
+async function handleUpdate(): Promise<void> {
+  await updateShortcut()
+
+  const paths = await getPathSegments()
+
+  await writeCLITrampoline()
+
+  const binPath = getBinPath()
+  if (paths.indexOf(binPath) > -1) { return }
+
+  return setPathSegments([ ...paths, binPath ])
+}
+
+function getBinPath(): string {
+  const appFolder = Path.resolve(process.execPath, '..')
+  const rootAppDir = Path.resolve(appFolder, '..')
+  return Path.join(rootAppDir, 'bin')
+}
+
+/**
+ * Here's the problem: our path contains our version number. So each time we
+ * update, the path to our app changes. So it's Real Hard to add our path
+ * directly to `Path` and remove stale entries.
+ *
+ * So instead, we write a trampoline out to a fixed path, still inside our
+ * `AppData` directory but outside the part that includes the version. Then we
+ * update the trampoline every time we update to launch the _real_ `github`
+ * tool.
+ */
+async function writeCLITrampoline(): Promise<void> {
+  const appFolder = Path.resolve(process.execPath, '..')
+  const versionedPath = Path.relative(getBinPath(), Path.join(appFolder, 'resources', 'app', 'static', 'github.bat'))
+  const trampline = `@echo off\n"%~dp0\\${versionedPath}" %*`
+  const trampolinePath = Path.join(getBinPath(), 'github.bat')
+  return new Promise<void>((resolve, reject) => {
+    Fs.writeFile(trampolinePath, trampline, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+async function spawnSquirrelUpdate(command: string): Promise<void> {
   const appFolder = Path.resolve(process.execPath, '..')
   const rootAppDir = Path.resolve(appFolder, '..')
   const updateDotExe = Path.resolve(Path.join(rootAppDir, 'Update.exe'))
   const exeName = Path.basename(process.execPath)
 
-  try {
-    const p = ChildProcess.spawn(updateDotExe, [ command, exeName ], { detached: true })
-    return new Promise<void>((resolve, reject) => {
-      p.on('exit', () => {
-        resolve()
-      })
-    })
-  } catch (error) {
-    return Promise.reject(error)
-  }
+  await spawn(updateDotExe, [ command, exeName ])
 }
 
 function createShortcut(): Promise<void> {
@@ -70,4 +106,81 @@ function updateShortcut(): Promise<void> {
   } else {
     return createShortcut()
   }
+}
+
+async function getPathSegments(): Promise<ReadonlyArray<string>> {
+  let powershellPath: string
+  const systemRoot = process.env['SystemRoot']
+  if (systemRoot) {
+    const system32Path = Path.join(process.env.SystemRoot, 'System32')
+    powershellPath = Path.join(system32Path, 'WindowsPowerShell', 'v1.0', 'powershell.exe')
+  } else {
+    powershellPath = 'powershell.exe'
+  }
+
+  const args = [
+    '-noprofile',
+    '-ExecutionPolicy',
+    'RemoteSigned',
+    '-command',
+    // Set encoding and execute the command, capture the output, and return it
+    // via .NET's console in order to have consistent UTF-8 encoding.
+    // See http://stackoverflow.com/questions/22349139/utf-8-output-from-powershell
+    // to address https://github.com/atom/atom/issues/5063
+    `
+      [Console]::OutputEncoding=[System.Text.Encoding]::UTF8
+      $output=[environment]::GetEnvironmentVariable('Path', 'User')
+      [Console]::WriteLine($output)
+    `,
+  ]
+
+  const stdout = await spawn(powershellPath, args)
+  const pathOutput = stdout.replace(/^\s+|\s+$/g, '')
+  return pathOutput
+    .split(/;+/)
+    .filter(segment => segment.length)
+}
+
+function spawn(command: string, args: ReadonlyArray<string>): Promise<string> {
+  try {
+    const child = ChildProcess.spawn(command, args as string[])
+    return new Promise<string>((resolve, reject) => {
+      let stdout = ''
+      child.stdout.on('data', data => {
+        stdout += data
+      })
+
+      child.on('close', code => {
+        if (code === 0) {
+          resolve(stdout)
+        } else {
+          reject(new Error(`Command failed: ${stdout}`))
+        }
+      })
+
+      child.on('error', (err: Error) => {
+        reject(err)
+      })
+
+      // This is necessary if using Powershell 2 on Windows 7 to get the events
+      // to raise.
+      // See http://stackoverflow.com/questions/9155289/calling-powershell-from-nodejs
+      child.stdin.end()
+    })
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
+
+async function setPathSegments(paths: ReadonlyArray<string>): Promise<void> {
+  let setxPath: string
+  const systemRoot = process.env['SystemRoot']
+  if (systemRoot) {
+    const system32Path = Path.join(systemRoot, 'System32')
+    setxPath = Path.join(system32Path, 'setx.exe')
+  } else {
+    setxPath = 'setx.exe'
+  }
+
+  await spawn(setxPath, [ 'Path', paths.join(';') ])
 }
--- app/static/win32/github.bat
+++ app/static/win32/github.bat
@@ -2,6 +2,6 @@
 setlocal
 
 set ELECTRON_RUN_AS_NODE=1
-call "%~dp0..\@@NAME@@.exe" "%~dp0..\resources\app\cli.js" %*
+call "%~dp0..\..\..\GitHub Desktop.exe" "%~dp0..\cli.js" %*
 
 endlocal
