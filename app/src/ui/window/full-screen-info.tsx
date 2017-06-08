4525a4038885d6e54323b941e58571455e14a79a
Don Okuda <don.okuda@gmail.com>
2017-06-07 22:30:01 -0700

    Add some basic rendering states

--- app/src/ui/window/full-screen-info.tsx
+++ app/src/ui/window/full-screen-info.tsx
@@ -1,7 +1,28 @@
 import * as React from 'react'
 
-export class FullScreenInfo extends React.Component<any, any> {
+interface IFullScreenInfoState {
+  readonly renderInfo: boolean
+}
+
+// const holdDuration = 750
+
+export class FullScreenInfo extends React.Component<any, IFullScreenInfoState> {
+
+  // private infoDisappearTimeoutId: number | null = null
+
+  public constructor() {
+    super()
+
+    this.state = {
+      renderInfo: false,
+    }
+  }
+
   public render () {
+    if (!this.state.renderInfo) {
+      return null
+    }
+
     return (
       <div className='toast-notification-container'>
         <div className='toast-notification'>
