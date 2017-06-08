c9ef8539d29469f43ac39bd9767d67e6a1b88d00
Josh Abernathy <joshaber@gmail.com>
2017-06-08 13:39:06 -0400

    Just bake the name in

--- app/static/win32/github.bat
+++ app/static/win32/github.bat
@@ -2,7 +2,6 @@
 setlocal
 
 set ELECTRON_RUN_AS_NODE=1
-for /f %%A in ("*Desktop*") do set APP_NAME=%%~nxA
-call "%~dp0..\..\..\%APP_NAME%" "%~dp0..\cli.js" %*
+call "%~dp0..\..\..\GitHub Desktop.exe" "%~dp0..\cli.js" %*
 
 endlocal
