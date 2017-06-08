2a1785c313aa961cfa4c8cae4e69ff2a672ea1c8
Don Okuda <don.okuda@gmail.com>
2017-06-08 11:41:18 -0700

    Set up CSSTransitionGroup

--- app/src/ui/window/full-screen-info.tsx
+++ app/src/ui/window/full-screen-info.tsx
@@ -1,21 +1,45 @@
 import * as React from 'react'
+import { CSSTransitionGroup } from 'react-transition-group'
 
 interface IFullScreenInfoState {
   readonly renderInfo: boolean
+  readonly renderTransitionGroup: boolean
 }
 
-// const holdDuration = 750
+const transitionDuration = 100
+const holdDuration = 750
 
 export class FullScreenInfo extends React.Component<any, IFullScreenInfoState> {
 
-  // private infoDisappearTimeoutId: number | null = null
+  private infoDisappearTimeoutId: number | null = null
+  private transitionGroupDisappearTimeoutId: number | null = null
 
   public constructor() {
     super()
 
     this.state = {
       renderInfo: false,
+      renderTransitionGroup: false,
     }
+  }
+
+  public componentWillReceiveProps(nextProps: any) {
+    if (this.infoDisappearTimeoutId !== null) {
+      clearTimeout(this.infoDisappearTimeoutId)
+    }
+
+    if (this.transitionGroupDisappearTimeoutId !== null) {
+      clearTimeout(this.transitionGroupDisappearTimeoutId)
+    }
+
+    this.transitionGroupDisappearTimeoutId = window.setTimeout(
+      this.onTransitionGroupDisappearTimeout,
+      holdDuration + transitionDuration,
+    )
+  }
+
+  private onTransitionGroupDisappearTimeout = () => {
+    this.setState({ renderTransitionGroup: false })
   }
 
   public render () {
@@ -24,11 +48,20 @@ export class FullScreenInfo extends React.Component<any, IFullScreenInfoState> {
     }
 
     return (
-      <div className='toast-notification-container'>
+      <CSSTransitionGroup
+        className='toast-notification-container'
+        id='full-screen-info'
+        transitionName='zoom-in' component='div'
+        transitionAppear={true}
+        transitionEnter={false}
+        transitionLeave={true}
+        transitionAppearTimeout={transitionDuration}
+        transitionLeaveTimeout={transitionDuration}
+      >
         <div className='toast-notification'>
-          Press <kbd className='kbd'>Esc</kbd> to exit fullscreen
+          Press <kbd className='kbd'>Esc</kbd> or <kbd className='kbd'>F11</kbd> to exit fullscreen
         </div>
-      </div>
+      </CSSTransitionGroup>
     )
   }
 }
