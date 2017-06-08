2dbeaffc08fe3db4888fe7f83b38c94314fe2240
Don Okuda <don.okuda@gmail.com>
2017-06-07 21:22:27 -0700

    Cargo cult some styles

--- app/src/ui/window/full-screen-info.tsx
+++ app/src/ui/window/full-screen-info.tsx
@@ -3,7 +3,11 @@ import * as React from 'react'
 export class FullScreenInfo extends React.Component<any, any> {
   public render () {
     return (
-      <div>FULL SCREEN AHEAD</div>
+      <div className='toast-notification-container'>
+        <div className='toast-notification'>
+          Press <kbd className='kbd'>Esc</kbd> to exit fullscreen
+        </div>
+      </div>
     )
   }
 }
