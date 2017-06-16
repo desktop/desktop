a94d0b232e41cd12a138363503e6d95931db9554
Brendan Forster <brendan@github.com>
2017-06-16 17:55:07 +0200

    clarify test

--- app/test/unit/git/diff-test.ts
+++ app/test/unit/git/diff-test.ts
@@ -161,7 +161,7 @@ describe('git/diff', () => {
       expect(second.lines[7].text).to.have.string('+vel sagittis nisl rutrum.')
     })
 
-    it('displays the text diff for a docx file', async () => {
+    it('displays a binary diff for a docx file', async () => {
       const repositoryPath = await setupFixtureRepository('diff-rendering-docx')
       const repo = new Repository(repositoryPath, -1, null, false)
 
@@ -170,10 +170,9 @@ describe('git/diff', () => {
 
       expect(files.length).to.equal(1)
 
-      const diff = await getTextDiff(repo, files[0])
+      const diff = await getWorkingDirectoryDiff(repo, files[0])
 
-      expect(diff.hunks.length).to.equal(1)
-      expect(diff.hunks[0].lines.length).to.equal(3)
+      expect(diff.kind).to.equal(DiffType.Binary)
     })
 
     it('is empty for a renamed file', async () => {
