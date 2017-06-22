f92eaddc0db9d64d60d9f378d5fb9027c5432b2d
iAmWillShepherd <williamrshepherd@gmail.com>
2017-06-21 23:03:41 -0500

    Throw some docs on it

--- app/src/lib/git/revert.ts
+++ app/src/lib/git/revert.ts
@@ -1,6 +1,14 @@
 import { git } from './core'
 import { Repository } from '../../models/repository'
 
+/**
+ * Creates a new commit that reverts the changes of a previous commit
+ *
+ * @param repository  - The repository to update
+ *
+ * @param SHA         - The SHA of the commit to be reverted
+ *
+ */
 export async function revertCommit(repository: Repository, SHA: string) {
   await git([ 'revert', '-m', '1', SHA ], repository.path, 'revert')
 }
