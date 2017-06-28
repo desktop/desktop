7e625097072251ace0e7adf498d32b2f0550d7f2
iAmWillShepherd <williamrshepherd@gmail.com>
2017-06-28 14:06:26 -0700

    Test

--- app/src/lib/dispatcher/actions.ts
+++ app/src/lib/dispatcher/actions.ts
@@ -1,53 +1,53 @@
-import { IRepository } from "../../models/repository";
-import { IAccount } from "../../models/account";
+import { IRepository } from '../../models/repository'
+import { IAccount } from '../../models/account'
 
 export interface IGetAccountsAction {
-  name: "get-accounts";
+  name: 'get-accounts'
 }
 
 export interface IGetRepositoriesAction {
-  name: "get-repositories";
+  name: 'get-repositories'
 }
 
 export interface IAddRepositoriesAction {
-  name: "add-repositories";
-  readonly paths: ReadonlyArray<string>;
+  name: 'add-repositories'
+  readonly paths: ReadonlyArray<string>
 }
 
 export interface IRemoveRepositoriesAction {
-  name: "remove-repositories";
-  readonly repositoryIDs: ReadonlyArray<number>;
+  name: 'remove-repositories'
+  readonly repositoryIDs: ReadonlyArray<number>
 }
 
 export interface IUpdateGitHubRepositoryAction {
-  name: "update-github-repository";
-  readonly repository: IRepository;
+  name: 'update-github-repository'
+  readonly repository: IRepository
 }
 
 /** Add a user to the app. */
 export interface IAddAccountAction {
-  readonly name: "add-account";
-  readonly account: IAccount;
+  readonly name: 'add-account'
+  readonly account: IAccount
 }
 
 /** Remove a user from the app. */
 export interface IRemoveAccountAction {
-  readonly name: "remove-account";
-  readonly account: IAccount;
+  readonly name: 'remove-account'
+  readonly account: IAccount
 }
 
 /** Change a repository's `missing` status. */
 export interface IUpdateRepositoryMissingAction {
-  readonly name: "update-repository-missing";
-  readonly repository: IRepository;
-  readonly missing: boolean;
+  readonly name: 'update-repository-missing'
+  readonly repository: IRepository
+  readonly missing: boolean
 }
 
 /** Change a repository's path. */
 export interface IUpdateRepositoryPathAction {
-  readonly name: "update-repository-path";
-  readonly repository: IRepository;
-  readonly path: string;
+  readonly name: 'update-repository-path'
+  readonly repository: IRepository
+  readonly path: string
 }
 
 export type Action =
@@ -59,4 +59,4 @@ export type Action =
   | IAddAccountAction
   | IRemoveAccountAction
   | IUpdateRepositoryMissingAction
-  | IUpdateRepositoryPathAction;
+  | IUpdateRepositoryPathAction
