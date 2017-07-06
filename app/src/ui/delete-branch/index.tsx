9eb28471e633c38f9fa6e998962b6c9003cb303b
iAmWillShepherd <williamrshepherd@gmail.com>
2017-07-06 10:58:23 -0500

    Add checkbox to allow user to choose to delete on remote as well

--- app/src/ui/delete-branch/index.tsx
+++ app/src/ui/delete-branch/index.tsx
@@ -5,6 +5,7 @@ import { Repository } from '../../models/repository'
 import { Branch } from '../../models/branch'
 import { Button } from '../lib/button'
 import { ButtonGroup } from '../lib/button-group'
+import { Checkbox, CheckboxValue } from '../lib/checkbox'
 import { Dialog, DialogContent, DialogFooter } from '../dialog'
 
 interface IDeleteBranchProps {
@@ -43,6 +44,15 @@ export class DeleteBranch extends React.Component<
             Delete branch "{this.props.branch.name}"?
           </p>
           <p>This cannot be undone.</p>
+          <Checkbox
+            label="Include branch on remote"
+            value={
+              this.state.includeRemoteBranch
+                ? CheckboxValue.On
+                : CheckboxValue.Off
+            }
+            onChange={this.onIncludeRemoteChanged}
+          />
         </DialogContent>
         <DialogFooter>
           <ButtonGroup destructive={true}>
@@ -52,6 +62,14 @@ export class DeleteBranch extends React.Component<
         </DialogFooter>
       </Dialog>
     )
+  }
+
+  private onIncludeRemoteChanged = (
+    event: React.FormEvent<HTMLInputElement>
+  ) => {
+    const value = event.currentTarget.checked
+
+    this.setState({ includeRemoteBranch: value })
   }
 
   private deleteBranch = () => {
