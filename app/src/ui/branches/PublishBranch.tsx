b18f74266b13f222a0a9e8689c7ac594249bed0b
iAmWillShepherd <williamrshepherd@gmail.com>
2017-07-07 14:36:13 -0500

    Add component

---
+++ app/src/ui/branches/PublishBranch.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react'
+import { Dispatcher } from '../../lib/dispatcher'
+import { Branch } from '../../models/branch'
+import { ButtonGroup } from '../../ui/lib/button-group'
+import { Button } from '../../ui/lib/button'
+import { Dialog, DialogContent, DialogFooter } from '../../ui/dialog'
+import { Repository } from '../../models/repository'
+
+interface IPublishBranchProps {
+  readonly dispatcher: Dispatcher
+  readonly repository: Repository
+  readonly branch: Branch
+  readonly onDismissed: () => void
+}
+
+export class PublishBranch extends React.Component<IPublishBranchProps> {
+  public render() {
+    return (
+      <Dialog
+        id="publish-branch"
+        key="push-branch"
+        title={__DARWIN__ ? 'Publish Repository' : 'Publish Repository'}
+        onDismissed={this.cancel}
+        onSubmit={this.push}
+      >
+        <DialogContent>
+          <p>
+            The branch must be published before opening a PR. Would you like to
+            publish <em>{this.props.branch.name}</em> and oepn a pull request?
+          </p>
+        </DialogContent>
+
+        <DialogFooter>
+          <ButtonGroup>
+            <Button type="submit">Publish branch and open pull request</Button>
+            <Button onClick={this.cancel}>Cancel</Button>
+          </ButtonGroup>
+        </DialogFooter>
+      </Dialog>
+    )
+  }
+
+  private cancel = () => {
+    this.props.onDismissed()
+  }
+
+  private push() {
+    this.props.dispatcher.push(this.props.repository)
+    this.props.onDismissed()
+  }
+}
