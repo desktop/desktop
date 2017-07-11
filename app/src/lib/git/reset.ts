91750ad6aa1673c3393adda65b52f415d2536092
Markus Olsson <j.markus.olsson@gmail.com>
2017-07-11 14:09:11 +0200

    Remove the lint

--- app/src/lib/git/reset.ts
+++ app/src/lib/git/reset.ts
@@ -53,7 +53,7 @@ export async function reset(
  * set of paths.
  *
  * @param repository The repository in which to reset the index.
-
+ *
  * @param mode      Which mode to use when resetting, see the GitResetMode
  *                  enum for more information.
  *
