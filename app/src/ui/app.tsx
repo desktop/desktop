1fe59b95c8dd96f958cf402c3d6720ea96c97149
joshaber <joshaber@gmail.com>
2017-07-12 12:11:00 -0400

    Check for updates with the proper env

--- app/src/ui/app.tsx
+++ app/src/ui/app.tsx
@@ -171,8 +171,8 @@ export class App extends React.Component<IAppProps, IAppState> {
       this.props.dispatcher.postError(error)
     })
 
-    setInterval(() => this.checkForUpdates(), UpdateCheckInterval)
-    this.checkForUpdates()
+    setInterval(() => this.checkForUpdates(true), UpdateCheckInterval)
+    this.checkForUpdates(true)
 
     ipcRenderer.on(
       'launch-timing-stats',
@@ -270,17 +270,15 @@ export class App extends React.Component<IAppProps, IAppState> {
     })
   }
 
-  private checkForUpdates() {
+  private checkForUpdates(inBackground: boolean) {
     if (__RELEASE_ENV__ === 'development' || __RELEASE_ENV__ === 'test') {
       return
     }
 
-    updateStore.checkForUpdates(this.getUsernameForUpdateCheck(), true)
-  }
-
-  private getUsernameForUpdateCheck() {
-    const dotComAccount = this.getDotComAccount()
-    return dotComAccount ? dotComAccount.login : ''
+    updateStore.checkForUpdates(
+      __RELEASE_ENV__ === 'beta' ? 'beta' : 'production',
+      inBackground
+    )
   }
 
   private getDotComAccount(): Account | null {
@@ -949,7 +947,7 @@ export class App extends React.Component<IAppProps, IAppState> {
             onDismissed={this.onPopupDismissed}
             applicationName={getName()}
             applicationVersion={getVersion()}
-            usernameForUpdateCheck={this.getUsernameForUpdateCheck()}
+            onCheckForUpdates={this.onCheckForUpdates}
             onShowAcknowledgements={this.showAcknowledgements}
             onShowTermsAndConditions={this.showTermsAndConditions}
           />
@@ -997,6 +995,10 @@ export class App extends React.Component<IAppProps, IAppState> {
       default:
         return assertNever(popup, `Unknown popup type: ${popup}`)
     }
+  }
+
+  private onCheckForUpdates = () => {
+    this.checkForUpdates(false)
   }
 
   private showAcknowledgements = () => {
