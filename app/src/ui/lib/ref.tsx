6a23399cb12b403640401151f92713bbd39f5277
Markus Olsson <j.markus.olsson@gmail.com>
2017-07-21 16:49:35 +0200

    :art:

--- app/src/ui/lib/ref.tsx
+++ app/src/ui/lib/ref.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 /**
  * A simple style component used to mark up arbitrary references such as
- * branches, commit shas, paths or other content which needs to be presented
+ * branches, commit SHAs, paths or other content which needs to be presented
  * in an emphasized way and that benefit from fixed-width fonts.
  *
  * While the styling of the component _may_ differs depending on what context
