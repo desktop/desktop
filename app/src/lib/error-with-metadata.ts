323583da8e7dffdf84cb6997178bf65a13b94ca6
joshaber <joshaber@gmail.com>
2017-07-26 15:26:59 -0400

    Capture the underlying error we're wrapping

--- app/src/lib/error-with-metadata.ts
+++ app/src/lib/error-with-metadata.ts
@@ -8,11 +8,15 @@ export class ErrorWithMetadata extends Error {
   /** The error's metadata. */
   public readonly metadata: IErrorMetadata
 
+  /** The underlying error to which the metadata is being attached. */
+  public readonly underlyingError: Error
+
   public constructor(error: Error, metadata: IErrorMetadata) {
     super(error.message)
 
     this.name = error.name
     this.stack = error.stack
+    this.underlyingError = error
     this.metadata = metadata
   }
 }
