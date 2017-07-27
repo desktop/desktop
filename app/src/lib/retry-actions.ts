2c3f4daab82fe9c3f83d3eae28336d2d32ca0031
joshaber <joshaber@gmail.com>
2017-07-27 14:33:52 -0400

    :book:

--- app/src/lib/retry-actions.ts
+++ app/src/lib/retry-actions.ts
@@ -1,7 +1,9 @@
 import { Repository } from '../models/repository'
 
+/** The types of actions that can be retried. */
 export const enum RetryActionType {
   Push = 1,
 }
 
+/** The retriable actions. */
 export type RetryAction = { type: RetryActionType.Push; repository: Repository }
