ba7051fc65ae6d981adbf953c9dc64df16b0af3a
Jed Fox <git@twopointzero.us>
2017-08-09 18:08:36 -0400

    Add npm run cli command to run the CLI with ts-node

---
+++ app/src/cli/dev-commands-gbl.js
@@ -0,0 +1,7 @@
+const Fs = require('fs')
+const path = require('path')
+
+global.__CLI_COMMANDS__ = Fs
+  .readdirSync(path.join(__dirname, 'commands'))
+  .filter(name => name.endsWith('.ts'))
+  .map(name => name.replace(/\.ts$/, ''))
