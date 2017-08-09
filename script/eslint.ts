0e32a686864b3135f664b4de9b3915ed09a323b0
Jed Fox <git@twopointzero.us>
2017-08-09 14:00:12 -0400

    Take args in script/eslint; ⚡

--- script/eslint.ts
+++ script/eslint.ts
@@ -1,22 +1,27 @@
-#!/usr/bin/env node
+#!/usr/bin/env ts-node
 
 import ChildProcess = require('child_process')
+import Path = require('path')
 
 const ESLINT_ARGS = [
   '--cache',
   '--rulesdir=./eslint-rules',
-  '--ext=.js,.ts,.jsx,.tsx',
-  './script',
-  './eslint-rules',
-  './tslint-rules/*.ts',
-  './app/{src,typings,test}',
+  './{script,eslint-rules}/**/*.{j,t}s?(x)',
+  './tslint-rules/**/*.ts',
+  './app/{src,typings,test}/**/*.{j,t}s?(x)',
+  ...process.argv.slice(2),
 ]
 const opts = {
   stdio: 'inherit',
 }
 
 if (process.env.CI) {
-  ChildProcess.spawn('eslint', ESLINT_ARGS, opts)
+  ChildProcess.spawn(
+    Path.resolve(__dirname, '..', 'node_modules', '.bin', 'eslint'),
+    ESLINT_ARGS,
+    opts
+  )
 } else {
-  ChildProcess.spawn('eslint_d', ESLINT_ARGS, opts)
+  console.log('> Spinning up eslint_d\n')
+  require('eslint_d/lib/client').lint(ESLINT_ARGS)
 }
