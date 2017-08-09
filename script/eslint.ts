c0b15df652fca7ba73e19f8d3947afee164d7bec
Jed Fox <git@twopointzero.us>
2017-08-09 13:50:48 -0400

    Use eslint_d to speed up linting

---
+++ script/eslint.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import ChildProcess = require('child_process')
+
+const ESLINT_ARGS = [
+  '--rulesdir=./eslint-rules',
+  '--ext=.js,.ts,.jsx,.tsx',
+  './eslint-rules',
+  './tslint-rules/*.ts',
+  './app/{src,typings,test}',
+]
+const opts = {
+  stdio: 'inherit',
+}
+
+if (process.env.CI) {
+  ChildProcess.spawn('eslint', ESLINT_ARGS, opts)
+} else {
+  ChildProcess.spawn('eslint_d', ESLINT_ARGS, opts)
+}
