a59226a7ada60b8e8e359f73121dd98b65fe93bd
Brendan Forster <brendan@github.com>
2017-08-11 10:19:35 +1000

    unify macOS lookup methods for finding apps

--- app/src/lib/editors/darwin.ts
+++ app/src/lib/editors/darwin.ts
@@ -1,91 +1,54 @@
 import * as Path from 'path'
 import { ExternalEditor } from '../../models/editors'
+import { LookupResult, FoundEditor, pathExists } from './shared'
+import { assertNever } from '../fatal-error'
 
 /**
  * appPath will raise an error if it cannot find the program.
  */
 const appPath: (bundleId: string) => Promise<string> = require('app-path')
 
-import { LookupResult, FoundEditor, pathExists } from './shared'
-
-async function findAtomApplication(): Promise<LookupResult> {
-  const editor = ExternalEditor.Atom
-
-  try {
-    const installPath = await appPath('com.github.atom')
-    const path = Path.join(
-      installPath,
-      'Contents',
-      'Resources',
-      'app',
-      'atom.sh'
-    )
-    const exists = await pathExists(path)
-    if (!exists) {
-      log.debug(`Command line interface for ${editor} not found at '${path}'`)
-      return {
-        editor,
-        installed: true,
-        pathExists: false,
-      }
-    }
-    return {
-      editor,
-      installed: true,
-      pathExists: true,
-      path,
-    }
-  } catch (error) {
-    log.debug(`Unable to locate ${editor} installation`, error)
-    return { editor, installed: false }
+function getBundleIdentifier(editor: ExternalEditor): string {
+  switch (editor) {
+    case ExternalEditor.Atom:
+      return 'com.github.atom'
+    case ExternalEditor.VisualStudioCode:
+      return 'com.microsoft.VSCode'
+    case ExternalEditor.SublimeText:
+      return 'com.sublimetext.3'
+    default:
+      return assertNever(editor, `Unknown external editor: ${editor}`)
   }
 }
 
-async function findCodeApplication(): Promise<LookupResult> {
-  const editor = ExternalEditor.VisualStudioCode
-  try {
-    const installPath = await appPath('com.microsoft.VSCode')
-    const path = Path.join(
-      installPath,
-      'Contents',
-      'Resources',
-      'app',
-      'bin',
-      'code'
-    )
-    const exists = await pathExists(path)
-    if (!exists) {
-      log.debug(`Command line interface for ${editor} not found at '${path}'`)
-      return {
-        editor,
-        installed: true,
-        pathExists: false,
-      }
-    }
-    return {
-      editor,
-      installed: true,
-      pathExists: true,
-      path,
-    }
-  } catch (error) {
-    log.debug(`Unable to locate ${editor} installation`, error)
-    return { editor, installed: false }
+function getExecutableShim(
+  editor: ExternalEditor,
+  installPath: string
+): string {
+  switch (editor) {
+    case ExternalEditor.Atom:
+      return Path.join(installPath, 'Contents', 'Resources', 'app', 'atom.sh')
+    case ExternalEditor.VisualStudioCode:
+      return Path.join(
+        installPath,
+        'Contents',
+        'Resources',
+        'app',
+        'bin',
+        'code'
+      )
+    case ExternalEditor.SublimeText:
+      return Path.join(installPath, 'Contents', 'SharedSupport', 'bin', 'subl')
+    default:
+      return assertNever(editor, `Unknown external editor: ${editor}`)
   }
 }
 
-async function findSublimeTextApplication(): Promise<LookupResult> {
-  const editor = ExternalEditor.SublimeText
+async function findApplication(editor: ExternalEditor): Promise<LookupResult> {
   try {
-    const sublimeApp = await appPath('com.sublimetext.3')
-    const path = Path.join(
-      sublimeApp,
-      'Contents',
-      'SharedSupport',
-      'bin',
-      'subl'
-    )
-
+    const identifier = getBundleIdentifier(editor)
+    const installPath = await appPath(identifier)
+    const path = getExecutableShim(editor, installPath)
     const exists = await pathExists(path)
     if (!exists) {
       log.debug(`Command line interface for ${editor} not found at '${path}'`)
@@ -108,8 +71,8 @@ async function findSublimeTextApplication(): Promise<LookupResult> {
 }
 
 /**
- * Lookup the known external editors using the bundle ID that each uses
- * to register itself on a user's machine.
+ * Lookup known external editors using the bundle ID that each uses
+ * to register itself on a user's machine when installing.
  */
 export async function getAvailableEditors(): Promise<
   ReadonlyArray<FoundEditor>
@@ -117,9 +80,9 @@ export async function getAvailableEditors(): Promise<
   const results: Array<FoundEditor> = []
 
   const [atom, code, sublime] = await Promise.all([
-    findAtomApplication(),
-    findCodeApplication(),
-    findSublimeTextApplication(),
+    findApplication(ExternalEditor.Atom),
+    findApplication(ExternalEditor.VisualStudioCode),
+    findApplication(ExternalEditor.SublimeText),
   ])
 
   if (atom.installed && atom.pathExists) {
@@ -142,17 +105,17 @@ export async function getAvailableEditors(): Promise<
  * no matches are found.
  */
 export async function getFirstEditorOrDefault(): Promise<FoundEditor | null> {
-  const atom = await findAtomApplication()
+  const atom = await findApplication(ExternalEditor.Atom)
   if (atom.installed && atom.pathExists) {
     return { editor: atom.editor, path: atom.path }
   }
 
-  const code = await findCodeApplication()
+  const code = await findApplication(ExternalEditor.VisualStudioCode)
   if (code.installed && code.pathExists) {
     return { editor: code.editor, path: code.path }
   }
 
-  const sublime = await findSublimeTextApplication()
+  const sublime = await findApplication(ExternalEditor.SublimeText)
   if (sublime.installed && sublime.pathExists) {
     return { editor: sublime.editor, path: sublime.path }
   }
