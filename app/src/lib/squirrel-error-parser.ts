6933055a52b08c7a982d162579e6ebccaa8d15bf
Brendan Forster <brendan@github.com>
2017-08-14 13:58:09 +1000

    added another common error

--- app/src/lib/squirrel-error-parser.ts
+++ app/src/lib/squirrel-error-parser.ts
@@ -1,18 +1,25 @@
 // an error that Electron raises when it can't find the installation for the running app
-const squirrelMissingError = /^Can not find Squirrel$/
+const squirrelMissingRegex = /^Can not find Squirrel$/
 
 // an error that occurs when Squirrel isn't able to reach the update server
-const squirrelNetworkError = /System\.Net\.WebException: The remote name could not be resolved: 'central\.github\.com'/
+const squirrelDNSRegex = /System\.Net\.WebException: The remote name could not be resolved: 'central\.github\.com'/
+
+const squirrelTimeoutRegex = /A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond/
 
 export function parseError(error: Error): Error | null {
-  if (squirrelMissingError.test(error.message)) {
+  if (squirrelMissingRegex.test(error.message)) {
     return new Error(
       'The application is missing a dependency it needs to check and install updates. This is very, very bad.'
     )
   }
-  if (squirrelNetworkError.test(error.message)) {
+  if (squirrelDNSRegex.test(error.message)) {
     return new Error(
-      'The application was not able to reach the update server. Ensure you have internet connectivity and try again.'
+      'GitHub Desktop was not able to contact the update server. Ensure you have internet connectivity and try again.'
+    )
+  }
+  if (squirrelTimeoutRegex.test(error.message)) {
+    return new Error(
+      'GitHub Desktop was not able to check for updates due to a timeout. Ensure you have internet connectivity and try again.'
     )
   }
 
