e1a6355023e3a52a9b9293c5e25d2931e16b1d44
Brendan Forster <brendan@github.com>
2017-08-14 13:59:44 +1000

    and a comment

--- app/src/lib/squirrel-error-parser.ts
+++ app/src/lib/squirrel-error-parser.ts
@@ -4,6 +4,7 @@ const squirrelMissingRegex = /^Can not find Squirrel$/
 // an error that occurs when Squirrel isn't able to reach the update server
 const squirrelDNSRegex = /System\.Net\.WebException: The remote name could not be resolved: 'central\.github\.com'/
 
+// an error that occurs when the connection times out during updating
 const squirrelTimeoutRegex = /A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond/
 
 export function parseError(error: Error): Error | null {
