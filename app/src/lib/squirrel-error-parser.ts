3f773dfcd19a9e3f77d2e6dc392e9d45e3504aaa
Brendan Forster <brendan@github.com>
2017-08-14 12:32:49 +1000

    finish them words

--- app/src/lib/squirrel-error-parser.ts
+++ app/src/lib/squirrel-error-parser.ts
@@ -1,7 +1,7 @@
 // an error that Electron raises when it can't find the installation for the running app
 const squirrelMissingError = /^Can not find Squirrel$/
 
-// an error that occurs when Squirrel
+// an error that occurs when Squirrel isn't able to reach the update server
 const squirrelNetworkError = /System\.Net\.WebException: The remote name could not be resolved: 'central\.github\.com'/
 
 export function parseError(error: Error): Error | null {
