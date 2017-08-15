5cbbfcd1f43799c82481f3d58f61dbf0e64509ea
joshaber <joshaber@gmail.com>
2017-08-15 14:12:31 -0400

    Flesh out the list of Windows shells

--- app/src/lib/shells/win32.ts
+++ app/src/lib/shells/win32.ts
@@ -2,16 +2,30 @@ import { spawn } from 'child_process'
 
 export enum Shell {
   Cmd = 'cmd',
+  PowerShell = 'PowerShell',
+  GitBash = 'Git Bash',
 }
 
 export const Default = Shell.Cmd
 
 export function parse(label: string): Shell {
+  if (label === Shell.Cmd) {
+    return Shell.Cmd
+  }
+
+  if (label === Shell.PowerShell) {
+    return Shell.PowerShell
+  }
+
+  if (label === Shell.GitBash) {
+    return Shell.GitBash
+  }
+
   return Default
 }
 
 export async function getAvailableShells(): Promise<ReadonlyArray<Shell>> {
-  return [Shell.Cmd]
+  return [Shell.Cmd, Shell.PowerShell, Shell.GitBash]
 }
 
 export async function launch(shell: Shell, path: string): Promise<void> {
