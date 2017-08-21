b8b6589c22a7481197ae45e5fd3ef6b8c8c4a6c5
Jed Fox <git@twopointzero.us>
2017-08-21 15:37:05 -0400

    Always use eslint_d

--- script/eslint.ts
+++ script/eslint.ts
@@ -1,7 +1,14 @@
 #!/usr/bin/env ts-node
 
-import ChildProcess = require('child_process')
-import Path = require('path')
+interface Client {
+  // there’s other stuff; I’m not including it
+  // because it’s not needed here.
+  // If you want to add to it, here’s the source:
+  // https://github.com/mantoni/eslint_d.js/blob/v5.1.0/lib/client.js
+  lint(args: string[], text?: string): void
+}
+
+const client: Client = require('eslint_d/lib/client')
 
 const ESLINT_ARGS = [
   '--cache',
@@ -11,17 +18,5 @@ const ESLINT_ARGS = [
   './app/{src,typings,test}/**/*.{j,t}s?(x)',
   ...process.argv.slice(2),
 ]
-const opts = {
-  stdio: 'inherit',
-}
 
-if (process.env.CI) {
-  ChildProcess.spawn(
-    Path.resolve(__dirname, '..', 'node_modules', '.bin', 'eslint'),
-    ESLINT_ARGS,
-    opts
-  )
-} else {
-  console.log('> Spinning up eslint_d\n')
-  require('eslint_d/lib/client').lint(ESLINT_ARGS)
-}
+client.lint(ESLINT_ARGS)
