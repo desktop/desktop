d16eb06cc01429e4f7b5347ccc4c8a9df1cd6859
joshaber <joshaber@gmail.com>
2017-09-05 09:47:58 -0400

    But actually install

--- app/src/lib/git/lfs.ts
+++ app/src/lib/git/lfs.ts
@@ -2,5 +2,9 @@ import { git } from './core'
 
 /** Install the global LFS filters. */
 export function installGlobalLFSFilters() {
-  return git(['lfs', '--skip-repo'], __dirname, 'installGlobalLFSFilter')
+  return git(
+    ['lfs', 'install', '--skip-repo'],
+    __dirname,
+    'installGlobalLFSFilter'
+  )
 }
