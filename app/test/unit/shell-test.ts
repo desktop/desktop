840616cfa91d94bfa8ae0df578f05e81759e06f9
Brendan Forster <brendan@github.com>
2017-09-07 12:41:06 +1000

    flatten test layout

--- app/test/unit/shell-test.ts
+++ app/test/unit/shell-test.ts
@@ -3,20 +3,18 @@ const expect = chai.expect
 
 import { getActiveCodePage } from '../../src/lib/shell'
 
-describe('shell', () => {
-  describe('getActiveCodePage', () => {
-    if (process.platform === 'win32') {
-      it('can resolve the current code page on Windows', async () => {
-        const codePage = await getActiveCodePage()
-        expect(codePage).to.be.greaterThan(0)
-      })
-    }
+describe('shell/getActiveCodePage', () => {
+  if (process.platform === 'win32') {
+    it('can resolve the current code page on Windows', async () => {
+      const codePage = await getActiveCodePage()
+      expect(codePage).to.be.greaterThan(0)
+    })
+  }
 
-    if (process.platform !== 'win32') {
-      it('returns null for non-Windows platforms', async () => {
-        const codePage = await getActiveCodePage()
-        expect(codePage).to.be.null
-      })
-    }
-  })
+  if (process.platform !== 'win32') {
+    it('returns null for non-Windows platforms', async () => {
+      const codePage = await getActiveCodePage()
+      expect(codePage).to.be.null
+    })
+  }
 })
