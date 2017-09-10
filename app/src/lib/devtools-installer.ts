6a7a362fd8256a277aad619f66c3f65f3b67c4ac
Jed Fox <git@twopointzero.us>
2017-09-10 10:21:50 -0400

    npm run prettify

--- app/src/lib/devtools-installer.ts
+++ app/src/lib/devtools-installer.ts
@@ -3,14 +3,18 @@ import installExtension, {
   REACT_PERF,
 } from 'electron-devtools-installer'
 
-
 if (__DEV__) {
-  const ready = Promise.all([
-    REACT_DEVELOPER_TOOLS,
-    REACT_PERF
-  ].map(id => installExtension(id)))
+  const ready = Promise.all(
+    [REACT_DEVELOPER_TOOLS, REACT_PERF].map(id => installExtension(id))
+  )
 
   ready
-  .then(names => console.log(`[devtools-installer] Installed: ${names.join(', ')}`))
-  .catch(err => console.error(`[devtools-installer] Failed to install devtools extension:\n${err.stack}`))
+    .then(names =>
+      console.log(`[devtools-installer] Installed: ${names.join(', ')}`)
+    )
+    .catch(err =>
+      console.error(
+        `[devtools-installer] Failed to install devtools extension:\n${err.stack}`
+      )
+    )
 }
