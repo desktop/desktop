7562e0c45822ac7d1b933e639bfa6dba1220f466
Brendan Forster <brendan@github.com>
2017-09-13 21:37:08 +0930

    switch back to using rm and save the git invocations

--- app/src/lib/git/rm.ts
+++ app/src/lib/git/rm.ts
@@ -1,6 +1,5 @@
+import { git } from './core'
 import { Repository } from '../../models/repository'
-import { getStatus } from './status'
-import { unstageFiles } from './update-index'
 
 /**
  * Remove all files from the index
@@ -8,8 +7,14 @@ import { unstageFiles } from './update-index'
  * @param repository the repository to update
  */
 export async function unstageAllFiles(repository: Repository): Promise<void> {
-  const result = await getStatus(repository)
-  if (result) {
-    await unstageFiles(repository, result.workingDirectory.files)
-  }
+  await git(
+    // these flags are important:
+    // --cached to only remove files from the index
+    // -r       to recursively remove files, in case files are in folders
+    // -f       to ignore differences between working directory and index
+    //          which will block this
+    ['rm', '--cached', '-r', '-f', '.'],
+    repository.path,
+    'unstageAllFiles'
+  )
 }
