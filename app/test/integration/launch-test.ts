ccc1009ba9d072a12a08ce737e5af8f14e69e43c
Jed Fox <git@twopointzero.us>
2017-09-15 08:31:28 -0400

    Revert changes to launch-test.ts

--- app/test/integration/launch-test.ts
+++ app/test/integration/launch-test.ts
@@ -32,9 +32,11 @@ describe('App', function(this: any) {
       path: appPath,
       args: [path.join(__dirname, '..', '..', '..', 'out')],
     })
-    return app.start().then(() => {
-      chaiAsPromised.transferPromiseness = app.transferPromiseness
-    })
+    return app.start()
+  })
+  
+  beforeEach(function() {
+    chaiAsPromised.transferPromiseness = app.transferPromiseness
   })
 
   afterEach(function() {
