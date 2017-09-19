46c941c72810d0921a3a6307e0df435e4d9993e8
joshaber <joshaber@gmail.com>
2017-09-19 15:34:40 -0400

    Don't check percentage

--- app/test/unit/progress/lfs-test.ts
+++ app/test/unit/progress/lfs-test.ts
@@ -12,11 +12,6 @@ describe('GitLFSProgressParser', () => {
     it('understands valid lines', () => {
       let result = parser.parse('download 1/2 5/300 my cool image.jpg')
       expect(result.kind).to.equal('progress')
-      expect(result.percent).to.be.closeTo(5 / 300, 0.01)
-
-      result = parser.parse('checkout 2/100 20/500 funny.gif')
-      expect(result.kind).to.equal('progress')
-      expect(result.percent).to.be.closeTo(20 / 500, 0.01)
     })
 
     it("ignores lines it doesn't understand", () => {
