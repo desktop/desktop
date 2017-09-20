d0b42c0e365ea217179731ff5ac3356e514bae63
joshaber <joshaber@gmail.com>
2017-09-20 15:34:52 -0400

    Extract the merge tool function into `config`

--- app/src/lib/git/config.ts
+++ app/src/lib/git/config.ts
@@ -74,3 +74,22 @@ export async function getGlobalConfigPath(): Promise<string | null> {
 
   return path[1]
 }
+
+export interface IMergeTool {
+  /** The name of the configured merge tool. */
+  readonly name: string
+
+  /** The command to run for the merge tool. */
+  readonly command: string | null
+}
+
+/** Get the configured merge tool. */
+export async function getMergeTool(): Promise<IMergeTool | null> {
+  const name = await getGlobalConfigValue('merge.tool')
+  if (name) {
+    const command = await getGlobalConfigValue(`mergetool.${name}.cmd`)
+    return { name, command }
+  } else {
+    return null
+  }
+}
