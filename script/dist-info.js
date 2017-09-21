6ebb9ad18f210fba90fa23918fc34a978ac726fb
Brendan Forster <brendan@github.com>
2017-09-21 16:56:41 +1000

    extract getSHA function so it can be used elsewhere

--- script/dist-info.js
+++ script/dist-info.js
@@ -159,6 +159,42 @@ function shouldMakeDelta() {
   return channelsWithDeltas.indexOf(getReleaseChannel()) > -1
 }
 
+/**
+ * Attempt to dereference the given ref without requiring a Git environment
+ * to be present. Note that this method will not be able to dereference packed
+ * refs but should suffice for simple refs like 'HEAD'.
+ *
+ * Will throw an error for unborn HEAD.
+ *
+ * @param {string} gitDir The path to the Git repository's .git directory
+ * @param {string} ref    A qualified git ref such as 'HEAD' or 'refs/heads/master'
+ */
+function revParse(gitDir, ref) {
+  const refPath = path.join(gitDir, ref)
+  const refContents = fs.readFileSync(refPath)
+  const refRe = /^([a-f0-9]{40})|(?:ref: (refs\/.*))$/m
+  const refMatch = refRe.exec(refContents)
+
+  if (!refMatch) {
+    throw new Error(
+      `Could not de-reference HEAD to SHA, invalid ref in ${refPath}: ${refContents}`
+    )
+  }
+
+  return refMatch[1] || revParse(gitDir, refMatch[2])
+}
+
+function getSHA() {
+  // CircleCI does some funny stuff where HEAD points to an packed ref, but
+  // luckily it gives us the SHA we want in the environment.
+  const circleSHA = process.env.CIRCLE_SHA1
+  if (circleSHA) {
+    return circleSHA
+  }
+
+  return revParse(path.resolve(__dirname, '../.git'), 'HEAD')
+}
+
 module.exports = {
   getDistPath,
   getProductName,
@@ -177,10 +213,12 @@ module.exports = {
   getWindowsIdentifierName,
   getBundleSizes,
   getReleaseChannel,
+  getReleaseSHA,
   getUpdatesURL,
   getWindowsDeltaNugetPackageName,
   getWindowsDeltaNugetPackagePath,
   shouldMakeDelta,
   getReleaseBranchName,
   getExecutableName,
+  getSHA,
 }
