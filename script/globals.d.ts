cda26123e1ee4c7dd4f0753edf5fe33a89af17ed
Brendan Forster <brendan@github.com>
2017-09-23 13:20:07 +1000

    add a global declaration for the distInfo module

--- script/globals.d.ts
+++ script/globals.d.ts
@@ -28,3 +28,31 @@ type LegalEagle = (
   options: LegalEagleOptions,
   callback: (error: Error | null, summary: LicenseLookup) => void
 ) => void
+
+type DistInfo = {
+  getDistPath: () => string
+  getProductName: () => string
+  getCompanyName: () => string
+  getVersion: () => string
+  getOSXZipName: () => string
+  getOSXZipPath: () => string
+  getWindowsInstallerName: () => string
+  getWindowsInstallerPath: () => string
+  getWindowsStandaloneName: () => string
+  getWindowsStandalonePath: () => string
+  getWindowsFullNugetPackageName: () => string
+  getWindowsFullNugetPackagePath: () => string
+  getBundleID: () => string
+  getUserDataPath: () => string
+  getWindowsIdentifierName: () => string
+  getBundleSizes: () => { rendererSize: number; mainSize: number }
+  getReleaseChannel: () => string | null
+  getReleaseSHA: () => string | null
+  getUpdatesURL: () => string
+  getWindowsDeltaNugetPackageName: () => string
+  getWindowsDeltaNugetPackagePath: () => string
+  shouldMakeDelta: () => boolean
+  getReleaseBranchName: () => string
+  getExecutableName: () => string
+  getSHA: () => string
+}
