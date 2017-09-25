bcc5ed0d956fc4017d62a1c940da7bfc68d52d85
joshaber <joshaber@gmail.com>
2017-09-25 11:44:15 -0400

    Enable preview features from localStorage too

--- app/src/lib/feature-flag.ts
+++ app/src/lib/feature-flag.ts
@@ -1,3 +1,5 @@
+const EnablePreviewFeaturesKey = 'enable-preview-features'
+
 /**
  * Enables the application to opt-in for preview features based on runtime
  * checks. This is backed by the GITHUB_DESKTOP_PREVIEW_FEATURES environment
@@ -9,6 +11,16 @@ export function enablePreviewFeatures(): boolean {
   }
 
   if (process.env.GITHUB_DESKTOP_PREVIEW_FEATURES) {
+    return true
+  }
+
+  const enableFeatures = localStorage.getItem(EnablePreviewFeaturesKey)
+  const enableFeaturesValue = enableFeatures && parseInt(enableFeatures, 10)
+  if (
+    enableFeaturesValue &&
+    !isNaN(enableFeaturesValue) &&
+    enableFeaturesValue > 0
+  ) {
     return true
   }
 
