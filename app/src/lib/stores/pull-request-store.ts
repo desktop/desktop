95e6493cbc3666d021ba588c53ceaefc49cf16c6
iAmWillShepherd <williamrshepherd@gmail.com>
2017-09-25 12:28:04 -0500

    Add code to remove old PRs

--- app/src/lib/stores/pull-request-store.ts
+++ app/src/lib/stores/pull-request-store.ts
@@ -12,10 +12,17 @@ export class PullRequestStore {
     this.db = db
   }
 
-  public async fetchPullRequests(repository: GitHubRepository, account: Account) {
+  public async fetchPullRequests(
+    repository: GitHubRepository,
+    account: Account
+  ) {
     const api = API.fromAccount(account)
 
-    const prs = await api.fetchPullRequests(repository.owner.login, repository.name, 'open')
+    const prs = await api.fetchPullRequests(
+      repository.owner.login,
+      repository.name,
+      'open'
+    )
 
     this.writePullRequests(prs, repository)
   }
@@ -30,10 +37,46 @@ export class PullRequestStore {
       fatalError(
         "Cannot store pull requests for a repository that hasn't been inserted into the database!"
       )
+
+      return
     }
 
-    const db = this.db
+    const prTable = this.db.pullRequests
+    const dbPRs = await prTable.toArray()
+    const apiPRs = pullRequests as Array<IAPIPullRequest>
+    const sorterdDbPRs = dbPRs.sort()
+    const sortedApiPRs = apiPRs.sort()
+    const itemsToDelete: Array<number> = []
 
-    //Diff database or overwrite from API
+    //Take the relative complement of A and B where B is the db and A is pullRequests argument
+    for (let i = 0; i < sorterdDbPRs.length; i++) {
+      if (sortedApiPRs[i].number !== sorterdDbPRs[i].number) {
+        itemsToDelete.push(sorterdDbPRs[i].number)
+      }
+    }
+
+    function findPullRequestByNumber(
+      repositoryId: number,
+      pullRequestNumber: number
+    ) {
+      return prTable
+        .where('[repo_id+number]')
+        .equals([repositoryId, pullRequestNumber])
+        .limit(1)
+        .first()
+    }
+
+    await this.db.transaction('rw', prTable, function*() {
+      for (const pullRequestNumber of itemsToDelete) {
+        const prToDelete = yield findPullRequestByNumber(
+          repoId,
+          pullRequestNumber
+        )
+
+        if (prToDelete) {
+          yield prTable.delete(prToDelete.id)
+        }
+      }
+    })
   }
 }
