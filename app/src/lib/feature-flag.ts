2b402db334d47100a1139d98af6d026f163d42b6
joshaber <joshaber@gmail.com>
2017-09-25 11:57:56 -0400

    Nevermind, just use a flag

--- app/src/lib/feature-flag.ts
+++ app/src/lib/feature-flag.ts
@@ -1,4 +1,4 @@
-const EnablePreviewFeaturesKey = 'enable-preview-features'
+const Disable = false
 
 /**
  * Enables the application to opt-in for preview features based on runtime
@@ -6,21 +6,15 @@ const EnablePreviewFeaturesKey = 'enable-preview-features'
  * variable, which is checked for non-development environments.
  */
 export function enablePreviewFeatures(): boolean {
+  if (Disable) {
+    return false
+  }
+
   if (__DEV__) {
     return true
   }
 
   if (process.env.GITHUB_DESKTOP_PREVIEW_FEATURES) {
-    return true
-  }
-
-  const enableFeatures = localStorage.getItem(EnablePreviewFeaturesKey)
-  const enableFeaturesValue = enableFeatures && parseInt(enableFeatures, 10)
-  if (
-    enableFeaturesValue &&
-    !isNaN(enableFeaturesValue) &&
-    enableFeaturesValue > 0
-  ) {
     return true
   }
 
