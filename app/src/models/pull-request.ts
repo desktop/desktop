99ae162c6b4530f1f2deef807ded25a4463c0129
joshaber <joshaber@gmail.com>
2017-09-29 10:21:27 -0400

    Add PullRequest model

--- app/src/models/pull-request.ts
+++ app/src/models/pull-request.ts
@@ -5,3 +5,22 @@ export interface IPullRequest extends IAPIPullRequest {
   readonly status: IAPIRefStatus
   readonly created: Date
 }
+
+export class PullRequest {
+  public readonly created: Date
+  public readonly status: IAPIRefStatus
+  public readonly title: string
+  public readonly number: number
+
+  public constructor(
+    created: Date,
+    status: IAPIRefStatus,
+    title: string,
+    number_: number
+  ) {
+    this.created = created
+    this.status = status
+    this.title = title
+    this.number = number_
+  }
+}
