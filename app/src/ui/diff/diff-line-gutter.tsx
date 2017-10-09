3019f27a5d9c5ea4fea0cb1bb70aab5324a04617
Markus Olsson <j.markus.olsson@gmail.com>
2017-10-09 17:50:28 +0200

    Use state rather than manipulate the DOM directly

--- app/src/ui/diff/diff-line-gutter.tsx
+++ app/src/ui/diff/diff-line-gutter.tsx
@@ -65,6 +65,21 @@ interface IDiffGutterProps {
   readonly onMouseMove: (index: number) => void
 }
 
+interface IDiffGutterState {
+  /**
+   * Whether or not the diff line gutter should render as hovered,
+   * i.e. highlighted. This is used when moused over directly or
+   * when the hunk that this line is part of is hovered.
+   */
+  readonly hover: boolean
+
+  /**
+   * Whether or not the diff line gutter should render that it's
+   * selected, i.e. included for commit.
+   */
+  readonly selected: boolean
+}
+
 /**
  * Detect if mouse cursor is within the range
  */
@@ -83,8 +98,20 @@ function isMouseCursorNearEdge(ev: MouseEvent): boolean {
 }
 
 /** The gutter for a diff's line. */
-export class DiffLineGutter extends React.Component<IDiffGutterProps, {}> {
+export class DiffLineGutter extends React.Component<
+  IDiffGutterProps,
+  IDiffGutterState
+> {
   private elem_?: HTMLSpanElement
+
+  public constructor(props: IDiffGutterProps) {
+    super(props)
+
+    this.state = {
+      hover: false,
+      selected: this.props.isIncluded,
+    }
+  }
 
   /**
    * Compute the width for the current element
@@ -100,34 +127,26 @@ export class DiffLineGutter extends React.Component<IDiffGutterProps, {}> {
    * Indicate whether the current gutter element is selected
    */
   public isIncluded(): boolean {
-    return this.props.line.isIncludeableLine() && this.props.isIncluded
+    return this.props.line.isIncludeableLine() && this.state.selected
   }
 
   /**
    * Set (or unset) the hover styling of the diff gutter
    */
-  public setHover(visible: boolean) {
+  public setHover(hover: boolean) {
     // only show the hover effect if the line isn't context
     if (!this.props.line.isIncludeableLine()) {
       return
     }
 
-    if (visible) {
-      this.setClass(hoverCssClass)
-    } else {
-      this.unsetClass(hoverCssClass)
-    }
+    this.setState({ hover })
   }
 
   /**
    * Set (or unset) the selected styling of the diff gutter
    */
-  public setSelected(visible: boolean) {
-    if (visible) {
-      this.setClass(selectedLineClass)
-    } else {
-      this.unsetClass(selectedLineClass)
-    }
+  public setSelected(selected: boolean) {
+    this.setState({ selected })
   }
 
   private getLineClassName(): string {
@@ -149,10 +168,17 @@ export class DiffLineGutter extends React.Component<IDiffGutterProps, {}> {
   private getLineClass(): string {
     const lineClass = this.getLineClassName()
     const selectedClass = this.isIncluded() ? selectedLineClass : null
+    const hoverClass = this.state.hover ? hoverCssClass : null
 
-    return classNames('diff-line-gutter', lineClass, selectedClass, {
-      'read-only': this.props.readOnly,
-    })
+    return classNames(
+      'diff-line-gutter',
+      lineClass,
+      selectedClass,
+      hoverClass,
+      {
+        'read-only': this.props.readOnly,
+      }
+    )
   }
 
   private updateHoverState(isRangeSelection: boolean, isActive: boolean) {
@@ -165,18 +191,6 @@ export class DiffLineGutter extends React.Component<IDiffGutterProps, {}> {
       this.props.updateRangeHoverState(range.start, range.end, isActive)
     } else {
       this.setHover(isActive)
-    }
-  }
-
-  private setClass(cssClass: string) {
-    if (this.elem_) {
-      this.elem_.classList.add(cssClass)
-    }
-  }
-
-  private unsetClass(cssClass: string) {
-    if (this.elem_) {
-      this.elem_.classList.remove(cssClass)
     }
   }
 
