a47b634e0f2e509dfa9e112007aec26ee2ff2af0
Markus Olsson <j.markus.olsson@gmail.com>
2017-10-19 19:18:33 +0200

    Get some of those sweet globals

--- app/test/globals.ts
+++ app/test/globals.ts
@@ -1,6 +1,4 @@
-// This shouldn't be necessary, but without this CI fails on Windows. Seems to
-// be a bug in TS itself or ts-node.
-/// <reference path="../../node_modules/@types/node/index.d.ts" />
+/// <reference path="../src/lib/globals.d.ts" />
 
 import 'mocha'
 import { use } from 'chai'
