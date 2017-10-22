b5876f3fb2338a7a743760f27547ae3f572d7b2f
Jed Fox <git@twopointzero.us>
2017-10-22 06:31:14 -0400

    📖

--- script/eslint.ts
+++ script/eslint.ts
@@ -2,11 +2,22 @@
 
 import chalk from 'chalk'
 
+/**
+ * A partial `eslint_d` client interface.
+ * It currently only includes the methods actually used by this file.
+ */
 interface IClient {
   // there’s other stuff; I’m not including it
   // because it’s not needed here.
   // If you want to add to it, here’s the source:
   // https://github.com/mantoni/eslint_d.js/blob/v5.1.0/lib/client.js
+  /**
+   * Lint a project using the ESLint server instance,
+   * or start a new instance if one is not available.
+   * @param args The arguments to pass to ESLint.
+   *             Interpreted as if passed after `eslint` on the command line.
+   * @param text The text to lint, if no files are passed as arguments.
+   */
   lint(args: string[], text?: string): void
 }
 
@@ -24,6 +35,7 @@ const ESLINT_ARGS = [
 client.lint(ESLINT_ARGS)
 
 type ProcessOnExit = (cb: (code: number) => void) => void
+/** HACK: allow process.on('exit') to be called with a `number` */
 const onExit = process.on.bind(process, 'exit') as ProcessOnExit
 
 onExit(code => {
