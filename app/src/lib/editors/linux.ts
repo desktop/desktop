716a112b8d60d8b321a0894d096cd1adcef94795
andrea <vanpivix@gmail.com>
2017-10-23 22:50:15 +0200

    Add Atom support on Linux

--- app/src/lib/editors/linux.ts
+++ app/src/lib/editors/linux.ts
@@ -3,10 +3,15 @@ import { pathExists } from '../file-system'
 import { assertNever } from '../fatal-error'
 
 export enum ExternalEditor {
+  Atom = 'Atom',
   VisualStudioCode = 'Visual Studio Code',
 }
 
 export function parse(label: string): ExternalEditor | null {
+  if (label === ExternalEditor.Atom) {
+    return ExternalEditor.Atom
+  }
+
   if (label === ExternalEditor.VisualStudioCode) {
     return ExternalEditor.VisualStudioCode
   }
@@ -22,6 +27,8 @@ function getEditorPath(editor: ExternalEditor): Promise<string | null> {
   switch (editor) {
     case ExternalEditor.VisualStudioCode:
       return getPathIfAvailable('/usr/bin/code')
+    case ExternalEditor.Atom:
+      return getPathIfAvailable('/usr/bin/atom')
     default:
       return assertNever(editor, `Unknown editor: ${editor}`)
   }
@@ -32,9 +39,14 @@ export async function getAvailableEditors(): Promise<
 > {
   const results: Array<IFoundEditor<ExternalEditor>> = []
 
-  const [codePath] = await Promise.all([
+  const [atomPath, codePath] = await Promise.all([
+    getEditorPath(ExternalEditor.Atom),
     getEditorPath(ExternalEditor.VisualStudioCode),
   ])
+
+  if (atomPath) {
+    results.push({ editor: ExternalEditor.Atom, path: atomPath })
+  }
 
   if (codePath) {
     results.push({ editor: ExternalEditor.VisualStudioCode, path: codePath })
