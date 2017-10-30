636644df6802e9716a481177a1aa6e2a8ef4f763
Markus Olsson <j.markus.olsson@gmail.com>
2017-10-30 22:25:46 +0100

    :book:

--- app/src/lib/git/show.ts
+++ app/src/lib/git/show.ts
@@ -41,6 +41,28 @@ export async function getBlobContents(
   return Buffer.from(blobContents.stdout, 'binary')
 }
 
+/**
+ * Retrieve some or all binary contents of a blob from the repository
+ * at a given reference, commit, or tree. This is almost identical
+ * to the getBlobContents method except that it supports only reading
+ * a maximum number of bytes.
+ *
+ * Returns a promise that will produce a Buffer instance containing
+ * the binary contents of the blob or an error if the file doesn't
+ * exists in the given revision.
+ *
+ * @param repository - The repository from where to read the blob
+ *
+ * @param commitish  - A commit SHA or some other identifier that
+ *                     ultimately dereferences to a commit/tree.
+ *
+ * @param path       - The file path, relative to the repository
+ *                     root from where to read the blob contents
+ *
+ * @param length     - The maximum number of bytes to read from
+ *                     the blob. Note that the number of bytes
+ *                     returned may always be less than this number.
+ */
 export async function getPartialBlobContents(
   repository: Repository,
   commitish: string,
