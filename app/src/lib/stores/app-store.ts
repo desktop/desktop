43a6882a14e03e7e799474236816fd835d7a71f3
Brendan Forster <github@brendanforster.com>
2017-11-29 08:37:30 +1100

    lint fix

--- app/src/lib/stores/app-store.ts
+++ app/src/lib/stores/app-store.ts
@@ -2191,7 +2191,9 @@ export class AppStore {
         if (timeSinceFetch < twoMinutes) {
           const timeInSeconds = Math.floor(timeSinceFetch / 1000)
           console.debug(
-            `skipping background fetch as repository was fetched ${timeInSeconds}s ago`
+            `skipping background fetch as repository was fetched ${
+              timeInSeconds
+            }s ago`
           )
           return Promise.resolve()
         }
