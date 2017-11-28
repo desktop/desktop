f6a4427387cb68c29d024fd78902a5788e20b1e3
joshaber <joshaber@gmail.com>
2017-11-28 11:45:29 -0500

    Use eslint's CLI engine
    Mostly written by @j-f1 :smile:

--- script/eslint.ts
+++ script/eslint.ts
@@ -2,28 +2,33 @@
 
 import * as Path from 'path'
 import chalk from 'chalk'
-import { execFile } from 'child_process'
 
-const args = [
-  '--cache',
-  '--rulesdir=./eslint-rules',
+const { CLIEngine } = require('eslint')
+
+const shouldFix = process.argv.indexOf('--fix') > -1
+const eslint = new CLIEngine({
+  cache: true,
+  cwd: Path.dirname(__dirname),
+  fix: shouldFix,
+  rulePaths: [Path.join(__dirname, '..', 'eslint-rules')],
+})
+
+const report = eslint.executeOnFiles([
   './{script,eslint-rules}/**/*.{j,t}s?(x)',
   './tslint-rules/**/*.ts',
   './app/*.js',
   './app/{src,typings,test}/**/*.{j,t}s?(x)',
-  ...process.argv.slice(2),
-]
+])
 
-const root = Path.join(__dirname, '..')
-const eslintPath = Path.join(root, 'node_modules', '.bin', 'eslint')
-const child = execFile(eslintPath, args, { cwd: root }, (err, stdout) => {
-  console.log(stdout)
-})
+if (shouldFix) {
+  CLIEngine.outputFixes(report)
+}
 
-child.on('exit', code => {
-  if (code && args.indexOf('--fix') === -1) {
-    console.error(
-      chalk`{bold.green → To fix some of these errors, run {underline yarn eslint:fix}}`
-    )
-  }
-})
+console.log(eslint.getFormatter()(report.results))
+
+if (report.errorCount > 0) {
+  process.exitCode = 1
+  console.error(
+    chalk`{bold.green → To fix some of these errors, run {underline yarn eslint:fix}}`
+  )
+}
