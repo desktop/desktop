519b0250ea6719741e88208fb3d7c88d5c6a6f83
say25 <say25@cornell.edu>
2017-12-03 19:05:19 -0600

    Relabel

--- app/src/ui/about/about.tsx
+++ app/src/ui/about/about.tsx
@@ -114,7 +114,7 @@ export class About extends React.Component<IAboutProps, IAboutState> {
       case UpdateStatus.UpdateReady:
         return (
           <Row>
-            <Button onClick={this.onQuitAndInstall}>Install Update</Button>
+            <Button onClick={this.onQuitAndInstall}>Quit and Install Update</Button>
           </Row>
         )
       case UpdateStatus.UpdateNotAvailable:
