26d3a077e51fe80ec8ae8c9088df2ebdf39703d4
Brendan Forster <github@brendanforster.com>
2017-12-06 11:37:11 +1100

    tidy up code

--- app/src/ui/lib/avatar-in-memory-cache.ts
+++ app/src/ui/lib/avatar-in-memory-cache.ts
@@ -1,6 +1,6 @@
 import * as crypto from 'crypto'
 
-const inMemoryCache = new Map<string, string>()
+const inMemoryCache = new Map<string, string | null>()
 
 function generateGravatarUrl(email: string): string {
   const input = email.trim().toLowerCase()
@@ -12,12 +12,6 @@ function generateGravatarUrl(email: string): string {
   return `https://www.gravatar.com/avatar/${hash}?s=200`
 }
 
-async function convertResponseToDataUrl(response: Response): Promise<string> {
-  const blob = await response.blob()
-  const objectURL = URL.createObjectURL(blob)
-  return objectURL
-}
-
 const myHeaders = new Headers()
 
 const defaultInit: RequestInit = {
@@ -27,46 +21,51 @@ const defaultInit: RequestInit = {
   cache: 'default',
 }
 
+async function fetchAndCache(requestUrl: string) {
+  let url: string | null = null
+
+  try {
+    const response = await fetch(requestUrl, defaultInit)
+    if (response.ok) {
+      const blob = await response.blob()
+      url = URL.createObjectURL(blob)
+    }
+  } catch {
+    // catch and ignore any network errors
+  }
+
+  inMemoryCache.set(requestUrl, url)
+
+  return url
+}
+
 export async function fetchAvatar(
   avatarURL: string,
   email: string
 ): Promise<string | null> {
-  // first off, let's try and fetch their account avatar
+  // first, lookup a cached value for the avatar
   const cachedAccountAvatar = inMemoryCache.get(avatarURL)
-  if (cachedAccountAvatar) {
+  if (cachedAccountAvatar !== undefined) {
     return cachedAccountAvatar
   }
 
-  try {
-    const avatarResponse = await fetch(avatarURL, defaultInit)
-    if (avatarResponse.ok) {
-      const url = await convertResponseToDataUrl(avatarResponse)
-      inMemoryCache.set(avatarURL, url)
-      return url
-    }
-  } catch {
-    // ignore any potential network errors
+  const accountAvatar = await fetchAndCache(avatarURL)
+  if (accountAvatar) {
+    return accountAvatar
   }
 
   // if that doesn't work, let's use their email address to generate a Gravatar
   // URL and try to resolve that
 
   const gravatarURL = generateGravatarUrl(email)
-
   const cachedGravatarAvatar = inMemoryCache.get(gravatarURL)
   if (cachedGravatarAvatar) {
     return cachedGravatarAvatar
   }
 
-  try {
-    const gravatarResponse = await fetch(gravatarURL, defaultInit)
-    if (gravatarResponse.ok) {
-      const url = await convertResponseToDataUrl(gravatarResponse)
-      inMemoryCache.set(gravatarURL, url)
-      return url
-    }
-  } catch {
-    // ignore any potential network errors
+  const gravatarAvatar = await fetchAndCache(gravatarURL)
+  if (gravatarAvatar) {
+    return gravatarAvatar
   }
 
   return null
