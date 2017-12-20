469520a3be93042500ad327c7bd16094c57d79ab
Brendan Forster <github@brendanforster.com>
2017-12-20 17:23:49 +1030

    this module doesn't exist any more

--- app/src/ui/install-globals.ts
+++ app/src/ui/install-globals.ts
@@ -3,10 +3,4 @@ export function installDevGlobals() {
   const g: any = global
   // Expose GitPerf as a global so it can be started.
   g.GitPerf = require('./lib/git-perf')
-
-  // Expose Perf on the window so that the React Perf dev tool extension can
-  // find it.
-  const w: any = window
-  const Perf = require('react-addons-perf')
-  w.Perf = Perf
 }
