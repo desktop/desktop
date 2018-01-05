4b942bbdc023f4a7a17b9e1ac3bec04a19ceef4e
Brendan Forster <github@brendanforster.com>
2018-01-05 14:32:46 -0400

    refined test

--- app/test/unit/git/submodule-test.ts
+++ app/test/unit/git/submodule-test.ts
@@ -13,8 +13,9 @@ describe('git/submodule', () => {
       const repository = new Repository(testRepoPath, -1, null, false)
       const result = await listSubmodules(repository)
       expect(result.length).to.equal(1)
-
+      expect(result[0].sha).to.equal('c59617b65080863c4ca72c1f191fa1b423b92223')
       expect(result[0].path).to.equal('foo/submodule')
+      expect(result[0].nearestTag).to.equal('first-tag~2')
     })
   })
 })
