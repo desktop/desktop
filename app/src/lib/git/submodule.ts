b9a93e664a92b8f6a9c824632626cd29e477cb83
Brendan Forster <github@brendanforster.com>
2018-01-05 10:03:44 -0400

    add some docs

--- app/src/lib/git/submodule.ts
+++ app/src/lib/git/submodule.ts
@@ -5,6 +5,9 @@ import { SubmoduleEntry } from '../../models/submodule'
 export async function listSubmodules(
   repository: Repository
 ): Promise<ReadonlyArray<SubmoduleEntry>> {
+  // We don't recurse when listing submodules here because we don't have a good
+  // story about managing these currently. So for now we're only listing
+  // changes to the top-level submodules to be consistent with `git status`
   const result = await git(
     ['submodule', 'status', '--'],
     repository.path,
@@ -20,11 +23,29 @@ export async function listSubmodules(
   const submodules = new Array<SubmoduleEntry>()
 
   for (const entry of entries) {
-    // entries are of the format
+    // entries are of the format:
+    //  1eaabe34fc6f486367a176207420378f587d3b48 git (v2.16.0-rc0)
+    //
+    // first character:
+    //   - " " if no change
+    //   - "-" if the submodule is not initialized
+    //   - "+" if the currently checked out submodule commit does not match the SHA-1 found in the index of the containing repository
+    //   - "U" if the submodule has merge conflicts
+    //
+    // then the 40-character SHA represents the current commit
+    //
+    // then the path to the submodule
+    //
+    // then the output of `git describe` for the submodule in braces:
+    //   - (tag) if the current commit is associated with a tag
+    //   - ({tag}-{count}-{short-sha}) if the current commit does not have a tag
+    //     {count} is the number of commits ahead of the tag
+    //     {shortsha} is the abbreviated SHA of the current commit
+    //
+    // TODO: what if there are no tags in the submodule?
     const sha = entry[0].substr(1)
     const path = entry[1]
 
-    // TODO: what if there's no tag here?
     const tagInBraces = entry[2]
     const nearestTag = entry[2].substr(1, tagInBraces.length - 2)
     submodules.push(new SubmoduleEntry(sha, path, nearestTag))
