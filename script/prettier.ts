7e4443baffd76c6095498c4c476d21e8300ac11e
Brendan Forster <github@brendanforster.com>
2018-01-08 08:35:44 -0400

    extract prettier invocation to script for better control

---
+++ script/prettier.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env ts-node
+
+import * as Path from 'path'
+import chalk from 'chalk'
+import { spawnSync } from 'child_process'
+
+const root = Path.dirname(__dirname)
+
+const prettier = Path.join(root, 'node_modules', '.bin', 'prettier')
+
+const result = spawnSync(prettier, ['**/*.{md,scss}', '--list-different'], {
+  cwd: root,
+})
+
+if (result.status > 0) {
+  process.exitCode = result.status
+
+  console.log('These files are not formatted correctly:\n')
+  console.log(result.stdout.toString())
+
+  console.error(
+    chalk`{bold.green → To fix these errors, run {underline yarn lint:fix}}`
+  )
+} else if (result.status < 0) {
+  process.exitCode = result.status
+
+  console.log(`prettier returned an unexpected exit code: ${result.status}.`)
+  console.log(`stdout: '${result.stdout.toString()}'`)
+  console.log(`stderr: '${result.stderr.toString()}'`)
+}
