a90ac03ae6bd50602f7fa7145229ac321f3ddd0e
Brendan Forster <github@brendanforster.com>
2018-01-10 12:22:48 -0400

    wrap every step of our askpass implementation in file logging on Windows

---
+++ app/src/ask-pass/logger.ts
@@ -0,0 +1,20 @@
+import * as Path from 'path'
+import * as Fs from 'fs'
+
+function getLogDirectoryPath() {
+  const appData = process.env.APPDATA
+  const folderName = __DEV__ ? 'GitHub Desktop-dev' : 'GitHub Desktop'
+  const directory = Path.join(appData, folderName, 'logs')
+  return directory
+}
+
+const askPassLogFile = 'ask-pass.log'
+
+export function appendToAskPassLog(message: string) {
+  if (!__WIN32__) {
+    return
+  }
+
+  const file = Path.join(getLogDirectoryPath(), askPassLogFile)
+  Fs.appendFileSync(file, `${message}\n`)
+}
--- app/src/ask-pass/main.ts
+++ app/src/ask-pass/main.ts
@@ -3,11 +3,24 @@
  */
 
 import { responseForPrompt } from './ask-pass'
+import { appendToAskPassLog } from './logger'
 
 const prompt = process.argv[2]
-responseForPrompt(prompt).then(response => {
-  if (response) {
-    process.stdout.write(response)
-    process.stdout.end()
-  }
-})
+
+appendToAskPassLog(`received arguments: ${JSON.stringify(process.argv)}`)
+appendToAskPassLog(`environment variables: ${JSON.stringify(process.env)}`)
+appendToAskPassLog(`received prompt: '${prompt}'`)
+
+responseForPrompt(prompt)
+  .then(response => {
+    if (response) {
+      appendToAskPassLog(`emitting response: ${response.length} characters`)
+      process.stdout.write(response)
+      process.stdout.end()
+    } else {
+      appendToAskPassLog(`skipping response as responseForPrompt returned null`)
+    }
+  })
+  .then(() => {
+    appendToAskPassLog(`exiting...`)
+  })
