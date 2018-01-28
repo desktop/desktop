1646deb9f45f5e48bf9cfb1226fa0c8312b47c6b
Markus Olsson <j.markus.olsson@gmail.com>
2018-01-28 04:40:18 +0100

    Add a component for showing attribution the way dotcom does it

---
+++ app/src/ui/lib/commit-attribution.tsx
@@ -0,0 +1,65 @@
+import { Commit } from '../../models/commit'
+import * as React from 'react'
+import { CommitIdentity } from '../../models/commit-identity'
+import { GitAuthor } from '../../models/git-author'
+
+interface ICommitAttributionProps {
+  readonly commit: Commit
+}
+
+export class CommitAttribution extends React.Component<
+  ICommitAttributionProps,
+  {}
+> {
+  private renderAuthorInline(author: CommitIdentity | GitAuthor) {
+    return <span className="author">{author.name}</span>
+  }
+
+  private renderAuthors(
+    authors: ReadonlyArray<CommitIdentity | GitAuthor>,
+    committerAttribution: boolean
+  ) {
+    if (authors.length === 1) {
+      return (
+        <span className="authors">{this.renderAuthorInline(authors[0])}</span>
+      )
+    } else if (authors.length === 2 && !committerAttribution) {
+      return (
+        <span className="authors">
+          {this.renderAuthorInline(authors[0])}
+          {' and '}
+          {this.renderAuthorInline(authors[1])}
+        </span>
+      )
+    } else {
+      return <span className="authors">{authors.length} people</span>
+    }
+  }
+
+  private renderCommitter(committer: CommitIdentity) {
+    return (
+      <span className="committer">
+        {' and '}
+        {this.renderAuthorInline(committer)}
+        {' committed'}
+      </span>
+    )
+  }
+
+  public render() {
+    const commit = this.props.commit
+    const { author, committer, coAuthors } = commit
+
+    const authors: Array<CommitIdentity | GitAuthor> = [author, ...coAuthors]
+
+    const committerAttribution = !commit.authoredByCommitter
+
+    return (
+      <span>
+        {this.renderAuthors(authors, committerAttribution)}
+        {committerAttribution ? ' authored' : ' committed'}
+        {committerAttribution ? this.renderCommitter(committer) : null}
+      </span>
+    )
+  }
+}
