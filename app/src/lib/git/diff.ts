5de9abe8471b4dec7de683f6836e6ea7f1837f55
Brendan Forster <github@brendanforster.com>
2018-01-31 14:02:19 +1100

    pass the old path to Git so that it will apply rename checks

--- app/src/lib/git/diff.ts
+++ app/src/lib/git/diff.ts
@@ -101,6 +101,10 @@ export async function getCommitDiff(
     file.path,
   ]
 
+  if (file.oldPath) {
+    args.push(file.oldPath)
+  }
+
   const { output } = await spawnAndComplete(
     args,
     repository.path,
