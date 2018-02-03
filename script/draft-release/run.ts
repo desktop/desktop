92af1b1f73cc055fd39387729c19899aa1f6288f
Brendan Forster <github@brendanforster.com>
2018-02-04 10:35:23 +1100

    first part of drafting a release, what's the version number?

---
+++ script/draft-release/run.ts
@@ -0,0 +1,61 @@
+import { spawn } from './spawn'
+import { sort as semverSort, inc, SemVer } from 'semver'
+
+async function getLatestRelease(excludeBetaReleases: boolean): Promise<string> {
+  const allTags = await spawn('git', ['tag'])
+  let releaseTags = allTags
+    .split('\n')
+    .filter(tag => tag.startsWith('release-'))
+    .filter(tag => tag.indexOf('-linux') === -1)
+    .filter(tag => tag.indexOf('-test') === -1)
+
+  if (excludeBetaReleases) {
+    releaseTags = releaseTags.filter(tag => tag.indexOf('-beta') === -1)
+  }
+
+  const releaseVersions = releaseTags.map(tag => tag.substr(8))
+
+  const sortedTags = semverSort(releaseVersions)
+  const latestTag = sortedTags[sortedTags.length - 1]
+
+  if (latestTag instanceof SemVer) {
+    console.log('we have types!?!?!?')
+    return latestTag.raw
+  }
+
+  return latestTag
+}
+
+export async function run(args: ReadonlyArray<string>): Promise<void> {
+  try {
+    await spawn('git', ['diff-index', '--quiet', 'HEAD'])
+  } catch {
+    throw new Error(
+      `There are uncommitted changes in the working directory. Aborting...`
+    )
+  }
+
+  if (args.length === 0) {
+    throw new Error(
+      `You have not specified a channel to draft this release for. Choose one of 'production' or 'beta'`
+    )
+  }
+
+  //
+  // first argument should be the channel
+  const channel = args[0]
+
+  if (channel === 'production') {
+    const latestVersion = await getLatestRelease(true)
+    const nextVersion = inc(latestVersion, 'patch')
+    throw new Error(
+      `Drafting a release from ${latestVersion} which will be ${nextVersion}`
+    )
+  } else if (channel === 'beta') {
+    const latestVersion = await getLatestRelease(false)
+    const nextVersion = inc(latestVersion, 'patch')
+    throw new Error(
+      `Drafting a release from ${latestVersion} which will be ${nextVersion}`
+    )
+  }
+}
