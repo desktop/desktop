d380d279b77c8b62335e296b394d17ffc00c24f5
Brendan Forster <github@brendanforster.com>
2018-02-09 15:11:20 +1100

    refactor into stages

--- script/draft-release/version.ts
+++ script/draft-release/version.ts
@@ -20,45 +20,55 @@ export function getNextVersionNumber(
     throw new Error(`Unable to parse input '${version}' into version`)
   }
 
-  if (channel === 'production') {
-    if (isBetaTag(semanticVersion)) {
+  switch (channel) {
+    case 'production':
+      if (isBetaTag(semanticVersion)) {
+        throw new Error(
+          `Unable to draft production release using beta version '${version}'`
+        )
+      }
+
+      if (isTestTag(semanticVersion)) {
+        throw new Error(
+          `Unable to draft production release using test version '${version}'`
+        )
+      }
+
+      const nextVersion = inc(version, 'patch')
+      if (nextVersion == null) {
+        throw new Error(
+          `Unable to increment next production version from release version '${version}'`
+        )
+      }
+
+      return nextVersion
+
+    case 'beta':
+      if (isTestTag(semanticVersion)) {
+        throw new Error(
+          `Unable to resolve beta release using test version '${version}'`
+        )
+      }
+
+      const betaTagIndex = version.indexOf('-beta')
+      if (betaTagIndex > -1) {
+        const betaNumber = version.substr(betaTagIndex + 5)
+        const newBeta = parseInt(betaNumber, 10) + 1
+
+        const newVersion = version.replace(
+          `-beta${betaNumber}`,
+          `-beta${newBeta}`
+        )
+        return newVersion
+      } else {
+        const nextVersion = inc(version, 'patch')
+        const firstBeta = `${nextVersion}-beta1`
+        return firstBeta
+      }
+
+    default:
       throw new Error(
-        `Unable to draft production release using beta version '${version}'`
+        `Resolving the next version is not implemented for channel ${channel}`
       )
-    }
-
-    if (isTestTag(semanticVersion)) {
-      throw new Error(
-        `Unable to draft production release using test version '${version}'`
-      )
-    }
-
-    const nextVersion = inc(version, 'patch')
-    if (nextVersion == null) {
-      throw new Error(
-        `Unable to increment next production version from release version '${version}'`
-      )
-    }
-
-    return nextVersion
-  }
-
-  if (isTestTag(semanticVersion)) {
-    throw new Error(
-      `Unable to resolve beta release using test version '${version}'`
-    )
-  }
-
-  const betaTagIndex = version.indexOf('-beta')
-  if (betaTagIndex > -1) {
-    const betaNumber = version.substr(betaTagIndex + 5)
-    const newBeta = parseInt(betaNumber, 10) + 1
-
-    const newVersion = version.replace(`-beta${betaNumber}`, `-beta${newBeta}`)
-    return newVersion
-  } else {
-    const nextVersion = inc(version, 'patch')
-    const firstBeta = `${nextVersion}-beta1`
-    return firstBeta
   }
 }
