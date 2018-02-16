17ed82e97e988331857e57eb65297ad485690c3a
Brendan Forster <github@brendanforster.com>
2018-02-16 16:31:17 +1030

    add error handler for stdout to handle some obscure error codes

---
+++ app/src/lib/process.ts
@@ -0,0 +1,24 @@
+import { ChildProcess } from 'child_process'
+
+export function protectProcessOutput(
+  childProcess: ChildProcess,
+  context: string
+) {
+  childProcess.stdout.on('error', err => {
+    const errWithCode = err as ErrorWithCode
+    let code = errWithCode.code
+
+    if (typeof code === 'string') {
+      if (code === 'EPIPE') {
+        log.error(`[${context}] stdout was terminated with an EPIPE`, err)
+        return
+      }
+      if (code === 'EOF') {
+        log.error(`[${context}] stdout was terminated with an EOF`, err)
+        return
+      }
+    }
+
+    log.error(`[${context}] stdout reported an error`, err)
+  })
+}
