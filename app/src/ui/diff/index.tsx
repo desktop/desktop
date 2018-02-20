a2d0ebabdb20a2cad81e63ec51a876bc912bc3b8
iAmWillShepherd <williamrshepherd@gmail.com>
2018-02-19 21:44:44 -0600

    Add const for image asset

--- app/src/ui/diff/index.tsx
+++ app/src/ui/diff/index.tsx
@@ -70,6 +70,10 @@ const narrowNoNewlineSymbol = new OcticonSymbol(
   8,
   'm 16,1 0,3 c 0,0.55 -0.45,1 -1,1 l -3,0 0,2 -3,-3 3,-3 0,2 2,0 0,-2 2,0 z M 8,4 C 8,6.2 6.2,8 4,8 1.8,8 0,6.2 0,4 0,1.8 1.8,0 4,0 6.2,0 8,1.8 8,4 Z M 1.5,5.66 5.66,1.5 C 5.18,1.19 4.61,1 4,1 2.34,1 1,2.34 1,4 1,4.61 1.19,5.17 1.5,5.66 Z M 7,4 C 7,3.39 6.81,2.83 6.5,2.34 L 2.34,6.5 C 2.82,6.81 3.39,7 4,7 5.66,7 7,5.66 7,4 Z'
 )
+
+// image used when no diff is displayed
+const NoDiffImage = encodePathAsUrl(__dirname, 'static/ufo-alert.svg')
+
 type ChangedFile = WorkingDirectoryFileChange | CommittedFileChange
 
 interface ILineFilters {
