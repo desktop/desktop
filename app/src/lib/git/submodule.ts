14940fd901356666a4d2fe580cbae838b5660a69
Sean Kaim <kmscode@users.noreply.github.com>
2018-03-06 14:29:32 -0500

    fix code style based on travis and cricle CI

--- app/src/lib/git/submodule.ts
+++ app/src/lib/git/submodule.ts
@@ -8,13 +8,13 @@ export async function listSubmodules(
   repository: Repository
 ): Promise<ReadonlyArray<SubmoduleEntry>> {
 
-	let submodulesFile = await pathExists(Path.join(repository.path, '.gitmodules'))
-	let submodulesDir = await pathExists(Path.join(repository.path, '.git', 'modules'))
+  const submodulesFile = await pathExists(Path.join(repository.path, '.gitmodules'))
+  const submodulesDir = await pathExists(Path.join(repository.path, '.git', 'modules'))
 
-	if(!submodulesFile && !submodulesDir) {
-		log.info('No submodules found. Skipping "git submodule status"')
-		return []
-	}
+  if(!submodulesFile && !submodulesDir) {
+    log.info('No submodules found. Skipping "git submodule status"')
+    return []
+  }
 
   // We don't recurse when listing submodules here because we don't have a good
   // story about managing these currently. So for now we're only listing
