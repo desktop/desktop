8369d8279a4b2901dd919954899ee16f04c59a4b
Brendan Forster <github@brendanforster.com>
2018-03-21 17:26:15 +1100

    add test for multiple closing statements

--- script/changelog/test/parser-test.ts
+++ script/changelog/test/parser-test.ts
@@ -15,17 +15,16 @@ quam vel augue.`
       expect(findIssueRef(body)).to.equal(' #2314')
     })
 
-    it('handles colon after message', () => {
+    it('detects multiple issues when linked', () => {
       const body = `
-Pellentesque pellentesque finibus fermentum. Aenean eget semper libero.
+Fixes #2314
+Fixes #1234
 
-Fixes: #2314
-
-Nam malesuada augue vel velit vehicula suscipit. Nunc posuere, velit at sodales
-malesuada, quam tellus rutrum orci, et tincidunt sem nunc non velit. Cras
-placerat, massa vel tristique iaculis, urna nisl tristique nibh, eget luctus
-nisl quam in metus.`
-      expect(findIssueRef(body)).to.equal(' #2314')
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin turpis
+tempor euismod fermentum. Nullam hendrerit neque eget risus faucibus volutpat. Donec
+ultrices, orci quis auctor ultrices, nulla lacus gravida lectus, non rutrum dolor
+quam vel augue.`
+      expect(findIssueRef(body)).to.equal(' #2314 #1234')
     })
   })
 })
