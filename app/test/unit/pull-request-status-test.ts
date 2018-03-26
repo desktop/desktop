05723a016f04c5c7a4ea9466fe9c2bf4dc9448be
Brendan Forster <github@brendanforster.com>
2018-03-26 14:30:49 +1100

    liiint

--- app/test/unit/pull-request-status-test.ts
+++ app/test/unit/pull-request-status-test.ts
@@ -3,11 +3,15 @@ import { expect } from 'chai'
 import { APIRefState } from '../../src/lib/api'
 import { getSummary } from '../../src/ui/branches/pull-request-status'
 
+const failure: APIRefState = 'failure'
+const pending: APIRefState = 'pending'
+const success: APIRefState = 'success'
+
 describe('pull request status', () => {
   it('uses the state when no statuses found', () => {
     const prStatus = {
       pullRequestNumber: 23,
-      state: <APIRefState>'success',
+      state: success,
       totalCount: 0,
       sha: '',
       statuses: [],
@@ -18,7 +22,7 @@ describe('pull request status', () => {
   it('changes the failure message to something more friendly', () => {
     const prStatus = {
       pullRequestNumber: 23,
-      state: <APIRefState>'failure',
+      state: failure,
       totalCount: 0,
       sha: '',
       statuses: [],
@@ -29,13 +33,10 @@ describe('pull request status', () => {
   it('reads the statuses when they are populated', () => {
     const prStatus = {
       pullRequestNumber: 23,
-      state: <APIRefState>'success',
+      state: success,
       totalCount: 0,
       sha: '',
-      statuses: [
-        { id: 1, state: <APIRefState>'success' },
-        { id: 2, state: <APIRefState>'success' },
-      ],
+      statuses: [{ id: 1, state: success }, { id: 2, state: success }],
     }
     expect(getSummary(prStatus)).to.equal('2/2 checks OK')
   })
@@ -43,13 +44,13 @@ describe('pull request status', () => {
   it('only counts the successful statuses', () => {
     const prStatus = {
       pullRequestNumber: 23,
-      state: <APIRefState>'success',
+      state: success,
       totalCount: 0,
       sha: '',
       statuses: [
-        { id: 1, state: <APIRefState>'success' },
-        { id: 2, state: <APIRefState>'pending' },
-        { id: 2, state: <APIRefState>'pending' },
+        { id: 1, state: success },
+        { id: 2, state: pending },
+        { id: 2, state: pending },
       ],
     }
     expect(getSummary(prStatus)).to.equal('1/3 checks OK')
