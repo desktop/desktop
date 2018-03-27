4e34d4a8706bf3dacccbdf423b0d871181847c60
Brendan Forster <github@brendanforster.com>
2018-03-27 12:39:46 +1100

    this message here refers to a non-existant command

--- script/eslint.ts
+++ script/eslint.ts
@@ -30,6 +30,6 @@ console.log(eslint.getFormatter()(report.results))
 if (report.errorCount > 0) {
   process.exitCode = 1
   console.error(
-    chalk`{bold.green → To fix some of these errors, run {underline yarn eslint:fix}}`
+    chalk`{bold.green → To fix some of these errors, run {underline yarn eslint --fix}}`
   )
 }
