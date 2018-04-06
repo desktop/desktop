7d44190145b891827920ed7baa76b2016f820457
Markus Olsson <j.markus.olsson@gmail.com>
2018-04-06 16:44:00 +0200

    Get rid of trailing separator character
    Co-Authored-By: MarvMan <24806960+MagicMarvMan@users.noreply.github.com>

--- app/src/lib/progress/from-process.ts
+++ app/src/lib/progress/from-process.ts
@@ -27,7 +27,7 @@ export async function executionOptionsWithProgress(
       lfsProgressPath = await createLFSProgressFile()
       env = { GIT_LFS_PROGRESS: lfsProgressPath }
     } catch (e) {
-      log.error('Error writing LFS progress file:', e)
+      log.error('Error writing LFS progress file', e)
       env = { GIT_LFS_PROGRESS: null }
     }
   }
