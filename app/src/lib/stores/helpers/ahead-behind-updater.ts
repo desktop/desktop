1b74db07505013a2e9abeab500aa5233eba5fea4
Brendan Forster <github@brendanforster.com>
2018-04-06 13:27:32 +1000

    use an existing queuing framework rather than a hand-rolled solution

--- app/src/lib/stores/helpers/ahead-behind-updater.ts
+++ app/src/lib/stores/helpers/ahead-behind-updater.ts
@@ -1,11 +1,35 @@
+const queue: (config: QueueConfig) => Queue = require('queue')
+
+interface QueueConfig {
+  // Max number of jobs the queue should process concurrently, defaults to Infinity.
+  readonly concurrency: number
+  // Ensures the queue is always running if jobs are available.
+  // Useful in situations where you are using a queue only for concurrency control.
+  readonly autostart: boolean
+}
+
+interface Queue extends NodeJS.EventEmitter {
+  readonly length: number
+
+  start(): void
+  end(): void
+  push<T>(
+    func: (callback: (error: Error | null, result: T) => void) => void
+  ): void
+}
+
 import { Repository } from '../../../models/repository'
 import { getAheadBehind } from '../../../lib/git'
-import { Branch } from '../../../models/branch'
+import { Branch, IAheadBehind } from '../../../models/branch'
 import { ComparisonCache } from '../../comparison-cache'
 
 export class AheadBehindUpdater {
   private comparisonCache = new ComparisonCache()
-  private abortInflightRequests = false
+
+  private q = queue({
+    concurrency: 1,
+    autostart: true,
+  })
 
   public constructor(
     private repository: Repository,
@@ -13,66 +37,87 @@ export class AheadBehindUpdater {
     private onCacheUpdate: (cache: ComparisonCache) => void
   ) {}
 
-  public start() {}
+  public start() {
+    this.q.on('success', (result: IAheadBehind | null, job: any) => {
+      if (result != null) {
+        this.onCacheUpdate(this.comparisonCache)
+      }
+    })
 
-  public stop() {
-    this.abortInflightRequests = true
+    this.q.on('error', (err: Error) => {
+      log.error(
+        '[AheadBehindUpdater] an error with the queue was reported',
+        err
+      )
+    })
+
+    this.q.on('end', (err?: Error) => {
+      if (err != null) {
+        log.warn(`[AheadBehindUpdater] ended with an error`, err)
+      }
+
+      this.onPerformingWork(false)
+    })
+
+    this.q.start()
   }
 
-  private async executeBackgroundCompares(
+  public stop() {
+    this.q.end()
+  }
+
+  private executeTask = (
     from: string,
-    uniqueBranchSha: Set<string>
-  ) {
-    this.abortInflightRequests = false
-    this.onPerformingWork(true)
+    to: string,
+    callback: (error: Error | null, result: IAheadBehind | null) => void
+  ) => {
+    if (this.comparisonCache.has(from, to)) {
+      return
+    }
 
-    let count = uniqueBranchSha.size
-
-    for (const sha of uniqueBranchSha) {
-      if (this.abortInflightRequests) {
-        log.debug(
-          `[AheadBehindUpdater] - aborting with ${count} branches unresolved`
-        )
-        this.onPerformingWork(false)
-        break
-      }
-
-      if (this.comparisonCache.has(from, sha)) {
-        count -= 1
-        continue
-      }
-
-      const range = `${from}...${sha}`
-      const aheadBehind = await getAheadBehind(this.repository, range)
-
-      if (aheadBehind != null) {
-        this.comparisonCache.set(from, sha, aheadBehind)
-        this.onCacheUpdate(this.comparisonCache)
+    const range = `${from}...${to}`
+    getAheadBehind(this.repository, range).then(result => {
+      if (result != null) {
+        this.comparisonCache.set(from, to, result)
       } else {
         log.debug(
           `[AheadBehindUpdater] unable to cache '${range}' as no result returned`
         )
       }
-      count -= 1
-    }
-
-    this.onPerformingWork(false)
+      callback(null, result)
+    })
   }
 
   public enqueue(currentBranch: Branch, branches: ReadonlyArray<Branch>) {
-    // signal to abandon any in-flight comparison checks
-    this.abortInflightRequests = true
+    // remove any queued work to prioritize this new set of tasks
+    this.q.end()
 
     const from = currentBranch.tip.sha
 
-    const uncachedBranchShas = branches
+    const branchesNotInCache = branches
       .map(b => b.tip.sha)
       .filter(to => !this.comparisonCache.has(from, to))
 
-    const uniqueBranchSha = new Set<string>(uncachedBranchShas)
+    const newRefsToCompare = new Set<string>(branchesNotInCache)
 
-    log.warn(`[Compare] - found ${uniqueBranchSha.size} comparisons to perform`)
+    log.warn(
+      `[AheadBehindUpdater] - found ${
+        newRefsToCompare.size
+      } comparisons to perform`
+    )
 
-    this.executeBackgroundCompares(from, uniqueBranchSha)
+    if (newRefsToCompare.size === 0) {
+      return
+    }
+
+    this.onPerformingWork(true)
+
+    for (const sha of newRefsToCompare) {
+      this.q.push<IAheadBehind | null>(callback =>
+        requestAnimationFrame(() => {
+          this.executeTask(from, sha, callback)
+        })
+      )
+    }
   }
 }
