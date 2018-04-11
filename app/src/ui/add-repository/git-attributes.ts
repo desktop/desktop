5ed71ca9ace9be742cf6f327a62d3e899724e85d
Brendan Forster <github@brendanforster.com>
2018-04-12 09:46:41 +1000

    tidy up the import * usages

--- app/src/ui/add-repository/git-attributes.ts
+++ app/src/ui/add-repository/git-attributes.ts
@@ -1,12 +1,12 @@
-import * as FSE from 'fs-extra'
-import * as Path from 'path'
+import { writeFile } from 'fs-extra'
+import { join } from 'path'
 
 /**
  * Write the .gitAttributes file to the given repository
  */
 export async function writeGitAttributes(path: string): Promise<void> {
-  const fullPath = Path.join(path, '.gitattributes')
+  const fullPath = join(path, '.gitattributes')
   const contents =
     '# Auto detect text files and perform LF normalization\n* text=auto\n'
-  await FSE.writeFile(fullPath, contents)
+  await writeFile(fullPath, contents)
 }
