ddb0af3b3c66afd08653a32a97e271f21853fdd4
iAmWillShepherd <iamwillshepherd@github.com>
2018-04-12 21:16:20 -0500

    Add new measures to db

--- app/src/lib/stats/stats-database.ts
+++ app/src/lib/stats/stats-database.ts
@@ -40,6 +40,21 @@ export interface IDailyMeasures {
 
   /** The number of commits created with one or more co-authors. */
   readonly coAuthoredCommits: number
+
+  /** The number of times a branch is compared to an arbitrary branch */
+  readonly branchComparisons: number
+
+  /** The number of times a branch is compared to `master` */
+  readonly comparisonsToMaster: number
+
+  /** The number of times a merge is initiated in the `compare` sidebar */
+  readonly mergesInitiatedFromComparison: number
+
+  /** The number of times the `Branch -> Update From Default Branch` menu item is used */
+  readonly updateFromDefaultBranchMenuCount: number
+
+  /** The number of times the `Branch -> Merge Into Current Branch` menu item is used */
+  readonly mergeIntoCurrentBranchMenuCount: number
 }
 
 export class StatsDatabase extends Dexie {
