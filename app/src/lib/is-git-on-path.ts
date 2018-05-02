29d2c058f5d48c7eb4f9224c487b715f8063c4eb
AndreaPivetta <vanpivix@gmail.com>
2018-05-02 22:52:59 +0200

    Check if git is found under PATH on Linux

--- app/src/lib/is-git-on-path.ts
+++ app/src/lib/is-git-on-path.ts
@@ -5,14 +5,13 @@ export function isGitOnPath(): Promise<boolean> {
   // Modern versions of macOS ship with a Git shim that guides you through
   // the process of setting everything up. We trust this is available, so
   // don't worry about looking for it here.
-  // I decide linux user have git too :)
-  if (__DARWIN__ || __LINUX__) {
+  if (__DARWIN__) {
     return Promise.resolve(true)
   }
 
   // adapted from http://stackoverflow.com/a/34953561/1363815
-  return new Promise<boolean>((resolve, reject) => {
-    if (__WIN32__) {
+  if (__WIN32__) {
+    return new Promise<boolean>((resolve, reject) => {
       const windowsRoot = process.env.SystemRoot || 'C:\\Windows'
       const wherePath = Path.join(windowsRoot, 'System32', 'where.exe')
 
@@ -29,9 +28,20 @@ export function isGitOnPath(): Promise<boolean> {
         resolve(code === 0)
       })
       return
-    }
+    })
+  }
 
-    // in case you're on a non-Windows/non-macOS platform
-    resolve(false)
-  })
+  if (__LINUX__) {
+    return new Promise<boolean>((resolve, reject) => {
+      const process = spawn('which', ['git'])
+
+      // `which` will return 0 when the executable
+      // is found under PATH, or 1 if it cannot be found
+      process.on('close', function(code) {
+        resolve(code === 0)
+      })
+    })
+  }
+
+  return Promise.resolve(false)
 }
