22fb0129f0df9b2f7fc8e1174617cee515b36d9e
Brendan Forster <github@brendanforster.com>
2018-05-07 18:49:55 +1000

    fix lint issue

--- app/src/lib/ssh/index.ts
+++ app/src/lib/ssh/index.ts
@@ -5,17 +5,28 @@ import * as moment from 'moment'
 export * from './ssh-environment'
 export * from './verification'
 
-export function saveLogFile(error: string) {
+export function saveLogFile(stderr: string) {
   const timestamp = moment().format('YYYYMMDD-HHmmss')
   const defaultPath = `ssh-output-${timestamp}.txt`
 
-  // TODO: null should be a valid argument here
-  const window: any = null
-  remote.dialog.showSaveDialog(window, { defaultPath }, filename => {
-    if (filename == null) {
-      log.warn('filename returned null, this needs to be in the signature')
-      return
-    }
-    Fs.writeFileSync(filename, error)
+  return new Promise<void>((resolve, reject) => {
+    // TODO: null should be a valid argument here
+    const window: any = null
+    remote.dialog.showSaveDialog(window, { defaultPath }, filename => {
+      if (filename == null) {
+        log.warn(
+          'TODO: filename returned null, this needs to be in the signature'
+        )
+        resolve()
+      } else {
+        Fs.writeFile(filename, stderr, err => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        })
+      }
+    })
   })
 }
