25f5fd56365f4ef7cc85d3a53c727709f5c4af4c
Brendan Forster <github@brendanforster.com>
2018-05-07 11:26:03 +1000

    extract isExecutableOnPath function for maximum reuse

--- app/src/lib/is-git-on-path.ts
+++ app/src/lib/is-git-on-path.ts
@@ -1,21 +1,14 @@
 import { spawn } from 'child_process'
 import * as Path from 'path'
 
-export function isGitOnPath(): Promise<boolean> {
-  // Modern versions of macOS ship with a Git shim that guides you through
-  // the process of setting everything up. We trust this is available, so
-  // don't worry about looking for it here.
-  if (__DARWIN__) {
-    return Promise.resolve(true)
-  }
-
+function isExecutableOnPath(executableName: string): Promise<boolean> {
   // adapted from http://stackoverflow.com/a/34953561/1363815
   if (__WIN32__) {
     return new Promise<boolean>((resolve, reject) => {
       const windowsRoot = process.env.SystemRoot || 'C:\\Windows'
       const wherePath = Path.join(windowsRoot, 'System32', 'where.exe')
 
-      const cp = spawn(wherePath, ['git'])
+      const cp = spawn(wherePath, [executableName])
 
       cp.on('error', error => {
         log.warn('Unable to spawn where.exe', error)
@@ -31,9 +24,9 @@ export function isGitOnPath(): Promise<boolean> {
     })
   }
 
-  if (__LINUX__) {
+  if (__LINUX__ || __DARWIN__) {
     return new Promise<boolean>((resolve, reject) => {
-      const process = spawn('which', ['git'])
+      const process = spawn('which', [executableName])
 
       // `which` will return 0 when the executable
       // is found under PATH, or 1 if it cannot be found
@@ -44,4 +37,15 @@ export function isGitOnPath(): Promise<boolean> {
   }
 
   return Promise.resolve(false)
+}
+
+export function isGitOnPath(): Promise<boolean> {
+  // Modern versions of macOS ship with a Git shim that guides you through
+  // the process of setting everything up. We trust this is available, so
+  // don't worry about looking for it here.
+  if (__DARWIN__) {
+    return Promise.resolve(true)
+  }
+
+  return isExecutableOnPath('git')
 }
