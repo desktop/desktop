5559fc3a4088162d56ad675d493c5a90c2d81528
Brendan Forster <github@brendanforster.com>
2018-05-14 12:44:09 +1000

    add third test for filter text scenario

--- app/test/unit/list-selection-test.ts
+++ app/test/unit/list-selection-test.ts
@@ -6,7 +6,15 @@ describe('list-selection', () => {
   describe('findNextSelectableRow', () => {
     const rowCount = 5
 
-    it('returns first row when selecting down', () => {
+    it('returns first row when selecting down outside list (filter text)', () => {
+      const selectedRow = findNextSelectableRow(rowCount, {
+        direction: 'down',
+        row: -1,
+      })
+      expect(selectedRow).to.equal(0)
+    })
+
+    it('returns first row when selecting down from last row', () => {
       const lastRow = rowCount - 1
       const selectedRow = findNextSelectableRow(rowCount, {
         direction: 'down',
@@ -15,7 +23,7 @@ describe('list-selection', () => {
       expect(selectedRow).to.equal(0)
     })
 
-    it('returns last row when selecting up from top', () => {
+    it('returns last row when selecting up from top row', () => {
       const selectedRow = findNextSelectableRow(rowCount, {
         direction: 'up',
         row: 0,
