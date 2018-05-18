5c3ae22a285d311b90eaa39f6bce3b118146f878
iAmWillShepherd <iamwillshepherd@github.com>
2018-05-17 23:19:44 -0500

    Only show menu item if compareSidebar feature is enabled

--- app/src/main-process/menu/build-default-menu.ts
+++ app/src/main-process/menu/build-default-menu.ts
@@ -6,6 +6,7 @@ import { ensureDir } from 'fs-extra'
 
 import { log } from '../log'
 import { openDirectorySafe } from '../shell'
+import { enableCompareSidebar } from '../../lib/feature-flag'
 
 const defaultEditorLabel = __DARWIN__
   ? 'Open in External Editor'
@@ -293,6 +294,7 @@ export function buildDefaultMenu(
         id: 'compare-to-branch',
         accelerator: 'CmdOrCtrl+Shift+B',
         click: emit('compare-to-branch'),
+        visible: enableCompareSidebar(),
       },
       {
         label: __DARWIN__
