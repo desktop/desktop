b794e29522bb10dbac8835cff8446105ca2f98d2
Brendan Forster <github@brendanforster.com>
2018-05-22 19:44:28 +1000

    filter out releases without external contributors

--- script/contributors/run.ts
+++ script/contributors/run.ts
@@ -32,6 +32,40 @@ export async function run(args: ReadonlyArray<string>): Promise<void> {
 
     const entries: Array<string> = releases[prop]
 
-    console.log(`Version: ${prop} has ${entries.length} entries`)
+    const externalContributionRe = /\. Thanks ([\@a-zA-Z0-9\-, ]*)\!$/
+
+    const externalChangelogEntries = entries.filter(entry =>
+      externalContributionRe.test(entry)
+    )
+
+    if (externalChangelogEntries.length === 0) {
+      continue
+    }
+
+    const contributors: Array<string> = []
+
+    for (const entry of externalChangelogEntries) {
+      const match = externalContributionRe.exec(entry)
+
+      if (match === null) {
+        continue
+      }
+
+      // strip out any commas and the 'and' suffix at the end
+      const formattedMatch = match[1].replace(',', '').replace(' and ', ' ')
+      const distinctContributors = formattedMatch.split(' ')
+
+      for (const user of distinctContributors) {
+        if (contributors.indexOf(user) === -1) {
+          contributors.push(user)
+        }
+      }
+    }
+
+    console.log(
+      `${prop} has ${
+        externalChangelogEntries.length
+      } external contributions from ${contributors.length} unique contributors!`
+    )
   }
 }
