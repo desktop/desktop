1f26156ec839423bad96701e2c25b07c5d9c2a38
iAmWillShepherd <iamwillshepherd@github.com>
2018-05-24 15:48:26 -0500

    Add logic for determining which branch to use for remotes
    This is mostly exploratory, the logic is most likely wrong at this stage.

--- app/src/lib/stores/app-store.ts
+++ app/src/lib/stores/app-store.ts
@@ -781,9 +781,10 @@ export class AppStore extends TypedBaseStore<IAppState> {
   }
 
   //TODO: make private; needed linter to leave me alone
+  //TODO: figure out if it's better to return branch name or SHA
   public getCompareBranch(repository: Repository): string | null {
     const state = this.getRepositoryState(repository)
-    const branchesState = state.branchesState
+    const { compareState, branchesState } = state
     const tip = branchesState.tip
     const currentBranch = tip.kind === TipState.Valid ? tip.branch : null
 
@@ -802,13 +803,50 @@ export class AppStore extends TypedBaseStore<IAppState> {
       // if the repository is a fork, use the default branch on upstream
       // otherwise use the default branch on origin
       // if the repo is a fork, parent must exist(???)
-      return githubRepository.fork
-        ? githubRepository.parent!.defaultBranch
-        : githubRepository.defaultBranch
+      if (githubRepository.fork) {
+        const defaultBranchName = githubRepository.defaultBranch
+
+        // TODO: figure out if this branch is stored in branch list
+        // TODO: figure out what to return
+        if (defaultBranchName === null) {
+          return null
+        }
+
+        const defaultBranch = branchesState.allBranches.find(
+          b => b.name === defaultBranchName
+        )
+
+        // TODO: figure out if it's a problem that
+        // we don't store the branch in our branches list
+        if (defaultBranch === undefined) {
+          return null
+        }
+
+        const defaultBranchAheadBehind = compareState.aheadBehindCache.get(
+          defaultBranch.tip.sha,
+          currentBranch.tip.sha
+        )
+
+        // If no changes exist on orgin/master
+        // then return upstream/master instead
+        if (
+          defaultBranchAheadBehind !== null &&
+          defaultBranchAheadBehind.behind > 0
+        ) {
+          return defaultBranch.name
+        }
+
+        return githubRepository.parent!.defaultBranch
+      } else {
+        return githubRepository.defaultBranch
+      }
     }
 
     // fall through to the local master branch
-    return 'master'
+    const defaultBranch = branchesState.allBranches.find(
+      b => b.name === 'master'
+    )
+    return defaultBranch!.name
   }
 
   /** This shouldn't be called directly. See `Dispatcher`. */
