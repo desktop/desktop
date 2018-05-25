96f69a803404655b308dad9961fb687f1410a505
Don Okuda <don.okuda@gmail.com>
2018-05-25 15:08:22 -0700

    Start building out the banner UI

--- app/src/ui/notification-banner/index.tsx
+++ app/src/ui/notification-banner/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { ButtonGroup } from '../lib/button-group';
 import { Button } from '../lib/button';
 import { Ref } from '../lib/ref';
+import { Octicon, OcticonSymbol } from '../octicons';
 
 interface INotificationBannerProps {
 }
@@ -9,9 +10,15 @@ interface INotificationBannerProps {
 export class NotificationBanner extends React.Component<INotificationBannerProps, {}> {
   public render() {
     return (<div className="notification-banner diverge-banner">
-      <p>
-        Your branch is <strong>42 commits</strong> behind <Ref>origin/master</Ref>
-      </p>
+      <div className="notification-banner-content">
+        <p>
+          Your branch is <strong>42 commits</strong> behind <Ref>origin/master</Ref>
+        </p>
+
+        <a className="close" aria-label="Dismiss banner">
+          <Octicon symbol={OcticonSymbol.x} />
+        </a>
+      </div>
 
       <ButtonGroup>
         <Button type="submit" onClick={this.noop}>
