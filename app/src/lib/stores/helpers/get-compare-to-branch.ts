b999bce1c9207f658f3578294c985810f4d464a6
iAmWillShepherd <iamwillshepherd@github.com>
2018-05-27 13:10:05 -0500

    Extract out new function

--- app/src/lib/stores/helpers/get-compare-to-branch.ts
+++ app/src/lib/stores/helpers/get-compare-to-branch.ts
@@ -1,6 +1,9 @@
 import { TipState } from '../../../models/tip'
-import { IRepositoryState } from '../../app-state'
+import { IRepositoryState, IBranchesState } from '../../app-state'
 import { Repository } from '../../../models/repository'
+import { GitHubRepository } from '../../../models/github-repository'
+import { ComparisonCache } from '../../comparison-cache'
+import { Branch } from '../../../models/branch'
 
 //TODO: make private; needed linter to leave me alone
 //TODO: figure out if it's better to return branch name or SHA
@@ -24,43 +27,13 @@ export function getCompareToBranch(
 
   const githubRepository = repository.gitHubRepository
   if (githubRepository !== null) {
-    // if the repository is a fork, use the default branch on upstream
-    // otherwise use the default branch on origin
-    // if the repo is a fork, parent must exist(???)
     if (githubRepository.fork) {
-      const defaultBranchName = githubRepository.defaultBranch
-
-      // TODO: figure out if this branch is stored in branch list
-      // TODO: figure out what to return
-      if (defaultBranchName === null) {
-        return null
-      }
-
-      const defaultBranch = branchesState.allBranches.find(
-        b => b.name === defaultBranchName
+      return inferCompareToBranchFromFork(
+        currentBranch,
+        githubRepository,
+        branchesState,
+        compareState.aheadBehindCache
       )
-
-      // TODO: figure out if it's a problem that
-      // we don't store the branch in our branches list
-      if (defaultBranch === undefined) {
-        return null
-      }
-
-      const defaultBranchAheadBehind = compareState.aheadBehindCache.get(
-        defaultBranch.tip.sha,
-        currentBranch.tip.sha
-      )
-
-      // If no changes exist on orgin/master
-      // then return upstream/master instead
-      if (
-        defaultBranchAheadBehind !== null &&
-        defaultBranchAheadBehind.behind > 0
-      ) {
-        return defaultBranch.name
-      }
-
-      return githubRepository.parent!.defaultBranch
     } else {
       return githubRepository.defaultBranch
     }
@@ -69,4 +42,48 @@ export function getCompareToBranch(
   // fall through to the local master branch
   const defaultBranch = branchesState.allBranches.find(b => b.name === 'master')
   return defaultBranch!.name
+}
+
+// if the repository is a fork, use the default branch on upstream
+// otherwise use the default branch on origin
+// if the repo is a fork, parent must exist(???)
+function inferCompareToBranchFromFork(
+  currentBranch: Branch,
+  ghRepository: GitHubRepository,
+  state: IBranchesState,
+  cache: ComparisonCache
+) {
+  const defaultBranchName = ghRepository.defaultBranch
+
+  // TODO: figure out if this branch is stored in branch list
+  // TODO: figure out what to return
+  if (defaultBranchName === null) {
+    return null
+  }
+
+  const defaultBranch = state.allBranches.find(
+    b => b.name === defaultBranchName
+  )
+
+  // TODO: figure out if it's a problem that
+  // we don't store the branch in our branches list
+  if (defaultBranch === undefined) {
+    return null
+  }
+
+  const defaultBranchAheadBehind = cache.get(
+    defaultBranch.tip.sha,
+    currentBranch.tip.sha
+  )
+
+  // If no changes exist on orgin/master
+  // then return upstream/master instead
+  if (
+    defaultBranchAheadBehind !== null &&
+    defaultBranchAheadBehind.behind > 0
+  ) {
+    return defaultBranch.name
+  }
+
+  return ghRepository.parent!.defaultBranch
 }
