ec0e6519021450bfce8e740d8f0e4e6b7f9515b2
Sanghyeon Lee <yongdamsh@gmail.com>
2018-06-01 01:41:09 +0900

    Follow the engineering values 'Passing values to functions is good'
    Reference: https://github.com/desktop/desktop/blob/master/docs/contributing/engineering-values.md#examples-2

--- app/src/ui/changes/files-changed-badge.tsx
+++ app/src/ui/changes/files-changed-badge.tsx
@@ -4,27 +4,47 @@ interface IFilesChangedBadgeProps {
   readonly numFilesChanged: number
 }
 
-/** The number that can be displayed as a specific value */
-const LargeNumber = 300;
+interface IFilesChangedBadgeState {
+  readonly badgeCount: string
+}
+
+function createState(props: IFilesChangedBadgeProps): IFilesChangedBadgeState {
+  if (props.numFilesChanged === 0) {
+    return { badgeCount: '' }
+  }
+
+  /** The number that can be displayed as a specific value */
+  const LargeNumber = 300
+  const badgeCount =
+    props.numFilesChanged > LargeNumber ? `${LargeNumber}+` : props.numFilesChanged.toString()
+
+  return { badgeCount }
+}
 
 /** Displays number of files that have changed */
 export class FilesChangedBadge extends React.Component<
   IFilesChangedBadgeProps,
-  {}
+  IFilesChangedBadgeState
 > {
-  private get badgeCount() {
-    const numFilesChanged = this.props.numFilesChanged;
+  static getDerivedStateFromProps(props: IFilesChangedBadgeProps): IFilesChangedBadgeState {
+    return createState(props)
+  }
 
-    return numFilesChanged > LargeNumber ? `${LargeNumber}+` : numFilesChanged;
+  public constructor(props: IFilesChangedBadgeProps) {
+    super(props)
+
+    this.state = createState(props)
   }
 
   public render() {
-    if (!this.props.numFilesChanged) {
-      return null;
+    const badgeCount = this.state.badgeCount
+
+    if (!badgeCount) {
+      return null
     }
 
     return (
-      <span className="counter">{this.badgeCount}</span>
+      <span className="counter">{badgeCount}</span>
     )
   }
 }
