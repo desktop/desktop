f129268c83337eab035cbd90bf52e9e47d87ad62
Kenneth Sweezy <KennethSweezy@gmail.com>
2018-06-12 01:12:16 -0400

    Ran yarn lint:prettier --fix

--- app/src/ui/updates/update-available.tsx
+++ app/src/ui/updates/update-available.tsx
@@ -25,7 +25,8 @@ export class UpdateAvailable extends React.Component<
           An updated version of GitHub Desktop is available and will be
           installed at the next launch. See{' '}
           <LinkButton uri={this.props.releaseNotesLink}>what's new</LinkButton>{' '}
-          or <LinkButton onClick={this.updateNow}>restart GitHub now</LinkButton>.
+          or{' '}
+          <LinkButton onClick={this.updateNow}>restart GitHub now</LinkButton>.
         </span>
 
         <a className="close" onClick={this.dismiss}>
