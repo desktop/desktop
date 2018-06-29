a6397aa934ccdc258512ea786475c888dc1fe4d6
Brendan Forster <github@brendanforster.com>
2018-06-29 14:08:45 -0300

    add function for resolving the symbolic ref

--- app/src/lib/git/refs.ts
+++ app/src/lib/git/refs.ts
@@ -1,3 +1,6 @@
+import { git } from './core'
+import { Repository } from '../../models/repository'
+
 /**
  * Format a local branch in the ref syntax, ensuring situations when the branch
  * is ambiguous are handled.
@@ -18,4 +21,25 @@ export function formatAsLocalRef(name: string): string {
     // - include this to ensure the ref is fully qualified.
     return `refs/heads/${name}`
   }
+}
+
+export async function getSymbolicRef(
+  repository: Repository,
+  ref: string
+): Promise<string | null> {
+  const result = await git(
+    ['symbolic-ref', '-q', ref],
+    repository.path,
+    'getSymbolicRef',
+    {
+      successExitCodes: new Set([0, 128]),
+    }
+  )
+
+  if (result.exitCode === 128) {
+    // ref was not a symbolic ref or ref does not exist
+    return null
+  }
+
+  return result.stdout.trim()
 }
