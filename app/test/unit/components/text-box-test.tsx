5882d22dc685a2a12a345097093e9a2fe662cec0
Brendan Forster <github@brendanforster.com>
2018-07-02 17:38:44 -0300

    add sample tests

---
+++ app/test/unit/components/text-box-test.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import { expect } from 'chai'
+
+import { render, cleanup } from 'react-testing-library'
+
+import { TextBox } from '../../../src/ui/lib/text-box'
+
+describe.only('<TextBox />', () => {
+  afterEach(cleanup)
+
+  it('assigns the value to an input', () => {
+    const { container } = render(<TextBox value="words are here" />)
+
+    const inputs = container.getElementsByTagName('input')
+    expect(inputs.length).equals(1)
+    const input = inputs[0]
+    expect(input.value).equals('words are here')
+  })
+
+  it('does not render a label by default', () => {
+    const { container } = render(<TextBox value="words are here" />)
+
+    const inputs = container.getElementsByTagName('label')
+    expect(inputs.length).equals(0)
+  })
+
+  it('renders a label if the label prop is defined', () => {
+    const { getByLabelText, container } = render(
+      <TextBox label="my cool label" />
+    )
+
+    expect(getByLabelText('my cool label')).is.not.null
+
+    const inputs = container.getElementsByTagName('input')
+    expect(inputs.length).equals(1)
+    const input = inputs[0]
+    expect(input.id).contains('my_cool_label')
+  })
+})
