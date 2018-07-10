54af6d35d9e1a403599bd28f7a8fecf5d20953e7
Brendan Forster <github@brendanforster.com>
2018-07-10 11:06:51 -0300

    just make this a function after all

--- app/test/unit/git/merge-parser-test.ts
+++ app/test/unit/git/merge-parser-test.ts
@@ -1,11 +1,13 @@
 import { expect } from 'chai'
 import * as Path from 'path'
 import * as FSE from 'fs-extra'
-import { MergeParser, IMergeSuccess } from '../../../src/lib/git/merge-parser'
+import {
+  parseMergeResult,
+  IMergeSuccess,
+} from '../../../src/lib/git/merge-parser'
 
 describe('merge-parser', () => {
   it('can process a successful merge result', async () => {
-    const parser = new MergeParser()
     const relativePath = Path.join(
       __dirname,
       '../../fixtures/merge-parser/valid-merge-master-into-script-upgrade.txt'
@@ -13,7 +15,7 @@ describe('merge-parser', () => {
     const filePath = Path.resolve(relativePath)
     const input = await FSE.readFile(filePath, { encoding: 'utf8' })
 
-    const result = parser.parse(input)
+    const result = parseMergeResult(input)
     expect(result.kind).equals('Success')
 
     const mergeResult = result as IMergeSuccess
