329791c149b417b75b85d3390d245ce94dbb314e
Brendan Forster <github@brendanforster.com>
2018-07-10 14:51:22 -0300

    add comment-slash-warning

--- app/src/lib/api.ts
+++ app/src/lib/api.ts
@@ -824,6 +824,11 @@ export function getEnterpriseAPIURL(endpoint: string): string {
 
 /** Get github.com's API endpoint. */
 export function getDotComAPIEndpoint(): string {
+  // NOTE:
+  // `DESKTOP_GITHUB_DOTCOM_API_ENDPOINT` only needs to be set if you are
+  // developing against a local version of GitHub the Website, and need to debug
+  // the server-side interaction. For all other cases you should leave this
+  // unset.
   const envEndpoint = process.env['DESKTOP_GITHUB_DOTCOM_API_ENDPOINT']
   if (envEndpoint && envEndpoint.length > 0) {
     return envEndpoint
