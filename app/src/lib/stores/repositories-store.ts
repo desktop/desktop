80af1ae44ea2e8b88e70b0b8a54cf8cb63eef57f
William Shepherd <williamrshepherd@gmail.com>
2018-08-02 12:28:10 -0500

    Remove empty line

--- app/src/lib/stores/repositories-store.ts
+++ app/src/lib/stores/repositories-store.ts
@@ -21,7 +21,6 @@ export class RepositoriesStore extends BaseStore {
   }
 
   /** Find the matching GitHub repository or add it if it doesn't exist. */
-
   public async upsertGitHubRepository(
     endpoint: string,
     apiRepository: IAPIRepository
