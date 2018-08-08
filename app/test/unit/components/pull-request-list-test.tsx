467eed2491114d5f3d38e137c79a53cf40065cae
Brendan Forster <github@brendanforster.com>
2018-08-08 17:28:26 -0300

    add another test that currently fails because of no ResizeObserver

--- app/test/unit/components/pull-request-list-test.tsx
+++ app/test/unit/components/pull-request-list-test.tsx
@@ -6,6 +6,7 @@ import { expect } from 'chai'
 import { render, cleanup } from 'react-testing-library'
 
 import { PullRequestList } from '../../../src/ui/branches/pull-request-list'
+import { PullRequest } from '../../../src/models/pull-request'
 
 const ignoreCallback = () => {}
 
@@ -38,5 +39,80 @@ describe('<PullRequestList />', () => {
     const title = container.getElementsByClassName('no-prs')
     expect(title.length).equals(1)
     expect(title[0].textContent!.includes(repositoryName))
+  })
+
+  it(`renders the filter list when a list of PRs received`, () => {
+    const repositoryName = 'shiftkey/desktop'
+
+    const someTimeAgo = new Date('2018-08-01T18:58:13Z')
+    const endpoint = 'https://api.github.com/'
+
+    const gitHubRepository = {
+      dbID: 1,
+      name: 'some-repo',
+      fullName: 'shiftkey/desktop',
+      owner: {
+        id: 1234,
+        login: 'shiftkey',
+        endpoint,
+        hash: 'some-hash',
+      },
+      private: false,
+      fork: false,
+      hash: 'another-hash',
+      htmlURL: 'https://github.com/shiftkey/desktop',
+      defaultBranch: 'master',
+      cloneURL: 'https://github.com/shiftkey/desktop.git',
+      parent: null,
+      endpoint,
+    }
+
+    const pullRequests: Array<PullRequest> = [
+      {
+        id: 20,
+        created: someTimeAgo,
+        title: 'the first PR',
+        number: 1,
+        head: { ref: 'some-branch', sha: 'deadbeef', gitHubRepository },
+        base: { ref: 'master', sha: 'deadbeef2', gitHubRepository },
+        author: 'shiftkey',
+        status: null,
+      },
+      {
+        id: 21,
+        created: someTimeAgo,
+        title: 'the second PR',
+        number: 2,
+        head: { ref: 'another-branch', sha: 'deadbeef', gitHubRepository },
+        base: { ref: 'master', sha: 'deadbeef2', gitHubRepository },
+        author: 'shiftkey',
+        status: null,
+      },
+    ]
+
+    const { container } = render(
+      <PullRequestList
+        pullRequests={pullRequests}
+        selectedPullRequest={null}
+        repositoryName={repositoryName}
+        isOnDefaultBranch={true}
+        filterText=""
+        onItemClick={ignoreCallback}
+        onDismiss={ignoreCallback}
+        onFilterTextChanged={ignoreCallback}
+        onCreateBranch={ignoreCallback}
+        onCreatePullRequest={ignoreCallback}
+      />
+    )
+
+    // confirm the filter list is shown
+    const outerElement = container.getElementsByClassName('pull-request-list')
+    expect(outerElement.length).equals(1)
+
+    // confirm two items inside pull request list
+    const items = container.getElementsByClassName('pull-request-item')
+    expect(items.length).equals(2)
+    expect(items[0].textContent!.includes('first'))
+    expect(items[1].textContent!.includes('second'))
   })
 })
