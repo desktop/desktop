03e12dcf468b2fea3908d0f25f9e622aa4063488
evelyn masso <outofambit@github.com>
2018-08-15 11:32:37 -0500

    ts-mocha

--- app/test/helpers/mocking.ts
+++ app/test/helpers/mocking.ts
@@ -5,8 +5,3 @@ require('jsdom-global')()
 
 rewiremock('electron').by('./fake-electron.ts')
 rewiremock.enable()
-
-global.localStorage = {
-  getItem: sinon.fake(),
-}
-global.Headers = sinon.fake()
