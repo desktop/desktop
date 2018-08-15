fa2fcd75169bd23e0a969b3370e4605b1092b169
evelyn masso <outofambit@github.com>
2018-08-15 16:43:44 -0500

    runs

--- app/test/helpers/mocking.ts
+++ app/test/helpers/mocking.ts
@@ -1,7 +1,20 @@
 import * as sinon from 'sinon'
 import rewiremock from 'rewiremock/node'
 
+import 'core-js'
+
 require('jsdom-global')()
 
 rewiremock('electron').by('./fake-electron.ts')
 rewiremock.enable()
+
+import { Headers, Request, Response, FetchError } from 'node-fetch'
+
+global.Headers = Headers
+global.Request = Request
+global.Response = Response
+global.FetchError = FetchError
+
+global.localStorage = {
+  getItem: sinon.fake(),
+}
