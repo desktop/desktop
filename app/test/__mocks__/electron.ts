ee2c71699b3b71800c133958f75d004fa22cec88
evelyn masso <outofambit@github.com>
2018-08-17 11:31:00 -0500

    mocks working for app-test

--- app/test/__mocks__/electron.ts
+++ app/test/__mocks__/electron.ts
@@ -3,8 +3,17 @@ export const shell = {
 }
 
 export const remote = {
-  getCurrentWindow: jest.fn(),
+  getCurrentWindow: jest.fn().mockImplementation(() => ({
+    webContents: {
+      getZoomFactor: jest.fn().mockImplementation(_ => null),
+    },
+  })),
   autoUpdater: {
     on: jest.fn(),
   },
+}
+
+export const ipcRenderer = {
+  on: jest.fn(),
+  send: jest.fn(),
 }
