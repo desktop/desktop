6e1352568f2e9a88d4947b9dd94e5ff63f3fd3a7
Markus Olsson <j.markus.olsson@gmail.com>
2018-08-20 12:29:41 -0500

    Add a word of caution

--- app/src/main-process/shell.ts
+++ app/src/main-process/shell.ts
@@ -8,6 +8,11 @@ import { shell } from 'electron'
  * window, which may confuse users. As a workaround, we will fallback to using
  * shell.openExternal for macOS until it can be fixed upstream.
  *
+ * CAUTION: This method should never be used to open user-provided or derived
+ * paths. It's sole use is to open _directories_ that we know to be safe, no
+ * verification is performed to ensure that the provided path isn't actually
+ * an executable.
+ *
  * @param path directory to open
  */
 export function openDirectorySafe(path: string) {
