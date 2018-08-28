27afdc873b6e5028d04d8bbc20f4f807d390a2b3
Brendan Forster <github@brendanforster.com>
2018-08-28 11:40:11 -0300

    add logic for parsing merge-tree output to detect conflicts

--- app/src/lib/git/merge.ts
+++ app/src/lib/git/merge.ts
@@ -1,5 +1,9 @@
 import { git } from './core'
 import { Repository } from '../../models/repository'
+import { Branch } from '../../models/branch'
+import { MergeResult, MergeResultKind } from '../../models/merge'
+import { parseMergeResult } from '../merge-tree-parser'
+import { spawnAndComplete } from './spawn'
 
 /** Merge the named branch into the current branch. */
 export async function merge(
@@ -20,4 +24,43 @@ export async function getMergeBase(
     'merge-base'
   )
   return process.stdout.trim()
+}
+
+/**
+ * Generate the merge result from two branches in a repository
+ *
+ * @param repository The repository containing the branches to merge
+ * @param ours The current branch
+ * @param theirs Another branch to merge into the current branch
+ */
+export async function mergeTree(
+  repository: Repository,
+  ours: Branch,
+  theirs: Branch
+): Promise<MergeResult | null> {
+  console.time('getMergeBase')
+  const mergeBase = await getMergeBase(repository, ours.tip.sha, theirs.tip.sha)
+  console.timeEnd('getMergeBase')
+
+  if (mergeBase === ours.tip.sha) {
+    return { kind: MergeResultKind.Success, entries: [] }
+  }
+
+  if (mergeBase === theirs.tip.sha) {
+    return { kind: MergeResultKind.Success, entries: [] }
+  }
+
+  console.time('mergeTree')
+  const result = await spawnAndComplete(
+    ['merge-tree', mergeBase, ours.tip.sha, theirs.tip.sha],
+    repository.path,
+    'mergeTree'
+  )
+  console.timeEnd('mergeTree')
+
+  const output = result.output.toString()
+  console.time('parseMergeResult')
+  const mergeResult = parseMergeResult(output)
+  console.timeEnd('parseMergeResult')
+  return mergeResult
 }
