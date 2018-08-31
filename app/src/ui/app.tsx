62042a479c8fbb0c244d9aa2663d4aebae0ba27a
Brendan Forster <github@brendanforster.com>
2018-08-31 14:21:59 -0300

    drop this hack

--- app/src/ui/app.tsx
+++ app/src/ui/app.tsx
@@ -189,11 +189,10 @@ export class App extends React.Component<IAppProps, IAppState> {
       const status = state.status
 
       if (
-        // HACK: just let me test this on my development machine
-        //!(
-        //  __RELEASE_CHANNEL__ === 'development' ||
-        //  __RELEASE_CHANNEL__ === 'test'
-        //) &&
+        !(
+          __RELEASE_CHANNEL__ === 'development' ||
+          __RELEASE_CHANNEL__ === 'test'
+        ) &&
         status === UpdateStatus.UpdateReady
       ) {
         this.props.dispatcher.setUpdateBannerVisibility(true)
