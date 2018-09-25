bf9ffd32f6f611137aa92dcf6c907d654f2aae87
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2018-09-24 19:20:03 -0500

    Refactored models/pull-request constructors with parameter properties
    Used parameter properties to reduce redundancy.

--- app/src/models/pull-request.ts
+++ app/src/models/pull-request.ts
@@ -2,27 +2,17 @@ import { APIRefState } from '../lib/api'
 import { GitHubRepository } from './github-repository'
 
 export class PullRequestRef {
-  /** The name of the ref. */
-  public readonly ref: string
-
-  /** The SHA of the ref. */
-  public readonly sha: string
-
   /**
-   * The GitHub repository in which this ref lives. It could be null if the
-   * repository was deleted after the PR was opened.
+   * @param ref The name of the ref.
+   * @param sha The SHA of the ref.
+   * @param gitHubRepository The GitHub repository in which this ref lives. It could be null if the
+   *                         repository was deleted after the PR was opened.
    */
-  public readonly gitHubRepository: GitHubRepository | null
-
   public constructor(
-    ref: string,
-    sha: string,
-    gitHubRepository: GitHubRepository | null
-  ) {
-    this.ref = ref
-    this.sha = sha
-    this.gitHubRepository = gitHubRepository
-  }
+    public readonly ref: string,
+    public readonly sha: string,
+    public readonly gitHubRepository: GitHubRepository | null
+  ) {}
 }
 
 /** The commit status and metadata for a given ref */
@@ -33,81 +23,42 @@ export interface ICommitStatus {
 }
 
 export class PullRequestStatus {
-  /** The pull request this status is associated with */
-  public readonly pullRequestNumber: number
-
-  /** The status' state. */
-  public readonly state: APIRefState
-
-  /** The number of statuses represented in this combined status. */
-  public readonly totalCount: number
-
-  /** The SHA for which this status applies. */
-  public readonly sha: string
-
-  /** The list of all statuses for a specific ref. */
-  public readonly statuses: ReadonlyArray<ICommitStatus>
-
+  /**
+   * @param pullRequestNumber The pull request this status is associated with.
+   * @param state The status' state.
+   * @param totalCount The number of statuses represented in this combined status.
+   * @param sha The SHA for which this status applies.
+   * @param statuses The list of all statuses for a specific ref.
+   */
   public constructor(
-    pullRequestNumber: number,
-    state: APIRefState,
-    totalCount: number,
-    sha: string,
-    statuses: ReadonlyArray<ICommitStatus>
-  ) {
-    this.pullRequestNumber = pullRequestNumber
-    this.state = state
-    this.totalCount = totalCount
-    this.sha = sha
-    this.statuses = statuses
-  }
+    public readonly pullRequestNumber: number,
+    public readonly state: APIRefState,
+    public readonly totalCount: number,
+    public readonly sha: string,
+    public readonly statuses: ReadonlyArray<ICommitStatus>
+  ) {}
 }
 
 export class PullRequest {
-  /** The database ID. */
-  public readonly id: number
-
-  /** The date on which the PR was created. */
-  public readonly created: Date
-
-  /** The title of the PR. */
-  public readonly title: string
-
-  /** The number. */
-  public readonly pullRequestNumber: number
-
-  /** The ref from which the pull request's changes are coming. */
-  public readonly head: PullRequestRef
-
-  /** The ref which the pull request is targetting. */
-  public readonly base: PullRequestRef
-
-  /** The author's login. */
-  public readonly author: string
-
   /**
-   * The status of the PR. This will be `null` if we haven't looked up its
-   * status yet or if an error occurred while looking it up.
+   * @param id The database ID.
+   * @param created The date on which the PR was created.
+   * @param status The status of the PR. This will be `null` if we haven't looked up its
+   *               status yet or if an error occurred while looking it up.
+   * @param title The title of the PR.
+   * @param number The number.
+   * @param head The ref from which the pull request's changes are coming.
+   * @param base The ref which the pull request is targetting.
+   * @param author The author's login.
    */
-  public readonly status: PullRequestStatus | null
-
   public constructor(
-    id: number,
-    created: Date,
-    status: PullRequestStatus | null,
-    title: string,
-    pullRequestNumber: number,
-    head: PullRequestRef,
-    base: PullRequestRef,
-    author: string
-  ) {
-    this.id = id
-    this.created = created
-    this.status = status
-    this.title = title
-    this.pullRequestNumber = pullRequestNumber
-    this.head = head
-    this.base = base
-    this.author = author
-  }
+    public readonly id: number,
+    public readonly created: Date,
+    public readonly status: PullRequestStatus | null,
+    public readonly title: string,
+    public readonly pullRequestNumber: number,
+    public readonly head: PullRequestRef,
+    public readonly base: PullRequestRef,
+    public readonly author: string
+  ) {}
 }
