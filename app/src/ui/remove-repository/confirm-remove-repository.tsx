a5c55ae59be5d42d2fee904f5300dd812f4d37f8
Nick Craver <nrcraver@gmail.com>
2018-10-03 14:48:22 -0400

    Wrap file path in <Ref> component for consistent styling
    This keeps rendering of the repository removal path in line with other places the app renders paths.

--- app/src/ui/remove-repository/confirm-remove-repository.tsx
+++ app/src/ui/remove-repository/confirm-remove-repository.tsx
@@ -3,6 +3,7 @@ import { ButtonGroup } from '../lib/button-group'
 import { Button } from '../lib/button'
 import { Checkbox, CheckboxValue } from '../lib/checkbox'
 import { Dialog, DialogContent, DialogFooter } from '../dialog'
+import { Ref } from '../lib/ref'
 import { Repository } from '../../models/repository'
 import { TrashNameLabel } from '../lib/context-menu'
 
@@ -68,7 +69,7 @@ export class ConfirmRemoveRepository extends React.Component<
           <p className="description">
             The repository will be removed from GitHub Desktop:
             <br />
-            {this.props.repository.path}
+            <Ref>{this.props.repository.path}</Ref>
           </p>
 
           <div>
