fff8dc69c7f3ae8d24395aab864b3ee25b00e8a6
say25 <say25@cornell.edu>
2018-10-08 00:37:47 -0500

    Adding More Unit Tests

--- app/test/unit/platform-case-test.ts
+++ app/test/unit/platform-case-test.ts
@@ -2,12 +2,39 @@ import { expect } from 'chai'
 import { toPlatformCase } from '../../src/lib/platform-case'
 
 describe('string to platform case', () => {
-  it('convert string to detected platform text case', () => {
+  it('converts lower string to detected platform text case', () => {
     if (__DARWIN__) {
       const result = toPlatformCase(' this should be title case.')
       expect(result).to.equal(' This Should Be Title Case.')
     } else {
       const result = toPlatformCase(' this should be sentence case.')
+      expect(result).to.equal(' This should be sentence case.')
+    }
+  })
+  it('converts title cased string to detected platform text case', () => {
+    if (__DARWIN__) {
+      const result = toPlatformCase(' This Should Be Title Case.')
+      expect(result).to.equal(' This Should Be Title Case.')
+    } else {
+      const result = toPlatformCase(' This Should Be Sentence Case.')
+      expect(result).to.equal(' This should be sentence case.')
+    }
+  })
+  it('converts sentence cased string to detected platform text case', () => {
+    if (__DARWIN__) {
+      const result = toPlatformCase(' This should be title case.')
+      expect(result).to.equal(' This Should Be Title Case.')
+    } else {
+      const result = toPlatformCase(' This should be sentence case.')
+      expect(result).to.equal(' This should be sentence case.')
+    }
+  })
+  it('converts upper string to detected platform text case', () => {
+    if (__DARWIN__) {
+      const result = toPlatformCase(' THIS SHOULD BE TITLE CASE.')
+      expect(result).to.equal(' This Should Be Title Case.')
+    } else {
+      const result = toPlatformCase(' THIS SHOULD BE SENTENCE CASE.')
       expect(result).to.equal(' This should be sentence case.')
     }
   })
