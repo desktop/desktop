3710c0dfe92756884653acd25eee770659f645a5
evelyn masso <outofambit@github.com>
2018-10-16 08:34:55 -0700

    add createMergeCommit and refactor out commit error handling logic

--- app/src/lib/git/commit.ts
+++ app/src/lib/git/commit.ts
@@ -4,6 +4,13 @@ import { Repository } from '../../models/repository'
 import { WorkingDirectoryFileChange } from '../../models/status'
 import { unstageAll } from './reset'
 
+/**
+ * Create a commit
+ *
+ * @param repository repository to execute merge in
+ * @param message commit message
+ * @param files files to commit
+ */
 export async function createCommit(
   repository: Repository,
   message: string,
@@ -22,23 +29,57 @@ export async function createCommit(
     })
     return true
   } catch (e) {
-    // Commit failures could come from a pre-commit hook rejection. So display
-    // a bit more context than we otherwise would.
-    if (e instanceof GitError) {
-      const output = e.result.stderr.trim()
+    handleCommitError(e)
+    return false
+  }
+}
 
-      let standardError = ''
-      if (output.length > 0) {
-        standardError = `, with output: '${output}'`
-      }
-      const exitCode = e.result.exitCode
-      const error = new Error(
-        `Commit failed - exit code ${exitCode} received${standardError}`
-      )
-      error.name = 'commit-failed'
-      throw error
-    } else {
-      throw e
+/**
+ * Creates a commit to finish an in-progress merge
+ * assumes that all conflicts have already been resolved
+ *
+ * @param repository repository to execute merge in
+ * @param files files to commit
+ */
+export async function createMergeCommit(
+  repository: Repository,
+  files: ReadonlyArray<WorkingDirectoryFileChange>
+): Promise<boolean> {
+  // Clear the staging area, our diffs reflect the difference between the
+  // working directory and the last commit (if any) so our commits should
+  // do the same thing.
+  await unstageAll(repository)
+
+  await stageFiles(repository, files)
+
+  try {
+    await git(['commit'], repository.path, 'createMergeCommit')
+    return true
+  } catch (e) {
+    handleCommitError(e)
+    return false
+  }
+}
+
+/**
+ * Commit failures could come from a pre-commit hook rejection.
+ * So display a bit more context than we otherwise would.
+ */
+function handleCommitError(e: Error): void {
+  if (e instanceof GitError) {
+    const output = e.result.stderr.trim()
+
+    let standardError = ''
+    if (output.length > 0) {
+      standardError = `, with output: '${output}'`
     }
+    const exitCode = e.result.exitCode
+    const error = new Error(
+      `Commit failed - exit code ${exitCode} received${standardError}`
+    )
+    error.name = 'commit-failed'
+    throw error
+  } else {
+    throw e
   }
 }
