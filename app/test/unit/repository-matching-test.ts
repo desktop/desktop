5770515c7cd2080ea6ab580e09780a0346a79707
Brendan Forster <github@brendanforster.com>
2018-10-17 13:05:35 -0300

    add new API for better comparing of GitHub URLs

--- app/test/unit/repository-matching-test.ts
+++ app/test/unit/repository-matching-test.ts
@@ -3,8 +3,10 @@ import { expect } from 'chai'
 import {
   matchGitHubRepository,
   urlMatchesRemote,
+  urlMatchesCloneURL,
 } from '../../src/lib/repository-matching'
 import { Account } from '../../src/models/account'
+import { GitHubRepository } from '../../src/models/github-repository'
 
 describe('repository-matching', () => {
   describe('matchGitHubRepository', () => {
@@ -129,6 +131,81 @@ describe('repository-matching', () => {
         const htmlURL = 'https://github.com/shiftkey/desktop'
         expect(urlMatchesRemote(htmlURL, remote)).to.be.true
       })
+    })
+  })
+
+  describe('cloneUrlMatches', () => {
+    const repository: GitHubRepository = {
+      dbID: 1,
+      name: 'desktop',
+      fullName: 'shiftkey/desktop',
+      cloneURL: 'https://github.com/shiftkey/desktop.git',
+      owner: {
+        login: 'shiftkey',
+        id: 1234,
+        endpoint: 'https://api.github.com/',
+        hash: 'whatever',
+      },
+      private: false,
+      htmlURL: 'https://github.com/shiftkey/desktop',
+      defaultBranch: 'master',
+      parent: null,
+      endpoint: 'https://api.github.com/',
+      fork: true,
+      hash: 'whatever',
+    }
+
+    const repositoryWithoutCloneURL: GitHubRepository = {
+      dbID: 1,
+      name: 'desktop',
+      fullName: 'shiftkey/desktop',
+      cloneURL: null,
+      owner: {
+        login: 'shiftkey',
+        id: 1234,
+        endpoint: 'https://api.github.com/',
+        hash: 'whatever',
+      },
+      private: false,
+      htmlURL: 'https://github.com/shiftkey/desktop',
+      defaultBranch: 'master',
+      parent: null,
+      endpoint: 'https://api.github.com/',
+      fork: true,
+      hash: 'whatever',
+    }
+
+    it('returns true for exact match', () => {
+      expect(
+        urlMatchesCloneURL(
+          'https://github.com/shiftkey/desktop.git',
+          repository
+        )
+      ).is.true
+    })
+
+    it(`returns true when URL doesn't have a .git suffix`, () => {
+      expect(
+        urlMatchesCloneURL('https://github.com/shiftkey/desktop', repository)
+      ).is.true
+    })
+
+    it(`returns false when URL belongs to a different owner`, () => {
+      expect(
+        urlMatchesCloneURL(
+          'https://github.com/outofambit/desktop.git',
+          repository
+        )
+      ).is.false
+    })
+
+    it(`returns false if GitHub repository does't have a cloneURL set`, () => {
+      expect(
+        urlMatchesCloneURL(
+          'https://github.com/shiftkey/desktop',
+          repositoryWithoutCloneURL
+        )
+      ).is.false
     })
   })
 })
