b9eeea3050444d0c1179391d253b0b755a7c5c82
WaleedAshraf <waleedashraf@outlook.com>
2018-10-24 17:06:58 +0200

    Fixed linting

--- app/src/ui/add-repository/sanitized-repository-name.ts
+++ app/src/ui/add-repository/sanitized-repository-name.ts
@@ -3,8 +3,10 @@ export function sanitizedRepositoryName(name: string): string {
   const ranges = [
     '\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
     '\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
-    '\ud83d[\ude80-\udeff]'  // U+1F680 to U+1F6FF
+    '\ud83d[\ude80-\udeff]', // U+1F680 to U+1F6FF
   ]
 
-  return name.replace(new RegExp(ranges.join('|'), 'g'), '-').replace(/[^\w.-]/g, '-')
+  return name
+    .replace(new RegExp(ranges.join('|'), 'g'), '-')
+    .replace(/[^\w.-]/g, '-')
 }
