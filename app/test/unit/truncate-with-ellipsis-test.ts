497b84badf8253c572cfdb4f291b3c62f8685175
Alexander Golovanov <a-golovanov@users.noreply.github.com>
2018-10-24 18:46:35 -0500

    Migrate tests from chai to jest

--- app/test/unit/truncate-with-ellipsis-test.ts
+++ app/test/unit/truncate-with-ellipsis-test.ts
@@ -1,41 +1,39 @@
-import { expect } from 'chai'
-
 import { truncateWithEllipsis } from '../../src/lib/truncate-with-ellipsis'
 
 describe('truncateWithEllipsis', () => {
   it('does not truncate a string that fits', () => {
     const str = 'short'
     const result = truncateWithEllipsis(str, 25)
-    expect(result).to.equal(str)
+    expect(result).toEqual(str)
   })
 
   it('does not truncate a max length string', () => {
     const str = 'this-is-max-length-string'
     const result = truncateWithEllipsis(str, str.length)
-    expect(result).to.equal(str)
+    expect(result).toEqual(str)
   })
 
   it('truncates a string that does not fit', () => {
     const str = 'this-string-exceeds-max-length'
     const result = truncateWithEllipsis(str, 25)
-    expect(result).to.equal('this-string-exceeds-max-l…')
+    expect(result).toEqual('this-string-exceeds-max-l…')
   })
 
   it('does not truncate a unicode string that fits', () => {
     const str = '🌝🌛🌜🌚🌕🌖🌗🌘🌑🌒🌓🌔☀\uFE0F'
     const result = truncateWithEllipsis(str, 25)
-    expect(result).to.equal(str)
+    expect(result).toEqual(str)
   })
 
   it('does not truncate a max length unicode string', () => {
     const str = '🌝🌛🌜🌚🌕🌖🌗🌘🌑🌒🌓🌔☀\uFE0F🌤⛅\uFE0F🌥☁\uFE0F🌦🌧⛈🌩🌨'
     const result = truncateWithEllipsis(str, 22)
-    expect(result).to.equal(str)
+    expect(result).toEqual(str)
   })
 
   it('truncates a unicode string that does not fit', () => {
     const str = '🌝🌛🌜🌚🌕🌖🌗🌘🌑🌒🌓🌔☀\uFE0F🌤⛅\uFE0F🌥☁\uFE0F🌦🌧⛈🌩🌨'
     const result = truncateWithEllipsis(str, 13)
-    expect(result).to.equal('🌝🌛🌜🌚🌕🌖🌗🌘🌑🌒🌓🌔☀\uFE0F…')
+    expect(result).toEqual('🌝🌛🌜🌚🌕🌖🌗🌘🌑🌒🌓🌔☀\uFE0F…')
   })
 })
