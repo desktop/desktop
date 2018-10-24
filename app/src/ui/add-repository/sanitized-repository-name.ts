2bf2a24f39431ed722e343e71b246e0e822c168d
WaleedAshraf <waleedashraf@outlook.com>
2018-10-24 16:35:34 +0200

    sanitized-repository-name: escape emojis

--- app/src/ui/add-repository/sanitized-repository-name.ts
+++ app/src/ui/add-repository/sanitized-repository-name.ts
@@ -1,4 +1,10 @@
 /** Sanitize a proposed repository name by replacing illegal characters. */
 export function sanitizedRepositoryName(name: string): string {
-  return name.replace(/[^\w.-]/g, '-')
+  const ranges = [
+    '\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
+    '\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
+    '\ud83d[\ude80-\udeff]'  // U+1F680 to U+1F6FF
+  ]
+
+  return name.replace(new RegExp(ranges.join('|'), 'g'), '-').replace(/[^\w.-]/g, '-')
 }
