b63a188c2a577b4a9e4b4b2b374d15bf092363f5
Brendan Forster <github@brendanforster.com>
2018-10-25 12:34:25 -0300

    iiiiiiimutability

--- app/src/lib/git-error-context.ts
+++ app/src/lib/git-error-context.ts
@@ -1,16 +1,13 @@
 import { Tip } from '../models/tip'
 
-type MergeErrorContext = {
-  kind: 'merge'
+type MergeConflictsErrorContext = {
+  /** The Git operation that triggered the conflicted state */
+  readonly kind: 'merge' | 'pull'
   /** The tip of the repository at the time of the merge operation */
   readonly tip?: Tip
   /** the branch passed to Git as part of the merge operation */
-  branch: string
-}
-
-type PullErrorContext = {
-  kind: 'pull'
+  readonly branch: string
 }
 
 /** A custom shape of data for actions to provide to help with error handling */
-export type IGitErrorContext = MergeErrorContext | PullErrorContext
+export type IGitErrorContext = MergeConflictsErrorContext
