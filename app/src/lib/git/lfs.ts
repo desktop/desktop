58353e4e6f3a52bf0efc0fa6ea014db9b29e5882
Brendan Forster <github@brendanforster.com>
2018-10-30 12:52:45 -0300

    extract common function to invoke git-lfs

--- app/src/lib/git/lfs.ts
+++ app/src/lib/git/lfs.ts
@@ -24,13 +24,17 @@ export async function installLFSHooks(
   await git(args, repository.path, 'installLFSHooks')
 }
 
-/** Is the repository configured to track any paths with LFS? */
-export async function isUsingLFS(repository: Repository): Promise<boolean> {
+function getLfsTrackOutput(repository: Repository) {
   const env = {
     GIT_LFS_TRACK_NO_INSTALL_HOOKS: '1',
   }
-  const result = await git(['lfs', 'track'], repository.path, 'isUsingLFS', {
+  return git(['lfs', 'track'], repository.path, 'isUsingLFS', {
     env,
   })
+}
+
+/** Is the repository configured to track any paths with LFS? */
+export async function isUsingLFS(repository: Repository): Promise<boolean> {
+  const result = await getLfsTrackOutput(repository)
   return result.stdout.length > 0
 }
