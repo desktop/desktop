d8f7373811fefba6cb82b1e5ee8ff9b2f3a8f5d9
Markus Olsson <j.markus.olsson@gmail.com>
2018-11-07 16:18:52 +0100

    A lil' breathing room

--- app/src/ui/blank-slate/blank-slate.tsx
+++ app/src/ui/blank-slate/blank-slate.tsx
@@ -65,9 +65,8 @@ export class BlankSlateView extends React.Component<IBlankSlateProps, {}> {
             <p className="drag-drop-info">
               <Octicon symbol={OcticonSymbol.lightBulb} />
               <div>
-                <strong>ProTip!</strong>
-                You can drag &amp; drop an existing repository folder here to
-                add it to Desktop
+                <strong>ProTip!</strong> You can drag &amp; drop an existing
+                repository folder here to add it to Desktop
               </div>
             </p>
           </div>
