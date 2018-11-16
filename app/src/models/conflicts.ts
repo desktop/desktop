86f90731fc55477cb0f09c8c183e38d2b283cd2f
Brendan Forster <github@brendanforster.com>
2018-11-16 10:45:13 -0400

    :fire: dead code

--- app/src/models/conflicts.ts
+++ app/src/models/conflicts.ts
@@ -1,15 +1,3 @@
-import { UnmergedEntry } from './status'
-
-/**
- * Meshing together the path and status information for a conflicted file,
- * because AppFileStatus lacks the granularity to understand what Git is
- * reporting.
- */
-export type ConflictedFile = {
-  path: string
-  status: UnmergedEntry
-}
-
 /**
  * The repository conflict state, for use when generating the status. By default
  * this will only be populated when Git reports that there are files with
