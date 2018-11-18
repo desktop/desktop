4a01a02ed8437c4944027c5c8dbb29ee5c93ffc7
msftrncs <msftrncs@midlands.net>
2018-11-18 03:25:39 -0600

    auto recover missing repository

--- app/src/ui/missing-repository.tsx
+++ app/src/ui/missing-repository.tsx
@@ -6,17 +6,35 @@ import { Repository } from '../models/repository'
 
 import { Button } from './lib/button'
 import { Row } from './lib/row'
+import { isGitRepository } from '../lib/git'
 
 interface IMissingRepositoryProps {
   readonly dispatcher: Dispatcher
   readonly repository: Repository
 }
 
+// The maximum value that can be used in setInterval or
+// setTimeout without it overflowing (2 ^ 31 - 1). See
+//  http://stackoverflow.com/a/16314807
+const MAX_INTERVAL = 2147483647
+
+const recoveryDelayLong = 10000
+const recoveryDelayShort = 1000
+
 /** The view displayed when a repository is missing. */
 export class MissingRepository extends React.Component<
   IMissingRepositoryProps,
   {}
 > {
+  private timer: number | null = null
+
+  private clearTimer() {
+    if (this.timer) {
+      window.clearTimeout(this.timer)
+      this.timer = null
+    }
+  }
+
   public render() {
     const buttons = new Array<JSX.Element>()
     buttons.push(
@@ -39,6 +57,8 @@ export class MissingRepository extends React.Component<
       </Button>
     )
 
+    this.awaitAutoRecovery(recoveryDelayShort) // attempt auto recovery, or set timer to try again later
+
     return (
       <UiView id="missing-repository-view">
         <div className="title-container">
@@ -60,10 +80,12 @@ export class MissingRepository extends React.Component<
   }
 
   private remove = () => {
+    this.clearTimer()
     this.props.dispatcher.removeRepositories([this.props.repository], false)
   }
 
   private locate = () => {
+    this.clearTimer()
     this.props.dispatcher.relocateRepository(this.props.repository)
   }
 
@@ -78,6 +100,7 @@ export class MissingRepository extends React.Component<
       return
     }
 
+    this.clearTimer()
     try {
       await this.props.dispatcher.cloneAgain(
         cloneURL,
@@ -85,6 +108,48 @@ export class MissingRepository extends React.Component<
       )
     } catch (error) {
       this.props.dispatcher.postError(error)
+    }
+  }
+
+  private awaitAutoRecovery(timeout: number) {
+    // set a timeout to wait to attempt an auto recovery
+    this.clearTimer()
+    this.timer = window.setTimeout(
+      this.attemptAutoRecovery,
+      Math.min(timeout, MAX_INTERVAL)
+    )
+  }
+
+  private readonly attemptAutoRecovery = () => {
+    this.AutoRecovery()
+  }
+
+  public componentWillUnmount() {
+    this.clearTimer()
+  }
+
+  private async AutoRecovery() {
+    if (this.timer && this.props.repository.missing) {
+      // do attempt to recover the missing repository, unless timer has been cleared
+      // if not, set timeout to try again
+
+      if (await isGitRepository(this.props.repository.path)) {
+        // a git repository was found on the original path
+        if (
+          (await this.props.dispatcher.updateRepositoryMissing(
+            this.props.repository,
+            false
+          )).missing
+        ) {
+          this.awaitAutoRecovery(recoveryDelayLong)
+        } else {
+          this.clearTimer() // repository is no longer missing
+        }
+      } else {
+        this.awaitAutoRecovery(recoveryDelayLong)
+      }
+    } else if (this.timer) {
+      this.clearTimer() // clear timer as repository is not marked missing ??
     }
   }
 }
