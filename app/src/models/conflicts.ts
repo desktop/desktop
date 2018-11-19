f2e6fbdf9748c2352dee64fc6f6ae4c6474b093b
Brendan Forster <github@brendanforster.com>
2018-11-19 17:20:21 -0400

    tidy up cruft for passing around conflict state

--- app/src/models/conflicts.ts
+++
@@ -1,8 +0,0 @@
-/**
- * The repository conflict state, for use when generating the status. By default
- * this will only be populated when Git reports that there are files with
- * conflicts in the repository.
- */
-export type ConflictState = {
-  readonly filesWithConflictMarkers: Map<string, number>
-}
