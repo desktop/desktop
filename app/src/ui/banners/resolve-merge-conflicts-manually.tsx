f447205d68cf538220b01633be54df2e71429e62
evelyn masso <outofambit@github.com>
2018-12-08 13:24:05 -0800

    fix banner styling

--- app/src/ui/banners/resolve-merge-conflicts-manually.tsx
+++ app/src/ui/banners/resolve-merge-conflicts-manually.tsx
@@ -3,6 +3,7 @@ import { Octicon, OcticonSymbol } from '../octicons'
 import { Banner } from './banner'
 import { Dispatcher } from '../../lib/dispatcher'
 import { Popup } from '../../models/popup'
+import { LinkButton } from '../lib/link-button'
 
 export function ResolveMergeConflictsManually({
   dispatcher,
@@ -16,11 +17,18 @@ export function ResolveMergeConflictsManually({
   readonly onDismissed: () => void
 }) {
   return (
-    <Banner id="successful-merge" onDismissed={onDismissed}>
+    <Banner id="successful-merge" dismissable={false} onDismissed={onDismissed}>
       <Octicon className="alert-icon" symbol={OcticonSymbol.alert} />
       <div className="banner-message">
-        Resolve conflicts and commit to merge into <strong>{ourBranch}</strong>
-        <a onClick={() => dispatcher.showPopup(popup)}>View conflicts</a>
+        <div>
+          Resolve conflicts and commit to merge into{' '}
+          <strong>{ourBranch}</strong>.&nbsp;
+        </div>
+        <div>
+          <LinkButton onClick={() => dispatcher.showPopup(popup)}>
+            View conflicts
+          </LinkButton>
+        </div>
       </div>
     </Banner>
   )
