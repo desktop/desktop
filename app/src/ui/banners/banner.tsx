ec3e906bf58d8aba067d5884770d800d07f3255a
evelyn masso <outofambit@github.com>
2018-12-08 13:18:05 -0800

    banners can be not dismissable

--- app/src/ui/banners/banner.tsx
+++ app/src/ui/banners/banner.tsx
@@ -4,6 +4,7 @@ import { Octicon, OcticonSymbol } from '../octicons'
 interface IBannerProps {
   readonly id?: string
   readonly timeout?: number
+  readonly dismissable?: boolean
   readonly onDismissed: () => void
 }
 
@@ -11,14 +12,19 @@ export class Banner extends React.Component<IBannerProps, {}> {
   private timeoutId: NodeJS.Timer | null = null
 
   public render() {
-    return (
-      <div id={this.props.id} className="banner">
-        <div className="contents">{this.props.children}</div>
+    const close =
+      this.props.dismissable !== undefined &&
+      this.props.dismissable === true ? (
         <div className="close">
           <a onClick={this.props.onDismissed}>
             <Octicon symbol={OcticonSymbol.x} />
           </a>
         </div>
+      ) : null
+    return (
+      <div id={this.props.id} className="banner">
+        <div className="contents">{this.props.children}</div>
+        {close}
       </div>
     )
   }
