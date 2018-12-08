1a216126b7efbdcde870a459e33d7238aeb51669
evelyn masso <outofambit@github.com>
2018-12-08 13:39:06 -0800

    make banner a class component and style more

--- app/src/ui/banners/resolve-merge-conflicts-manually.tsx
+++ app/src/ui/banners/resolve-merge-conflicts-manually.tsx
@@ -5,31 +5,37 @@ import { Dispatcher } from '../../lib/dispatcher'
 import { Popup } from '../../models/popup'
 import { LinkButton } from '../lib/link-button'
 
-export function ResolveMergeConflictsManually({
-  dispatcher,
-  ourBranch,
-  popup,
-  onDismissed,
-}: {
+interface IMergeConflictsBannerProps {
   readonly dispatcher: Dispatcher
   readonly ourBranch: string
   readonly popup: Popup
   readonly onDismissed: () => void
-}) {
-  return (
-    <Banner id="successful-merge" dismissable={false} onDismissed={onDismissed}>
-      <Octicon className="alert-icon" symbol={OcticonSymbol.alert} />
-      <div className="banner-message">
-        <div>
-          Resolve conflicts and commit to merge into{' '}
-          <strong>{ourBranch}</strong>.&nbsp;
+}
+
+export class ResolveMergeConflictsManually extends React.Component<
+  IMergeConflictsBannerProps,
+  {}
+> {
+  private openDialog = () => {
+    this.props.onDismissed()
+    this.props.dispatcher.showPopup(this.props.popup)
+  }
+  public render() {
+    return (
+      <Banner
+        id="merge-conflicts-banner"
+        dismissable={false}
+        onDismissed={this.props.onDismissed}
+      >
+        <Octicon className="alert-icon" symbol={OcticonSymbol.alert} />
+        <div className="banner-message">
+          <span>
+            Resolve conflicts and commit to merge into{' '}
+            <strong>{this.props.ourBranch}</strong>.
+          </span>
+          <LinkButton onClick={this.openDialog}>View conflicts</LinkButton>
         </div>
-        <div>
-          <LinkButton onClick={() => dispatcher.showPopup(popup)}>
-            View conflicts
-          </LinkButton>
-        </div>
-      </div>
-    </Banner>
-  )
+      </Banner>
+    )
+  }
 }
