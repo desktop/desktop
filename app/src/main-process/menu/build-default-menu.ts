5868f320269eaab02b6a7972b15a81fedb56fd38
Cem Schemel <koralcem@gmail.com>
2018-12-13 16:52:06 -0800

    Add a dev-only menu item to show the release notes popup
    The addition is meant to facilitate working on the release notes popup easier. When modifying the popup, one can see the effect of their changes on a popup for a fake release, rather than trying to conjure up an actual pending update.

--- app/src/main-process/menu/build-default-menu.ts
+++ app/src/main-process/menu/build-default-menu.ts
@@ -415,6 +415,15 @@ export function buildDefaultMenu({
       {
         label: 'Crash renderer process…',
         click: emit('boomtown'),
+      },
+      {
+        label: 'Show popup',
+        submenu: [
+          {
+            label: 'Release notes',
+            click: emit('show-release-notes-popup'),
+          },
+        ],
       }
     )
   }
