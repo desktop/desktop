e6b70083d22338bd9b49a120ac7e6b3af84b2ad5
Brendan Forster <github@brendanforster.com>
2019-01-02 13:10:08 -0400

    update integration tests to emit to specific file

--- app/jest.integration.config.js
+++ app/jest.integration.config.js
@@ -6,7 +6,16 @@ module.exports = {
   testMatch: ['**/integration/**/*-test.ts{,x}'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupTestFrameworkScriptFile: '<rootDir>/test/setup-test-framework.ts',
-  reporters: ['default', 'jest-junit'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '.',
+        outputName: 'junit-integration-tests.xml',
+      },
+    ],
+  ],
   globals: {
     'ts-jest': {
       useBabelrc: true,
