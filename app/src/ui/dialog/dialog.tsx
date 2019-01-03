22363183422a8209f33edb2abc3b675c06daf603
Altaf <ashaikh@mobiquityinc.com>
2019-01-03 18:05:41 +0530

    Revert changes

--- app/src/ui/dialog/dialog.tsx
+++ app/src/ui/dialog/dialog.tsx
@@ -248,8 +248,6 @@ export class Dialog extends React.Component<IDialogProps, IDialogState> {
   }
 
   private onDialogCancel = (e: Event) => {
-    console.log(e)
-
     e.preventDefault()
     this.onDismiss()
   }
@@ -317,8 +315,6 @@ export class Dialog extends React.Component<IDialogProps, IDialogState> {
   }
 
   private onKeyDown = (event: KeyboardEvent) => {
-    // console.log(event)
-    console.log(this.props)
     const shortcutKey = __DARWIN__ ? event.metaKey : event.ctrlKey
     if ((shortcutKey && event.key === 'w') || event.key === 'Escape') {
       this.onDialogCancel(event)
