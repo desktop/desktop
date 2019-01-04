78a3ad184d6965bc6881c9aaae443415c93fbaec
Brendan Forster <github@brendanforster.com>
2019-01-04 12:12:35 -0400

    extract function to compute relative time information

--- app/src/ui/relative-time.tsx
+++ app/src/ui/relative-time.tsx
@@ -1,6 +1,65 @@
 import * as React from 'react'
 import * as moment from 'moment'
 
+const SECOND = 1000
+const MINUTE = SECOND * 60
+const HOUR = MINUTE * 60
+const DAY = HOUR * 24
+
+type RelativeTimeResult = {
+  /**
+   * Represents the relative time between the current time and the provided date
+   */
+  readonly relativeText: string
+  /**
+   * The absolute timestamp for the provided date
+   */
+  readonly absoluteText: string
+  /**
+   * A timer duration to set before updating the relative text, or `null` if it
+   * should be skipped
+   */
+  readonly timerDuration: number | null
+}
+
+/**
+ * Compute the relative date information between a provided date and the current
+ * date.
+ */
+export function getRelativeDateInfo(date: Date): RelativeTimeResult {
+  const then = moment(date)
+  const now = moment()
+  const diff = then.diff(now)
+  const duration = Math.abs(diff)
+  const absoluteText = then.format('LLLL')
+
+  let relativeText: string
+  let timerDuration: number | null = null
+
+  if (diff > 0 && duration > MINUTE) {
+    // Future date, let's just show as absolute and reschedule. If it's less
+    // than a minute into the future we'll treat it as 'just now'.
+    relativeText = then.format('lll')
+    timerDuration = duration
+  } else if (duration < MINUTE) {
+    relativeText = 'just now'
+    timerDuration = MINUTE - duration
+  } else if (duration < HOUR) {
+    relativeText = then.from(now)
+    timerDuration = MINUTE
+  } else if (duration < DAY) {
+    relativeText = then.from(now)
+    timerDuration = HOUR
+  } else if (duration < 7 * DAY) {
+    relativeText = then.from(now)
+    timerDuration = 6 * HOUR
+  } else {
+    relativeText = then.format('ll')
+  }
+
+  return { timerDuration, absoluteText, relativeText }
+}
+
 interface IRelativeTimeProps {
   /**
    * The date instance that will be used for calculating
