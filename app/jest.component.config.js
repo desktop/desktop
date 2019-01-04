1575bea13aea318f3460b21f3a9675a8a3218737
Brendan Forster <github@brendanforster.com>
2019-01-04 12:04:51 -0400

    add component tests to root script

---
+++ app/jest.component.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  roots: ['<rootDir>/src/', '<rootDir>/test/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testMatch: ['**/component/**/*-test.ts{,x}'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['<rootDir>/test/globals.ts', '<rootDir>/test/unit-test-env.ts'],
+  setupTestFrameworkScriptFile: '<rootDir>/test/setup-test-framework.ts',
+  collectCoverageFrom: [
+    'src/ui/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/*.d.*',
+    // not focused on testing these areas currently
+    '!src/ask-pass/**/*',
+    '!src/cli/**/*',
+    '!src/crash/**/*',
+    '!src/highlighter/**/*',
+    // ignore index files
+    '!**/index.ts',
+  ],
+  reporters: ['default', 'jest-junit'],
+  coverageReporters: ['text-summary', 'json', 'html', 'cobertura'],
+  globals: {
+    'ts-jest': {
+      useBabelrc: true,
+    },
+  },
+}
