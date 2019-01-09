f50720a02699e5e9cef6b25288f22e81209e6264
Markus Olsson <j.markus.olsson@gmail.com>
2019-01-09 18:28:29 +0100

    :book: blankslate action props

--- app/src/ui/changes/blankslate-action.tsx
+++ app/src/ui/changes/blankslate-action.tsx
@@ -3,12 +3,49 @@ import * as classNames from 'classnames'
 import { Button } from '../lib/button'
 
 interface IBlankSlateActionProps {
+  /**
+   * The title, or "header" text for a blank slate
+   * action.
+   */
   readonly title: string
+
+  /**
+   * An optional description to be rendered directly
+   * underneath the title.
+   */
   readonly description?: string | JSX.Element
+
+  /**
+   * A text or set of elements used to present information
+   * to the user about how and where to access the action
+   * outside of the blank slate action.
+   */
   readonly discoverabilityContent: string | JSX.Element
+
+  /**
+   * The text, or "label", for the action button.
+   */
   readonly buttonText: string | JSX.Element
+
+  /**
+   * A callback which is invoked when the user clicks
+   * or activates the action using their keyboard.
+   */
   readonly onClick: () => void
+
+  /**
+   * The type of action, currently supported actions are
+   * normal, and primary. Primary actions are visually
+   * distinct from normal actions in order to stand out
+   * as a highly probable next step action.
+   */
   readonly type?: 'normal' | 'primary'
+
+  /**
+   * Whether or not the action should be disabled. Disabling
+   * the action means that the button will no longer be
+   * clickable.
+   */
   readonly disabled?: boolean
 }
 
@@ -24,9 +61,7 @@ export class BlankslateAction extends React.Component<
 > {
   public render() {
     const primary = this.props.type === 'primary'
-    const cn = classNames('blankslate-action', this.props.className, {
-      primary,
-    })
+    const cn = classNames('blankslate-action', { primary })
     const description =
       this.props.description === undefined ? (
         undefined
