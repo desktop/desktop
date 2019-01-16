3f2be3ba3200fc6811e3e518227cf0c281782058
Brendan Forster <github@brendanforster.com>
2019-01-16 10:46:42 -0400

    promisify these APIs to simplify control flow

--- script/licenses/update-license-dump.ts
+++ script/licenses/update-license-dump.ts
@@ -2,74 +2,61 @@
 
 import * as path from 'path'
 import * as fs from 'fs-extra'
+import { promisify } from 'util'
 
 import { licenseOverrides } from './license-overrides'
 
 import * as legalEagle from 'legal-eagle'
 import { getVersion } from '../../app/package-info'
 
-export function updateLicenseDump(
+export async function updateLicenseDump(
   projectRoot: string,
   outRoot: string
 ): Promise<void> {
   const appRoot = path.join(projectRoot, 'app')
   const outPath = path.join(outRoot, 'static', 'licenses.json')
 
-  return new Promise((resolve, reject) => {
-    legalEagle(
-      { path: appRoot, overrides: licenseOverrides, omitPermissive: true },
-      (err, summary) => {
-        if (err) {
-          reject(err)
-          return
-        }
+  let summary = await promisify(legalEagle)({
+    path: appRoot,
+    overrides: licenseOverrides,
+    omitPermissive: true,
+  })
 
-        if (Object.keys(summary).length > 0) {
-          let licensesMessage = ''
-          for (const key in summary) {
-            const license = summary[key]
-            licensesMessage += `${key} (${license.repository}): ${
-              license.license
-            }\n`
-          }
+  if (Object.keys(summary).length > 0) {
+    let licensesMessage = ''
+    for (const key in summary) {
+      const license = summary[key]
+      licensesMessage += `${key} (${license.repository}): ${license.license}\n`
+    }
 
-          const overridesPath = path.join(__dirname, 'license-overrides.ts')
+    const overridesPath = path.join(__dirname, 'license-overrides.ts')
 
-          const message = `The following dependencies have unknown or non-permissive licenses. Check it out and update ${overridesPath} if appropriate:\n${licensesMessage}`
-          reject(new Error(message))
-        } else {
-          legalEagle(
-            { path: appRoot, overrides: licenseOverrides },
-            (err, summary) => {
-              if (err) {
-                reject(err)
-                return
-              }
+    const message = `The following dependencies have unknown or non-permissive licenses. Check it out and update ${overridesPath} if appropriate:\n${licensesMessage}`
+    throw new Error(message)
+  }
 
-              // legal-eagle still chooses to ignore the LICENSE at the root
-              // this injects the current license and pins the source URL before we
-              // dump the JSON file to disk
-              const licenseSource = path.join(projectRoot, 'LICENSE')
-              const licenseText = fs.readFileSync(licenseSource, {
-                encoding: 'utf-8',
-              })
-              const appVersion = getVersion()
+  summary = await promisify(legalEagle)({
+    path: appRoot,
+    overrides: licenseOverrides,
+  })
 
-              summary[`desktop@${appVersion}`] = {
-                repository: 'https://github.com/desktop/desktop',
-                license: 'MIT',
-                source: `https://github.com/desktop/desktop/blob/release-${appVersion}/LICENSE`,
-                sourceText: licenseText,
-              }
+  // legal-eagle still chooses to ignore the LICENSE at the root
+  // this injects the current license and pins the source URL before we
+  // dump the JSON file to disk
+  const licenseSource = path.join(projectRoot, 'LICENSE')
+  const licenseText = fs.readFileSync(licenseSource, {
+    encoding: 'utf-8',
+  })
+  const appVersion = getVersion()
 
-              fs.writeFileSync(outPath, JSON.stringify(summary), {
-                encoding: 'utf8',
-              })
-              resolve()
-            }
-          )
-        }
-      }
-    )
+  summary[`desktop@${appVersion}`] = {
+    repository: 'https://github.com/desktop/desktop',
+    license: 'MIT',
+    source: `https://github.com/desktop/desktop/blob/release-${appVersion}/LICENSE`,
+    sourceText: licenseText,
+  }
+
+  fs.writeFileSync(outPath, JSON.stringify(summary), {
+    encoding: 'utf8',
   })
 }
