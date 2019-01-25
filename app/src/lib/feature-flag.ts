b90d203f485ee299dc8443eccad53139044fc42b
say25 <say25@cornell.edu>
2019-01-25 14:06:37 -0500

    :x: enableFileSizeWarningCheck Feature Flag

--- app/src/lib/feature-flag.ts
+++ app/src/lib/feature-flag.ts
@@ -42,11 +42,6 @@ export function enableRecurseSubmodulesFlag(): boolean {
   return enableBetaFeatures()
 }
 
-/** Should the app check and warn the user about committing large files? */
-export function enableFileSizeWarningCheck(): boolean {
-  return true
-}
-
 /** Should the app set protocol.version=2 for any fetch/push/pull/clone operation? */
 export function enableGitProtocolVersionTwo(): boolean {
   return enableBetaFeatures()
