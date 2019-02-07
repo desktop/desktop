9caba2d13e57640188ca885499fe95ed14611d80
William Shepherd <iamwillshepherd@github.com>
2019-02-07 10:55:18 -0600

    Increment when menu is clicked
    Co-Authored-By: Billy Griffin <billygriffin@users.noreply.github.com>

--- app/src/ui/app.tsx
+++ app/src/ui/app.tsx
@@ -1832,6 +1832,7 @@ export class App extends React.Component<IAppProps, IAppState> {
 
     if (currentPullRequest == null) {
       dispatcher.createPullRequest(state.repository)
+      dispatcher.recordCreatePullRequestFromMenu()
     } else {
       dispatcher.showPullRequest(state.repository)
     }
