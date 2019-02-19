c7787b114a3d223402f8f7452e57ba55b110a788
Brendan Forster <github@brendanforster.com>
2019-02-19 17:52:05 -0400

    extract rendering of banner to standalone function

---
+++ app/src/ui/banners/render-banner.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+
+import { assertNever } from '../../lib/fatal-error'
+
+import { Banner, BannerType } from '../../models/banner'
+
+import { Dispatcher } from '../dispatcher'
+import { MergeConflictsBanner } from './merge-conflicts-banner'
+
+import { SuccessfulMerge } from './successful-merge'
+
+export function renderBanner(
+  banner: Banner | null,
+  dispatcher: Dispatcher,
+  onDismissed: () => void
+): JSX.Element | null {
+  if (!banner) {
+    return null
+  }
+
+  switch (banner.type) {
+    case BannerType.SuccessfulMerge:
+      return (
+        <SuccessfulMerge
+          ourBranch={banner.ourBranch}
+          theirBranch={banner.theirBranch}
+          onDismissed={onDismissed}
+          key={'successful-merge'}
+        />
+      )
+    case BannerType.MergeConflictsFound:
+      return (
+        <MergeConflictsBanner
+          dispatcher={dispatcher}
+          ourBranch={banner.ourBranch}
+          popup={banner.popup}
+          onDismissed={onDismissed}
+          key={'merge-conflicts'}
+        />
+      )
+
+    default:
+      return assertNever(banner, `Unknown popup type: ${banner}`)
+  }
+}
