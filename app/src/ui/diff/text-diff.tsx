a71b099e9c461591143b5e32347adb4ff7523f02
evelyn masso <outofambit@github.com>
2019-02-26 13:12:47 -0800

    more readable syntax

--- app/src/ui/diff/text-diff.tsx
+++ app/src/ui/diff/text-diff.tsx
@@ -702,12 +702,14 @@ export class TextDiff extends React.Component<ITextDiffProps, {}> {
       return
     }
 
-    if (canSelect(this.props.file)) {
-      if (
-        !canSelect(prevProps.file) ||
-        this.props.file.selection !== prevProps.file.selection ||
-        this.props.text === prevProps.text
-      ) {
+    const { file } = this.props
+    const { file: prevFile } = prevProps
+    if (canSelect(file)) {
+      if (!canSelect(prevFile)) {
+        this.updateViewport()
+      } else if (file.selection !== prevFile.selection) {
+        this.updateViewport()
+      } else if (this.props.text === prevProps.text) {
         // If the text has changed the gutters will be recreated
         // regardless but if it hasn't then we'll need to update
         // the viewport.
