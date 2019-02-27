75634dc7b290482cb36b16fab3ce604671d029b9
HashimotoYT <hashimoto.stream@gmail.com>
2019-02-27 23:06:21 +0200

    naming

--- app/src/ui/about/about.tsx
+++ app/src/ui/about/about.tsx
@@ -14,7 +14,7 @@ import { assertNever } from '../../lib/fatal-error'
 import { ReleaseNotesUri } from '../lib/releases'
 import { encodePathAsUrl } from '../../lib/path'
 
-const DesktopLogo = encodePathAsUrl(__dirname, 'static/logo-64x64@2.png')
+const DesktopLogo = encodePathAsUrl(__dirname, 'static/logo-64x64@2x.png')
 
 interface IAboutProps {
   /**
