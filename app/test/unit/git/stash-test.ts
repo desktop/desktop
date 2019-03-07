b0a9e679f8520d50ecb5ccc0f71c7481a2a77af8
William Shepherd <iamwillshepherd@github.com>
2019-03-07 16:35:42 -0600

    Use empty repo

--- app/test/unit/git/stash-test.ts
+++ app/test/unit/git/stash-test.ts
@@ -1,27 +1,19 @@
 import * as FSE from 'fs-extra'
 import * as path from 'path'
 import { Repository } from '../../../src/models/repository'
-import { setupFixtureRepository } from '../../helpers/repositories'
+import { setupEmptyRepository } from '../../helpers/repositories'
 import { GitProcess } from 'dugite'
 import { MagicStashString, getStashEntries } from '../../../src/lib/git/stash'
 
 describe('git/stash', () => {
-  let repository: Repository | null = null
-
-  beforeEach(async () => {
-    const testRepoPath = await setupFixtureRepository('stashing-test-repo')
-    repository = new Repository(testRepoPath, -1, null, false)
-  })
-
   describe('getStashEntries', () => {
     it('returns all stash entries created by desktop', async () => {
-      await FSE.writeFile(
-        path.join(repository!.path, 'README.md'),
-        'Hi world\n'
-      )
-      await stash(repository!)
+      const repository = await setupEmptyRepository()
 
-      const stashEntries = await getStashEntries(repository!)
+      await FSE.writeFile(path.join(repository.path, 'README.md'), 'Hi world\n')
+      await stash(repository)
+
+      const stashEntries = await getStashEntries(repository)
 
       expect(stashEntries).toHaveLength(1)
     })
