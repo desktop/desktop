02be754147a93ba30743fea789973e254da6f9da
William Shepherd <iamwillshepherd@github.com>
2019-03-07 22:35:38 -0600

    add function signature

--- app/src/lib/git/stash.ts
+++ app/src/lib/git/stash.ts
@@ -5,3 +5,12 @@ export interface IStashEntry {
   /** The SHA of the commit object created as a result of stashing. */
   readonly stashSha: string
 }
+
+/**
+ * Applies the stash entry identified by matching `stashSha` to its commit hash.
+ *
+ * To see the commit hash of stash entry, run
+ * `git log -g refs/stash --pretty="%nentry: %gd%nsubject: %gs%nhash: %H%n"`
+ * in a repo with some stash entries.
+ */
+export async function applyStashEntry(stashSha: string): Promise<void> {}
