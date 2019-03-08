7c2f75c2ba938ba5b2fdb5f524f03b1bf09fb260
William Shepherd <iAmWillShepherd@users.noreply.github.com>
2019-03-07 23:54:37 -0600

    Minor type fix

--- app/src/lib/git/core.ts
+++ app/src/lib/git/core.ts
@@ -44,7 +44,7 @@ export interface IGitExecutionOptions extends DugiteExecutionOptions {
  */
 export interface IGitResult extends DugiteResult {
   /**
-   * The parsed git error. This will be null when the exit code is include in
+   * The parsed git error. This will be null when the exit code is included in
    * the `successExitCodes`, or when dugite was unable to parse the
    * error.
    */
