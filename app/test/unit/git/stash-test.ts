1432bb55abb4ece5a41cac186704a842e83c5c93
William Shepherd <iamwillshepherd@github.com>
2019-03-13 10:48:39 -0500

    Cleanup test setup

--- app/test/unit/git/stash-test.ts
+++ app/test/unit/git/stash-test.ts
@@ -7,15 +7,18 @@ import { MagicStashString, getStashEntries } from '../../../src/lib/git/stash'
 
 describe('git/stash', () => {
   describe('getStashEntries', () => {
-    it('returns all stash entries created by desktop', async () => {
-      const repository = await setupEmptyRepository()
+    let repository: Repository
+    let readme: string
 
-      const readme = path.join(repository.path, 'README.md')
+    beforeEach(async () => {
+      repository = await setupEmptyRepository()
+      readme = path.join(repository.path, 'README.md')
       await FSE.writeFile(readme, '')
       await GitProcess.exec(['add', 'README.md'], repository.path)
       await GitProcess.exec(['commit', '-m', 'initial commit'], repository.path)
-      await FSE.appendFile(readme, 'Hello\n')
-      await stash(repository)
+    })
+
+    it('returns all stash entries created by Desktop', async () => {
 
       const stashEntries = await getDesktopStashEntries(repository)
 
