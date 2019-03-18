5c4bdd1800fb4d90a268c9d0fe68dd2720a412f5
Brendan Forster <github@brendanforster.com>
2019-03-18 15:39:00 -0300

    we did these metrics

--- app/src/ui/dispatcher/error-handlers.ts
+++ app/src/ui/dispatcher/error-handlers.ts
@@ -394,8 +394,6 @@ export async function rebaseConflictsHandler(
     return error
   }
 
-  // TODO: metrics - https://github.com/desktop/desktop/issues/6550
-
   const { currentBranch } = gitContext
 
   dispatcher.showPopup({
