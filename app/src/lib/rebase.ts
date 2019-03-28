3e49a6ee86aff5da70fc2e69aaa6496efa2511f4
Brendan Forster <github@brendanforster.com>
2019-03-28 15:34:55 -0300

    add helper function for formatting rebase percentage

---
+++ app/src/lib/rebase.ts
@@ -0,0 +1,10 @@
+import { clamp } from './clamp'
+
+/**
+ * Format rebase percentage to ensure it's a value between 0 and 1, but to also
+ * constrain it to two significant figures, avoiding the remainder that comes
+ * with floating point division.
+ */
+export function formatRebaseValue(value: number) {
+  return Math.round(clamp(value, 0, 1) * 100) / 100
+}
