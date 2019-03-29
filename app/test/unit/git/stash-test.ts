a3057e091661f06a250398c7d82db7690406c4d8
William Shepherd <iamwillshepherd@github.com>
2019-03-29 13:45:54 -0500

    Bringer over changes from create-stash branch

--- app/test/unit/git/stash-test.ts
+++ app/test/unit/git/stash-test.ts
@@ -6,7 +6,9 @@ import { GitProcess } from 'dugite'
 import {
   getDesktopStashEntries,
   createDesktopStashMessage,
+  createDesktopStashEntry,
 } from '../../../src/lib/git/stash'
+import { getTipOrError } from '../../helpers/tip'
 
 describe('git/stash', () => {
   describe('getDesktopStashEntries', () => {
@@ -23,18 +25,16 @@ describe('git/stash', () => {
 
     it('handles unborn repo by returning empty list', async () => {
       const repo = await setupEmptyRepository()
-      let didFail = false
-      readme = path.join(repo.path, 'README.md')
-      await FSE.writeFile(readme, '')
-      await stash(repo)
 
-      try {
-        await getDesktopStashEntries(repo)
-      } catch (e) {
-        didFail = true
-      }
+      const entries = await getDesktopStashEntries(repo)
 
-      expect(didFail).toBe(true)
+      expect(entries).toHaveLength(0)
+    })
+
+    it('returns an empty list when no stash entries have been created', async () => {
+      const entries = await getDesktopStashEntries(repository)
+
+      expect(entries).toHaveLength(0)
     })
 
     it('returns all stash entries created by Desktop', async () => {
@@ -46,22 +46,69 @@ describe('git/stash', () => {
       expect(stashEntries[0].branchName).toBe('master')
     })
   })
+
+  describe('createStashEntry', () => {
+    let repository: Repository
+    let readme: string
+
+    beforeEach(async () => {
+      repository = await setupEmptyRepository()
+      readme = path.join(repository.path, 'README.md')
+      await FSE.writeFile(readme, '')
+      await GitProcess.exec(['add', 'README.md'], repository.path)
+      await GitProcess.exec(['commit', '-m', 'initial commit'], repository.path)
+    })
+
+    it('creates a stash entry when repo is not unborn or in any kind of conflict or rebase state', async () => {
+      const branchName = 'master'
+      await FSE.appendFile(readme, 'just testing stuff')
+
+      const tipCommit = await getTipOrError(repository)
+      await createDesktopStashEntry(repository, branchName, tipCommit.sha)
+
+      const result = await getDesktopStashEntries(repository)
+      expect(result).toHaveLength(1)
+    })
+  })
+
+  describe('createDesktopStashMessage', () => {
+    it('creates message that matches Desktop stash entry format', () => {
+      const branchName = 'master'
+      const tipSha = 'bc45b3b97993eed2c3d7872a0b766b3e29a12e4b'
+
+      const message = createDesktopStashMessage(branchName, tipSha)
+
+      expect(message).toBe(
+        '!!GitHub_Desktop<master@bc45b3b97993eed2c3d7872a0b766b3e29a12e4b>'
+      )
+    })
+  })
 })
 
+/**
+ * Creates a stash entry using `git stash push` to allow for similating
+ * entries created via the CLI and Desktop
+ *
+ * @param repository the repository to create the stash entry for
+ * @param message passing no message will similate Desktop creating the entry
+ */
 async function stash(repository: Repository, message?: string) {
-  const result = await GitProcess.exec(['rev-parse', 'HEAD'], repository.path)
-  const tipSha = result.stdout.trim()
+  const tipCommit = await getTipOrError(repository)
   await GitProcess.exec(
     [
       'stash',
       'push',
       '-m',
-      message || createDesktopStashMessage('master', tipSha),
+      message || createDesktopStashMessage('master', tipCommit.sha),
     ],
     repository.path
   )
 }
 
+/**
+ * Generates a several stash entries where 1 of the entries
+ * is created by Desktop
+ */
 async function generateTestStashEntries(repository: Repository) {
   const readme = path.join(repository.path, 'README.md')
 
