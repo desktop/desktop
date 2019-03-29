028b7f8bf30b0f0472fa1de5cc086eededa08da8
Brendan Forster <github@brendanforster.com>
2019-03-29 17:13:49 -0300

    avoid unnecessary setState calls if the component is no longer mounted

--- app/src/ui/rebase/rebase-flow.tsx
+++ app/src/ui/rebase/rebase-flow.tsx
@@ -92,6 +92,8 @@ export class RebaseFlow extends React.Component<
   IRebaseFlowProps,
   IRebaseFlowState
 > {
+  private ignoreUpdateEvents = false
+
   public constructor(props: IRebaseFlowProps) {
     super(props)
 
@@ -105,6 +107,10 @@ export class RebaseFlow extends React.Component<
       },
       userHasResolvedConflicts: false,
     }
+  }
+
+  public componentWillUnmount() {
+    this.ignoreUpdateEvents = true
   }
 
   public async componentDidUpdate() {
@@ -131,6 +137,10 @@ export class RebaseFlow extends React.Component<
         // waiting before the CSS animation to give the progress UI a chance to
         // show it reaches 100%
         await timeout(1000)
+
+        if (this.ignoreUpdateEvents) {
+          return
+        }
 
         this.setState(
           {
@@ -195,6 +205,10 @@ export class RebaseFlow extends React.Component<
   }
 
   private moveToCompletedState = () => {
+    if (this.ignoreUpdateEvents) {
+      return
+    }
+
     // this ensures the progress bar fills to 100%, while `componentDidUpdate`
     // detects and handles the state transition after a period of time to ensure
     // the UI shows _something_ before closing the dialog
