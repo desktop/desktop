6ddbe41d2224c194667a19c50b67380eb2b2bc43
Markus Olsson <j.markus.olsson@gmail.com>
2019-04-11 10:17:20 -0500

    Update app/src/ui/stash-changes/overwrite-stashed-changes-dialog.tsx
    Co-Authored-By: iAmWillShepherd <iAmWillShepherd@users.noreply.github.com>

--- app/src/ui/stash-changes/overwrite-stashed-changes-dialog.tsx
+++ app/src/ui/stash-changes/overwrite-stashed-changes-dialog.tsx
@@ -44,7 +44,7 @@ export class OverwriteStash extends React.Component<
         onSubmit={this.onSubmit}
         onDismissed={this.props.onDismissed}
       >
-        <DialogContent className="dialog-content">
+        <DialogContent>
           <Row>
             Clear or restore your current stash before continuing, or your
             current stash will be overwritten.
