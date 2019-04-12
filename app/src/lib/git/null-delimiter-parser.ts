093bab88071ccb03f54f89efc05aa02e7a47f879
Markus Olsson <j.markus.olsson@gmail.com>
2019-04-12 16:23:02 +0200

    Stupid for-each-ref
    For each ref always terminates records with a newline. So much for our super elegant split solution. Here we go full on parsing mode

--- app/src/lib/git/null-delimiter-parser.ts
+++ app/src/lib/git/null-delimiter-parser.ts
@@ -2,23 +2,43 @@ export class NullDelimiterParser<T extends { [name: string]: string }> {
   private readonly keys = new Array<string>()
   public readonly format: string = ''
 
-  public constructor(fields: T) {
+  public constructor(fields: T, delimiterString = '%x00') {
     for (const [key, value] of Object.entries(fields)) {
       this.keys.push(key)
-      this.format = this.format ? `${this.format}%x00${value}` : value
+      this.format = `${this.format}${value}${delimiterString}`
     }
   }
 
   public *parse(output: string): Iterable<T> {
-    const fields = output.split('\0')
     const { keys } = this
 
-    for (let i = 0; i < fields.length - keys.length; i += keys.length) {
-      const entry = {} as T
-      for (let j = 0; j < keys.length; j++) {
-        entry[keys[j]] = fields[i + j]
+    let head = 0
+    let tail = 0
+    let fieldIndex = 0
+    let entry = {} as T
+
+    while (head < output.length && (tail = output.indexOf('\0', head)) !== -1) {
+      const value = output.substring(head, tail)
+      const key = keys[fieldIndex % keys.length]
+      entry[key] = value
+
+      head = tail + 1
+      fieldIndex++
+
+      if (fieldIndex % keys.length === 0) {
+        yield entry
+        entry = {} as T
+
+        if (head < output.length) {
+          if (output[head] !== '\0' && output[head] !== '\n') {
+            const char = output.charCodeAt(head)
+            throw new Error(
+              `Unexpected character at end of record: '0x${char.toString(16)}'`
+            )
+          }
+          head++
+        }
       }
-      yield entry
     }
   }
 }
