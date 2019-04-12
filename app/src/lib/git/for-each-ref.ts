28e0db1260070429190dc916912d492def170c3d
Markus Olsson <j.markus.olsson@gmail.com>
2019-04-12 16:30:57 +0200

    Convert for-each-ref to use NDP

--- app/src/lib/git/for-each-ref.ts
+++ app/src/lib/git/for-each-ref.ts
@@ -1,29 +1,32 @@
 import { git } from './core'
 import { GitError } from 'dugite'
-
 import { Repository } from '../../models/repository'
-import { Branch, BranchType } from '../../models/branch'
+import { Branch, BranchType, IBranchTip } from '../../models/branch'
 import { CommitIdentity } from '../../models/commit-identity'
+import { NullDelimiterParser } from './null-delimiter-parser'
 
 /** Get all the branches. */
 export async function getBranches(
   repository: Repository,
   ...prefixes: string[]
 ): Promise<ReadonlyArray<Branch>> {
-  const delimiter = '1F'
-  const delimiterString = String.fromCharCode(parseInt(delimiter, 16))
-
-  const format = [
-    '%(refname)',
-    '%(refname:short)',
-    '%(upstream:short)',
-    '%(objectname)', // SHA
-    '%(objectname:short)', // short SHA
-    '%(author)',
-    '%(committer)',
-    '%(symref)',
-    `%${delimiter}`, // indicate end-of-line as %(body) may contain newlines
-  ].join('%00')
+  const parser = new NullDelimiterParser(
+    {
+      fullName: '%(refname)',
+      shortName: '%(refname:short)',
+      upstreamShortName: '%(upstream:short)',
+      sha: '%(objectname)',
+      shortSha: '%(objectname:short)',
+      author: '%(author)',
+      committer: '%(committer)',
+      parent: '%(parent)',
+      symRef: '%(symref)',
+      subject: '%(subject)',
+      body: '%(body)',
+      trailers: '%(trailers:unfold,only)',
+    },
+    '%00'
+  )
 
   if (!prefixes || !prefixes.length) {
     prefixes = ['refs/heads', 'refs/remotes']
@@ -33,7 +36,7 @@ export async function getBranches(
   // see https://github.com/desktop/desktop/pull/5299#discussion_r206603442 for
   // discussion about what needs to change
   const result = await git(
-    ['for-each-ref', `--format=${format}`, ...prefixes],
+    ['for-each-ref', `--format=${parser.format}`, ...prefixes],
     repository.path,
     'getBranches',
     { expectedErrors: new Set([GitError.NotAGitRepository]) }
@@ -43,59 +46,39 @@ export async function getBranches(
     return []
   }
 
-  const names = result.stdout
-  const lines = names.split(delimiterString)
-
-  // Remove the trailing newline
-  lines.splice(-1, 1)
-
-  if (lines.length === 0) {
-    return []
-  }
-
   const branches = []
 
-  for (const [ix, line] of lines.entries()) {
-    // preceding newline character after first row
-    const pieces = (ix > 0 ? line.substr(1) : line).split('\0')
-
-    const ref = pieces[0]
-    const name = pieces[1]
-    const upstream = pieces[2]
-    const sha = pieces[3]
-    const shortSha = pieces[4]
-
-    const authorIdentity = pieces[5]
-    const author = CommitIdentity.parseIdentity(authorIdentity)
+  for (const ref of parser.parse(result.stdout)) {
+    const author = CommitIdentity.parseIdentity(ref.author)
 
     if (!author) {
-      throw new Error(`Couldn't parse author identity for '${shortSha}'`)
+      throw new Error(`Couldn't parse author identity ${ref.author}`)
     }
 
-    const committerIdentity = pieces[6]
-    const committer = CommitIdentity.parseIdentity(committerIdentity)
+    const committer = CommitIdentity.parseIdentity(ref.committer)
 
     if (!committer) {
-      throw new Error(`Couldn't parse committer identity for '${shortSha}'`)
+      throw new Error(`Couldn't parse committer identity ${ref.committer}`)
     }
 
-    const symref = pieces[7]
-    const branchTip = {
-      sha,
-      author,
-    }
+    const tip: IBranchTip = { sha: ref.sha, author }
 
-    const type = ref.startsWith('refs/head')
+    const type = ref.fullName.startsWith('refs/head')
       ? BranchType.Local
       : BranchType.Remote
 
-    if (symref.length > 0) {
-      // exclude symbolic refs from the branch list
+    // excude symbolic refs from the branch list
+    if (ref.symRef.length > 0) {
       continue
     }
 
     branches.push(
-      new Branch(name, upstream.length > 0 ? upstream : null, branchTip, type)
+      new Branch(
+        ref.shortName,
+        ref.upstreamShortName.length > 0 ? ref.upstreamShortName : null,
+        tip,
+        type
+      )
     )
   }
 
