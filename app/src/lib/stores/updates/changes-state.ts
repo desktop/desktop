4fde10c5fb3f007cc1b07ea14ecf48bd0aa91e11
Markus Olsson <j.markus.olsson@gmail.com>
2019-04-16 11:54:45 +0200

    updateChangedFile support for new changes selection shape

--- app/src/lib/stores/updates/changes-state.ts
+++ app/src/lib/stores/updates/changes-state.ts
@@ -10,11 +10,14 @@ import {
   isMergeConflictState,
   isRebaseConflictState,
   RebaseConflictState,
+  ChangesSelection,
+  ChangesSelectionKind,
 } from '../../app-state'
-import { DiffSelectionType, IDiff } from '../../../models/diff'
+import { DiffSelectionType } from '../../../models/diff'
 import { caseInsensitiveCompare } from '../../compare'
 import { IStatsStore } from '../../stats/stats-store'
 import { ManualConflictResolution } from '../../../models/manual-conflict-resolution'
+import { assertNever } from '../../fatal-error'
 
 /**
  * Internal shape of the return value from this response because the compiler
@@ -23,8 +26,7 @@ import { ManualConflictResolution } from '../../../models/manual-conflict-resolu
  */
 type ChangedFilesResult = {
   readonly workingDirectory: WorkingDirectoryStatus
-  readonly selectedFileIDs: string[]
-  readonly diff: IDiff | null
+  readonly selection: ChangesSelection
 }
 
 export function updateChangedFiles(
@@ -62,19 +64,6 @@ export function updateChangedFiles(
   // lookups using .find on the mergedFiles array.
   const mergedFileIds = new Set(mergedFiles.map(x => x.id))
 
-  // The previously selected files might not be available in the working
-  // directory any more due to having been committed or discarded so we'll
-  // do a pass over and filter out any selected files that aren't available.
-  let selectedFileIDs = state.selectedFileIDs.filter(id =>
-    mergedFileIds.has(id)
-  )
-
-  // Select the first file if we don't have anything selected and we
-  // have something to select.
-  if (selectedFileIDs.length === 0 && mergedFiles.length > 0) {
-    selectedFileIDs = [mergedFiles[0].id]
-  }
-
   // The file selection could have changed if the previously selected files
   // are no longer selectable (they were discarded or committed) but if they
   // were not changed we can reuse the diff. Note, however that we only render
@@ -83,17 +72,46 @@ export function updateChangedFiles(
   // diff we had, if not we'll clear it.
   const workingDirectory = WorkingDirectoryStatus.fromFiles(mergedFiles)
 
-  const diff =
-    selectedFileIDs.length === 1 &&
-    state.selectedFileIDs.length === 1 &&
-    state.selectedFileIDs[0] === selectedFileIDs[0]
-      ? state.diff
-      : null
+  const selectionKind = state.selection.kind
+  if (state.selection.kind === ChangesSelectionKind.WorkingDirectory) {
+    // The previously selected files might not be available in the working
+    // directory any more due to having been committed or discarded so we'll
+    // do a pass over and filter out any selected files that aren't available.
+    let selectedFileIDs = state.selection.selectedFileIDs.filter(id =>
+      mergedFileIds.has(id)
+    )
 
-  return {
-    workingDirectory,
-    selectedFileIDs,
-    diff,
+    // Select the first file if we don't have anything selected and we
+    // have something to select.
+    if (selectedFileIDs.length === 0 && mergedFiles.length > 0) {
+      selectedFileIDs = [mergedFiles[0].id]
+    }
+
+    const diff =
+      selectedFileIDs.length === 1 &&
+      state.selection.selectedFileIDs.length === 1 &&
+      state.selection.selectedFileIDs[0] === selectedFileIDs[0]
+        ? state.selection.diff
+        : null
+
+    return {
+      workingDirectory,
+      selection: {
+        kind: ChangesSelectionKind.WorkingDirectory,
+        selectedFileIDs,
+        diff,
+      },
+    }
+  } else if (state.selection.kind === ChangesSelectionKind.Stash) {
+    return {
+      workingDirectory,
+      selection: state.selection,
+    }
+  } else {
+    return assertNever(
+      state.selection,
+      `Unknown selection kind ${selectionKind}`
+    )
   }
 }
 
