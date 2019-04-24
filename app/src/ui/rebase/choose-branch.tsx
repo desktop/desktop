edeec59abcd5caacd5ea16a18f5322f7699fb904
Brendan Forster <github@brendanforster.com>
2019-04-24 10:16:51 -0300

    first pass at updating copy

--- app/src/ui/rebase/choose-branch.tsx
+++ app/src/ui/rebase/choose-branch.tsx
@@ -158,7 +158,7 @@ export class ChooseBranchDialog extends React.Component<
         dismissable={true}
         title={
           <>
-            Rebase <strong>{truncatedCurrentBranchName}</strong> onto…
+            Rebase <strong>{truncatedCurrentBranchName}</strong>…
           </>
         }
       >
@@ -180,8 +180,7 @@ export class ChooseBranchDialog extends React.Component<
           {this.renderRebaseStatus()}
           <ButtonGroup>
             <Button type="submit" disabled={disabled} tooltip={tooltip}>
-              Rebase <strong>{currentBranchName}</strong> onto{' '}
-              <strong>{selectedBranch ? selectedBranch.name : ''}</strong>
+              Rebase <strong>{currentBranchName}</strong>
             </Button>
           </ButtonGroup>
         </DialogFooter>
@@ -264,11 +263,9 @@ export class ChooseBranchDialog extends React.Component<
     const pluralized = commitsToRebase === 1 ? 'commit' : 'commits'
     return (
       <>
-        This will rebase
+        This will apply
         <strong>{` ${commitsToRebase} ${pluralized}`}</strong>
-        {` from `}
-        <strong>{currentBranch.name}</strong>
-        {` onto `}
+        {` on top of `}
         <strong>{baseBranch.name}</strong>
       </>
     )
