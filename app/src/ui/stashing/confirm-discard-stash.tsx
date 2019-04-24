99274c9c3203dcb72e28dab8e4cc0cb2a6ef952a
Brendan Forster <brendan@github.com>
2019-04-24 14:23:48 -0700

    import react
    Co-Authored-By: outofambit <outofambit@github.com>

--- app/src/ui/stashing/confirm-discard-stash.tsx
+++ app/src/ui/stashing/confirm-discard-stash.tsx
@@ -1,4 +1,4 @@
-import React = require('react')
+import * as React from 'react'
 import { Dialog, DialogContent, DialogFooter } from '../dialog'
 import { Repository } from '../../models/repository'
 import { Dispatcher } from '../dispatcher'
