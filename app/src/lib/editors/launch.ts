09653508a2e558e62ad6d86ea139026a8d8748dd
roottool <smart.dog.lilienthal@gmail.com>
2019-04-30 00:41:33 +0900

    Added a logic to open workspace file in VSCode

--- app/src/lib/editors/launch.ts
+++ app/src/lib/editors/launch.ts
@@ -1,5 +1,6 @@
 import { spawn } from 'child_process'
 import { pathExists } from 'fs-extra'
+import { join } from 'path'
 import { ExternalEditorError, FoundEditor } from './shared'
 
 /**
@@ -18,14 +19,73 @@ export async function launchExternalEditor(
     const label = __DARWIN__ ? 'Preferences' : 'Options'
     throw new ExternalEditorError(
       `Could not find executable for '${editor.editor}' at path '${
-        editor.path
+      editor.path
       }'.  Please open ${label} and select an available editor.`,
       { openPreferences: true }
     )
   }
-  if (editor.usesShell) {
-    spawn(`"${editorPath}"`, [`"${fullPath}"`], { shell: true })
+  if (editor.editor === 'Visual Studio Code') {
+    launchVisualStudioCode(editor, fullPath)
   } else {
-    spawn(editorPath, [fullPath])
+    const usesShell = editor.usesShell ? editor.usesShell : false
+    spwanExternalEditor(editorPath, fullPath, usesShell)
+  }
+}
+
+function launchVisualStudioCode(
+  editor: FoundEditor,
+  repoRootFolderPath: string
+) {
+  const workspacePattern = join(repoRootFolderPath, '*.code-workspace')
+  const glob = require("glob")
+  glob(workspacePattern, (error: Error, files: string[]) => {
+    if (error) {
+      throw error
+    }
+    else {
+      const workspaceFilePath = chooseWorkspaceFileToOpen(files, repoRootFolderPath)
+      const openTarget = workspaceFilePath === '' ? repoRootFolderPath : workspaceFilePath
+      const usesShell = editor.usesShell ? editor.usesShell : false
+
+      spwanExternalEditor(editor.path, openTarget, usesShell)
+    }
+  })
+}
+
+function chooseWorkspaceFileToOpen(
+  files: string[],
+  repoRootFolderPath: string
+): string {
+  let workspaceFilePath: string | undefined
+  if (files.length === 0) {
+    workspaceFilePath = ''
+  } else if (files.length === 1) {
+    workspaceFilePath = files.pop()
+  } else {
+    const dialog = require('electron').remote.dialog;
+    const fileName = dialog.showOpenDialog({
+      properties: ['openFile'],
+      title: 'Open Workspace',
+      defaultPath: repoRootFolderPath,
+      filters: [
+        { name: 'Code Workspace', extensions: ['code-workspace'] }
+      ]
+    });
+
+    workspaceFilePath = fileName.pop()
+  }
+
+  return workspaceFilePath === undefined ? '' : workspaceFilePath
+}
+
+function spwanExternalEditor(
+  editorPath: string,
+  repoRootFolderPath: string,
+  usesShell: boolean
+) {
+  if (usesShell) {
+    spawn(`"${editorPath}"`, [`"${repoRootFolderPath}"`], { shell: true })
+  } else {
+    spawn(editorPath, [repoRootFolderPath])
   }
 }
