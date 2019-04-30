db6e2cb5bab28a90cf58ed624064c816e848a090
Markus Olsson <j.markus.olsson@gmail.com>
2019-04-30 18:25:00 +0200

    Clean up and use setInterval, not setTimeout

--- app/src/lib/stores/helpers/pull-request-updater.ts
+++ app/src/lib/stores/helpers/pull-request-updater.ts
@@ -1,24 +1,16 @@
 import { PullRequestStore } from '../pull-request-store'
 import { Account } from '../../../models/account'
-import { fatalError } from '../../fatal-error'
 import { GitHubRepository } from '../../../models/github-repository'
 
 //** Interval to check for pull requests */
 const PullRequestInterval = 1000 * 60 * 10
-
-enum TimeoutHandles {
-  PullRequest = 'PullRequestHandle',
-  Status = 'StatusHandle',
-  PushedPullRequest = 'PushedPullRequestHandle',
-}
 
 /**
  * Acts as a service for downloading the latest pull request
  * and status info from GitHub.
  */
 export class PullRequestUpdater {
-  private readonly timeoutHandles = new Map<TimeoutHandles, number>()
-  private isStopped: boolean = true
+  private intervalId: number | null = null
 
   public constructor(
     private readonly repository: GitHubRepository,
@@ -28,30 +20,16 @@ export class PullRequestUpdater {
 
   /** Starts the updater */
   public start() {
-    if (!this.isStopped) {
-      fatalError(
-        'Cannot start the Pull Request Updater that is already running.'
-      )
-
-      return
-    }
-
-    this.timeoutHandles.set(
-      TimeoutHandles.PullRequest,
-
-      window.setTimeout(() => {
-        this.store.refreshPullRequests(this.repository, this.account)
-      }, PullRequestInterval)
-    )
+    this.stop()
+    this.intervalId = window.setInterval(() => {
+      this.store.refreshPullRequests(this.repository, this.account)
+    }, PullRequestInterval)
   }
 
   public stop() {
-    this.isStopped = true
-
-    for (const timeoutHandle of this.timeoutHandles.values()) {
-      window.clearTimeout(timeoutHandle)
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId)
+      this.intervalId = null
     }
-
-    this.timeoutHandles.clear()
   }
 }
