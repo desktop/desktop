402febca5c8623de842f09f3108ada489a8369d8
roottool <smart.dog.lilienthal@gmail.com>
2019-05-01 18:44:08 +0900

    Created the state to save selected value

--- app/src/ui/open-repository-in-vscode/location-list.tsx
+++ app/src/ui/open-repository-in-vscode/location-list.tsx
@@ -7,16 +7,36 @@ interface ISelectFromLocationListProps {
   /** The location list to open the repository in Visual Studio Code */
   readonly locationList: ILocationList[]
 }
-/*
-interface ISelectLocationListState {
-  readonly selectedLocation: string
+
+interface ISelectFromLocationListState {
+  readonly selectedLocationPath: string
 }
-*/
+
 export class SelectFromLocationList extends React.Component<
-  ISelectFromLocationListProps
+  ISelectFromLocationListProps,
+  ISelectFromLocationListState
 > {
   public constructor(props: ISelectFromLocationListProps) {
     super(props)
+
+    const initialLocation = this.props.locationList[0]
+    let initialLocationPath: string
+
+    if (initialLocation === undefined) {
+      initialLocationPath = ''
+    } else {
+      initialLocationPath = initialLocation.path
+    }
+
+    this.state = {
+      selectedLocationPath: initialLocationPath,
+    }
+  }
+
+  private onLocationChange = (event: React.FormEvent<HTMLSelectElement>) => {
+    const location = event.currentTarget.value
+
+    this.setState({ selectedLocationPath: location })
   }
 
   public render() {
@@ -25,7 +45,11 @@ export class SelectFromLocationList extends React.Component<
 
     return (
       <Row>
-        <Select label={__DARWIN__ ? darwinLabel : windowsLabel}>
+        <Select
+          label={__DARWIN__ ? darwinLabel : windowsLabel}
+          value={this.state.selectedLocationPath}
+          onChange={this.onLocationChange}
+        >
           {this.props.locationList.map((file, index) => (
             <option key={index} value={file.path}>
               {file.locationName}
