0fa8d88cc5539da1afbc0739a554686c2f56c118
William Shepherd <iamwillshepherd@github.com>
2019-05-07 11:21:36 -0500

    Filter out entries created by Desktop beta

--- app/src/lib/git/stash.ts
+++ app/src/lib/git/stash.ts
@@ -1,6 +1,17 @@
 import { Repository } from '../../models/repository'
 import { git } from '.'
 
+/**
+ * RegEx for determining if a stash entry is created by Desktop
+ *
+ * This is done by looking for a magic string with the following
+ * format: `!!GitHub_Desktop<branch>`
+ */
+const desktopStashEntryMessageRe = /!!GitHub_Desktop<(.+)>$/
+
+/**
+ * Gets the number of stash entries for the given repository
+ */
 export async function getStashSize(repository: Repository): Promise<number> {
   const delimiter = '1F'
   const format = ['%gd', '%H', '%gs'].join(`%x${delimiter}`)
@@ -20,7 +31,10 @@ export async function getStashSize(repository: Repository): Promise<number> {
     return 0
   }
 
-  const stashEntries = result.stdout.split('\0').filter(s => s !== '')
+  const stashEntries = result.stdout
+    .split('\0')
+    .filter(s => s !== '')
+    .filter(s => !desktopStashEntryMessageRe.test(s))
 
   return stashEntries.length
 }
