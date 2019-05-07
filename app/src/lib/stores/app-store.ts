dcf7bf0a18a13cb9882d8857d37420d764b294ff
Brendan Forster <github@brendanforster.com>
2019-05-07 12:14:27 -0300

    take this out of the log for now

--- app/src/lib/stores/app-store.ts
+++ app/src/lib/stores/app-store.ts
@@ -741,8 +741,6 @@ export class AppStore extends TypedBaseStore<IAppState> {
     repository: Repository,
     initialAction?: CompareAction
   ) {
-    log.debug('[AppStore] initializing compare state')
-
     const state = this.repositoryStateCache.get(repository)
 
     const { branchesState, compareState } = state
