e1e025ed0c048ab5c7b3389e9cef51381736f96c
William Shepherd <iamwillshepherd@github.com>
2019-05-10 17:51:07 -0500

    Cleanup test setup and add new test

--- app/test/ui/ui-test.ts
+++ app/test/ui/ui-test.ts
@@ -2,20 +2,41 @@ import * as puppeteer from 'puppeteer'
 import { startApp } from '../../../script/start-app'
 import { ChildProcess } from 'child_process'
 import fetch from 'node-fetch'
+import { homedir } from 'os'
+import * as FSE from 'fs-extra'
+import * as path from 'path'
+
 const waitForLocalHost = require('wait-for-localhost')
 
 const url = `http://localhost:1234/json/version`
 
+async function setup() {
+  const rootPath = `${homedir()}/Library/Application Support/GitHub Desktop-dev`
+  const pathsToDelete = ['IndexedDB', 'Local Storage']
+
+  for (const thing of pathsToDelete) {
+    try {
+      await FSE.remove(path.join(rootPath, thing))
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+
 describe('ui', () => {
-  let browser: puppeteer.Browser | null = null
-  let endpoint: string | null = null
-  let app: ChildProcess | null = null
+  let browser: puppeteer.Browser
+  let app: ChildProcess | null
+  let appWindow: puppeteer.Page
 
   beforeAll(async () => {
+    await setup()
+
     app = startApp() || null
 
-    // Investigate using wait-port/wait-for-localhost or some other
-    // method to ensure the server is running
+    if (app === null) {
+      process.exit(0)
+      return
+    }
 
     try {
       await waitForLocalHost({ port: 1234 })
@@ -25,7 +46,18 @@ describe('ui', () => {
       console.log('FETCHING COMPLETE')
 
       const json = await response.json()
-      endpoint = json.webSocketDebuggerUrl
+      const endpoint: string = json.webSocketDebuggerUrl
+
+      browser = await puppeteer.connect({ browserWSEndpoint: endpoint })
+
+      const pages = await browser.pages()
+      const page = pages.find(x => x.url().includes('file:///'))
+
+      if (page === undefined) {
+        throw new Error('Page could ne be found')
+      }
+
+      appWindow = page
     } catch (err) {
       console.error('Something went wrong', err)
     }
@@ -38,21 +70,13 @@ describe('ui', () => {
   })
 
   it('works', async () => {
-    browser = await puppeteer.connect({ browserWSEndpoint: endpoint! })
-
-    const pages = await browser!.pages()
-    const page = pages.find(x => x.url().includes('file:///'))
-
-    if (!page) {
-      console.log('Page not found...')
-      return
-    }
-
-    const content = await page.$eval(
+    const content = await appWindow.$eval(
       '#desktop-app-container',
       el => el.textContent
     )
 
     expect(content).not.toBeNull()
   })
+
+  it('launces to welcome flow', async () => {})
 })
