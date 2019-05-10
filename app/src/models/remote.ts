f82d5811fdc063d55358ff1c0753b8805790bdd0
Markus Olsson <j.markus.olsson@gmail.com>
2019-05-10 15:50:18 +0200

    Add a method for comparing two remotes for equality

--- app/src/models/remote.ts
+++ app/src/models/remote.ts
@@ -14,3 +14,19 @@ export interface IRemote {
   readonly name: string
   readonly url: string
 }
+
+/**
+ * Gets a value indicating whether two remotes can be considered
+ * structurally equivalent to each other.
+ */
+export function remoteEquals(x: IRemote | null, y: IRemote | null) {
+  if (x === y) {
+    return true
+  }
+
+  if (x === null || y === null) {
+    return false
+  }
+
+  return x.name === y.name && x.url === y.url
+}
