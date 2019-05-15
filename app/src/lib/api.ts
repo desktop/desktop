4197e80c0af0b36a2993ad4339a9b5a0b14ca67a
Markus Olsson <j.markus.olsson@gmail.com>
2019-05-15 17:08:57 +0200

    Attempt to clarify the suppressError setting

--- app/src/lib/api.ts
+++ app/src/lib/api.ts
@@ -48,8 +48,10 @@ interface IFetchAllOptions<T> {
 
   /**
    * Whether or not to silently suppress request errors and
-   * return the results retrieved thus far. This is the default
-   * behavior. Setting this field to false will cause the
+   * return the results retrieved thus far. If this field is
+   * `true` the fetchAll method will suppress errors (this is
+   * also the default behavior if not value is provided for
+   * this field). Setting this field to false will cause the
    * fetchAll method to throw if it encounters an API error
    * on any page.
    */
