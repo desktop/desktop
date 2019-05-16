5ee4ff0fbcfa11251903ea7ceb706b14fe284ab5
Markus Olsson <j.markus.olsson@gmail.com>
2019-05-16 09:31:50 +0200

    :art: spelling

--- app/test/unit/api-test.ts
+++ app/test/unit/api-test.ts
@@ -2,7 +2,7 @@ import { getNextPagePathWithIncreasingPageSize } from '../../src/lib/api'
 
 describe('API', () => {
   describe('getNextPagePathWithIncreasingPageSize', () => {
-    it("return null when there's no link header", () => {
+    it("returns null when there's no link header", () => {
       expect(getNextPagePathWithIncreasingPageSize(new Response())).toBeNull()
     })
   })
