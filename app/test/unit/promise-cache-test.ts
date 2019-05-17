7a98b4f162c140442b04ab8184903f35557278e0
Brendan Forster <github@brendanforster.com>
2019-05-17 11:45:52 -0300

    added another test

--- app/test/unit/promise-cache-test.ts
+++ app/test/unit/promise-cache-test.ts
@@ -1,11 +1,9 @@
 import { PromiseCache } from '../../src/lib/promise-cache'
 
 describe('PromiseCache', () => {
-  it('returns the same promise', () => {
-    // this promise never completes, to ensure it is not evacuated
-    // from the cache
-
+  it('returns the same promise when the same key is provided and the promise has not expired', () => {
     const createPromise = jest.fn(
+      // ensuring this promise never completes
       // tslint:disable-next-line:promise-must-complete
       (s: string) => new Promise((resolve, reject) => {})
     )
@@ -21,5 +19,45 @@ describe('PromiseCache', () => {
 
     expect(first).toBe(second)
     expect(createPromise).toHaveBeenCalledTimes(1)
+  })
+
+  it('returns different promise when different key used', () => {
+    const createPromise = jest.fn(
+      // ensuring this promise never completes
+      // tslint:disable-next-line:promise-must-complete
+      (s: string) => new Promise((resolve, reject) => {})
+    )
+
+    const cache = new PromiseCache<string, void>(
+      'test',
+      s => s,
+      s => createPromise(s)
+    )
+
+    const first = cache.get('thing')
+    const second = cache.get('another-thing')
+
+    expect(first).not.toBe(second)
+    expect(createPromise).toHaveBeenCalledTimes(2)
+  })
+
+  it('returns different promise when different key used', () => {
+    const createPromise = jest.fn(
+      // ensuring this promise never completes
+      // tslint:disable-next-line:promise-must-complete
+      (s: string) => new Promise((resolve, reject) => {})
+    )
+
+    const cache = new PromiseCache<string, void>(
+      'test',
+      s => s,
+      s => createPromise(s)
+    )
+
+    const first = cache.get('thing')
+    const second = cache.get('another-thing')
+
+    expect(first).not.toBe(second)
+    expect(createPromise).toHaveBeenCalledTimes(2)
   })
 })
