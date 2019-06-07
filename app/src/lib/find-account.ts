8ddf603615a273cac164008f8856dd2a6b84389b
Nathan Henderson <nathos@github.com>
2019-06-07 17:12:35 -0400

    Update 'Enterprise' comment references to 'Enterprise Server'

--- app/src/lib/find-account.ts
+++ app/src/lib/find-account.ts
@@ -33,7 +33,7 @@ async function canAccessRepositoryUsingAPI(
  * @param urlOrRepositoryAlias - the URL or repository alias whose account
  *                               should be found
  * @param accounts             - the list of active GitHub and GitHub Enterprise
- *                               accounts
+ *                               Server accounts
  */
 export async function findAccountForRemoteURL(
   urlOrRepositoryAlias: string,
@@ -74,11 +74,11 @@ export async function findAccountForRemoteURL(
 
     // This chunk of code is designed to sort the user's accounts in this order:
     //  - authenticated GitHub account
-    //  - GitHub Enterprise accounts
+    //  - GitHub Enterprise Server accounts
     //  - unauthenticated GitHub account (access public repositories)
     //
     // As this needs to be done efficiently, we consider endpoints not matching
-    // `getDotComAPIEndpoint()` to be GitHub Enterprise accounts, and accounts
+    // `getDotComAPIEndpoint()` to be GitHub Enterprise Server accounts, and accounts
     // without a token to be unauthenticated.
     const sortedAccounts = Array.from(allAccounts).sort((a1, a2) => {
       if (a1.endpoint === getDotComAPIEndpoint()) {
