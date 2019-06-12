1a8b2fbff863431ec06fa86153f41d0d30f12064
evelyn masso <outofambit@github.com>
2019-06-12 16:10:59 -0700

    minor type change

--- app/src/ui/add-repository/create-repository.tsx
+++ app/src/ui/add-repository/create-repository.tsx
@@ -159,11 +159,11 @@ export class CreateRepository extends React.Component<
 
   private showFilePicker = async () => {
     const window = remote.getCurrentWindow()
-    const directory: string[] | null = remote.dialog.showOpenDialog(window, {
+    const directory = remote.dialog.showOpenDialog(window, {
       properties: ['createDirectory', 'openDirectory'],
     })
 
-    if (!directory) {
+    if (directory === undefined) {
       return
     }
 
