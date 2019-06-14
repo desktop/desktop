789b088b6d99f654c5e0e5a316da8b688a614591
Nathan Henderson <nathos@github.com>
2019-06-14 14:16:35 -0400

    Update account list header to 'GitHub Enterprise Server'

--- app/src/ui/preferences/accounts.tsx
+++ app/src/ui/preferences/accounts.tsx
@@ -32,7 +32,7 @@ export class Accounts extends React.Component<IAccountsProps, {}> {
           ? this.renderAccount(this.props.dotComAccount)
           : this.renderSignIn(SignInType.DotCom)}
 
-        <h2>Enterprise</h2>
+        <h2>GitHub Enterprise Server</h2>
         {this.props.enterpriseAccount
           ? this.renderAccount(this.props.enterpriseAccount)
           : this.renderSignIn(SignInType.Enterprise)}
