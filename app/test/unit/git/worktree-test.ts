7c7776d54d315fa252de76c6d8ed7e7aa47cfc6b
Brendan Forster <github@brendanforster.com>
2019-06-17 14:27:45 -0300

    add test for another case

--- app/test/unit/git/worktree-test.ts
+++ app/test/unit/git/worktree-test.ts
@@ -1,17 +1,23 @@
 import { setupEmptyRepository } from '../../helpers/repositories'
 import { listWorktrees } from '../../../src/lib/git/worktree'
+import { Repository } from '../../../src/models/repository'
+import { GitProcess } from 'dugite'
 
 describe('git/worktree', () => {
   describe('list', () => {
     describe('for an unborn repository', () => {
+      let repository: Repository
+
+      beforeEach(async () => {
+        repository = await setupEmptyRepository()
+      })
+
       it('returns one entry', async () => {
-        const repository = await setupEmptyRepository()
         const result = await listWorktrees(repository)
         expect(result).toHaveLength(1)
       })
 
       it('contains the head and path of the main repository', async () => {
-        const repository = await setupEmptyRepository()
         const { path } = repository
         const result = await listWorktrees(repository)
         const first = result[0]
@@ -23,6 +29,35 @@ describe('git/worktree', () => {
         // so we need to manage and reconcile these two cases
         const endsWith = first.path.endsWith(path)
         expect(endsWith).toBe(true)
+      })
+    })
+
+    describe('for a repository containing commits', () => {
+      let repository: Repository
+      let currentHeadSha: string
+
+      beforeEach(async () => {
+        repository = await setupEmptyRepository()
+        await GitProcess.exec(
+          ['commit', '--allow-empty', '-m', '"first commit!"'],
+          repository.path
+        )
+        await GitProcess.exec(
+          ['commit', '--allow-empty', '-m', '"second commit!"'],
+          repository.path
+        )
+
+        const result = await GitProcess.exec(
+          ['rev-parse', 'HEAD'],
+          repository.path
+        )
+        currentHeadSha = result.stdout.trim()
+      })
+
+      it('the head points to the right commit', async () => {
+        const result = await listWorktrees(repository)
+        const first = result[0]
+        expect(first.head).toBe(currentHeadSha)
       })
     })
   })
