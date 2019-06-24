fe05dd97c910aa5f437482d4734c7db6906da753
evelyn masso <outofambit@github.com>
2019-06-24 11:36:37 -0700

    don't throw away sha info

--- app/src/lib/git/branch.ts
+++ app/src/lib/git/branch.ts
@@ -172,12 +172,12 @@ export async function getBranchesPointedAt(
  *
  * @param repository The repository in which to search
  * @param branchName The to be used as the base branch
- * @returns list of each branch's canonical ref
+ * @returns map of branch canonical refs paired to its sha
  */
 export async function getMergedBranches(
   repository: Repository,
   branchName: string
-): Promise<ReadonlyArray<string>> {
+): Promise<Map<string, string>> {
   const canonicalBranchRef = formatAsLocalRef(branchName)
 
   const args = [
@@ -192,7 +192,7 @@ export async function getMergedBranches(
 
   // Remove the trailing newline
   lines.splice(-1, 1)
-  const mergedBranches = new Array<string>()
+  const mergedBranches = new Map<string, string>()
 
   for (const line of lines) {
     const [sha, canonicalRef] = line.split('\0')
@@ -207,7 +207,7 @@ export async function getMergedBranches(
       continue
     }
 
-    mergedBranches.push(canonicalRef)
+    mergedBranches.set(canonicalRef, sha)
   }
 
   return mergedBranches
