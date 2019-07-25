005ac00b7401c956f2557d074e16c4a1d578f0d9
evelyn masso <evoutofambit@gmail.com>
2019-07-25 15:47:20 -0700

    super basic formatPatch test

---
+++ app/test/unit/git/format-patch-test.ts
@@ -0,0 +1,39 @@
+import { setupTwoCommitRepo } from '../../helpers/repositories'
+import { Repository } from '../../../src/models/repository'
+import { formatPatch } from '../../../src/lib/git'
+import {
+  cloneLocalRepository,
+  makeCommit,
+} from '../../helpers/repository-scaffolding'
+import { GitProcess } from 'dugite'
+
+describe('formatPatch', () => {
+  describe('in a repo with commits', () => {
+    let repository: Repository
+    beforeEach(async () => {
+      repository = await setupTwoCommitRepo()
+    })
+    it('returns a string for a commit range', async () => {
+      expect(await formatPatch(repository, 'HEAD~', 'HEAD')).toBeTruthy()
+    })
+    it('returns empty string for no range', async () => {
+      expect(await formatPatch(repository, 'HEAD', 'HEAD')).toBeFalsy()
+    })
+    describe('applied in a related repo', () => {
+      let clonedRepository: Repository
+      beforeEach(async () => {
+        clonedRepository = await cloneLocalRepository(repository)
+        await makeCommit(clonedRepository, {
+          entries: [{ path: 'okay-file', contents: 'okay' }],
+        })
+      })
+      it('is valid', async () => {
+        const patch = await formatPatch(repository, 'HEAD~', 'HEAD')
+        const result = await GitProcess.exec(['apply'], clonedRepository.path, {
+          stdin: patch,
+        })
+        expect(result).toBeTruthy()
+      })
+    })
+  })
+})
