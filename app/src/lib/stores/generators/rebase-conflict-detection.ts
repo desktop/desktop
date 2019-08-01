ef27c5a03c0e057ca5bf894291f32cc8f6fee8d7
evelyn masso <evoutofambit@gmail.com>
2019-08-01 13:13:38 -0700

    bring the optimizations back

--- app/src/lib/stores/generators/rebase-conflict-detection.ts
+++ app/src/lib/stores/generators/rebase-conflict-detection.ts
@@ -3,6 +3,7 @@ import {
   formatPatch,
   checkPatch,
   getCommitsInRange,
+  getMergeBase,
 } from '../../git'
 import { ComputedAction } from '../../../models/computed-action'
 import { Branch } from '../../../models/branch'
@@ -22,12 +23,35 @@ export async function* checkPotentialRebase({
   baseBranch: Branch
   targetBranch: Branch
 }): AsyncIterableIterator<RebasePreview> {
-  const commits =
-    (await getCommitsInRange(
-      repository,
-      baseBranch.tip.sha,
-      targetBranch.tip.sha
-    )) || []
+  yield loadingStatus
+
+  const [commits, base] = await Promise.all([
+    getCommitsInRange(repository, baseBranch.tip.sha, targetBranch.tip.sha),
+    // TODO: in what situations might this not be possible to compute?
+    getMergeBase(repository, baseBranch.tip.sha, targetBranch.tip.sha),
+  ])
+
+  // if we are unable to find any commits to rebase, indicate that we're
+  // unable to proceed with the rebase
+  if (commits === null) {
+    yield {
+      kind: ComputedAction.Invalid,
+    }
+    return
+  }
+
+  // the target branch is a direct descendant of the base branch
+  // which means the target branch is already up to date and the commits
+  // do not need to be applied
+  if (base === baseBranch.tip.sha) {
+    yield {
+      kind: ComputedAction.Clean,
+      commits: [],
+    }
+    return
+  }
+
+  yield loadingStatus
 
   const worktree = await findOrCreateTemporaryWorkTree(
     repository,
