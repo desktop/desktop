e9d6598e0addb29f308de5df8af05d32bf58f35d
Katrina Uychaco <kuychaco@gmail.com>
2019-09-23 14:38:40 -0700

    Add descriptions to welcome pane

--- app/src/ui/tutorial/welcome-tutorial-pane.tsx
+++ app/src/ui/tutorial/welcome-tutorial-pane.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react'
 
+import { encodePathAsUrl } from '../../lib/path'
+
+const TutorialPanelImage = encodePathAsUrl(
+  __dirname,
+  'static/required-status-check.svg'
+)
 export class WelcomeTutorialPane extends React.Component {
   public render() {
     return (
@@ -12,6 +18,28 @@ export class WelcomeTutorialPane extends React.Component {
                 Use this tutorial to get comfortable with Git, GitHub, and
                 GitHub Desktop.
               </p>
+            </div>
+          </div>
+          <div className="descriptions">
+            <div>
+              <img src={TutorialPanelImage} />
+              <div>
+                <strong>Git</strong> is the version control system.
+              </div>
+            </div>
+            <div>
+              <img src={TutorialPanelImage} />
+              <div>
+                <strong>GitHub</strong> is where you store your code and
+                collaborate with others.
+              </div>
+            </div>
+            <div>
+              <img src={TutorialPanelImage} />
+              <div>
+                <strong>GitHub Desktop</strong> helps you work with GitHub
+                locally.
+              </div>
             </div>
           </div>
         </div>
