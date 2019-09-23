5cd46811ed2cb922f2558fe2bcd60fc3f697268d
evelyn masso <evoutofambit@gmail.com>
2019-09-22 22:54:02 -0700

    lint

--- app/src/ui/tutorial-panel/tutorial-panel.tsx
+++ app/src/ui/tutorial-panel/tutorial-panel.tsx
@@ -186,9 +186,9 @@ export class TutorialPanel extends React.Component<
             onSummaryClick={this.onStepSummaryClick}
           >
             <p className="description">
-              A commit allows you to save sets of changes. In the bottom left area, 
-              write a message that describes the changes you made. 
-              When you’re done, click the blue Commit button to finish.
+              A commit allows you to save sets of changes. In the bottom left
+              area, write a message that describes the changes you made. When
+              you’re done, click the blue Commit button to finish.
             </p>
             <div className="action">
               <kbd>⌘</kbd>
