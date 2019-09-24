217c3e5eb3fe5591b73a948345bb4bf40f7c9b5a
evelyn masso <evoutofambit@gmail.com>
2019-09-24 15:49:56 -0700

    reduce nesting

--- app/src/ui/tutorial/welcome.tsx
+++ app/src/ui/tutorial/welcome.tsx
@@ -15,40 +15,36 @@ const CloudServerImage = encodePathAsUrl(
 export class TutorialWelcome extends React.Component {
   public render() {
     return (
-      <div id="welcome-tutorial">
-        <div className="content">
-          <div className="header">
-            <div className="text">
-              <h1>Welcome to GitHub Desktop</h1>
-              <p>
-                Use this tutorial to get comfortable with Git, GitHub, and
-                GitHub Desktop.
-              </p>
-            </div>
-          </div>
-          <ul className="descriptions">
-            <li>
-              <img src={CodeImage} />
-              <div>
-                <strong>Git</strong> is the version control system.
-              </div>
-            </li>
-            <li>
-              <img src={TeamDiscussionImage} />
-              <div>
-                <strong>GitHub</strong> is where you store your code and
-                collaborate with others.
-              </div>
-            </li>
-            <li>
-              <img src={CloudServerImage} />
-              <div>
-                <strong>GitHub Desktop</strong> helps you work with GitHub
-                locally.
-              </div>
-            </li>
-          </ul>
+      <div className="tutorial-welcome-component">
+        <div className="header">
+          <h1>Welcome to GitHub Desktop</h1>
+          <p>
+            Use this tutorial to get comfortable with Git, GitHub, and GitHub
+            Desktop.
+          </p>
         </div>
+        <ul className="definitions">
+          <li>
+            <img src={CodeImage} />
+            <p>
+              <strong>Git</strong> is the version control system.
+            </p>
+          </li>
+          <li>
+            <img src={TeamDiscussionImage} />
+            <p>
+              <strong>GitHub</strong> is where you store your code and
+              collaborate with others.
+            </p>
+          </li>
+          <li>
+            <img src={CloudServerImage} />
+            <p>
+              <strong>GitHub Desktop</strong> helps you work with GitHub
+              locally.
+            </p>
+          </li>
+        </ul>
       </div>
     )
   }
