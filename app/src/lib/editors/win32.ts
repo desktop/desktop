d7f783f213d9a3733a2ce002f4b68cd808e0b9d4
Mark Silva <github@markesilva.com>
2019-10-15 12:28:54 -0500

    Don't use comma, for backwards compatibility

--- app/src/lib/editors/win32.ts
+++ app/src/lib/editors/win32.ts
@@ -395,7 +395,7 @@ function isExpectedInstallation(
       return displayName.startsWith('Typora') && publisher === 'typora.io'
     case ExternalEditor.SlickEdit:
       return (
-        displayName.startsWith('SlickEdit') && publisher === 'SlickEdit, Inc.'
+        displayName.startsWith('SlickEdit') && publisher === 'SlickEdit Inc.'
       )
     case ExternalEditor.Webstorm:
       return (
@@ -516,7 +516,7 @@ function extractApplicationInformation(
           item.type === RegistryValueType.REG_SZ &&
           item.data.endsWith('win')
         ) {
-          publisher = 'SlickEdit, Inc.'
+          publisher = 'SlickEdit Inc.'
           installLocation = item.data
           displayName = 'SlickEdit'
         } else if (
