11b0dde96981527a735b0e74aa822f751be883cb
Markus Olsson <j.markus.olsson@gmail.com>
2019-10-25 15:42:07 +0200

    :book: shape

--- app/src/lib/errno-exception.ts
+++ app/src/lib/errno-exception.ts
@@ -1,5 +1,22 @@
+/**
+ * A type describing a specific type of errors thrown by Node.js
+ * when encountering errors in low-level operations (such as IO, network,
+ * processes) containing additional information related to the error
+ * itself.
+ */
 interface IErrnoException extends Error {
+  /**
+   * The string name for a numeric error code that comes from a Node.js API.
+   * See https://nodejs.org/api/util.html#util_util_getsystemerrorname_err
+   */
   readonly code: string
+
+  /**
+   * The "system call" (i.e. the Node abstraction) such as 'spawn', 'open', etc
+   * which was responsible for triggering the exception.
+   *
+   * See https://github.com/nodejs/node/blob/v10.16.0/lib/internal/errors.js#L333-L351
+   */
   readonly syscall: string
 }
 
