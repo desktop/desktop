abc8bface80c1580297f2e7f698e1abd51c537ca
Markus Olsson <j.markus.olsson@gmail.com>
2019-10-25 15:41:57 +0200

    Type guard

--- app/src/lib/errno-exception.ts
+++ app/src/lib/errno-exception.ts
@@ -2,3 +2,11 @@ interface IErrnoException extends Error {
   readonly code: string
   readonly syscall: string
 }
+
+export function isErrnoException(err: any): err is IErrnoException {
+  return (
+    err instanceof Error &&
+    typeof (err as any).code === 'string' &&
+    typeof (err as any).syscall === 'string'
+  )
+}
