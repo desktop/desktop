b6bd45efc26695506f580ccf5ce8c82a2a5eff0a
Markus Olsson <j.markus.olsson@gmail.com>
2019-10-28 11:21:53 +0100

    :art: naming

--- app/src/ui/preferences/identifier-rules.ts
+++ app/src/ui/preferences/identifier-rules.ts
@@ -3,10 +3,10 @@
 // least one valid character in the name it'll strip the
 // crud characters and be happy.
 // See https://github.com/git/git/blob/e629a7d28a405e/ident.c#L191-L203
-const disallowedCharactersRe = /^[\x00-\x20.,:;<>"\\']+$/
+const crudCharactersRe = /^[\x00-\x20.,:;<>"\\']+$/
 
 export function disallowedCharacters(values: string): string | null {
-  if (disallowedCharactersRe.test(values)) {
+  if (crudCharactersRe.test(values)) {
     return values
   }
 
