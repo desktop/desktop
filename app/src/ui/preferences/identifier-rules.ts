1157c46c45c75fc560a65517d51de619ba40a79a
Markus Olsson <j.markus.olsson@gmail.com>
2019-10-28 11:21:04 +0100

    :book:

--- app/src/ui/preferences/identifier-rules.ts
+++ app/src/ui/preferences/identifier-rules.ts
@@ -1,7 +1,7 @@
 // Git forbits names consisting entirely of these forbidden
 // characters (what Git calls crud) but as long as there's at
-// least one valid character in the name it'll strip the leading
-// and trailing crud characters and be happy.
+// least one valid character in the name it'll strip the
+// crud characters and be happy.
 // See https://github.com/git/git/blob/e629a7d28a405e/ident.c#L191-L203
 const disallowedCharactersRe = /^[\x00-\x20.,:;<>"\\']+$/
 
