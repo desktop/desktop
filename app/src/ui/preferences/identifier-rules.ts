702eaa9d9460c4cc61d92f50e7b4eeea21a8890d
Markus Olsson <j.markus.olsson@gmail.com>
2019-10-28 11:21:00 +0100

    This is already covered by the regex

--- app/src/ui/preferences/identifier-rules.ts
+++ app/src/ui/preferences/identifier-rules.ts
@@ -6,10 +6,6 @@
 const disallowedCharactersRe = /^[\x00-\x20.,:;<>"\\']+$/
 
 export function disallowedCharacters(values: string): string | null {
-  if (values.length === 0) {
-    return null
-  }
-
   if (disallowedCharactersRe.test(values)) {
     return values
   }
