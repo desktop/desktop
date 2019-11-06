4a01817e6737478b08ec421420c72992a50e3aeb
Markus Olsson <j.markus.olsson@gmail.com>
2019-11-06 22:31:10 +0100

    :book: language

--- app/src/ui/dialog/ok-cancel-button-group.tsx
+++ app/src/ui/dialog/ok-cancel-button-group.tsx
@@ -9,7 +9,7 @@ interface IOkCancelButtonGroupProps {
   readonly className?: string
 
   /**
-   * Does affirmitive (Ok) button perform a destructive action? This controls
+   * Does the affirmitive (Ok) button perform a destructive action? This controls
    * whether the Ok button, or the Cancel button will be the default button.
    */
   readonly destructive?: boolean
