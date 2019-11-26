09e35e9ea63ef2cb62bf6a89d1c10217c5ea17ee
evelyn masso <outofambit@github.com>
2019-11-26 09:06:05 -0800

    permissions can be undefined in the db

--- app/src/lib/databases/repositories-database.ts
+++ app/src/lib/databases/repositories-database.ts
@@ -22,7 +22,7 @@ export interface IDatabaseGitHubRepository {
   /** The last time a prune was attempted on the repository */
   readonly lastPruneDate: number | null
 
-  readonly permissions: 'read' | 'write' | 'admin' | null
+  readonly permissions?: 'read' | 'write' | 'admin' | null
 }
 
 /** A record to track the protected branch information for a GitHub repository */
