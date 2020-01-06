3c72eddfe882b962c9b26e1892d03b990f601c5c
Katrina Uychaco <kuychaco@github.com>
2020-01-06 15:34:40 -0800

    Use arrow up/down for navigation

--- app/src/ui/tab-bar.tsx
+++ app/src/ui/tab-bar.tsx
@@ -104,6 +104,7 @@ export class TabBar extends React.Component<ITabBarProps, {}> {
           onClick={this.onTabClicked}
           onSelectAdjacent={this.onSelectAdjacentTab}
           onButtonRef={this.onTabRef}
+          type={this.props.type}
         >
           {child}
         </TabBarItem>
@@ -124,6 +125,7 @@ interface ITabBarItemProps {
     index: number,
     button: HTMLButtonElement | null
   ) => void
+  readonly type?: TabBarType
 }
 
 class TabBarItem extends React.Component<ITabBarItemProps, {}> {
@@ -132,11 +134,14 @@ class TabBarItem extends React.Component<ITabBarItemProps, {}> {
   }
 
   private onKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
-    if (event.key === 'ArrowLeft') {
-      this.props.onSelectAdjacent('previous', this.props.index)
+    const { type, index } = this.props
+    const previousKey = type === TabBarType.Vertical ? 'ArrowUp' : 'ArrowLeft'
+    const nextKey = type === TabBarType.Vertical ? 'ArrowDown' : 'ArrowRight'
+    if (event.key === previousKey) {
+      this.props.onSelectAdjacent('previous', index)
       event.preventDefault()
-    } else if (event.key === 'ArrowRight') {
-      this.props.onSelectAdjacent('next', this.props.index)
+    } else if (event.key === nextKey) {
+      this.props.onSelectAdjacent('next', index)
       event.preventDefault()
     }
   }
