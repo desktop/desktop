4cc599e67742676950962f1cc935d3b6d1653ebc
Evelyn Masso <outofambit@github.com>
2020-01-14 15:52:08 -0800

    typo

--- app/src/lib/stores/pull-request-coordinator.ts
+++ app/src/lib/stores/pull-request-coordinator.ts
@@ -162,7 +162,7 @@ export class PullRequestCoordinator {
    *
    * Since `PullRequestStore` stores these timestamps by
    * GitHubRepository, we gather the timestamps for all
-   * the repository and its parent and return _oldest one._
+   * the repository and its parent and return the _oldest one._
    *
    * If we can't find any timestamps stored, returns `undefined`
    */
