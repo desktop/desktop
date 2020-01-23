823e75a5e6dd299ce76277b2ef3d32977bca0065
Katrina Uychaco <kuychaco@github.com>
2020-01-23 11:21:44 -0800

    Revert "New dark mode API is supported on all platforms"
    This reverts commit 05e9a3c8adc316be0fd26d86a97f42022e3668fc.

--- app/src/ui/lib/dark-theme.ts
+++ app/src/ui/lib/dark-theme.ts
@@ -1,5 +1,18 @@
 import { nativeTheme } from 'electron'
+import { isMojaveOrLater } from '../../lib/get-os'
+
+export function supportsDarkMode() {
+  if (!__DARWIN__) {
+    return false
+  }
+
+  return isMojaveOrLater()
+}
 
 export function isDarkModeEnabled() {
+  if (!supportsDarkMode()) {
+    return false
+  }
+
   return nativeTheme.shouldUseDarkColors
 }
