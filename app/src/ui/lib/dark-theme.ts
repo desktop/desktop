05e9a3c8adc316be0fd26d86a97f42022e3668fc
Katrina Uychaco <kuychaco@github.com>
2020-01-23 11:12:14 -0800

    New dark mode API is supported on all platforms

--- app/src/ui/lib/dark-theme.ts
+++ app/src/ui/lib/dark-theme.ts
@@ -1,18 +1,5 @@
 import { nativeTheme } from 'electron'
-import { isMojaveOrLater } from '../../lib/get-os'
-
-export function supportsDarkMode() {
-  if (!__DARWIN__) {
-    return false
-  }
-
-  return isMojaveOrLater()
-}
 
 export function isDarkModeEnabled() {
-  if (!supportsDarkMode()) {
-    return false
-  }
-
   return nativeTheme.shouldUseDarkColors
 }
