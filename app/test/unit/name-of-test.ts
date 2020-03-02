21705b98cf9821fa16dc94e646fe1d784995abf1
evelyn masso <coding@outofambit.com>
2020-03-02 11:35:54 -0800

    use it everywhere we can in tests

--- app/test/unit/name-of-test.ts
+++ app/test/unit/name-of-test.ts
@@ -1,6 +1,5 @@
 import { Repository, nameOf } from '../../src/models/repository'
-import { GitHubRepository } from '../../src/models/github-repository'
-import { Owner } from '../../src/models/owner'
+import { plainGitHubRepo } from '../helpers/github-repo-builder'
 
 const repoPath = '/some/cool/path'
 
@@ -14,11 +13,7 @@ describe('nameOf', () => {
   })
 
   it('Returns the name of the repo', () => {
-    const ghRepo = new GitHubRepository(
-      'name',
-      new Owner('desktop', '', null),
-      null
-    )
+    const ghRepo = plainGitHubRepo({ owner: 'desktop', name: 'name' })
     const repo = new Repository(repoPath, -1, ghRepo, false)
 
     const name = nameOf(repo)
--- app/test/unit/repositories-list-grouping-test.ts
+++ app/test/unit/repositories-list-grouping-test.ts
@@ -3,10 +3,8 @@ import {
   KnownRepositoryGroup,
 } from '../../src/ui/repositories-list/group-repositories'
 import { Repository, ILocalRepositoryState } from '../../src/models/repository'
-import { GitHubRepository } from '../../src/models/github-repository'
-import { Owner } from '../../src/models/owner'
-import { getDotComAPIEndpoint } from '../../src/lib/api'
 import { CloningRepository } from '../../src/models/cloning-repository'
+import { plainGitHubRepo } from '../helpers/github-repo-builder'
 
 describe('repository list grouping', () => {
   const repositories: Array<Repository | CloningRepository> = [
@@ -14,17 +12,13 @@ describe('repository list grouping', () => {
     new Repository(
       'repo2',
       2,
-      new GitHubRepository(
-        'my-repo2',
-        new Owner('me', getDotComAPIEndpoint(), null),
-        1
-      ),
+      plainGitHubRepo({ owner: 'me', name: 'my-repo2' }),
       false
     ),
     new Repository(
       'repo3',
       3,
-      new GitHubRepository('my-repo3', new Owner('', '', null), 1),
+      plainGitHubRepo({ owner: '', name: 'my-repo3', endpoint: '' }),
       false
     ),
   ]
@@ -59,22 +53,14 @@ describe('repository list grouping', () => {
     const repoB = new Repository(
       'b',
       2,
-      new GitHubRepository(
-        'b',
-        new Owner('me', getDotComAPIEndpoint(), null),
-        1
-      ),
+      plainGitHubRepo({ owner: 'me', name: 'b' }),
       false
     )
     const repoC = new Repository('c', 2, null, false)
     const repoD = new Repository(
       'd',
       2,
-      new GitHubRepository(
-        'd',
-        new Owner('me', getDotComAPIEndpoint(), null),
-        1
-      ),
+      plainGitHubRepo({ owner: 'me', name: 'd' }),
       false
     )
     const repoZ = new Repository('z', 3, null, false)
@@ -105,41 +91,33 @@ describe('repository list grouping', () => {
     const repoA = new Repository(
       'repo',
       1,
-      new GitHubRepository(
-        'repo',
-        new Owner('user1', getDotComAPIEndpoint(), null),
-        1
-      ),
+      plainGitHubRepo({ owner: 'user1', name: 'repo' }),
       false
     )
     const repoB = new Repository(
       'repo',
       2,
-      new GitHubRepository(
-        'repo',
-        new Owner('user2', getDotComAPIEndpoint(), null),
-        2
-      ),
+      plainGitHubRepo({ owner: 'user2', name: 'repo' }),
       false
     )
     const repoC = new Repository(
       'enterprise-repo',
       3,
-      new GitHubRepository(
-        'enterprise-repo',
-        new Owner('business', '', null),
-        1
-      ),
+      plainGitHubRepo({
+        owner: 'business',
+        name: 'enterprise-repo',
+        endpoint: '',
+      }),
       false
     )
     const repoD = new Repository(
       'enterprise-repo',
       3,
-      new GitHubRepository(
-        'enterprise-repo',
-        new Owner('silliness', '', null),
-        1
-      ),
+      plainGitHubRepo({
+        owner: 'silliness',
+        name: 'enterprise-repo',
+        endpoint: '',
+      }),
       false
     )
 
