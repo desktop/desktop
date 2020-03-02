6387913796e5c1fc728791008a41171b588528ca
evelyn masso <coding@outofambit.com>
2020-03-02 11:35:24 -0800

    create github repo test fixture

---
+++ app/test/helpers/github-repo-builder.ts
@@ -0,0 +1,40 @@
+import { GitHubRepository } from '../../src/models/github-repository'
+import { Owner } from '../../src/models/owner'
+import { getDotComAPIEndpoint } from '../../src/lib/api'
+
+let id_counter = 0
+
+// TODO: rename me!
+export function plainGitHubRepo({
+  owner,
+  name,
+  parent,
+  defaultBranch,
+  endpoint,
+}: {
+  owner: string
+  name: string
+  parent?: GitHubRepository
+  defaultBranch?: string
+  endpoint?: string
+}) {
+  return new GitHubRepository(
+    name,
+    new Owner(
+      owner,
+      endpoint !== undefined ? endpoint : getDotComAPIEndpoint(),
+      null
+    ),
+    id_counter++,
+    null,
+    endpoint !== undefined
+      ? `${endpoint}/${owner}/${name}`
+      : `https://github.com/${owner}/${name}`,
+    defaultBranch || 'master',
+    endpoint !== undefined
+      ? `${endpoint}/${owner}/${name}.git`
+      : `https://github.com/${owner}/${name}.git`,
+    null,
+    parent
+  )
+}
