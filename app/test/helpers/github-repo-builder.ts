f661aec9ab2da5e6fe05037ad4c45b121ae4f409
evelyn masso <coding@outofambit.com>
2020-03-02 11:46:14 -0800

    fixture docs

--- app/test/helpers/github-repo-builder.ts
+++ app/test/helpers/github-repo-builder.ts
@@ -4,19 +4,38 @@ import { getDotComAPIEndpoint } from '../../src/lib/api'
 
 let id_counter = 0
 
+/**
+ * Most of these fields are passed on to the
+ * GitHubRepository constructor directly.
+ *
+ * Notable exception: `endpoint`
+ */
+interface IGitHubRepoFixtureOptions {
+  owner: string
+  name: string
+  parent?: GitHubRepository
+  defaultBranch?: string
+  /**
+   * Defaults to github.com if omitted.
+   * We make an attempt at constructing a meaningful non-github.com
+   * clone url and html url from this, even if its ''.
+   */
+  endpoint?: string
+}
+
+/**
+ * Makes a fairly standard `GitHubRepository` for use in tests.
+ * Ensures a unique `dbID` for each, during a test run.
+ * @param options
+ * @returns a new GitHubRepository model
+ */
 export function gitHubRepoFixture({
   owner,
   name,
   parent,
   defaultBranch,
   endpoint,
-}: {
-  owner: string
-  name: string
-  parent?: GitHubRepository
-  defaultBranch?: string
-  endpoint?: string
-}) {
+}: IGitHubRepoFixtureOptions): GitHubRepository {
   return new GitHubRepository(
     name,
     new Owner(
