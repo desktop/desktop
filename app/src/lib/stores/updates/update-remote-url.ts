c3ddbd1f9b6e4104454fb7f2bdd3ba1c2862f531
Katrina Uychaco <kuychaco@github.com>
2020-03-06 14:50:48 -0800

    Restore check for matching protocols
    Co-Authored-By: evelyn masso <coding@outofambit.com>

--- app/src/lib/stores/updates/update-remote-url.ts
+++ app/src/lib/stores/updates/update-remote-url.ts
@@ -2,6 +2,8 @@ import { Repository } from '../../../models/repository'
 import { IAPIRepository } from '../../api'
 import { GitStore } from '../git-store'
 import { urlMatchesRemote } from '../../repository-matching'
+import { remoteRegexes } from '../../remote-parsing'
+import * as URL from 'url'
 
 export async function updateRemoteUrl(
   gitStore: GitStore,
@@ -17,8 +19,23 @@ export async function updateRemoteUrl(
     return
   }
 
+  const remoteUrl = gitStore.defaultRemote.url
   const updatedRemoteUrl = apiRepo.clone_url
   const urlsMatch = urlMatchesRemote(updatedRemoteUrl, gitStore.defaultRemote)
+
+  // Verify that protocol hasn't changed. If it has we don't want
+  // to alter the protocol in case they are relying on a specific one.
+  // If protocol is null that implies the url is a ssh url
+  // of the format git@github.com:octocat/Hello-World.git, which
+  // can't be parsed by URL.parse. In this case we assume the user
+  // manually configured their remote to use this format and we don't
+  // want to change what they've done just to be safe
+  const parsedRemoteUrl = URL.parse(remoteUrl)
+  const parsedUpdaedRemoteUrl = URL.parse(updatedRemoteUrl)
+  const protocolsMatch =
+    parsedRemoteUrl.protocol !== null &&
+    parsedUpdaedRemoteUrl.protocol !== null &&
+    parsedRemoteUrl.protocol === parsedUpdaedRemoteUrl.protocol
 
   const remoteUrlUnchanged =
     gitStore.defaultRemote &&
@@ -27,13 +44,7 @@ export async function updateRemoteUrl(
       gitStore.defaultRemote
     )
 
-  if (remoteUrlUnchanged && !urlsMatch) {
-    // Frankenstein the existing remote url to have the updated user/repo path
-    const updatedUrl = new URL(gitStore.defaultRemote.url)
-    updatedUrl.pathname = new URL(updatedRemoteUrl).pathname
-    await gitStore.setRemoteURL(
-      gitStore.defaultRemote.name,
-      updatedUrl.toString()
-    )
+  if (protocolsMatch && remoteUrlUnchanged && !urlsMatch) {
+    await gitStore.setRemoteURL(gitStore.defaultRemote.name, updatedRemoteUrl)
   }
 }
