9e045a1ef5301b6186b252cd1abfd034632fd2f1
evelyn masso <outofambit@github.com>
2020-03-09 16:44:08 -0700

    Update app/src/lib/databases/repositories-database.ts
    Co-Authored-By: Markus Olsson <j.markus.olsson@gmail.com>

--- app/src/lib/databases/repositories-database.ts
+++ app/src/lib/databases/repositories-database.ts
@@ -22,7 +22,7 @@ export interface IDatabaseGitHubRepository {
   /** The last time a prune was attempted on the repository */
   readonly lastPruneDate: number | null
 
-  readonly issuesEnabled: boolean | null
+  readonly issuesEnabled?: boolean | null
 
   readonly permissions?: 'read' | 'write' | 'admin' | null
 }
