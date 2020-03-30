cd8bf4083f33479c529f57fe854079298e7d030a
jfgordon2 <55799997+jfgordon2@users.noreply.github.com>
2020-03-30 12:06:52 -0500

    polish About modal for Linux build (#241)
    Updated the "About GitHub Desktop" model to remove the button to check for updates (since it didn't do anything on Linux) and replace with a link to the linux releases page

--- app/src/ui/about/about.tsx
+++ app/src/ui/about/about.tsx
@@ -14,7 +14,7 @@ import { Disposable } from 'event-kit'
 import { Loading } from '../lib/loading'
 import { RelativeTime } from '../relative-time'
 import { assertNever } from '../../lib/fatal-error'
-import { ReleaseNotesUri } from '../lib/releases'
+import { ReleaseNotesUri, LinuxReleasesUri } from '../lib/releases'
 import { encodePathAsUrl } from '../../lib/path'
 import { isTopMostDialog } from '../dialog/is-top-most'
 import { isOSNoLongerSupportedByElectron } from '../../lib/get-os'
@@ -137,6 +137,17 @@ export class About extends React.Component<IAboutProps, IAboutState> {
       return null
     }
 
+    if (__LINUX__) {
+      const linuxReleaseLink = (
+        <LinkButton uri={LinuxReleasesUri}>View Releases</LinkButton>
+      )
+      return (
+        <Row>
+          <p className="no-padding">{linuxReleaseLink}</p>
+        </Row>
+      )
+    }
+
     const updateStatus = this.state.updateState.status
 
     switch (updateStatus) {
@@ -229,7 +240,12 @@ export class About extends React.Component<IAboutProps, IAboutState> {
 
   private renderUpdateDetails() {
     if (__LINUX__) {
-      return null
+      return (
+        <p>
+          Please visit the GitHub Desktop for Linux release page for
+          Linux-specific release notes and to download the latest version.
+        </p>
+      )
     }
 
     if (__RELEASE_CHANNEL__ === 'development') {
