f8471277f4cfb323e98421e1e2eb5e24f92a7925
jfgordon2 <55799997+jfgordon2@users.noreply.github.com>
2020-03-30 12:06:52 -0500

    polish About modal for Linux build (#241)
    Updated the "About GitHub Desktop" model to remove the button to check for updates (since it didn't do anything on Linux) and replace with a link to the linux releases page

--- app/src/ui/lib/releases.ts
+++ app/src/ui/lib/releases.ts
@@ -2,3 +2,5 @@ export const ReleaseNotesUri =
   __RELEASE_CHANNEL__ === 'beta'
     ? 'https://desktop.github.com/release-notes/?env=beta'
     : 'https://desktop.github.com/release-notes/'
+
+export const LinuxReleasesUri = 'https://github.com/shiftkey/desktop/releases/'
