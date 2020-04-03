6ca52f77b5bc59f7dc36ebc390cbf12824288dcb
Rafael Oleza <rafeca@github.com>
2020-04-03 16:44:40 +0200

    Fix code comments

--- app/src/lib/git/tag.ts
+++ app/src/lib/git/tag.ts
@@ -4,8 +4,8 @@ import { Repository } from '../../models/repository'
 /**
  * Create a new tag on the given target commit.
  *
- * @param repository        - The repository in which to create the new branch.
- * @param name              - The name of the new branch.
+ * @param repository        - The repository in which to create the new tag.
+ * @param name              - The name of the new tag.
  * @param targetCommitSha   - The SHA of the commit where the new tag will live on.
  */
 export async function createTag(
