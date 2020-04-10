31b91b068224ec87f7b784f929e156abad06d3df
say25pp <say25@cornell.edu>
2020-04-10 00:18:07 -0500

    Create invalidGitAuthorNameMessage for reuse later

--- app/src/ui/preferences/preferences.tsx
+++ app/src/ui/preferences/preferences.tsx
@@ -17,7 +17,10 @@ import {
 import { lookupPreferredEmail } from '../../lib/email'
 import { Shell, getAvailableShells } from '../../lib/shells'
 import { getAvailableEditors } from '../../lib/editors/lookup'
-import { gitAuthorNameIsValid } from '../lib/identifier-rules'
+import {
+  gitAuthorNameIsValid,
+  invalidGitAuthorNameMessage,
+} from '../lib/identifier-rules'
 import { Appearance } from './appearance'
 import { ApplicationTheme } from '../lib/application-theme'
 import { OkCancelButtonGroup } from '../dialog/ok-cancel-button-group'
@@ -377,7 +380,7 @@ export class Preferences extends React.Component<
       committerName,
       disallowedCharactersMessage: gitAuthorNameIsValid(committerName)
         ? null
-        : 'Name is invalid, it consists only of disallowed characters.',
+        : invalidGitAuthorNameMessage,
     })
   }
 
