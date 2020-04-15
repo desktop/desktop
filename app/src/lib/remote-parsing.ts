24c84e294fdeab56a42fe09de5b722bab7db58ce
Daniel Hix <danieljacobhix@gmail.com>
2020-04-15 17:19:11 -0500

    Change regex expressions to exclude slash character
    This will make sure that our last match will contain the extra slashes.

--- app/src/lib/remote-parsing.ts
+++ app/src/lib/remote-parsing.ts
@@ -27,19 +27,19 @@ interface IGitRemoteURL {
 const remoteRegexes: ReadonlyArray<{ protocol: Protocol; regex: RegExp }> = [
   {
     protocol: 'https',
-    regex: new RegExp('^https?://(?:.+@)?(.+)/(.+)/(.+?)(?:/|.git/?)?$'),
+    regex: new RegExp('^https?://(?:.+@)?([^/]+)/([^/]+)/(.+?)(?:/|.git/?)?$'),
   },
   {
     protocol: 'ssh',
-    regex: new RegExp('^git@(.+):(.+)/(.+?)(?:/|.git)?$'),
+    regex: new RegExp('^git@([^/]+):([^/]+)/(.+?)(?:/|.git)?$'),
   },
   {
     protocol: 'ssh',
-    regex: new RegExp('^git:(.+)/(.+)/(.+?)(?:/|.git)?$'),
+    regex: new RegExp('^git:([^/]+)/([^/]+)/(.+?)(?:/|.git)?$'),
   },
   {
     protocol: 'ssh',
-    regex: new RegExp('^ssh://git@(.+)/(.+)/(.+?)(?:/|.git)?$'),
+    regex: new RegExp('^ssh://git@([^/]+)/([^/]+)/(.+?)(?:/|.git)?$'),
   },
 ]
 
