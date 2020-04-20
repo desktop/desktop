ad7a849bb1d690748848df141645b62ec58270b2
Andrei Maga <andreimariomaga@gmail.com>
2020-04-20 21:01:06 +0300

    Better LFS output
    Passing LFS output stright to the parser and removed the "middle man"

--- app/src/lib/progress/from-process.ts
+++ app/src/lib/progress/from-process.ts
@@ -1,13 +1,10 @@
 import { ChildProcess } from 'child_process'
-import * as Fs from 'fs'
-import * as Path from 'path'
 import * as byline from 'byline'
 
 import { GitProgressParser, IGitProgress, IGitOutput } from './git'
 import { IGitExecutionOptions } from '../git/core'
 import { merge } from '../merge'
-import { GitLFSProgressParser, createLFSProgressFile } from './lfs'
-import { tailByLine } from '../file-system'
+import { GitLFSProgressParser } from './lfs'
 
 /**
  * Merges an instance of IGitExecutionOptions with a process callback provided
@@ -21,25 +18,14 @@ export async function executionOptionsWithProgress(
   parser: GitProgressParser,
   progressCallback: (progress: IGitProgress | IGitOutput) => void
 ): Promise<IGitExecutionOptions> {
-  let lfsProgressPath = null
-  let env = {}
-  if (options.trackLFSProgress) {
-    try {
-      lfsProgressPath = await createLFSProgressFile()
-      env = { GIT_LFS_PROGRESS: lfsProgressPath }
-    } catch (e) {
-      log.error('Error writing LFS progress file', e)
-      env = { GIT_LFS_PROGRESS: null }
-    }
-  }
 
   return merge(options, {
     processCallback: createProgressProcessCallback(
       parser,
-      lfsProgressPath,
+      options.trackLFSProgress ? options.trackLFSProgress : null,
       progressCallback
     ),
-    env: merge(options.env, env),
+    env: options.env
   })
 }
 
@@ -50,38 +36,16 @@ export async function executionOptionsWithProgress(
  */
 function createProgressProcessCallback(
   parser: GitProgressParser,
-  lfsProgressPath: string | null,
+  lfsProgressPath: boolean | null,
   progressCallback: (progress: IGitProgress | IGitOutput) => void
 ): (process: ChildProcess) => void {
   return process => {
-    if (lfsProgressPath) {
-      const lfsParser = new GitLFSProgressParser()
-      const disposable = tailByLine(lfsProgressPath, line => {
-        const progress = lfsParser.parse(line)
-        progressCallback(progress)
-      })
-
-      process.on('close', () => {
-        disposable.dispose()
-        // the order of these callbacks is important because
-        //  - unlink can only be done on files
-        //  - rmdir can only be done when the directory is empty
-        //  - we don't want to surface errors to the user if something goes
-        //    wrong (these files can stay in TEMP and be cleaned up eventually)
-        Fs.unlink(lfsProgressPath, err => {
-          if (err == null) {
-            const directory = Path.dirname(lfsProgressPath)
-            Fs.rmdir(directory, () => {})
-          }
-        })
-      })
-    }
-
+    const lfsParser = lfsProgressPath ? new GitLFSProgressParser() : null
     // If Node.js encounters a synchronous runtime error while spawning
     // `stderr` will be undefined and the error will be emitted asynchronously
     if (process.stderr) {
       byline(process.stderr).on('data', (line: string) => {
-        progressCallback(parser.parse(line))
+        progressCallback(lfsParser ? lfsParser.parse(line) : parser.parse(line))
       })
     }
   }
