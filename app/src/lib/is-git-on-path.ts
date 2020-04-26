0ab2f867e3142bef0faf82b3bd8baeba3fd88e42
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-04-26 23:57:05 +0200

    Split out `findGitOnPath()` from `isGitOnPath()`
    An upcoming patch will want to find out _where_ on the PATH the Git executable lives, not just that it is there. The `isGitOnPath()` function almost did that already, except that it just ignored the standard output of the command that found the path.
    
    Note: there is a shortcut in `isGitOnPath()` for macOS, where it expects the executable to be on the PATH, and does not even look for it. We keep that shortcut there, but teach the `findGitOnPath()` function to handle macOS, too, just in case that this functionality will be needed any time in the future.

--- app/src/lib/is-git-on-path.ts
+++ app/src/lib/is-git-on-path.ts
@@ -5,33 +5,38 @@ function captureCommandOutput(
   command: string,
   args?: string[] | undefined,
   options: SpawnOptions = {}
-): Promise<boolean> {
-  return new Promise<boolean>((resolve, reject) => {
+): Promise<string | undefined> {
+  return new Promise<string | undefined>((resolve, reject) => {
     const cp = spawn(command, args, options)
 
     cp.on('error', error => {
       log.warn(`Unable to spawn ${command}`, error)
-      resolve(false)
+      resolve(undefined)
+    })
+
+    const chunks = new Array<Buffer>()
+    let total = 0
+
+    cp.stdout.on('data', (chunk: Buffer) => {
+      chunks.push(chunk)
+      total += chunk.length
     })
 
     cp.on('close', function(code) {
       if (code !== 0) {
-        resolve(false)
+        resolve(undefined)
       } else {
-        resolve(true)
+        resolve(
+          Buffer.concat(chunks, total)
+            .toString()
+            .replace(/\r?\n[^]*/m, '')
+        )
       }
     })
   })
 }
 
-export function isGitOnPath(): Promise<boolean> {
-  // Modern versions of macOS ship with a Git shim that guides you through
-  // the process of setting everything up. We trust this is available, so
-  // don't worry about looking for it here.
-  if (__DARWIN__) {
-    return Promise.resolve(true)
-  }
-
+export function findGitOnPath(): Promise<string | undefined> {
   // adapted from http://stackoverflow.com/a/34953561/1363815
   if (__WIN32__) {
     const windowsRoot = process.env.SystemRoot || 'C:\\Windows'
@@ -44,11 +49,27 @@ export function isGitOnPath(): Promise<boolean> {
     return captureCommandOutput(wherePath, ['git'], { cwd: windowsRoot })
   }
 
-  if (__LINUX__) {
+  if (__DARWIN__ || __LINUX__) {
     // `which` will print the path and return 0 when the executable
     // is found under PATH, or return 1 if it cannot be found
     return captureCommandOutput('which', ['git'])
   }
 
-  return Promise.resolve(false)
+  return Promise.resolve(undefined)
+}
+
+export function isGitOnPath(): Promise<boolean> {
+  // Modern versions of macOS ship with a Git shim that guides you through
+  // the process of setting everything up. We trust this is available, so
+  // don't worry about looking for it here.
+  if (__DARWIN__) {
+    return Promise.resolve(true)
+  }
+
+  return findGitOnPath().then(
+    (path: string | undefined) =>
+      new Promise<boolean>((resolve, reject) => {
+        resolve(path !== undefined)
+      })
+  )
 }
