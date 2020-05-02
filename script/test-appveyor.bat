f2fcdae54b03a3faf4260e2f9315e588a8fd3f19
Dennis Ameling <dennis@dennisameling.com>
2020-05-02 12:19:31 +0200

    Add support for Windows ARM

--- script/test-appveyor.bat
+++ script/test-appveyor.bat
@@ -1,2 +1,7 @@
-yarn test
+if "%npm_config_arch%" == "arm64" (
+  echo "Skipping yarn test due to arch arm64 Electron not being supported by the host OS"
+) else (
+  yarn test
+)
+
 set APPVEYOR_TEST_RESULT=%ERRORLEVEL%
