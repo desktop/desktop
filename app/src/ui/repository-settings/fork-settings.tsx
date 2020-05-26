72ecb65f9d46a003c2382a57df1ba860bc848972
Rafael Oleza <rafeca@gmail.com>
2020-05-26 10:00:29 +0200

    Create component that displays the description of a fork contribution target
    We're going to reuse this component on the dialog that's shown after cloning a forked repository

--- app/src/ui/repository-settings/fork-settings.tsx
+++ app/src/ui/repository-settings/fork-settings.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { DialogContent } from '../dialog'
 import { ForkContributionTarget } from '../../models/workflow-preferences'
 import { RepositoryWithForkedGitHubRepository } from '../../models/repository'
+import { ForkSettingsDescription } from './fork-contribution-target-description'
 
 interface IGitIgnoreProps {
   readonly forkContributionTarget: ForkContributionTarget
@@ -51,38 +52,11 @@ export class ForkSettings extends React.Component<IGitIgnoreProps, {}> {
           <label htmlFor={RadioButtonId.Self}>For my own purposes</label>
         </div>
 
-        {this.renderDescription()}
+        <ForkSettingsDescription
+          repository={this.props.repository}
+          forkContributionTarget={this.props.forkContributionTarget}
+        />
       </DialogContent>
-    )
-  }
-
-  private renderDescription() {
-    // We can't use the getNonForkGitHubRepository() helper since we need to calculate
-    // the value based on the temporary form state.
-    const targetRepository =
-      this.props.forkContributionTarget === ForkContributionTarget.Self
-        ? this.props.repository.gitHubRepository
-        : this.props.repository.gitHubRepository.parent
-
-    return (
-      <ul className="fork-settings-description">
-        <li>
-          Pull requests targeting <strong>{targetRepository.fullName}</strong>{' '}
-          will be shown in the pull request list.
-        </li>
-        <li>
-          Issues will be created in <strong>{targetRepository.fullName}</strong>
-          .
-        </li>
-        <li>
-          "View on Github" will open{' '}
-          <strong>{targetRepository.fullName}</strong> in the browser.
-        </li>
-        <li>
-          New branches will be based on{' '}
-          <strong>{targetRepository.fullName}</strong>'s default branch.
-        </li>
-      </ul>
     )
   }
 
