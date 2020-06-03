913c8d6515542f76f61b1f9541971a578dcd9e0d
Markus Olsson <j.markus.olsson@gmail.com>
2020-06-03 15:06:11 +0200

    :book:

--- app/src/ui/history/commit-summary.tsx
+++ app/src/ui/history/commit-summary.tsx
@@ -107,12 +107,21 @@ function createState(
 
   for (const token of tokens) {
     if (remainder <= 0) {
+      // There's no room left in the summary, everything needs to
+      // go into the overflow
       overflow.push(token)
     } else if (remainder >= token.text.length) {
+      // The token fits without us having to think about wrapping!
       summary.push(token)
       remainder -= token.text.length
     } else {
+      // There's not enough room to include the token in its entirety,
+      // we've got to make a decision between hard wrapping or pushing
+      // to overflow.
       if (token.kind === TokenType.Text) {
+        // We always hard-wrap text, it'd be nice if we could attempt
+        // to break at word boundaries in the future but that's too
+        // complex for now.
         summary.push({
           kind: TokenType.Text,
           text: token.text.substr(0, remainder),
@@ -122,6 +131,10 @@ function createState(
           text: token.text.substr(remainder),
         })
       } else if (token.kind === TokenType.Emoji) {
+        // There's room for improvement here, we look at the length of
+        // token.text which could be something like ":white_square_button:"
+        // which would still only take up a little bit more space than a
+        // regular character when rendered as an image.
         overflow.push(token)
       } else if (token.kind === TokenType.Link) {
         // Hard wrapping an issue link is confusing so we treat them
