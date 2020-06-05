ee41ad97e98f4d292bd5d56e2232d399f1d3b502
Markus Olsson <j.markus.olsson@gmail.com>
2020-06-05 17:14:02 +0200

    Add a smoke test for carriage return parser

---
+++ app/test/unit/parse-carriage-return-test.ts
@@ -0,0 +1,75 @@
+import { parseCarriageReturn } from '../../src/lib/parse-carriage-return'
+
+describe('parseCarriageReturn', () => {
+  it('parses git clone output correctly', () => {
+    const cloneOutput =
+      "Cloning into '/Users/markus/Documents/GitHub/delete-branch-test'...\n" +
+      'remote: Enumerating objects: 8, done.        \n' +
+      'remote: Counting objects:  12% (1/8)        \r' +
+      'remote: Counting objects:  25% (2/8)        \r' +
+      'remote: Counting objects:  37% (3/8)        \r' +
+      'remote: Counting objects:  50% (4/8)        \r' +
+      'remote: Counting objects:  62% (5/8)        \r' +
+      'remote: Counting objects:  75% (6/8)        \r' +
+      'remote: Counting objects:  87% (7/8)        \r' +
+      'remote: Counting objects: 100% (8/8)        \r' +
+      'remote: Counting objects: 100% (8/8), done.        \n' +
+      'remote: Compressing objects:  16% (1/6)        \r' +
+      'remote: Compressing objects:  33% (2/6)        \r' +
+      'remote: Compressing objects:  50% (3/6)        \r' +
+      'remote: Compressing objects:  66% (4/6)        \r' +
+      'remote: Compressing objects:  83% (5/6)        \r' +
+      'remote: Compressing objects: 100% (6/6)        \r' +
+      'remote: Compressing objects: 100% (6/6), done.        \n' +
+      'remote: Total 8 (delta 1), reused 7 (delta 0), pack-reused 0        \n' +
+      '\r' +
+      '                                                                                \r' +
+      'Receiving objects:  12% (1/8)\r' +
+      '\r' +
+      '                                                                                \r' +
+      'Receiving objects:  25% (2/8)\r' +
+      '\r' +
+      '                                                                                \r' +
+      'Receiving objects:  37% (3/8)\r' +
+      '\r' +
+      '                                                                                \r' +
+      'Receiving objects:  50% (4/8)\r' +
+      '\r' +
+      '                                                                                \r' +
+      'Receiving objects:  62% (5/8)\r' +
+      '\r' +
+      '                                                                                \r' +
+      'Receiving objects:  75% (6/8)\r' +
+      '\r' +
+      '                                                                                \r' +
+      'Receiving objects:  87% (7/8)\r' +
+      '\r' +
+      '                                                                                \r' +
+      'Receiving objects: 100% (8/8)\r' +
+      '\r' +
+      '                                                                                \r' +
+      'Receiving objects: 100% (8/8), done.\n' +
+      '\r' +
+      '                                                                                \r' +
+      'Resolving deltas:   0% (0/1)\r' +
+      '\r' +
+      '                                                                                \r' +
+      'Resolving deltas: 100% (1/1)\r' +
+      '\r' +
+      '                                                                                \r' +
+      'Resolving deltas: 100% (1/1), done.\n' +
+      "fatal: multiple updates for ref 'refs/remotes/origin/pr/1' not allowed\n"
+
+    const expected =
+      "Cloning into '/Users/markus/Documents/GitHub/delete-branch-test'...\n" +
+      'remote: Enumerating objects: 8, done.        \n' +
+      'remote: Counting objects: 100% (8/8), done.        \n' +
+      'remote: Compressing objects: 100% (6/6), done.        \n' +
+      'remote: Total 8 (delta 1), reused 7 (delta 0), pack-reused 0        \n' +
+      'Receiving objects: 100% (8/8), done.                                            \n' +
+      'Resolving deltas: 100% (1/1), done.                                             \n' +
+      "fatal: multiple updates for ref 'refs/remotes/origin/pr/1' not allowed\n"
+
+    expect(parseCarriageReturn(cloneOutput)).toBe(expected)
+  })
+})
