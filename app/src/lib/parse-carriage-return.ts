308c611ffb6223b999b211e465e1dd6b37a20ebf
Markus Olsson <j.markus.olsson@gmail.com>
2020-06-11 16:16:30 +0200

    Drop the regex approach for a straightforward split and join
    Co-Authored-By: Rafael Oleza <rafeca@users.noreply.github.com>

--- app/src/lib/parse-carriage-return.ts
+++ app/src/lib/parse-carriage-return.ts
@@ -24,44 +24,15 @@ export function parseCarriageReturn(text: string) {
     return text
   }
 
-  // JavaScript `.` includes unicode line and paragraph break
-  // characters which is why we use [\s\S] instead of '.'. The
-  // first group matches lazily (as few times as possible) and
-  // the second group matches the next following \r or \n
-  // character or the end of the string. Matching the end of the
-  // string lets us avoid dealing with leftover characters outside
-  // of the matching loop.
-  const crLfOrEnd = /([\s\S]*?)([\r\n]|$)/g
-  const lines = new Array<string>('')
-
-  let match
-
-  while ((match = crLfOrEnd.exec(text)) !== null) {
-    // If we match the $ (end of string) we'll get a zero width
-    // match, so we'll need to bump the regexp cursor to ensure
-    // it fails to match on the next round
-    if (match.index === crLfOrEnd.lastIndex) {
-      crLfOrEnd.lastIndex++
-    }
-
-    if (match[1].length > 0) {
-      const line = lines[lines.length - 1]
-
-      if (line.length <= match[1].length) {
-        // Happy path, the new line is equal to or longer
+  return text
+    .split('\n')
+    .map(line =>
+      line.split('\r').reduce((buf, cur) =>
+        // Happy path, if the new line is equal to or longer
         // than the previous, we can just use the new one
         // without creating any new strings.
-        lines[lines.length - 1] = match[1]
-      } else {
-        const remainder = line.substring(match[1].length)
-        lines[lines.length - 1] = `${match[1]}${remainder}`
-      }
-    }
-
-    if (match[2] === '\n') {
-      lines.push('')
-    }
-  }
-
-  return lines.join('\n')
+        cur.length >= buf.length ? cur : cur + buf.substring(cur.length)
+      )
+    )
+    .join('\n')
 }
