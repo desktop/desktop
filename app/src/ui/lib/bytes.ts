cd2d6f270484260dbb2e9a6b4a52a63b92f8454c
Markus Olsson <j.markus.olsson@gmail.com>
2020-07-06 19:47:26 +0200

    Keep units outside of closure

--- app/src/ui/lib/bytes.ts
+++ app/src/ui/lib/bytes.ts
@@ -1,5 +1,6 @@
 import { round } from './round'
 
+const units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
 
 /**
  * Display bytes in human readable format like:
@@ -14,9 +15,8 @@ export function formatBytes(bytes: number, decimals = 0, includeSign = false) {
   if (!Number.isFinite(bytes)) {
     return 'Unknown'
   }
-  const sizes = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB']
   const sizeIndex = Math.floor(Math.log(Math.abs(bytes)) / Math.log(1024))
   const sign = includeSign && bytes > 0 ? '+' : ''
   const value = round(bytes / Math.pow(1024, sizeIndex), decimals)
-  return `${sign}${value} ${sizes[sizeIndex]}`
+  return `${sign}${value} ${units[sizeIndex]}`
 }
