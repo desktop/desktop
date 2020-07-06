ef5d8d25edc41ac93f49587f23ec402911e00604
Markus Olsson <j.markus.olsson@gmail.com>
2020-07-06 19:37:09 +0200

    Test zero  and negative number of decimals

--- app/test/unit/round-test.ts
+++ app/test/unit/round-test.ts
@@ -2,6 +2,7 @@ import { round } from '../../src/ui/lib/round'
 
 describe('round', () => {
   it('rounds to the desired number decimals', () => {
+    expect(round(1.23456789, 0)).toBe(1)
     expect(round(1.23456789, 1)).toBe(1.2)
     expect(round(1.23456789, 2)).toBe(1.23)
     expect(round(1.23456789, 3)).toBe(1.235)
@@ -17,5 +18,11 @@ describe('round', () => {
   it("doesn't attempt to round infinity", () => {
     expect(round(Infinity, 1)).not.toBeFinite()
     expect(round(-Infinity, 1)).not.toBeFinite()
+  })
+
+  it("doesn't attempt to round to less than zero decimals", () => {
+    expect(round(1.23456789, 0)).toBe(1)
+    expect(round(1.23456789, -1)).toBe(1)
+    expect(round(1.23456789, -2)).toBe(1)
   })
 })
