05afe016af333dd5d9df93513080504648ff4691
Markus Olsson <j.markus.olsson@gmail.com>
2020-07-06 22:37:19 +0200

    :book:

--- app/src/ui/lib/bytes.ts
+++ app/src/ui/lib/bytes.ts
@@ -3,13 +3,27 @@ import { round } from './round'
 const units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
 
 /**
- * Display bytes in human readable format like:
+ * Formats a number of bytes into a human readable string.
+ *
+ * This method will uses the IEC representation for orders
+ * of magnitute (KiB/MiB rather than MB/KB) in order to match
+ * the format that Git uses.
+ *
+ * Example output:
+ *
  *    23 GiB
  *   -43 B
  * It's also possible to force sign in order to get the
  * plus sign in case of positive numbers like:
  *   +23 GiB
  *   -43 B
+ *
+ * @param bytes       - The number of bytes to reformat into human
+ *                      readable form
+ * @param decimals    - The number of decimals to round the result
+ *                      to, defaults to zero
+ * @param includeSign - Whether to include the `+` sign for positive
+ *                      results
  */
 export function formatBytes(bytes: number, decimals = 0, includeSign = false) {
   if (!Number.isFinite(bytes)) {
