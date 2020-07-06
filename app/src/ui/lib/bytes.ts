93e3faf7bbc9044c7f7d35acec3c5d358a5fda90
Markus Olsson <j.markus.olsson@gmail.com>
2020-07-06 19:37:29 +0200

    Support rounding bytes to arbitrary number of decimals

--- app/src/ui/lib/bytes.ts
+++ app/src/ui/lib/bytes.ts
@@ -1,3 +1,5 @@
+import { round } from './round'
+
 /**
  * Number sign display mode
  */
@@ -15,13 +17,17 @@ export const enum Sign {
  *   +23GB
  *   -43B
  */
-export const formatBytes = (bytes: number, signType: Sign = Sign.Normal) => {
+export const formatBytes = (
+  bytes: number,
+  signType: Sign = Sign.Normal,
+  decimals = 0
+) => {
   if (!Number.isFinite(bytes)) {
     return 'Unknown'
   }
   const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
   const sizeIndex = Math.floor(Math.log(Math.abs(bytes)) / Math.log(1024))
   const sign = signType === Sign.Forced && bytes > 0 ? '+' : ''
-  const value = Math.round(bytes / Math.pow(1024, sizeIndex))
   return `${sign}${value}${sizes[sizeIndex]}`
+  const value = round(bytes / Math.pow(1024, sizeIndex), decimals)
 }
