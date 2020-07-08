bcee6d371a7f3c53efd4ca27ebc7c7f72d58ad30
Markus Olsson <j.markus.olsson@gmail.com>
2020-07-08 19:20:38 +0200

    :book:

--- app/src/lib/is-application-bundle.ts
+++ app/src/lib/is-application-bundle.ts
@@ -1,5 +1,18 @@
 import getFileMetadata from 'file-metadata'
 
+/**
+ * Attempts to determine if the provided path is an application bundle or not.
+ *
+ * macOS differs from the other platforms we support in that a directory can
+ * also be an application and therefore executable making it unsafe to open
+ * directories on macOS as we could conceivably end up launching an application.
+ *
+ * This application uses file metadata (the `mdls` tool to be exact) to
+ * determine whether a path is actually an application bundle or otherwise
+ * executable.
+ *
+ * NOTE: This method will always return false when not running on macOS.
+ */
 export async function isApplicationBundle(path: string): Promise<boolean> {
   if (!__DARWIN__) {
     return false
