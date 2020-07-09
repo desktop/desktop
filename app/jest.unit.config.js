87d3607c8f335bb39fca8235e339c496f0e12add
Markus Olsson <j.markus.olsson@gmail.com>
2020-07-09 19:48:45 +0200

    Revert accidental changes to jest unit config

--- app/jest.unit.config.js
+++ app/jest.unit.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src/', '<rootDir>/test/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   testMatch: ['**/unit/**/*-test.ts{,x}'],
-  moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'ts', 'tsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFiles: ['<rootDir>/test/globals.ts', '<rootDir>/test/unit-test-env.ts'],
   setupTestFrameworkScriptFile: '<rootDir>/test/setup-test-framework.ts',
   collectCoverageFrom: [
@@ -9,18 +12,19 @@ module.exports = {
     '!**/node_modules/**',
     '!**/vendor/**',
     '!**/*.d.*',
+    // not focused on testing these areas currently
     '!src/ask-pass/**/*',
     '!src/cli/**/*',
     '!src/crash/**/*',
     '!src/highlighter/**/*',
+    // ignore index files
     '!**/index.ts',
   ],
   reporters: ['default', '<rootDir>../script/jest-actions-reporter.js'],
   coverageReporters: ['text-summary', 'json', 'html', 'cobertura'],
   globals: {
     'ts-jest': {
-      babelConfig: true,
+      useBabelrc: true,
     },
   },
-  preset: 'ts-jest',
 }
