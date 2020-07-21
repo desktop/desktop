4e9b06deb1fea261b00e27109f9726b44dc8a0b4
Rafael Oleza <rafeca@gmail.com>
2020-07-21 10:42:59 +0200

    Do not export octicons from the octicons/index.ts file
    WebPack v4 does not apply tree shaking in this scenario (see https://github.com/webpack/webpack/issues/9607), so it's better to remove it in order to avoid people using it in the future.

--- app/src/ui/octicons/index.ts
+++ app/src/ui/octicons/index.ts
@@ -1,5 +1,3 @@
-import * as OcticonSymbol from './octicons.generated'
-export { OcticonSymbol }
 export { OcticonSymbolType } from './octicons.generated'
 export { Octicon } from './octicon'
 export { iconForRepository } from './repository'
