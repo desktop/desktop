de84ffcb8183c32df074e6d9ffec8830a0724324
rafeca <rafeca@gmail.com>
2020-07-24 11:35:38 +0200

    Created <RefNameTextBox/> component
    This component encapsulates the logic that sanitizes a user-provided ref name and displays a warning when the sanitized value is different than the user-provided one.

---
+++ app/src/ui/lib/ref-name-text-box.tsx
@@ -0,0 +1,146 @@
+import * as React from 'react'
+
+import { sanitizedRefName } from '../../lib/sanitize-ref-name'
+import { TextBox } from './text-box'
+import { Octicon, OcticonSymbol } from '../octicons'
+import { Ref } from './ref'
+
+interface IRefNameProps {
+  /**
+   * The initial value for the ref name.
+   *
+   * Note that updates to this prop will be ignored.
+   */
+  readonly initialValue?: string
+
+  /**
+   * The label of the text box.
+   */
+  readonly label?: string | JSX.Element
+
+  /**
+   * Whether to autofocus the input component.
+   */
+  readonly autoFocus?: boolean
+
+  /**
+   * Called when the user changes the ref name.
+   *
+   * A sanitized value for the ref name is passed.
+   */
+  readonly onValueChange: (sanitizedValue: string) => void
+
+  /**
+   * Called when the user-entered ref name is not valid.
+   *
+   * This gives the opportunity to the caller to specify
+   * a custom warning message explaining that the sanitized
+   * value will be used instead.
+   */
+  readonly renderWarningMessage?: (
+    sanitizedValue: string,
+    proposedValue: string
+  ) => JSX.Element | string
+}
+
+interface IRefNameState {
+  readonly proposedValue: string
+  readonly sanitizedValue: string
+}
+
+export class RefNameTextBox extends React.Component<
+  IRefNameProps,
+  IRefNameState
+> {
+  public constructor(props: IRefNameProps) {
+    super(props)
+
+    const proposedValue = props.initialValue || ''
+
+    this.state = {
+      proposedValue: proposedValue,
+      sanitizedValue: sanitizedRefName(proposedValue),
+    }
+  }
+
+  public componentDidMount() {
+    if (this.state.sanitizedValue !== this.props.initialValue) {
+      this.props.onValueChange(this.state.sanitizedValue)
+    }
+  }
+
+  public render() {
+    return (
+      <div className="ref-name-text-box">
+        <TextBox
+          label={this.props.label}
+          value={this.state.proposedValue}
+          autoFocus={this.props.autoFocus}
+          onValueChanged={this.onValueChange}
+        />
+
+        {this.renderRefValueWarning()}
+      </div>
+    )
+  }
+
+  private onValueChange = (proposedValue: string) => {
+    const sanitizedValue = sanitizedRefName(proposedValue)
+
+    this.setState({
+      proposedValue,
+      sanitizedValue,
+    })
+
+    // If the sanitized value didn't change we don't need to
+    // call the onValue change prop.
+    if (sanitizedValue === this.state.sanitizedValue) {
+      return
+    }
+
+    this.props.onValueChange(sanitizedValue)
+  }
+
+  private renderRefValueWarning() {
+    const { proposedValue, sanitizedValue } = this.state
+
+    if (proposedValue === sanitizedValue) {
+      return null
+    }
+
+    const renderWarningMessage =
+      this.props.renderWarningMessage !== undefined
+        ? this.props.renderWarningMessage
+        : this.defaultRenderWarningMessage
+
+    return (
+      <div className="warning-helper-text">
+        <Octicon symbol={OcticonSymbol.alert} />
+
+        <p>{renderWarningMessage(sanitizedValue, proposedValue)}</p>
+      </div>
+    )
+  }
+
+  private defaultRenderWarningMessage(
+    sanitizedValue: string,
+    proposedValue: string
+  ) {
+    // If the proposed value ends up being sanitized as
+    // an empty string we show a message saying that the
+    // proposed value is invalid.
+    if (sanitizedValue.length === 0) {
+      return (
+        <>
+          <Ref>{proposedValue}</Ref> is not a valid name.
+        </>
+      )
+    }
+
+    return (
+      <>
+        Will be created as <Ref>{sanitizedValue}</Ref>.
+      </>
+    )
+  }
+}
