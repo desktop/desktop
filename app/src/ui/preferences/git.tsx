b14f0a4e5d31d370a6f70071380c7f16e44a55c9
rafeca <rafeca@gmail.com>
2020-07-24 13:00:34 +0200

    Add setting to allow users to change the default git branch
    This new settings is available in the git tab on the preferences dialog

--- app/src/ui/preferences/git.tsx
+++ app/src/ui/preferences/git.tsx
@@ -2,17 +2,31 @@ import * as React from 'react'
 import { TextBox } from '../lib/text-box'
 import { Row } from '../lib/row'
 import { DialogContent } from '../dialog'
+import { SuggestedBranchNames } from '../../lib/helpers/default-branch'
+import { RefNameTextBox } from '../lib/ref-name-text-box'
+import { Ref } from '../lib/ref'
+import { RadioButton } from '../lib/radio-button'
 
 interface IGitProps {
   readonly name: string
   readonly email: string
+  readonly defaultBranch: string
 
   readonly onNameChanged: (name: string) => void
   readonly onEmailChanged: (email: string) => void
+  readonly onDefaultBranchChanged: (defaultBranch: string) => void
 }
 
-export class Git extends React.Component<IGitProps, {}> {
+// This will be the prepopulated branch name on the "other" input
+// field when the user selects it.
+const OtherNameForDefaultBranch = ''
+
+export class Git extends React.Component<IGitProps> {
   public render() {
+    const defaultBranchIsOther = !SuggestedBranchNames.includes(
+      this.props.defaultBranch
+    )
+
     return (
       <DialogContent>
         <Row>
@@ -29,7 +43,64 @@ export class Git extends React.Component<IGitProps, {}> {
             onValueChanged={this.props.onEmailChanged}
           />
         </Row>
+
+        <p className="default-branch-title">
+          Default branch on new repositories
+        </p>
+
+        <div className="default-branch-component">
+          {SuggestedBranchNames.map((branchName: string) => (
+            <RadioButton
+              key={branchName}
+              checked={this.props.defaultBranch === branchName}
+              value={branchName}
+              label={branchName}
+              onSelected={this.onDefaultBranchChanged}
+            />
+          ))}
+          <RadioButton
+            key={OtherNameForDefaultBranch}
+            checked={defaultBranchIsOther}
+            value={OtherNameForDefaultBranch}
+            label="Other…"
+            onSelected={this.onDefaultBranchChanged}
+          />
+        </div>
+
+        {defaultBranchIsOther && (
+          <RefNameTextBox
+            initialValue={this.props.defaultBranch}
+            renderWarningMessage={this.renderWarningMessage}
+            onValueChange={this.props.onDefaultBranchChanged}
+          />
+        )}
+        <p className="git-settings-description">
+          These preferences will edit your global Git config.
+        </p>
       </DialogContent>
     )
+  }
+
+  private renderWarningMessage = (
+    sanitizedBranchName: string,
+    proposedBranchName: string
+  ) => {
+    if (sanitizedBranchName === '') {
+      return (
+        <>
+          <Ref>{proposedBranchName}</Ref> is an invalid branch name.
+        </>
+      )
+    }
+
+    return (
+      <>
+        Will be saved as <Ref>{sanitizedBranchName}</Ref>.
+      </>
+    )
+  }
+
+  private onDefaultBranchChanged = (defaultBranch: string) => {
+    this.props.onDefaultBranchChanged(defaultBranch)
   }
 }
