54520be4278388911f038f39cc1d24e4f2785bcb
Markus Olsson <j.markus.olsson@gmail.com>
2020-08-18 12:26:28 +0200

    Update tests with new types

--- app/test/unit/pull-request-status-test.ts
+++ app/test/unit/pull-request-status-test.ts
@@ -1,6 +1,6 @@
 import { APIRefState } from '../../src/lib/api'
 import { getRefStatusSummary } from '../../src/ui/branches/pull-request-status'
-import { IRefStatus } from '../../src/lib/stores/commit-status-store'
+import { ICombinedRefStatus } from '../../src/lib/stores/commit-status-store'
 
 const failure: APIRefState = 'failure'
 const pending: APIRefState = 'pending'
@@ -8,162 +8,94 @@ const success: APIRefState = 'success'
 
 describe('pull request status', () => {
   it('uses the state when no statuses found', () => {
-    const status: IRefStatus = {
+    const status: ICombinedRefStatus = {
       state: success,
-      totalCount: 0,
-      statuses: {
-        total_count: 0,
-        state: pending,
-        statuses: [],
-      },
-      checkRuns: {
-        total_count: 0,
-        check_runs: [],
-      },
+      checks: [],
     }
     expect(getRefStatusSummary(status)).toBe('Commit status: success')
   })
 
   it('changes the failure message to something more friendly', () => {
-    const status: IRefStatus = {
+    const status: ICombinedRefStatus = {
       state: failure,
-      totalCount: 0,
-      statuses: {
-        total_count: 0,
-        state: pending,
-        statuses: [],
-      },
-      checkRuns: {
-        total_count: 0,
-        check_runs: [],
-      },
+      checks: [],
     }
     expect(getRefStatusSummary(status)).toBe('Commit status: failed')
   })
 
   it('reads the statuses when they are populated', () => {
-    const status: IRefStatus = {
+    const status: ICombinedRefStatus = {
       state: success,
-      totalCount: 2,
-      statuses: {
-        total_count: 2,
-        state: success,
-        statuses: [
-          {
-            id: 1,
-            state: success,
-            description: 'first',
-            target_url: '',
-            context: '2',
-          },
-          {
-            id: 2,
-            state: success,
-            description: 'second',
-            target_url: '',
-            context: '2',
-          },
-        ],
-      },
-      checkRuns: {
-        total_count: 0,
-        check_runs: [],
-      },
+      checks: [
+        {
+          state: success,
+          name: '1',
+          description: 'first',
+        },
+        {
+          state: success,
+          name: '2',
+          description: 'second',
+        },
+      ],
     }
     expect(getRefStatusSummary(status)).toBe('2/2 checks OK')
   })
 
   it('a successful status shows the description', () => {
-    const status: IRefStatus = {
+    const status: ICombinedRefStatus = {
       state: success,
-      totalCount: 1,
-      statuses: {
-        total_count: 1,
-        state: success,
-        statuses: [
-          {
-            id: 1,
-            state: success,
-            description: 'The Travis CI build passed',
-            target_url: '',
-            context: '1',
-          },
-        ],
-      },
-      checkRuns: {
-        total_count: 0,
-        check_runs: [],
-      },
+      checks: [
+        {
+          state: success,
+          name: 'travis',
+          description: 'The Travis CI build passed',
+        },
+      ],
     }
 
     expect(getRefStatusSummary(status)).toBe(
-      'Success: The Travis CI build passed'
+      'travis: The Travis CI build passed'
     )
   })
 
   it('an error status shows the description', () => {
-    const status: IRefStatus = {
+    const status: ICombinedRefStatus = {
       state: failure,
-      totalCount: 1,
-      statuses: {
-        total_count: 1,
-        state: failure,
-        statuses: [
-          {
-            id: 1,
-            state: failure,
-            description: 'The Travis CI build failed',
-            target_url: '',
-            context: '1',
-          },
-        ],
-      },
-      checkRuns: {
-        total_count: 0,
-        check_runs: [],
-      },
+      checks: [
+        {
+          state: failure,
+          name: 'travis',
+          description: 'The Travis CI build failed',
+        },
+      ],
     }
 
     expect(getRefStatusSummary(status)).toBe(
-      'Failure: The Travis CI build failed'
+      'travis: The Travis CI build failed'
     )
   })
 
   it('only counts the successful statuses', () => {
-    const status: IRefStatus = {
+    const status: ICombinedRefStatus = {
       state: success,
-      totalCount: 3,
-      statuses: {
-        total_count: 3,
-        state: success,
-        statuses: [
-          {
-            id: 1,
-            state: success,
-            description: 'first',
-            target_url: '',
-            context: '1',
-          },
-          {
-            id: 2,
-            state: pending,
-            description: 'second',
-            target_url: '',
-            context: '2',
-          },
-          {
-            id: 2,
-            state: pending,
-            description: 'third',
-            target_url: '',
-            context: '3',
-          },
-        ],
-      },
-      checkRuns: {
-        total_count: 0,
-        check_runs: [],
-      },
+      checks: [
+        {
+          state: success,
+          description: 'first',
+          name: '1',
+        },
+        {
+          state: pending,
+          description: 'second',
+          name: '2',
+        },
+        {
+          state: pending,
+          description: 'third',
+          name: '3',
+        },
+      ],
     }
 
     expect(getRefStatusSummary(status)).toBe('1/3 checks OK')
