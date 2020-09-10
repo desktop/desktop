ac411830033685759350c5db27ef451b179dc59f
Markus Olsson <j.markus.olsson@gmail.com>
2020-09-10 21:50:14 +0200

    Come on, you know better than this

--- app/src/models/popup.ts
+++ app/src/models/popup.ts
@@ -264,5 +264,5 @@ export type Popup =
       type: PopupType.LocalChangesOverwritten
       repository: Repository
       retryAction: RetryAction
-      files: Array<string>
+      files: ReadonlyArray<string>
     }
