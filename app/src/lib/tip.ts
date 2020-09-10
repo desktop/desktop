4d19e2e7e1e5c4ac617ccbec8ed1e5cc576855ae
Markus Olsson <j.markus.olsson@gmail.com>
2020-09-10 21:58:08 +0200

    Okay, let's try a linting error that doesn't fail the build

--- app/src/lib/tip.ts
+++ app/src/lib/tip.ts
@@ -1,6 +1,6 @@
 import { TipState, Tip } from '../models/tip'
 
-export function getTipSha(tip: Tip) {
+export function   getTipSha(tip: Tip) {
   if (tip.kind === TipState.Valid) {
     return tip.branch.tip.sha
   }
