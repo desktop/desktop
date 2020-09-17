7e4f50b97c0c06b3a45c6809172d71748f7d5733
Markus Olsson <j.markus.olsson@gmail.com>
2020-09-17 15:55:53 +0200

    :book:

--- app/src/lib/app-state.ts
+++ app/src/lib/app-state.ts
@@ -235,6 +235,13 @@ export interface IAppState {
   /** Which step the user is on in the Onboarding Tutorial */
   readonly currentOnboardingTutorialStep: TutorialStep
 
+  /**
+   * Whether or not the app should update the repository indicators (the
+   * blue dot and the ahead/behind arrows in the repository list used to
+   * indicate that the repository has uncommitted changes or is out of sync
+   * with its remote) in the background. See `RepositoryIndicatorUpdater`
+   * for more information
+   */
   readonly repositoryIndicatorsEnabled: boolean
 }
 
