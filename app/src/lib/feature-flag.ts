c133794e945b4b93de8160173c12e70603a6ee8e
Markus Olsson <j.markus.olsson@gmail.com>
2020-10-09 13:23:30 +0200

    Enable side by side diffs for the beta channel

--- app/src/lib/feature-flag.ts
+++ app/src/lib/feature-flag.ts
@@ -130,7 +130,7 @@ export function enableDiscardLines(): boolean {
  * Note: side by side diffs will use the new diff viewer.
  */
 export function enableSideBySideDiffs(): boolean {
-  return enableDevelopmentFeatures()
+  return enableBetaFeatures()
 }
 
 /**
