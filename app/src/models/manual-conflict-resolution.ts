09495f542371545382c3743c0f48acc665786d63
Markus Olsson <j.markus.olsson@gmail.com>
2020-10-22 14:12:14 +0200

    Finish that thought

--- app/src/models/manual-conflict-resolution.ts
+++ app/src/models/manual-conflict-resolution.ts
@@ -1,6 +1,6 @@
-// NOTE: These strings have semantic value, they're
-// passed directly as `--ours` and `--theirs` to
-// git checkout. Take care if ever
+// NOTE: These strings have semantic value, they're passed directly
+// as `--ours` and `--theirs` to git checkout. Please be careful
+// when modifying this type.
 export enum ManualConflictResolutionKind {
   theirs = 'theirs',
   ours = 'ours',
