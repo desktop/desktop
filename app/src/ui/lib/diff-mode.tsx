877375c13504023b71d137dd5dfc495c680b350d
Markus Olsson <j.markus.olsson@gmail.com>
2020-11-10 17:01:04 +0100

    :book:

--- app/src/ui/lib/diff-mode.tsx
+++ app/src/ui/lib/diff-mode.tsx
@@ -3,10 +3,18 @@ import { getBoolean, setBoolean } from '../../lib/local-storage'
 export const showSideBySideDiffDefault = false
 export const showSideBySideDiffKey = 'show-side-by-side-diff'
 
+/**
+ * Gets a value indicating whether not to present diffs in a split view mode
+ * as opposed to unified (the default).
+ */
 export function getShowSideBySideDiff(): boolean {
   return getBoolean(showSideBySideDiffKey, showSideBySideDiffDefault)
 }
 
+/**
+ * Sets a local storage key indicating whether not to present diffs in a split
+ * view mode as opposed to unified (the default).
+ */
 export function setShowSideBySideDiff(showSideBySideDiff: boolean) {
   setBoolean(showSideBySideDiffKey, showSideBySideDiff)
 }
