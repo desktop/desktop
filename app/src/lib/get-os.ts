60ad431676b0b87935233ccdde293691fca622fa
Markus Olsson <j.markus.olsson@gmail.com>
2020-11-12 15:28:57 +0100

    getSystemVersion only exists when running under Electron

--- app/src/lib/get-os.ts
+++ app/src/lib/get-os.ts
@@ -2,10 +2,23 @@ import * as OS from 'os'
 import { compare } from 'compare-versions'
 import memoizeOne from 'memoize-one'
 
+function getSystemVersionSafe() {
+  // getSystemVersion only exists when running under Electron, and not when
+  // running unit tests which frequently end up calling this. There are no
+  // other known reasons why getSystemVersion() would return anything other
+  // than a string
+  return 'getSystemVersion' in process ? process.getSystemVersion() : undefined
+}
+
+function systemVersionGreaterThanOrEqualTo(version: string) {
+  const sysver = getSystemVersionSafe()
+  return sysver === undefined ? false : compare(sysver, version, '>=')
+}
+
 /** Get the OS we're currently running on. */
 export function getOS() {
   if (__DARWIN__) {
-    return `Mac OS ${process.getSystemVersion()}`
+    return `Mac OS ${getSystemVersionSafe()}`
   } else if (__WIN32__) {
     return `Windows ${OS.release()}`
   } else {
@@ -15,24 +28,15 @@ export function getOS() {
 
 /** We're currently running macOS and it is at least Mojave. */
 export const isMacOsAndMojaveOrLater = memoizeOne(
-  () => __DARWIN__ && compare(process.getSystemVersion(), '10.13.0', '>=')
+  () => __DARWIN__ && systemVersionGreaterThanOrEqualTo('10.13.0')
 )
 
 /** We're currently running macOS and it is at least Big Sur. */
 export const isMacOSBigSurOrLater = memoizeOne(
-  () => __DARWIN__ && compare(process.getSystemVersion(), '11.0.0', '>=')
+  () => __DARWIN__ && systemVersionGreaterThanOrEqualTo('11.0.0')
 )
 
 /** We're currently running Windows 10 and it is at least 1809 Preview Build 17666. */
-export function isWindows10And1809Preview17666OrLater() {
-  if (__WIN32__) {
-    const version = OS.release()
-
-    if (version === undefined) {
-      return false
-    }
-
-    return compare(version, '10.0.17666', '>=')
-  }
-  return false
-}
+export const isWindows10And1809Preview17666OrLater = memoizeOne(
+  () => __WIN32__ && systemVersionGreaterThanOrEqualTo('10.0.17666')
+)
