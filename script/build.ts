c04d18618284ff09a7b9a510300a3ccecc635581
Markus Olsson <j.markus.olsson@gmail.com>
2020-11-12 17:40:37 +0100

    Bump electron-packager to latest

--- script/build.ts
+++ script/build.ts
@@ -5,10 +5,10 @@ import * as path from 'path'
 import * as cp from 'child_process'
 import * as fs from 'fs-extra'
 import packager, {
-  arch,
-  ElectronNotarizeOptions,
-  ElectronOsXSignOptions,
+  OsxNotarizeOptions,
+  OsxSignOptions,
   Options,
+  OfficialArch,
 } from 'electron-packager'
 import frontMatter from 'front-matter'
 import { externals } from '../app/webpack.common'
@@ -122,7 +122,7 @@ interface IPackageAdditionalOptions {
     readonly name: string
     readonly schemes: ReadonlyArray<string>
   }>
-  readonly osxSign: ElectronOsXSignOptions & {
+  readonly osxSign: OsxSignOptions & {
     readonly hardenedRuntime?: boolean
   }
 }
@@ -139,7 +139,7 @@ function packageApp() {
     )
   }
 
-  const toPackageArch = (targetArch: string | undefined): arch => {
+  const toPackageArch = (targetArch: string | undefined): OfficialArch => {
     if (targetArch === undefined) {
       return 'x64'
     }
@@ -426,7 +426,7 @@ ${licenseText}`
   fs.removeSync(chooseALicense)
 }
 
-function getNotarizationCredentials(): ElectronNotarizeOptions | undefined {
+function getNotarizationCredentials(): OsxNotarizeOptions | undefined {
   const appleId = process.env.APPLE_ID
   const appleIdPassword = process.env.APPLE_ID_PASSWORD
   if (appleId === undefined || appleIdPassword === undefined) {
