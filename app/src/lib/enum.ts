23a94daf5b9ed5a01d5738a48e1f0872cc0cec89
Markus Olsson <j.markus.olsson@gmail.com>
2020-11-24 14:00:44 +0100

    Give me whatever past me was drinking, new measurements show this as faster!

--- app/src/lib/enum.ts
+++ app/src/lib/enum.ts
@@ -5,22 +5,10 @@ type StringEnum<T extends string> = {
 /**
  * Parse a string into the given (string) enum type. Returns undefined if the
  * enum type provided did not match any of the keys in the enum.
- *
- * Resist the urge to use Object.entries().find() to get this down to a
- * one-liner. I measured it and this is 200x faster and doesn't allocate any
- * unnecessary arrays.
  */
 export function parseEnumValue<T extends string>(
   enumObj: StringEnum<T>,
   value: string
 ): T | undefined {
-  for (const key in enumObj) {
-    if (
-      Object.prototype.hasOwnProperty.call(enumObj, key) &&
-      enumObj[key] === value
-    ) {
-      return enumObj[key]
-    }
-  }
-  return undefined
+  return Object.values(enumObj).find(v => v === value)
 }
