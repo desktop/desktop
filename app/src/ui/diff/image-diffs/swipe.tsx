70e8cf06c23d3b3cb9602b14dc4c230997c3e343
Markus Olsson <j.markus.olsson@gmail.com>
2020-11-25 11:38:57 +0100

    Swap previous/before and align with dotcom

--- app/src/ui/diff/image-diffs/swipe.tsx
+++ app/src/ui/diff/image-diffs/swipe.tsx
@@ -25,6 +25,14 @@ export class Swipe extends React.Component<
       width: this.props.maxSize.width,
     }
 
+    const swiperWidth = this.props.maxSize.width * (1 - this.state.percentage)
+
+    const currentStyle: React.CSSProperties = {
+      height: this.props.maxSize.height,
+      width: this.props.maxSize.width,
+      left: -(this.props.maxSize.width - swiperWidth),
+    }
+
     const maxSize: React.CSSProperties = {
       maxHeight: this.props.maxSize.height,
       maxWidth: this.props.maxSize.width,
@@ -34,24 +42,24 @@ export class Swipe extends React.Component<
       <div className="image-diff-swipe">
         <div className="sizing-container" ref={this.props.onContainerRef}>
           <div className="image-container" style={style}>
-            <div className="image-diff-current" style={style}>
+            <div className="image-diff-previous" style={style}>
               <ImageContainer
-                image={this.props.current}
-                onElementLoad={this.props.onCurrentImageLoad}
+                image={this.props.previous}
+                onElementLoad={this.props.onPreviousImageLoad}
                 style={maxSize}
               />
             </div>
             <div
               className="swiper"
               style={{
-                width: this.props.maxSize.width * (1 - this.state.percentage),
+                width: swiperWidth,
                 height: this.props.maxSize.height,
               }}
             >
-              <div className="image-diff-previous" style={style}>
+              <div className="image-diff-current" style={currentStyle}>
                 <ImageContainer
-                  image={this.props.previous}
-                  onElementLoad={this.props.onPreviousImageLoad}
+                  image={this.props.current}
+                  onElementLoad={this.props.onCurrentImageLoad}
                   style={maxSize}
                 />
               </div>
