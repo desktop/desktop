0e9e3d13b7e35ee29c4e1ff5b24f414fd2d5b749
Markus Olsson <j.markus.olsson@gmail.com>
2020-11-27 17:19:03 +0100

    Rip out old AheadBehindUpdater and ComparisonCache

--- app/src/lib/stores/repository-state-cache.ts
+++ app/src/lib/stores/repository-state-cache.ts
@@ -19,7 +19,6 @@ import {
   IRebaseState,
   ChangesSelectionKind,
 } from '../app-state'
-import { ComparisonCache } from '../comparison-cache'
 import { merge } from '../merge'
 import { DefaultCommitMessage } from '../../models/commit-message'
 
@@ -148,7 +147,6 @@ function getInitialRepositoryState(): IRepositoryState {
       showBranchList: false,
       filterText: '',
       commitSHAs: [],
-      aheadBehindCache: new ComparisonCache(),
       allBranches: new Array<Branch>(),
       recentBranches: new Array<Branch>(),
       defaultBranch: null,
