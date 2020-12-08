180b3b57bdd3562c92e7dfecd9464f6831172401
Markus Olsson <j.markus.olsson@gmail.com>
2020-12-08 13:08:13 +0100

    Fix merge mistake

--- app/src/lib/git/log.ts
+++ app/src/lib/git/log.ts
@@ -89,7 +89,7 @@ export async function getCommits(
       `--max-count=${limit}`,
       '--no-show-signature',
       '--no-color',
-      ...parser.format,
+      `--format=${parser.format}`,
       ...additionalArgs,
       revisionRange,
       '--',
