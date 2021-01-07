cff1fec0ee5cf30d1af6eb84259fe42f5d8f8aca
Sergio Padrino <sergio.padrino@gmail.com>
2021-01-07 17:49:48 +0100

    Revert "Improve repository change detection. Fixes #11327"
    This reverts commit a8f6198b1b659d7e3fc9a8f8de210e99352d2587.

--- app/src/lib/stores/app-store.ts
+++ app/src/lib/stores/app-store.ts
@@ -1817,31 +1817,6 @@ export class AppStore extends TypedBaseStore<IAppState> {
     })
   }
 
-  private hasRepositoryChanged(
-    oldRepository: Repository | CloningRepository | null,
-    newRepository: Repository | CloningRepository | null
-  ) {
-    if (oldRepository && newRepository) {
-      // If this is a GitHub repository, check if the workflow preferences
-      // changed too.
-      if (
-        oldRepository instanceof Repository &&
-        isRepositoryWithGitHubRepository(oldRepository) &&
-        newRepository instanceof Repository &&
-        isRepositoryWithGitHubRepository(newRepository) &&
-        oldRepository.workflowPreferences !== newRepository.workflowPreferences
-      ) {
-        return true
-      }
-
-      return oldRepository.hash !== newRepository.hash
-    }
-
-    return (
-      (oldRepository && !newRepository) || (!oldRepository && newRepository)
-    )
-  }
-
   private updateRepositorySelectionAfterRepositoriesChanged() {
     const selectedRepository = this.selectedRepository
     let newSelectedRepository: Repository | CloningRepository | null = this
@@ -1869,7 +1844,13 @@ export class AppStore extends TypedBaseStore<IAppState> {
       }
     }
 
-    if (this.hasRepositoryChanged(selectedRepository, newSelectedRepository)) {
+    const repositoryChanged =
+      (selectedRepository &&
+        newSelectedRepository &&
+        selectedRepository.hash !== newSelectedRepository.hash) ||
+      (selectedRepository && !newSelectedRepository) ||
+      (!selectedRepository && newSelectedRepository)
+    if (repositoryChanged) {
       this._selectRepository(newSelectedRepository)
       this.emitUpdate()
     }
