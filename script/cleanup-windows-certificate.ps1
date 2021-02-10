e77cc2953d38e716be73f9df467b12d52d64245f
mlgsfe76 <mlgsfe76@gmail.com>
2021-02-10 09:28:52 +0000

    Delete script directory

--- script/cleanup-windows-certificate.ps1
+++
@@ -1,7 +0,0 @@
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-
-$file = "$scriptPath\windows-certificate.pfx"
-
-if ((Test-Path $file)) {
-  Remove-Item $file
-}
--- script/draft-release/channel.ts
+++
@@ -1,1 +0,0 @@
-export type Channel = 'production' | 'beta' | 'test'
