7d040979bcafd1f56ee14a5354f308471a0cf106
Sergio Padrino <sergio.padrino@gmail.com>
2021-02-15 17:12:43 +0100

    Allow retrieving only the local value of a git config key #610

--- app/src/lib/git/config.ts
+++ app/src/lib/git/config.ts
@@ -2,12 +2,20 @@ import { git } from './core'
 import { Repository } from '../../models/repository'
 import { normalize } from 'path'
 
-/** Look up a config value by name in the repository. */
+/**
+ * Look up a config value by name in the repository.
+ *
+ * @param onlyLocal Whether or not the value to be retrieved should stick to
+ *                  the local repository settings. It is false by default. This
+ *                  is equivalent to using the `--local` argument in the
+ *                  `git config` invocation.
+ */
 export function getConfigValue(
   repository: Repository,
-  name: string
+  name: string,
+  onlyLocal: boolean = false
 ): Promise<string | null> {
-  return getConfigValueInPath(name, repository.path)
+  return getConfigValueInPath(name, repository.path, onlyLocal)
 }
 
 /** Look up a global config value by name. */
@@ -17,7 +25,7 @@ export function getGlobalConfigValue(
     HOME: string
   }
 ): Promise<string | null> {
-  return getConfigValueInPath(name, null, undefined, env)
+  return getConfigValueInPath(name, null, false, undefined, env)
 }
 
 /**
@@ -33,24 +41,29 @@ export async function getGlobalBooleanConfigValue(
     HOME: string
   }
 ): Promise<boolean | null> {
-  const value = await getConfigValueInPath(name, null, 'bool', env)
+  const value = await getConfigValueInPath(name, null, false, 'bool', env)
   return value === null ? null : value !== 'false'
 }
 
 /**
  * Look up a config value by name
  *
- * @param path The path to execute the `git` command in. If null
- *             we'll use the global configuration (i.e. --global)
- *             and execute the Git call from the same location that
- *             GitHub Desktop is installed in.
- * @param type Canonicalize configuration values according to the
- *             expected type (i.e. 0 -> false, "on" -> true etc).
- *             See `--type` documentation in `git config`
+ * @param path      The path to execute the `git` command in. If null
+ *                  we'll use the global configuration (i.e. --global)
+ *                  and execute the Git call from the same location that
+ *                  GitHub Desktop is installed in.
+ * @param onlyLocal Whether or not the value to be retrieved should stick to
+ *                  the local repository settings (if a path is specified). It
+ *                  is false by default. It is equivalent to using the `--local`
+ *                  argument in the `git config` invocation.
+ * @param type      Canonicalize configuration values according to the
+ *                  expected type (i.e. 0 -> false, "on" -> true etc).
+ *                  See `--type` documentation in `git config`
  */
 async function getConfigValueInPath(
   name: string,
   path: string | null,
+  onlyLocal: boolean = false,
   type?: 'bool' | 'int' | 'bool-or-int' | 'path' | 'expiry-date' | 'color',
   env?: {
     HOME: string
@@ -59,6 +72,8 @@ async function getConfigValueInPath(
   const flags = ['config', '-z']
   if (!path) {
     flags.push('--global')
+  } else if (onlyLocal) {
+    flags.push('--local')
   }
 
   if (type !== undefined) {
@@ -152,13 +167,15 @@ async function setConfigValueInPath(
 ): Promise<void> {
   const options = env ? { env } : undefined
 
-  const flags = [
-    'config',
-    ...(!path ? ['--global'] : []),
-    '--replace-all',
-    name,
-    value,
-  ]
+  const flags = ['config']
+
+  if (!path) {
+    flags.push('--global')
+  }
+
+  flags.push('--replace-all', name, value)
+
+  await git(flags, path || __dirname, 'setConfigValueInPath', options)
 
   await git(flags, __dirname, 'setConfigValueInPath', options)
 }
