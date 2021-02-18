624018c15879eef54543f85dffdd1f838f4f4d2a
tidy-dev <tidy-dev@github.com>
2021-02-18 09:32:41 -0500

    Cherry Pick Flow Component with choose target branch

---
+++ app/src/ui/cherry-pick/cherry-pick-flow.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react'
+
+import { assertNever } from '../../lib/fatal-error'
+import { CherryPickFlowStep, CherryPickStep } from '../../models/cherry-pick'
+
+import { Repository } from '../../models/repository'
+import { Dispatcher } from '../dispatcher'
+import { ChooseTargetBranchDialog } from './choose-target-branch'
+
+interface ICherryPickFlowProps {
+  readonly repository: Repository
+  readonly dispatcher: Dispatcher
+  readonly step: CherryPickFlowStep
+
+  readonly onDismissed: () => void
+}
+
+/** A component for initiating and performing a cherry pick. */
+export class CherryPickFlow extends React.Component<ICherryPickFlowProps> {
+  private onFlowEnded = () => {
+    this.props.onDismissed()
+  }
+
+  private onCherryPick() {
+    // TODO: call this.props.dispatcher.cherryPick
+  }
+
+  public render() {
+    const { step } = this.props
+
+    switch (step.kind) {
+      case CherryPickStep.ChooseTargetBranch: {
+        const {
+          allBranches,
+          defaultBranch,
+          currentBranch,
+          recentBranches,
+        } = step
+        return (
+          <ChooseTargetBranchDialog
+            key="choose-branch"
+            allBranches={allBranches}
+            defaultBranch={defaultBranch}
+            recentBranches={recentBranches}
+            currentBranch={currentBranch}
+            onCherryPick={this.onCherryPick}
+            onDismissed={this.onFlowEnded}
+          />
+        )
+      }
+      default:
+        return assertNever(step.kind, 'Unknown cherry pick step found')
+    }
+  }
+}
