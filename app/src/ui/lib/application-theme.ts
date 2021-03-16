0a194064c2ddd92002a68506eef4fd5c240e3b48
Steven Yeh <say25@cornell.edu>
2021-03-16 14:29:58 -0500

    Fix Language Typo

--- app/src/ui/lib/application-theme.ts
+++ app/src/ui/lib/application-theme.ts
@@ -129,7 +129,7 @@ export function supportsSystemThemeChanges(): boolean {
     return isMacOSMojaveOrLater()
   } else if (__WIN32__) {
     // Its technically possible this would still work on prior versions of Windows 10 but 1809
-    // was released October 2nd, 2018 that the feature can just be "attained" by upgrading
+    // was released October 2nd, 2018 and the feature can just be "attained" by upgrading
     // See https://github.com/desktop/desktop/issues/9015 for more
     return isWindows10And1809Preview17666OrLater()
   }
