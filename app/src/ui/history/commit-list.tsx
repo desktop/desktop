5935e4f8c3efccf13327e63f4b2dd2736e086d7d
Becca <tidy-dev@github.com>
2021-04-21 12:20:57 -0400

    Use spread operator for copy

--- app/src/ui/history/commit-list.tsx
+++ app/src/ui/history/commit-list.tsx
@@ -194,7 +194,7 @@ export class CommitList extends React.Component<ICommitListProps, {}> {
     // cherry-picking results. If user wants to use cherry-picking for
     // reordering, they will need to do multiple cherry-picks.
     // Goal: first commit in history -> first on array
-    const sorted = rows.map(r => r).sort((a, b) => b - a)
+    const sorted = [...rows].sort((a, b) => b - a)
 
     const selectedShas = sorted.map(r => this.props.commitSHAs[r])
     const selectedCommits = this.lookupCommits(selectedShas)
