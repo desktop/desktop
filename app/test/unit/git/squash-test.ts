788e6ed4ea5ac892737af263ad5f6f15416037cc
Becca <tidy-dev@github.com>
2021-04-28 15:42:28 -0400

    Multiple commits squashed test

--- app/test/unit/git/squash-test.ts
+++ app/test/unit/git/squash-test.ts
@@ -1,4 +1,9 @@
-import { getCommit, getCommits, RebaseResult } from '../../../src/lib/git'
+import {
+  getChangedFiles,
+  getCommit,
+  getCommits,
+  RebaseResult,
+} from '../../../src/lib/git'
 import { CommitOneLine } from '../../../src/models/commit'
 import { Repository } from '../../../src/models/repository'
 import { setupEmptyRepositoryDefaultMain } from '../../helpers/repositories'
@@ -34,6 +39,46 @@ describe('git/cherry-pick', () => {
     expect(squashed.summary).toBe('Test Summary')
     expect(squashed.body).toBe('Test Body\n')
     expect(log.length).toBe(2)
+
+    // verify squashed commit contains changes from squashed commits
+    const squashedFiles = await getChangedFiles(repository, squashed.sha)
+    const squashedFilePaths = squashedFiles.map(f => f.path).join(' ')
+    expect(squashedFilePaths.includes('first.md')).toBeTrue()
+    expect(squashedFilePaths.includes('second.md')).toBeTrue()
+  })
+
+  it('squashes multiple commit onto one (non-conflicting)', async () => {
+    const firstCommit = await makeSquashCommit(repository, 'first')
+    const secondCommit = await makeSquashCommit(repository, 'second')
+    const thirdCommit = await makeSquashCommit(repository, 'third')
+    const fourthCommit = await makeSquashCommit(repository, 'fourth')
+
+    const result = await squash(
+      repository,
+      [secondCommit, thirdCommit, fourthCommit],
+      firstCommit,
+      initialCommit.sha,
+      'Test Summary',
+      'Test Body'
+    )
+
+    expect(result).toBe(RebaseResult.CompletedWithoutError)
+
+    const log = await getCommits(repository, 'HEAD', 5)
+    const squashed = log[0]
+    expect(squashed.summary).toBe('Test Summary')
+    expect(squashed.body).toBe('Test Body\n')
+    expect(log.length).toBe(2)
+
+    // verify squashed commit contains changes from squashed commits
+    const squashedFiles = await getChangedFiles(repository, squashed.sha)
+    const squashedFilePaths = squashedFiles.map(f => f.path).join(' ')
+    expect(squashedFilePaths.includes('first.md')).toBeTrue()
+    expect(squashedFilePaths.includes('second.md')).toBeTrue()
+    expect(squashedFilePaths.includes('third.md')).toBeTrue()
+    expect(squashedFilePaths.includes('fourth.md')).toBeTrue()
+  })
+
   })
 })
 
