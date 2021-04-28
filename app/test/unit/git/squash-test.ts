f33217f6b59082a0fd101769516a8d31655388ee
Becca <tidy-dev@github.com>
2021-04-28 15:43:18 -0400

    Squash/Reorder test

--- app/test/unit/git/squash-test.ts
+++ app/test/unit/git/squash-test.ts
@@ -79,6 +79,51 @@ describe('git/cherry-pick', () => {
     expect(squashedFilePaths.includes('fourth.md')).toBeTrue()
   })
 
+  it('squashes multiple commit non-sequential commits (reorders, non-conflicting)', async () => {
+    const firstCommit = await makeSquashCommit(repository, 'first')
+    const secondCommit = await makeSquashCommit(repository, 'second')
+    await makeSquashCommit(repository, 'third')
+    const fourthCommit = await makeSquashCommit(repository, 'fourth')
+
+    // From oldest to newest, log looks like:
+    // - initial commit
+    // - 'first' commit
+    // - 'second' commit
+    // - 'third' commit
+    // - 'fourth' commit
+
+    // Squashing 'second' and 'fourth' onto 'first'
+    // Thus, reordering 'fourth' to be before 'third'
+    const result = await squash(
+      repository,
+      [secondCommit, fourthCommit],
+      firstCommit,
+      initialCommit.sha,
+      'Test Summary',
+      'Test Body'
+    )
+
+    expect(result).toBe(RebaseResult.CompletedWithoutError)
+
+    // From oldest to newest, log should look like:
+    // - initial commit - log[2]
+    // - 'third' commit - log[1]
+    // - The squashed commit 'Test Summary' - log[0]
+    const log = await getCommits(repository, 'HEAD', 5)
+    const squashed = log[0]
+    expect(squashed.summary).toBe('Test Summary')
+    expect(squashed.body).toBe('Test Body\n')
+    expect(log[1].summary).toBe('third')
+    expect(log.length).toBe(3)
+
+    // verify squashed commit contains changes from squashed commits
+    const squashedFiles = await getChangedFiles(repository, squashed.sha)
+    const squashedFilePaths = squashedFiles.map(f => f.path).join(' ')
+    expect(squashedFilePaths.includes('first.md')).toBeTrue()
+    expect(squashedFilePaths.includes('second.md')).toBeTrue()
+    expect(squashedFilePaths.includes('fourth.md')).toBeTrue()
+    expect(squashedFilePaths.includes('third.md')).toBeFalse()
+  })
   })
 })
 
