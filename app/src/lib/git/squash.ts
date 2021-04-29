8fb1db5be87afbc50294f318dc33c830bca64051
Becca <tidy-dev@github.com>
2021-04-29 07:27:09 -0400

    Refactor to use loop and append file for better performance

--- app/src/lib/git/squash.ts
+++ app/src/lib/git/squash.ts
@@ -1,6 +1,6 @@
 import * as FSE from 'fs-extra'
 import { getCommits, revRange } from '.'
-import { Commit, CommitOneLine } from '../../models/commit'
+import { CommitOneLine } from '../../models/commit'
 import { Repository } from '../../models/repository'
 import { getTempFilePath } from '../file-system'
 import { rebaseInteractive, RebaseResult } from './rebase'
@@ -22,48 +22,64 @@ export async function squash(
   commitSummary: string,
   commitDescription: string
 ): Promise<RebaseResult> {
-  const commits: Commit[] = [
-    ...(await getCommits(repository, revRange(lastRetainedCommitRef, 'HEAD'))),
-  ]
+  let messagePath, todoPath
 
-  let todoOutput = ''
-  commits.forEach(c => {
-    if (toSquash.map(sq => sq.sha).includes(c.sha)) {
-      return
+  try {
+    const commits = await getCommits(
+      repository,
+      revRange(lastRetainedCommitRef, 'HEAD')
+    )
+
+    todoPath = await getTempFilePath('squashTodo')
+
+    // need to traverse in reverse so we do oldest to newest (replay commits)
+    for (let i = commits.length - 1; i >= 0; i--) {
+      // Ignore commits to squash because those are written right next to the target commit
+      if (toSquash.map(sq => sq.sha).includes(commits[i].sha)) {
+        continue
+      }
+
+      await FSE.appendFile(
+        todoPath,
+        `pick ${commits[i].sha} ${commits[i].summary}\n`
+      )
+
+      // If it's the target commit, write a `squash` line for every commit to squash
+      if (commits[i].sha === squashOnto.sha) {
+        for (let j = 0; j < toSquash.length; j++) {
+          await FSE.appendFile(
+            todoPath,
+            `squash ${toSquash[j].sha} ${toSquash[j].summary}\n`
+          )
+        }
+      }
     }
 
-    if (c.sha === squashOnto.sha) {
-      todoOutput += `pick ${c.sha} ${c.summary}\n`
-      toSquash.forEach(async sq => {
-        todoOutput += `squash ${sq.sha} ${sq.summary}\n`
-      })
-      return
+    messagePath = await getTempFilePath('squashCommitMessage')
+    const message =
+      commitDescription !== ''
+        ? `${commitSummary}\n\n${commitDescription}`
+        : commitSummary
+    await FSE.writeFile(messagePath, message)
+
+    const result = await rebaseInteractive(
+      repository,
+      todoPath,
+      lastRetainedCommitRef,
+      'squash',
+      `cat "${messagePath}" >`
+      // TODO: add progress
+    )
+    return result
+  } catch (e) {
+    if (todoPath !== undefined) {
+      FSE.remove(todoPath)
     }
 
-    todoOutput += `pick ${c.sha} ${c.summary}\n`
-  })
+    if (messagePath !== undefined) {
+      FSE.remove(messagePath)
+    }
+  }
 
-  const todoPath = await getTempFilePath('squashTodo')
-  await FSE.writeFile(todoPath, todoOutput)
-
-  const messagePath = await getTempFilePath('squashCommitMessage')
-  const message =
-    commitDescription !== ''
-      ? `${commitSummary}\n\n${commitDescription}`
-      : commitSummary
-  await FSE.writeFile(messagePath, message)
-
-  const result = await rebaseInteractive(
-    repository,
-    todoPath,
-    lastRetainedCommitRef,
-    'squash',
-    `cat "${messagePath}" >`
-    // TODO: add progress
-  )
-
-  FSE.remove(todoPath)
-  FSE.remove(messagePath)
-
-  return result
+  return RebaseResult.Error
 }
