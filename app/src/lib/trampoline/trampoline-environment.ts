1715adbd39f12845b92e029a5ca253bc9b426d0e
Sergio Padrino <sergio.padrino@gmail.com>
2021-08-11 18:25:29 +0200

    Change the way SSH and trampoline env variables are passed to git

--- app/src/lib/trampoline/trampoline-environment.ts
+++ app/src/lib/trampoline/trampoline-environment.ts
@@ -1,40 +1,54 @@
-import { IGitAccount } from '../../models/git-account'
-import { envForRemoteOperation } from '../git/environment'
 import { trampolineServer } from './trampoline-server'
 import { withTrampolineToken } from './trampoline-tokens'
+import * as Path from 'path'
+import { enableDesktopTrampoline } from '../feature-flag'
+import { getDesktopTrampolineFilename } from 'desktop-trampoline'
+import { TrampolineCommandIdentifier } from '../trampoline/trampoline-command'
 
 /**
  * Allows invoking a function with a set of environment variables to use when
- * invoking a Git subcommand that needs to communicate with a remote (i.e.
- * fetch, clone, push, pull, ls-remote, etc etc) and with a token to use in the
+ * invoking a Git subcommand that needs to use the trampoline (mainly git
+ * operations requiring an askpass script) and with a token to use in the
  * trampoline server.
  *
- * For more info about the specific environment variables used in the remote
- * git operation, please see `envForRemoteOperation`.
- *
- * @param account   The authentication information (if available) to provide
- *                  to Git for use when connecting to the remote
- * @param remoteUrl The primary remote URL for this operation. Note that Git
- *                  might connect to other remotes in order to fulfill the
- *                  operation. As an example, a clone of
- *                  https://github.com/desktop/desktop could contain a submodule
- *                  pointing to another host entirely. Used to resolve which
- *                  proxy (if any) should be used for the operation.
  * @param fn        Function to invoke with all the necessary environment
  *                  variables.
  */
-export async function withTrampolineEnvForRemoteOperation<T>(
-  account: IGitAccount | null,
-  remoteUrl: string,
+export async function withTrampolineEnv<T>(
   fn: (env: Object) => Promise<T>
 ): Promise<T> {
-  const env = await envForRemoteOperation(account, remoteUrl)
+  const askPassPath = enableDesktopTrampoline()
+    ? getDesktopTrampolinePath()
+    : getAskPassTrampolinePath()
 
   return withTrampolineToken(async token =>
     fn({
-      ...env,
       DESKTOP_PORT: await trampolineServer.getPort(),
       DESKTOP_TRAMPOLINE_TOKEN: token,
+      GIT_ASKPASS: askPassPath,
+      DESKTOP_TRAMPOLINE_IDENTIFIER: TrampolineCommandIdentifier.AskPass,
+
+      // Env variables specific to the old askpass trampoline
+      DESKTOP_PATH: process.execPath,
+      DESKTOP_ASKPASS_SCRIPT: getAskPassScriptPath(),
     })
   )
+}
+
+/** Returns the path of the desktop-trampoline binary. */
+export function getDesktopTrampolinePath(): string {
+  return Path.resolve(
+    __dirname,
+    'desktop-trampoline',
+    getDesktopTrampolineFilename()
+  )
+}
+
+function getAskPassTrampolinePath(): string {
+  const extension = __WIN32__ ? 'bat' : 'sh'
+  return Path.resolve(__dirname, 'static', `ask-pass-trampoline.${extension}`)
+}
+
+function getAskPassScriptPath(): string {
+  return Path.resolve(__dirname, 'ask-pass.js')
 }
