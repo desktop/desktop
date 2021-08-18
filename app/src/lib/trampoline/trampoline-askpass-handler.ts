734c6f306110cb484e4843273b7b4e0a6e8c4ae5
Sergio Padrino <sergio.padrino@gmail.com>
2021-08-18 13:42:00 +0200

    Small refactor to store SSH key passphrases after a git command succeeds

--- app/src/lib/trampoline/trampoline-askpass-handler.ts
+++ app/src/lib/trampoline/trampoline-askpass-handler.ts
@@ -1,5 +1,8 @@
 import { getKeyForEndpoint } from '../auth'
-import { getSSHKeyPassphrase } from '../ssh/ssh'
+import {
+  getSSHKeyPassphrase,
+  keepSSHKeyPassphraseToStore,
+} from '../ssh/ssh-key-passphrase'
 import { TokenStore } from '../stores'
 import { TrampolineCommandHandler } from './trampoline-command'
 import { trampolineUIHelper } from './trampoline-ui-helper'
@@ -37,6 +40,7 @@ async function handleSSHHostAuthenticity(
 }
 
 async function handleSSHKeyPassphrase(
+  operationGUID: string,
   prompt: string
 ): Promise<string | undefined> {
   const promptRegex = /^Enter passphrase for key '(.+)': $/
@@ -67,6 +71,10 @@ async function handleSSHKeyPassphrase(
     storePassphrase,
   } = await trampolineUIHelper.promptSSHKeyPassphrase(keyPath)
 
+  if (passphrase !== undefined && storePassphrase) {
+    keepSSHKeyPassphraseToStore(operationGUID, keyPath, passphrase)
+  }
+
   return passphrase ?? ''
 }
 
@@ -82,7 +90,7 @@ export const askpassTrampolineHandler: TrampolineCommandHandler = async command 
   }
 
   if (firstParameter.startsWith('Enter passphrase for key ')) {
-    return handleSSHKeyPassphrase(firstParameter)
+    return handleSSHKeyPassphrase(command.trampolineToken, firstParameter)
   }
 
   const username = command.environmentVariables.get('DESKTOP_USERNAME')
