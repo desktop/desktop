8acbdfe2cce1ac8209adb0faf16fd6e8619b0f34
tidy-dev <tidy-dev@github.com>
2021-08-24 06:51:00 -0400

    Fix vertical menu item movement in preferences

--- app/src/ui/app-theme.tsx
+++ app/src/ui/app-theme.tsx
@@ -87,6 +87,7 @@ export class AppTheme extends React.PureComponent<IAppThemeProps> {
         --diff-add-border: 1px solid green;
         --diff-delete-border: 1px solid crimson;
         --tab-bar-hover-border: 2px solid ${border} !important;
+        --tab-bar-item-border: 2px solid ${background};
         --foldout-border: 1px solid ${border};
     `
 
