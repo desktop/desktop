b471206dfb158aebc0939125a25470bdc6280eac
Sergio Padrino <sergio.padrino@gmail.com>
2021-08-30 16:28:40 +0200

    Merge pull request #12863 from desktop/fix-win32-external-editors
    Fix Notepad++ and RStudio integration on Windows

--- app/src/lib/editors/win32.ts
+++ app/src/lib/editors/win32.ts
@@ -313,7 +313,7 @@ const editors: IWindowsExternalEditor[] = [
       // 32-bit version of Notepad++
       Wow64LocalMachineUninstallKey('Notepad++'),
     ],
-    executableShimPaths: [],
+    executableShimPaths: [[]],
     installLocationRegistryKey: 'DisplayIcon',
     expectedInstallationChecker: (displayName, publisher) =>
       displayName.startsWith('Notepad++') && publisher === 'Notepad++ Team',
@@ -329,7 +329,7 @@ const editors: IWindowsExternalEditor[] = [
   {
     name: 'RStudio',
     registryKeys: [Wow64LocalMachineUninstallKey('RStudio')],
-    executableShimPaths: [],
+    executableShimPaths: [[]],
     installLocationRegistryKey: 'DisplayIcon',
     expectedInstallationChecker: (displayName, publisher) =>
       displayName === 'RStudio' && publisher === 'RStudio',
