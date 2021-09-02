00ae984175958ff67c27a753f01e80fd11564a3d
Thomas Schmidt <thomas@thsmdt.com>
2021-09-02 18:01:52 +0200

    Mention host key verification as possible auth issue

--- app/src/lib/git/core.ts
+++ app/src/lib/git/core.ts
@@ -314,6 +314,7 @@ function getDescriptionForError(error: DugiteError): string | null {
 - You do not have permission to access this repository.
 - The repository is archived on GitHub. Check the repository settings to confirm you are still permitted to push commits.
 - If you use SSH authentication, check that your key is added to the ssh-agent and associated with your account.
+- If you use SSH authentication, ensure the host key verification passes for your repository hosting service.
 - If you used username / password authentication, you might need to use a Personal Access Token instead of your account password. Check the documentation of your repository hosting service.`
   }
 
