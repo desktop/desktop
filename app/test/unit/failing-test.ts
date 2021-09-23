d189c9e8d2e66290d9e8036b389feed6d7d625e1
tidy-dev <tidy-dev@github.com>
2021-09-23 13:58:06 -0400

    Rerun..?

--- app/test/unit/failing-test.ts
+++ app/test/unit/failing-test.ts
@@ -1,9 +1,5 @@
-
 describe('I want this to fail for check run testing', () => {
-
-
-  it('finds one conflicted file', async () => {
+    it('finds one conflicted file', async () => {
     expect(1).toBe(2)
   })
-
 })
