c8aed25d8b0521f26f77d0815c05176b9ac456b8
tidy-dev <tidy-dev@github.com>
2021-09-24 12:08:58 -0400

    Conflict

--- app/demo.ts
+++ app/demo.ts
@@ -5,36 +5,6 @@ enum ProgrammingLanguage {
   Java,
 }
 
-function describeProgrammingLanguage(language: ProgrammingLanguage): string {
-  switch (language) {
-    case ProgrammingLanguage.JavaScript:
-      return `Yeah.. It is pretty good.`
-    case ProgrammingLanguage.TypeScript:
-      return 'Better than javascript.'
-    case ProgrammingLanguage.HTML:
-      return 'Are you sure that is a programming language? - The real changes'
-    case ProgrammingLanguage.Java:
-      return 'Java java!'
-    default:
-      return `Don't know  what you are talking about..`
-  }
-}
-
-
-describe('universe-demo-describeProgrammingLanguage', () => {
-  it('describes  javascript', () => {
-    const result = describeProgrammingLanguage(ProgrammingLanguage.JavaScript)
-    expect(result).toBe('Yeah..  It is pretty good.')
-  })
-
-    it('cannot describe java', () => {
-    const result = describeProgrammingLanguage(ProgrammingLanguage.Java)
-    expect(result).toBe(
-      `Don't know what you are talking  about.. Another change`
-    )
-  })
-})
-
 function getProgrammingLanguageName(language: ProgrammingLanguage): string {
   switch (language) {
     case ProgrammingLanguage.JavaScript:
@@ -63,3 +33,5 @@ describe('universe-demo-getProgrammingLanguageName', () => {
     expect(result).toBe(`Java java!`)
   })
 })
+
+
