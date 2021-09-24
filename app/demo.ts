f347d0b5a772805b5b89b71972a96a6179071703
tidy-dev <tidy-dev@github.com>
2021-09-24 11:59:45 -0400

    Add Method for programming language
    Seeing whitespaces
    
    Co-Authored-By: Sergio Padrino <1083228+sergiou87@users.noreply.github.com>

--- app/demo.ts
+++ app/demo.ts
@@ -1,10 +1,33 @@
+enum ProgrammingLanguage {
+  JavaScript,
+  TypeScript,
+  HTML,
+  Java,
+}
+
+function describeProgrammingLanguage(language: ProgrammingLanguage): string {
+  switch (language) {
+    case ProgrammingLanguage.JavaScript:
+      return `Yeah.. It is pretty good.`
+    case ProgrammingLanguage.TypeScript:
+      return 'Better than javascript.'
+    case ProgrammingLanguage.HTML:
+      return 'Are you sure that is a programming language? - The real changes'
+    case ProgrammingLanguage.Java:
+      return 'Java java!'
+    default:
+      return `Don't know  what you are talking about..`
+  }
+}
+
+
 describe('universe-demo-describeProgrammingLanguage', () => {
-  it('describes javascript', () => {
+  it('describes  javascript', () => {
     const result = describeProgrammingLanguage(ProgrammingLanguage.JavaScript)
     expect(result).toBe('Yeah..  It is pretty good.')
   })
 
-  it('cannot describe java', () => {
+    it('cannot describe java', () => {
     const result = describeProgrammingLanguage(ProgrammingLanguage.Java)
     expect(result).toBe(
       `Don't know what you are talking  about.. Another change`
