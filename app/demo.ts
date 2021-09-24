9203e56f7217e27ba4fafab452d16e5c91e95e92
tidy-dev <tidy-dev@github.com>
2021-09-24 11:59:45 -0400

    Add Method for programming language
    Co-Authored-By: Sergio Padrino <1083228+sergiou87@users.noreply.github.com>

--- app/demo.ts
+++ app/demo.ts
@@ -1,3 +1,26 @@
+enum ProgrammingLanguage {
+  JavaScript,
+  TypeScript,
+  HTML,
+  Java,
+}
+
+function describeProgrammingLanguage(language: ProgrammingLanguage): string {
+  switch (language) {
+    case ProgrammingLanguage.JavaScript:
+      return `Yeah.. It is pretty good.`
+    case ProgrammingLanguage.TypeScript:
+      return 'Better than javascript.'
+    case ProgrammingLanguage.HTML:
+      return 'Are you sure that is a programming language?'
+    case ProgrammingLanguage.Java:
+      return 'Java java!'
+    default:
+      return `Don't know what you are talking about..`
+  }
+}
+
+
 describe('universe-demo-describeProgrammingLanguage', () => {
   it('describes javascript', () => {
     const result = describeProgrammingLanguage(ProgrammingLanguage.JavaScript)
