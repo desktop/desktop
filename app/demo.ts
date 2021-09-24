bdcef1f861f8c4fa56669c45def29aeef2e81112
tidy-dev <tidy-dev@github.com>
2021-09-24 12:04:18 -0400

    Add name method

--- app/demo.ts
+++ app/demo.ts
@@ -34,3 +34,32 @@ describe('universe-demo-describeProgrammingLanguage', () => {
     )
   })
 })
+
+function getProgrammingLanguageName(language: ProgrammingLanguage): string {
+  switch (language) {
+    case ProgrammingLanguage.JavaScript:
+      return `JavaScript`
+    case ProgrammingLanguage.TypeScript:
+      return 'TypeScript'
+    case ProgrammingLanguage.HTML:
+      return 'HyperText Markup Language'
+    case ProgrammingLanguage.CSS:
+      return 'Cascading Style Sheets'
+    case ProgrammingLanguage.Java:
+      return 'Java java!'
+    default:
+      return `Don't know what you are talking about..`
+  }
+}
+
+describe('universe-demo-getProgrammingLanguageName', () => {
+  it('gets javascript name', () => {
+    const result = getProgrammingLanguageName(ProgrammingLanguage.JavaScript)
+    expect(result).toBe('JavaScript')
+  })
+
+  it('gets java name', () => {
+    const result = getProgrammingLanguageName(ProgrammingLanguage.Java)
+    expect(result).toBe(`Java java!`)
+  })
+})
