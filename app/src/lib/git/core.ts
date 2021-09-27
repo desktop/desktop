454e99c82cfa7d46d170de0ee5f43841adea7d67
Daniel Hix <danieljacobhix@gmail.com>
2021-09-26 23:42:10 -0500

    Fix some merge error.

--- app/src/lib/git/core.ts
+++ app/src/lib/git/core.ts
@@ -241,16 +241,17 @@ export async function git(
 
     log.error(errorMessage.join('\n'))
 
-  if (gitError === DugiteError.PushWithFileSizeExceedingLimit) {
-    const result = getFileFromExceedsError(errorMessage.join())
-    const files = result.join('\n')
+    if (gitError === DugiteError.PushWithFileSizeExceedingLimit) {
+      const result = getFileFromExceedsError(errorMessage.join())
+      const files = result.join('\n')
 
-    if (files !== '') {
-      gitResult.gitErrorDescription += '\n\nFile causing error:\n\n' + files
+      if (files !== '') {
+        gitResult.gitErrorDescription += '\n\nFile causing error:\n\n' + files
+      }
     }
-  }
 
-  throw new GitError(gitResult, args)
+    throw new GitError(gitResult, args)
+  })
 }
 
 /**
