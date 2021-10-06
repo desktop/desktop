c2f804b97465b6b63d311514a58fa4aa66825473
Markus Olsson <j.markus.olsson@gmail.com>
2021-10-06 14:51:25 +0200

    Take two on functions-with-properties

--- app/src/ui/lib/observable-ref.ts
+++ app/src/ui/lib/observable-ref.ts
@@ -1,21 +1,25 @@
 type RefCallback<T> = (instance: T | null) => void
-export type ObservableRef<T> = RefCallback<T> & {
+export type ObservableRef<T> = {
   current: T | null
   subscribe: (cb: RefCallback<T>) => void
   unsubscribe: (cb: RefCallback<T>) => void
+  (instance: T): void
 }
 
 export function createObservableRef<T>(): ObservableRef<T> {
   const subscribers = new Set<RefCallback<T>>()
 
-  const ref = {
-    current: null as T | null,
-    subscribe: (cb: RefCallback<T>) => subscribers.add(cb),
-    unsubscribe: (cb: RefCallback<T>) => subscribers.delete(cb),
-  }
+  const callback: ObservableRef<T> = Object.assign(
+    (instance: T | null) => {
+      callback.current = instance
+      subscribers.forEach(cb => cb(instance))
+    },
+    {
+      current: null,
+      subscribe: (cb: RefCallback<T>) => subscribers.add(cb),
+      unsubscribe: (cb: RefCallback<T>) => subscribers.delete(cb),
+    }
+  )
 
-  return Object.assign((instance: T | null) => {
-    ref.current = instance
-    subscribers.forEach(cb => cb(instance))
-  }, ref)
+  return callback
 }
