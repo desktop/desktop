418a4e57bd9e83c9a09f01df4f6d7cf8b56f2890
Markus Olsson <j.markus.olsson@gmail.com>
2021-10-12 19:04:48 +0200

    Custom tooltips for avatars

--- app/src/ui/lib/avatar.tsx
+++ app/src/ui/lib/avatar.tsx
@@ -4,6 +4,7 @@ import { shallowEquals } from '../../lib/equality'
 import { generateGravatarUrl } from '../../lib/gravatar'
 import { Octicon } from '../octicons'
 import { getDotComAPIEndpoint } from '../../lib/api'
+import { TooltippedContent } from './tooltipped-content'
 
 interface IAvatarProps {
   /** The user whose avatar should be displayed. */
@@ -151,7 +152,7 @@ export class Avatar extends React.Component<IAvatarProps, IAvatarState> {
     }
   }
 
-  private getTitle(): string | undefined {
+  private getTitle(): string | JSX.Element | undefined {
     if (this.props.title === null) {
       return undefined
     }
@@ -162,9 +163,15 @@ export class Avatar extends React.Component<IAvatarProps, IAvatarState> {
 
     const user = this.props.user
     if (user) {
-      const name = user.name
-      if (name) {
-        return `${name} <${user.email}>`
+      if (user.name) {
+        return (
+          <>
+            <div>
+              <strong>{user.name}</strong>
+            </div>
+            <div>{user.email}</div>
+          </>
+        )
       } else {
         return user.email
       }
@@ -181,8 +188,9 @@ export class Avatar extends React.Component<IAvatarProps, IAvatarState> {
 
   public render() {
     const title = this.getTitle()
-    const ariaLabel = this.props.user
-      ? `Avatar for ${this.props.user.name || this.props.user.email}`
+    const { user } = this.props
+    const alt = user
+      ? `Avatar for ${user.name || user.email}`
       : `Avatar for unknown user`
 
     if (this.state.candidates.length === 0) {
@@ -198,14 +206,7 @@ export class Avatar extends React.Component<IAvatarProps, IAvatarState> {
     const src = this.state.candidates[0]
 
     const img = (
-      <img
-        className="avatar"
-        title={title}
-        src={src}
-        alt={title}
-        aria-label={ariaLabel}
-        onError={this.onImageError}
-      />
+      <img className="avatar" src={src} alt={alt} onError={this.onImageError} />
     )
 
     if (title === undefined) {
@@ -213,9 +214,14 @@ export class Avatar extends React.Component<IAvatarProps, IAvatarState> {
     }
 
     return (
-      <span title={title} className="avatar-container">
+      <TooltippedContent
+        className="avatar-container"
+        tooltip={title}
+        direction="n"
+        wrapperElement="div"
+      >
         {img}
-      </span>
+      </TooltippedContent>
     )
   }
 
