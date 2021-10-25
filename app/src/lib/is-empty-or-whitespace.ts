a7806fb07eb71fbd52116b9cee4bccef2278371c
Markus Olsson <j.markus.olsson@gmail.com>
2021-10-25 11:20:37 +0200

    Update app/src/lib/is-empty-or-whitespace.ts
    Co-authored-by: Sergio Padrino <sergio.padrino@gmail.com>

--- app/src/lib/is-empty-or-whitespace.ts
+++ app/src/lib/is-empty-or-whitespace.ts
@@ -1,6 +1,6 @@
 /**
- * Indicates whether the given string is null, undefined, empty, or consists
- * only of white-space characters.
+ * Indicates whether the given string is empty or consists only of white-space
+ * characters.
  */
 export const isEmptyOrWhitespace = (s: string) =>
   s.length === 0 || !/\S/.test(s)
