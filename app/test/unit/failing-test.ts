aa043c3b41803e23ddb760c91a38aa431a35c6ee
Sergio Padrino <sergio.padrino@gmail.com>
2021-10-27 13:05:30 +0200

    Another update

--- app/test/unit/failing-test.ts
+++ app/test/unit/failing-test.ts
@@ -1,5 +1,5 @@
 describe('I want this to fail for check run testing', () => {
-  // Can I break things too?
+  // Can I break things too? I don't know.
   it('finds one conflicted file', async () => {
     expect(1).toBe(2)
   })
