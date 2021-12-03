da6b9e0b483363173e4b101761154cd395e73c2d
Markus Olsson <j.markus.olsson@gmail.com>
2021-12-03 15:14:04 +0100

    We can just set these as default instead

--- app/src/lib/clamp.ts
+++ app/src/lib/clamp.ts
@@ -9,18 +9,12 @@ export function clamp(value: number, min: number, max: number): number
 export function clamp(value: IConstrainedValue): number
 export function clamp(
   value: IConstrainedValue | number,
-  min?: number,
-  max?: number
+  min = -Infinity,
+  max = Infinity
 ): number {
   if (typeof value !== 'number') {
     return clamp(value.value, value.min, value.max)
   }
-
-  // This condition could only occur at runtime if someone called clamp(1234).
-  // That's pretty nonsensical so we'll assume that if we're called without
-  // min/max we'll just be an identify function.
-  min = min ?? -Infinity
-  max = max ?? Infinity
 
   if (value < min) {
     return min
