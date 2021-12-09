47ed5e43b1b3d5ce01a6b9275a2a17e2a8725a5d
Becca <tidy-dev@github.com>
2021-12-09 12:48:13 -0500

    Calculate position of quick view card relative to pull request item

--- app/src/ui/pull-request-quick-view.tsx
+++ app/src/ui/pull-request-quick-view.tsx
@@ -65,9 +65,51 @@ export class PullRequestQuickView extends React.Component<
     this.props.onMouseLeave()
   }
 
+  private calculatePosition(
+    prListItemTop: number
+  ): React.CSSProperties | undefined {
+    /** The max height of the visible quick view card is 556 (500 for scrollable
+     * body and 56 for header)
+     */
+    const maxQuickViewHeight = 556
+
+    // Important to retrieve as it changes for maximization on macOS and quick
+    // view is relative to the top of branch container = foldout-container. But
+    // if we can't find it (unlikely) we can atleast compensate for the toolbar
+    // being 50px
+    const topOfPRList =
+      document.getElementById('foldout-container')?.getBoundingClientRect()
+        .top ?? 50
+
+    // This is currently staticly defined so not bothering to attain it from
+    // dom searching.
+    const heightPRListItem = 47
+
+    // Top half of list with room to display aligned to top
+    if (window.innerHeight - prListItemTop > maxQuickViewHeight) {
+      return { top: prListItemTop - topOfPRList }
+    }
+
+    // Bottom half of list with room to display aligned to bottom.
+    if (prListItemTop - maxQuickViewHeight > 0) {
+      return { bottom: window.innerHeight - prListItemTop - heightPRListItem }
+    }
+
+    // If not enough room to display aligned top or bottom, attempt to center on
+    // pr list item (won't be centered but close for prs with a body < max
+    // height)
+    const middlePrListItem = prListItemTop + heightPRListItem / 2
+    const middleQuickView = maxQuickViewHeight / 2
+    return { top: middlePrListItem - middleQuickView }
+  }
+
   public render() {
     return (
-      <div id="pull-request-quick-view" onMouseLeave={this.onMouseLeave}>
+      <div
+        id="pull-request-quick-view"
+        onMouseLeave={this.onMouseLeave}
+        style={this.calculatePosition(this.props.pullRequestItemTop)}
+      >
         <div className="pull-request-quick-view-contents">
           {this.renderHeader()}
           {this.renderPR()}
