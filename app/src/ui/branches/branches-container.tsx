03d4c5bd7037c32ef5b74051b75d65c30e8151c9
Becca <tidy-dev@github.com>
2021-12-09 12:51:20 -0500

    Close pull request view on leave of quick view card

--- app/src/ui/branches/branches-container.tsx
+++ app/src/ui/branches/branches-container.tsx
@@ -121,11 +121,9 @@ export class BranchesContainer extends React.Component<
   }
 
   private onMouseLeavePullRequestQuickView = () => {
-    /*
     this.setState({
       pullRequestBeingViewed: null,
     })
-    */
   }
 
   private renderMergeButtonRow() {
