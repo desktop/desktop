bebd97b60eae8a3405548962ae2d23f1f5171aa9
Becca <tidy-dev@github.com>
2021-12-09 14:11:02 -0500

    Move calculate into didUpdate so we can calculate based on actual quick view height

--- app/src/ui/pull-request-quick-view.tsx
+++ app/src/ui/pull-request-quick-view.tsx
@@ -8,6 +8,12 @@ import { SandboxedMarkdown } from './lib/sandboxed-markdown'
 import { Octicon } from './octicons'
 import * as OcticonSymbol from './octicons/octicons.generated'
 
+/**
+ * The max height of the visible quick view card is 556 (500 for scrollable
+ * body and 56 for header)
+ */
+const maxQuickViewHeight = 556
+
 interface IPullRequestQuickViewProps {
   readonly dispatcher: Dispatcher
   readonly pullRequest: PullRequest
@@ -18,11 +24,87 @@ interface IPullRequestQuickViewProps {
   readonly onMouseLeave: () => void
 }
 
+interface IPullRequestQuickViewState {
+  readonly position: React.CSSProperties | undefined
+}
+
 export class PullRequestQuickView extends React.Component<
   IPullRequestQuickViewProps,
-  {}
+  IPullRequestQuickViewState
 > {
+  private quickViewRef: HTMLDivElement | null = null
   private baseHref = 'https://github.com/'
+
+  public constructor(props: IPullRequestQuickViewProps) {
+    super(props)
+
+    this.state = {
+      position: this.calculatePosition(
+        props.pullRequestItemTop,
+        maxQuickViewHeight
+      ),
+    }
+  }
+
+  public componentDidUpdate = (prevProps: IPullRequestQuickViewProps) => {
+    if (
+      prevProps.pullRequest.body === this.props.pullRequest.body ||
+      this.quickViewRef === null
+    ) {
+      return
+    }
+
+    this.setState({
+      position: this.calculatePosition(
+        this.props.pullRequestItemTop,
+        this.quickViewRef.clientHeight
+      ),
+    })
+  }
+
+  /**
+   * Important to retrieve as it changes for maximization on macOS and quick
+   * view is relative to the top of branch container = foldout-container. But if
+   * we can't find it (unlikely) we can atleast compensate for the toolbar being
+   * 50px
+   */
+  private getTopPRList(): number {
+    return (
+      document.getElementById('foldout-container')?.getBoundingClientRect()
+        .top ?? 50
+    )
+  }
+
+  private calculatePosition(
+    prListItemTop: number,
+    quickViewHeight: number
+  ): React.CSSProperties | undefined {
+    const topOfPRList = this.getTopPRList()
+    // This is currently staticly defined so not bothering to attain it from
+    // dom searching.
+    const heightPRListItem = 47
+
+    // Check if it has room to display aligned to top (likely top half of list)
+    if (window.innerHeight - prListItemTop > quickViewHeight) {
+      return { top: prListItemTop - topOfPRList }
+    }
+
+    // Can't align to top -> likely bottom half of list check if has room to display aligned to bottom.
+    if (prListItemTop - quickViewHeight > 0) {
+      return { bottom: window.innerHeight - prListItemTop - heightPRListItem }
+    }
+
+    // If not enough room to display aligned top or bottom, attempt to center
+    // with a minimum top/bottom pr list item
+    const middlePrListItem = prListItemTop + heightPRListItem / 2
+    const middleQuickView = quickViewHeight / 2
+    const maxTop = window.innerHeight - topOfPRList - quickViewHeight
+    return { top: clamp(middlePrListItem - middleQuickView, 0, maxTop) }
+  }
+
+  private onQuickViewRef = (quickViewRef: HTMLDivElement) => {
+    this.quickViewRef = quickViewRef
+  }
 
   private renderHeader = (): JSX.Element => {
     return (
@@ -66,51 +148,13 @@ export class PullRequestQuickView extends React.Component<
     this.props.onMouseLeave()
   }
 
-  private calculatePosition(
-    prListItemTop: number
-  ): React.CSSProperties | undefined {
-    /** The max height of the visible quick view card is 556 (500 for scrollable
-     * body and 56 for header)
-     */
-    const maxQuickViewHeight = 556
-
-    // Important to retrieve as it changes for maximization on macOS and quick
-    // view is relative to the top of branch container = foldout-container. But
-    // if we can't find it (unlikely) we can atleast compensate for the toolbar
-    // being 50px
-    const topOfPRList =
-      document.getElementById('foldout-container')?.getBoundingClientRect()
-        .top ?? 50
-
-    // This is currently staticly defined so not bothering to attain it from
-    // dom searching.
-    const heightPRListItem = 47
-
-    // Top half of list with room to display aligned to top
-    if (window.innerHeight - prListItemTop > maxQuickViewHeight) {
-      return { top: prListItemTop - topOfPRList }
-    }
-
-    // Bottom half of list with room to display aligned to bottom.
-    if (prListItemTop - maxQuickViewHeight > 0) {
-      return { bottom: window.innerHeight - prListItemTop - heightPRListItem }
-    }
-
-    // If not enough room to display aligned top or bottom, attempt to center
-    // with a minimum top/bottom pr list item (won't be centered but close for
-    // prs with a body < max height)
-    const middlePrListItem = prListItemTop + heightPRListItem / 2
-    const middleQuickView = maxQuickViewHeight / 2
-    const maxTop = window.innerHeight - topOfPRList - maxQuickViewHeight
-    return { top: clamp(middlePrListItem - middleQuickView, 0, maxTop) }
-  }
-
   public render() {
     return (
       <div
         id="pull-request-quick-view"
         onMouseLeave={this.onMouseLeave}
-        style={this.calculatePosition(this.props.pullRequestItemTop)}
+        style={this.state.position}
+        ref={this.onQuickViewRef}
       >
         <div className="pull-request-quick-view-contents">
           {this.renderHeader()}
