17ade5009ac646a0588462149c2b3a5e88224c3b
Becca <tidy-dev@github.com>
2021-12-13 12:38:10 -0500

    Add timer for opening pull request quick view

--- app/src/ui/pull-request-quick-view.tsx
+++ app/src/ui/pull-request-quick-view.tsx
@@ -12,76 +12,72 @@ interface IPullRequestQuickViewProps {
   readonly pullRequest: PullRequest
 
   /** When mouse leaves the PR quick view */
+  readonly onMouseEnter: () => void
+
+  /** When mouse leaves the PR quick view */
   readonly onMouseLeave: () => void
 }
 
-export const PullRequestQuickView = React.forwardRef<
-  HTMLDivElement,
-  IPullRequestQuickViewProps
->((props, ref) => {
-  class PullRequestQuickView extends React.Component<
-    IPullRequestQuickViewProps,
-    {}
-  > {
-    private baseHref = 'https://github.com/'
+export class PullRequestQuickView extends React.Component<
+  IPullRequestQuickViewProps,
+  {}
+> {
+  private baseHref = 'https://github.com/'
 
-    private renderHeader = () => {
-      return (
-        <header className="header">
-          <Octicon symbol={OcticonSymbol.listUnordered} />
-          <div className="action-needed">Review requested</div>
-          <Button className="button-with-icon">
-            View on GitHub
-            <Octicon symbol={OcticonSymbol.linkExternal} />
-          </Button>
-        </header>
-      )
-    }
-
-    private renderPR = () => {
-      const { title, pullRequestNumber, base, body } = this.props.pullRequest
-      const displayBody =
-        body !== undefined && body !== null && body.trim() !== ''
-          ? body
-          : '_No description provided._'
-      return (
-        <div className="pull-request">
-          <div className="status">
-            <Octicon className="icon" symbol={OcticonSymbol.gitPullRequest} />
-            <span className="state">Open</span>
-          </div>
-          <div className="title">
-            <h2>{title}</h2>
-            <PullRequestBadge
-              number={pullRequestNumber}
-              dispatcher={this.props.dispatcher}
-              repository={base.gitHubRepository}
-            />
-          </div>
-          <SandboxedMarkdown markdown={displayBody} baseHref={this.baseHref} />
-        </div>
-      )
-    }
-
-    private onMouseLeave = () => {
-      this.props.onMouseLeave()
-    }
-
-    public render() {
-      return (
-        <div
-          id="pull-request-quick-view"
-          ref={ref}
-          onMouseLeave={this.onMouseLeave}
-        >
-          <div className="pull-request-quick-view-contents">
-            {this.renderHeader()}
-            {this.renderPR()}
-          </div>
-        </div>
-      )
-    }
+  private renderHeader = () => {
+    return (
+      <header className="header">
+        <Octicon symbol={OcticonSymbol.listUnordered} />
+        <div className="action-needed">Review requested</div>
+        <Button className="button-with-icon">
+          View on GitHub
+          <Octicon symbol={OcticonSymbol.linkExternal} />
+        </Button>
+      </header>
+    )
   }
 
-  return <PullRequestQuickView {...props} />
-})
+  private renderPR = () => {
+    const { title, pullRequestNumber, base, body } = this.props.pullRequest
+    const displayBody =
+      body !== undefined && body !== null && body.trim() !== ''
+        ? body
+        : '_No description provided._'
+    return (
+      <div className="pull-request">
+        <div className="status">
+          <Octicon className="icon" symbol={OcticonSymbol.gitPullRequest} />
+          <span className="state">Open</span>
+        </div>
+        <div className="title">
+          <h2>{title}</h2>
+          <PullRequestBadge
+            number={pullRequestNumber}
+            dispatcher={this.props.dispatcher}
+            repository={base.gitHubRepository}
+          />
+        </div>
+        <SandboxedMarkdown markdown={displayBody} baseHref={this.baseHref} />
+      </div>
+    )
+  }
+
+  private onMouseLeave = () => {
+    this.props.onMouseLeave()
+  }
+
+  public render() {
+    return (
+      <div
+        id="pull-request-quick-view"
+        onMouseEnter={this.props.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+      >
+        <div className="pull-request-quick-view-contents">
+          {this.renderHeader()}
+          {this.renderPR()}
+        </div>
+      </div>
+    )
+  }
+}
