a721b1033955fc48eb1996742e77182b855ba6b1
Sergio Padrino <sergio.padrino@gmail.com>
2022-01-11 12:42:22 +0100

    Make tests pass
    Co-Authored-By: Markus Olsson <634063+niik@users.noreply.github.com>

--- app/jest.unit.config.js
+++ app/jest.unit.config.js
@@ -2,6 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src/', '<rootDir>/test/'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
+    '\\.m?jsx?$': 'jest-esm-transformer',
   },
   testMatch: ['**/unit/**/*-test.ts{,x}'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
@@ -21,9 +22,5 @@ module.exports = {
   ],
   reporters: ['default', '<rootDir>../script/jest-actions-reporter.js'],
   coverageReporters: ['text-summary', 'json', 'html', 'cobertura'],
-  globals: {
-    'ts-jest': {
-      useBabelrc: true,
-    },
-  },
+  transformIgnorePatterns: ['node_modules/temp'],
 }
