f397e446e7f57bc0dbf7675b2d00fc1011a241df
Becca <tidy-dev@github.com>
2022-01-20 12:35:25 -0500

    Add another jest mock

--- app/test/__mocks__/electron.ts
+++ app/test/__mocks__/electron.ts
@@ -5,6 +5,9 @@ export const shell = {
 export const remote = {
   app: {
     on: jest.fn(),
+    client: {
+      waitUntil: jest.fn(),
+    },
   },
   getCurrentWebContents: jest.fn().mockImplementation(() => ({
     on: jest.fn().mockImplementation(() => true),
