9624c70c94c503ff8ea91996558364e66be94da0
Sergio Padrino <sergio.padrino@gmail.com>
2022-01-31 17:47:32 +0100

    Fix Windows notifications with desktop-notifications

--- app/src/lib/stores/helpers/show-notification.ts
+++ app/src/lib/stores/helpers/show-notification.ts
@@ -1,4 +1,29 @@
 import { focusWindow } from '../../../ui/main-process-proxy'
+import {
+  DesktopNotification,
+  initializeNotifications,
+} from 'desktop-notifications'
+import { findToastActivatorClsid } from '../../find-toast-activator-clsid'
+
+let windowsToastActivatorClsid: string | undefined = undefined
+
+function initializeWindowsNotifications() {
+  if (windowsToastActivatorClsid !== undefined) {
+    return
+  }
+
+  windowsToastActivatorClsid = findToastActivatorClsid()
+
+  if (windowsToastActivatorClsid === undefined) {
+    console.error(
+      'Toast activator CLSID not found in any of the shortucts. Notifications will not work.'
+    )
+    return
+  }
+
+  console.log(`Found toast activator CLSID ${windowsToastActivatorClsid}`)
+  initializeNotifications(windowsToastActivatorClsid)
+}
 
 /**
  * Shows a notification with a title, a body, and a function to handle when the
@@ -9,6 +34,18 @@ export function showNotification(
   body: string,
   onClick: () => void
 ) {
+  if (__WIN32__) {
+    initializeWindowsNotifications()
+
+    const notification = new DesktopNotification(title, body)
+    notification.onclick = () => {
+      focusWindow()
+      onClick()
+    }
+    notification.show()
+    return
+  }
+
   const notification = new Notification(title, {
     body,
   })
