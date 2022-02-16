a76a38b9c9a75f773806c73d823892ef1cf6f38b
Sergio Padrino <sergio.padrino@gmail.com>
2022-02-16 14:02:47 +0100

    Use our logger, not console :facepalm:

--- app/src/lib/trampoline/trampoline-server.ts
+++ app/src/lib/trampoline/trampoline-server.ts
@@ -185,7 +185,7 @@ export class TrampolineServer {
   }
 
   private onClientError = (error: Error) => {
-    console.error('Trampoline client error', error)
+    log.error('Trampoline client error', error)
   }
 }
 
