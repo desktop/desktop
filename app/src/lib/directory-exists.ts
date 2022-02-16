fd5bf249a63c96f21b9d007326367f4ccad4854b
Markus Olsson <j.markus.olsson@gmail.com>
2022-02-16 16:56:03 +0100

    Async await is pretty cool too
    Co-Authored-By: Sergio Padrino <1083228+sergiou87@users.noreply.github.com>

--- app/src/lib/directory-exists.ts
+++ app/src/lib/directory-exists.ts
@@ -4,7 +4,11 @@ import { stat } from 'fs/promises'
  * Helper method to stat a path and check both that it exists and that it's
  * a directory.
  */
-export const directoryExists = (path: string) =>
-  stat(path)
-    .catch(null)
-    .then(s => s?.isDirectory() ?? false)
+export const directoryExists = async (path: string) => {
+  try {
+    const s = await stat(path)
+    return s.isDirectory()
+  } catch (e) {
+    return false
+  }
+}
