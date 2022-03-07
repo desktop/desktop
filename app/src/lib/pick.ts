58fae10c4c4eddad8b4c659ba422d5f3310c51e6
Markus Olsson <j.markus.olsson@gmail.com>
2022-03-07 12:10:39 +0100

    Eh.. maybe we should just keep it simple
    Pick<T, K> doesn't (can't) differentiate between prototypically inherited properties and regular once so it feels weird if the type system lets you do something that the runtime then ignores

--- app/src/lib/pick.ts
+++ app/src/lib/pick.ts
@@ -2,8 +2,5 @@
  * Returns a shallow copy of the given object containing only the given subset
  * of keys. This is to be thought of as a runtime equivalent of Pick<T, K>
  */
-export function pick<T extends object, K extends keyof T>(o: T, ...keys: K[]) {
-  const isOwn = (k: K) => Object.prototype.hasOwnProperty.call(o, k)
-  const toEntry = (k: K) => [k, o[k]]
-  return Object.fromEntries(keys.filter(isOwn).map(toEntry)) as Pick<T, K>
-}
+export const pick = <T extends object, K extends keyof T>(o: T, ...keys: K[]) =>
+  Object.fromEntries(keys.map(k => [k, o[k]])) as Pick<T, K>
