8978cfa9498679d0d9eddaed41688a547ce08be0
Markus Olsson <j.markus.olsson@gmail.com>
2022-03-07 11:08:04 +0100

    :book:

--- app/src/lib/pick.ts
+++ app/src/lib/pick.ts
@@ -1,3 +1,7 @@
+/**
+ * Returns a shallow copy of the given object containing only the given subset
+ * of keys. This is to be thought of as a runtime equivalent of Pick<T, K>
+ */
 export function pick<T extends object, K extends keyof T>(o: T, ...keys: K[]) {
   const isOwn = (k: K) => Object.prototype.hasOwnProperty.call(o, k)
   const toEntry = (k: K) => [k, o[k]]
