43282ce2f5c4abae17b4c60725749064a20c3608
Markus Olsson <j.markus.olsson@gmail.com>
2022-03-07 11:07:57 +0100

    :art: cleanup

--- app/src/lib/pick.ts
+++ app/src/lib/pick.ts
@@ -1,5 +1,5 @@
 export function pick<T extends object, K extends keyof T>(o: T, ...keys: K[]) {
-  const own = (k: K) => Object.prototype.hasOwnProperty.call(o, k)
-  const entry = (k: K) => [k, o[k]]
-  return Object.fromEntries(keys.filter(own).map(entry)) as Pick<T, K>
+  const isOwn = (k: K) => Object.prototype.hasOwnProperty.call(o, k)
+  const toEntry = (k: K) => [k, o[k]]
+  return Object.fromEntries(keys.filter(isOwn).map(toEntry)) as Pick<T, K>
 }
