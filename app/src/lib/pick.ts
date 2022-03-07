aba6450dae2de58508b4ac35ff80d2d30003dfac
Markus Olsson <j.markus.olsson@gmail.com>
2022-03-07 12:14:53 +0100

    Take three
    Still need to check if it's defined

--- app/src/lib/pick.ts
+++ app/src/lib/pick.ts
@@ -2,5 +2,8 @@
  * Returns a shallow copy of the given object containing only the given subset
  * of keys. This is to be thought of as a runtime equivalent of Pick<T, K>
  */
-export const pick = <T extends object, K extends keyof T>(o: T, ...keys: K[]) =>
-  Object.fromEntries(keys.map(k => [k, o[k]])) as Pick<T, K>
+export function pick<T extends object, K extends keyof T>(o: T, ...keys: K[]) {
+  const hasProperty = (k: K) => k in o
+  const toEntry = (k: K) => [k, o[k]]
+  return Object.fromEntries(keys.filter(hasProperty).map(toEntry)) as Pick<T, K>
+}
