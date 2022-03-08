09781c31794c6481ddaebe1a8d58bcbf2f3ccf2b
Markus Olsson <j.markus.olsson@gmail.com>
2022-03-08 15:08:41 +0100

    Add an escape hatch

--- app/src/lib/format-date.ts
+++ app/src/lib/format-date.ts
@@ -1,8 +1,12 @@
 import mem from 'mem'
+import QuickLRU from 'quick-lru'
 
 // Initializing a date formatter is expensive but formatting is relatively cheap
-// so we cache them based on the locale and their options.
+// so we cache them based on the locale and their options. The maxSize of a 100
+// is only as an escape hatch, we don't expect to ever create more than a
+// handful different formatters.
 const getDateFormatter = mem(Intl.DateTimeFormat, {
+  cache: new QuickLRU({ maxSize: 100 }),
   cacheKey: (...args) => JSON.stringify(args),
 })
 
