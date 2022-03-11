dd13b3567ba0aa921096b0a545605d011922fcb0
Markus Olsson <j.markus.olsson@gmail.com>
2022-03-11 12:32:09 +0100

    :art: cleanup

--- app/src/lib/format-relative.ts
+++ app/src/lib/format-relative.ts
@@ -17,9 +17,10 @@ const getRelativeFormatter = mem(
 export function formatRelative(ms: number) {
   const formatter = getRelativeFormatter('en-US', { numeric: 'auto' })
 
-  // https://github.com/github/time-elements/blob/428b02c9/src/relative-time.ts#L57
-  const sign = ms < 0 ? -1 : 1
+  const sign = ms < 0 ? 1 : -1
 
+  // Lifted and adopted from
+  // https://github.com/github/time-elements/blob/428b02c9/src/relative-time.ts#L57
   const sec = Math.round(Math.abs(ms) / 1000)
   const min = Math.round(sec / 60)
   const hr = Math.round(min / 60)
@@ -29,16 +30,10 @@ export function formatRelative(ms: number) {
 
   if (sec < 45) {
     return formatter.format(sec * sign, 'second')
-  } else if (sec < 90) {
-    return formatter.format(min * sign, 'minute')
   } else if (min < 45) {
     return formatter.format(min * sign, 'minute')
-  } else if (min < 90) {
-    return formatter.format(hr * sign, 'hour')
   } else if (hr < 24) {
     return formatter.format(hr * sign, 'hour')
-  } else if (hr < 36) {
-    return formatter.format(day * sign, 'day')
   } else if (day < 30) {
     return formatter.format(day * sign, 'day')
   } else if (month < 18) {
