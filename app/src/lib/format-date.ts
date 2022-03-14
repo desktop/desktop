b1500d5e3cfb5b7bfee5eb3a71e0e390c1db58d4
Markus Olsson <j.markus.olsson@gmail.com>
2022-03-14 22:44:37 +0100

    Use user locale when formatting absolute dates

--- app/src/lib/format-date.ts
+++ app/src/lib/format-date.ts
@@ -9,20 +9,31 @@ let dateLocale: string | undefined = undefined
 // so we cache them based on the locale and their options. The maxSize of a 100
 // is only as an escape hatch, we don't expect to ever create more than a
 // handful different formatters.
-const getDateFormatter = mem(Intl.DateTimeFormat, {
-  cache: new QuickLRU({ maxSize: 100 }),
-  cacheKey: (...args) => JSON.stringify(args),
-})
+const getDateFormatter = mem(
+  (locale: string | string[], options: Intl.DateTimeFormatOptions) => {
+    try {
+      return Intl.DateTimeFormat(locale, options)
+    } catch (e) {
+      return Intl.DateTimeFormat(undefined, options)
+    }
+  },
+  {
+    cache: new QuickLRU({ maxSize: 100 }),
+    cacheKey: (...args) => JSON.stringify(args),
+  }
+)
 
 /**
  * Format a date in en-US locale, customizable with Intl.DateTimeFormatOptions.
  *
  * See Intl.DateTimeFormat for more information
  */
-export const formatDate = (date: Date, options: Intl.DateTimeFormatOptions) =>
-  isNaN(date.valueOf())
+export const formatDate = (date: Date, options: Intl.DateTimeFormatOptions) => {
+  const locale = dateLocale ? [dateLocale, defaultLocale] : defaultLocale
+  return isNaN(date.valueOf())
     ? 'Invalid date'
-    : getDateFormatter('en-US', options).format(date)
+    : getDateFormatter(locale, options).format(date)
+}
 
 export const setDateLocale = (locale: string | undefined) => {
   if (locale === undefined) {
