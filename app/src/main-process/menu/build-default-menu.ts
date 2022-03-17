f1420b5ef615a7798275d5980359824bc0c70ff0
Sergio Padrino <sergio.padrino@gmail.com>
2022-03-17 12:50:46 +0100

    Bump version and changelog to 2.9.12-test2

--- app/src/main-process/menu/build-default-menu.ts
+++ app/src/main-process/menu/build-default-menu.ts
@@ -514,34 +514,34 @@ export function buildDefaultMenu({
     showLogsItem,
   ]
 
-  if (__DEV__) {
-    helpItems.push(
-      separator,
-      {
-        label: 'Crash main process…',
-        click() {
-          throw new Error('Boomtown!')
+  //if (__DEV__) {
+  helpItems.push(
+    separator,
+    {
+      label: 'Crash main process…',
+      click() {
+        throw new Error('Boomtown!')
+      },
+    },
+    {
+      label: 'Crash renderer process…',
+      click: emit('boomtown'),
+    },
+    {
+      label: 'Show popup',
+      submenu: [
+        {
+          label: 'Release notes',
+          click: emit('show-release-notes-popup'),
         },
-      },
-      {
-        label: 'Crash renderer process…',
-        click: emit('boomtown'),
-      },
-      {
-        label: 'Show popup',
-        submenu: [
-          {
-            label: 'Release notes',
-            click: emit('show-release-notes-popup'),
-          },
-        ],
-      },
-      {
-        label: 'Prune branches',
-        click: emit('test-prune-branches'),
-      }
-    )
-  }
+      ],
+    },
+    {
+      label: 'Prune branches',
+      click: emit('test-prune-branches'),
+    }
+  )
+  //}
 
   if (__RELEASE_CHANNEL__ === 'development' || __RELEASE_CHANNEL__ === 'test') {
     helpItems.push({
