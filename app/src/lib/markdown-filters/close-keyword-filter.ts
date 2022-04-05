1318cefd5374b018ec4e4515c899576e4d479674
tidy-dev <tidy-dev@github.com>
2022-04-05 09:56:43 -0400

    Filter tree walker (no text inside pre, code, a tags)

--- app/src/lib/markdown-filters/close-keyword-filter.ts
+++ app/src/lib/markdown-filters/close-keyword-filter.ts
@@ -1,8 +1,19 @@
 import { INodeFilter } from './node-filter'
 
 export class CloseKeywordFilter implements INodeFilter {
+  /**
+   *  Close keyword filter iterates on all text nodes that are not inside a pre,
+   *  code, or anchor tag.
+   */
   public createFilterTreeWalker(doc: Document): TreeWalker {
-    throw new Error('Method not implemented.')
+    return doc.createTreeWalker(doc, NodeFilter.SHOW_TEXT, {
+      acceptNode: node => {
+        return node.parentNode !== null &&
+          ['CODE', 'PRE', 'A'].includes(node.parentNode.nodeName)
+          ? NodeFilter.FILTER_SKIP
+          : NodeFilter.FILTER_ACCEPT
+      },
+    })
   }
 
   public filter(node: Node): Promise<readonly Node[] | null> {
