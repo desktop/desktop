79b19366d6463495f834ee71ef81d674a32b964f
tidy-dev <tidy-dev@github.com>
2022-04-05 13:23:54 -0400

    Replace close mentions with tooltipped span

--- app/src/lib/markdown-filters/close-keyword-filter.ts
+++ app/src/lib/markdown-filters/close-keyword-filter.ts
@@ -21,16 +21,11 @@ export class CloseKeywordFilter implements INodeFilter {
    * owner/fixops#1
    */
   private closeText =
-    /\b(?<close_text>close[sd]?|fix(e[sd])?|resolve[sd]?)(?<close_spacer>\s*:?\s+)/i
+    /\b(?<closeText>close[sd]?|fix(e[sd])?|resolve[sd]?)(\s*:?\s+)/i
 
   private closesWithTextReference = new RegExp(
     this.closeText.source + '(?<issue_reference>' + IssueReference.source + ')',
-    'i'
-  )
-
-  private closesAtEndOfText = new RegExp(
-    this.closeText.source + /$/.source,
-    'i'
+    'ig'
   )
 
   public constructor(
@@ -60,9 +55,43 @@ export class CloseKeywordFilter implements INodeFilter {
       return null
     }
 
-    console.log(this.closesWithTextReference)
-    console.log(this.closesAtEndOfText)
+    const matches = [...text.matchAll(this.closesWithTextReference)]
+    if (matches.length === 0) {
+      return null
+    }
 
-    return null
+    let lastMatchEndingPosition = 0
+    const nodes: Array<Text | HTMLSpanElement> = []
+    for (const match of matches) {
+      if (match.groups === undefined || match.index === undefined) {
+        continue
+      }
+      const { closeText, issue_reference } = match.groups
+
+      const span = this.createTooltipContent(closeText, issue_reference)
+
+      const textBefore = text.slice(lastMatchEndingPosition, match.index)
+      nodes.push(document.createTextNode(textBefore))
+      nodes.push(span)
+
+      lastMatchEndingPosition = match.index + closeText.length
+    }
+
+    const trailingText = text.slice(lastMatchEndingPosition)
+    if (trailingText !== '') {
+      nodes.push(document.createTextNode(trailingText))
+    }
+
+    return nodes
+  }
+
+  private createTooltipContent(closesText: string, issueNumber: string) {
+    const tooltipSpan = document.createElement('span')
+    tooltipSpan.textContent = closesText
+    tooltipSpan.classList.add('issue-keyword')
+    tooltipSpan.title = `This ${
+      this.markdownContext === 'Commit' ? 'commit' : 'pull request'
+    } closes issue ${issueNumber}.`
+    return tooltipSpan
   }
 }
