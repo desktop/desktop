4686d0006ab535ea3d018c17f223f74d4fe3092b
Markus Olsson <j.markus.olsson@gmail.com>
2022-05-04 12:13:38 +0200

    Get rid of unused coverage reporting

--- app/jest.unit.config.js
+++ app/jest.unit.config.js
@@ -8,20 +8,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFiles: ['<rootDir>/test/globals.ts', '<rootDir>/test/unit-test-env.ts'],
   setupFilesAfterEnv: ['<rootDir>/test/setup-test-framework.ts'],
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!**/node_modules/**',
-    '!**/vendor/**',
-    '!**/*.d.*',
-    // not focused on testing these areas currently
-    '!src/cli/**/*',
-    '!src/crash/**/*',
-    '!src/highlighter/**/*',
-    // ignore index files
-    '!**/index.ts',
-  ],
   reporters: ['default', '<rootDir>../script/jest-actions-reporter.js'],
-  coverageReporters: ['text-summary', 'json', 'html', 'cobertura'],
   // For now, @github Node modules required to be transformed by jest-esm-transformer
   transformIgnorePatterns: ['node_modules/(?!(@github))'],
 }
