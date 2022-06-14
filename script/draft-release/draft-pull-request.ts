7726a94fbd41fc3f6f853d077565889dccde6f84
Sergio Padrino <sergio.padrino@gmail.com>
2022-06-14 18:37:34 +0200

    Reuse logic from bash script in yarn draft-release:pr

--- script/draft-release/draft-pull-request.ts
+++ script/draft-release/draft-pull-request.ts
@@ -2,44 +2,24 @@
 
 import appPackage from '../../app/package.json'
 import { createPR } from '../pr-api'
+import { sh } from '../sh'
 
-const numberToOrdinal = (n: number) => {
-  const s = ['th', 'st', 'nd', 'rd']
-  const v = n % 100
-  return n + (s[(v - 20) % 10] || s[v] || s[0])
+async function getPullRequestContent(
+  contentType: 'title' | 'body',
+  fullVersion: string
+): Promise<string> {
+  const result = await sh(
+    './script/draft-release/release-pr-content.sh',
+    contentType,
+    fullVersion
+  )
+  return result.trim()
 }
 
-function getPullRequestBody(fullVersion: string): string {
-  const versionComponents = fullVersion.split('-')
-  const version = versionComponents[0]
-
-  let releaseDescription = `v${version} production release`
-  if (versionComponents.length > 1) {
-    const channelVersion = versionComponents[1]
-    if (!channelVersion.startsWith('beta')) {
-      throw new Error('We should not create release PRs for test builds')
-    }
-
-    const buildNumber = parseInt(channelVersion.substring('beta'.length))
-    releaseDescription = `${numberToOrdinal(
-      buildNumber
-    )} beta of the v${version} series`
-  }
-
-  return `## Description
-Looking for the PR for the upcoming ${releaseDescription}? Well, you've just found it, congratulations!
-
-## Release checklist
-
-- [ ] Check to see if there are any errors in Sentry that have only occurred since the last production release
-- [ ] Verify that all feature flags are flipped appropriately
-- [ ] If there are any new metrics, ensure that central and desktop.github.com have been updated
-`
-}
-
-function getPullRequestTitle(fullVersion: string): string {
-  return `Release ${fullVersion}`
-}
+const getPullRequestBody = (fullVersion: string) =>
+  getPullRequestContent('body', fullVersion)
+const getPullRequestTitle = (fullVersion: string) =>
+  getPullRequestContent('title', fullVersion)
 
 process.on('unhandledRejection', error => {
   console.error(error.message)
@@ -47,9 +27,14 @@ process.on('unhandledRejection', error => {
 
 async function run() {
   const version = appPackage.version
+  if (version.includes('test')) {
+    console.error('Cannot create a PR for a test version')
+    process.exit(1)
+  }
+
   console.log(`Creating release Pull Request for ${version}...`)
-  const title = getPullRequestTitle(version)
-  const body = getPullRequestBody(version)
+  const title = await getPullRequestTitle(version)
+  const body = await getPullRequestBody(version)
   const response = await createPR(title, body, `releases/${version}`)
   if (response === null) {
     console.error('Failed to create release Pull Request')
