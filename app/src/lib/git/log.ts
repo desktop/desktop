c5a471090a2f6b46ed2260ed492649721eeede73
Markus Olsson <j.markus.olsson@gmail.com>
2022-07-06 22:01:26 +0200

    Of course @tidy-dev had already written this
    Combining our efforts into one, picked my implementation just because it happened to be a little more compact but happy to revert to @tidy-dev's just as great implementation.

--- app/src/lib/git/log.ts
+++ app/src/lib/git/log.ts
@@ -186,6 +186,38 @@ export async function getChangedFiles(
   ]
 
   const { stdout } = await git(args, repository.path, 'getChangesFiles')
+  return parseRawLogWithNumstat(stdout, sha)
+}
+
+/**
+ * Parses output of diff flags -z --raw --numstat.
+ *
+ * Given the -z flag the new lines are separated by \0 character (left them as
+ * new lines below for ease of reading)
+ *
+ * For modified, added, deleted, untracked:
+ *    100644 100644 5716ca5 db3c77d M
+ *    file_one_path
+ *    :100644 100644 0835e4f 28096ea M
+ *    file_two_path
+ *    1    0       file_one_path
+ *    1    0       file_two_path
+ *
+ * For copied or renamed:
+ *    100644 100644 5716ca5 db3c77d M
+ *    file_one_original_path
+ *    file_one_new_path
+ *    :100644 100644 0835e4f 28096ea M
+ *    file_two_original_path
+ *    file_two_new_path
+ *    1    0
+ *    file_one_original_path
+ *    file_one_new_path
+ *    1    0
+ *    file_two_original_path
+ *    file_two_new_path
+ */
+export function parseRawLogWithNumstat(stdout: string, sha: string) {
   const files = new Array<CommittedFileChange>()
   let linesAdded = 0
   let linesDeleted = 0
@@ -210,6 +242,9 @@ export async function getChangedFiles(
       linesAdded += added === '-' ? 0 : parseInt(added, 10)
       linesDeleted += deleted === '-' ? 0 : parseInt(deleted, 10)
 
+      // If this entry denotes a rename or copy the old and new paths are on
+      // two separate fields (separated by \0). Otherwise they're on the same
+      // line as the added and deleted lines.
       if (isCopyOrRename(files[numStatCount].status)) {
         i += 2
       }
