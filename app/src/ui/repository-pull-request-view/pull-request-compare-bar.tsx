fca26184e1151f1881bb924542230d0236d75545
tidy-dev <tidy-dev@github.com>
2022-07-27 16:09:51 -0400

    Create pull-request-compare-bar

---
+++ app/src/ui/repository-pull-request-view/pull-request-compare-bar.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export class PullRequestCompareBar extends React.Component<{}> {
+  public render() {
+    return (
+      <div className="pull-request-compare-bar">pull-request compare bar</div>
+    )
+  }
+}
