1486a3665095b9d64f86d68f262e00c7226c32ac
tidy-dev <tidy-dev@github.com>
2022-07-27 16:09:51 -0400

    Create pull-request-compare-bar

--- app/src/ui/repository-pull-request-view/repository-pull-request-view.tsx
+++ app/src/ui/repository-pull-request-view/repository-pull-request-view.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react'
+import { UiView } from '../ui-view'
+import { PullRequestCompareBar } from './pull-request-compare-bar'
 
 export class RepositoryPullRequestView extends React.Component<{}> {
   public render() {
-    return 'Pull request view!'
+    return (
+      <UiView id="repository-pull-request-view">
+        <PullRequestCompareBar />
+      </UiView>
+    )
   }
 }
