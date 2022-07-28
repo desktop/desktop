3338e8ccb886949fb04e4e6e754a16f48b5a4e39
tidy-dev <tidy-dev@github.com>
2022-07-28 08:31:16 -0400

    Add input components and style it

--- app/src/ui/repository-pull-request-view/pull-request-compare-bar.tsx
+++ app/src/ui/repository-pull-request-view/pull-request-compare-bar.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react'
+import * as OcticonSymbol from '../octicons/octicons.generated'
+import { FancyTextBox } from '../lib/fancy-text-box'
+import { Select } from '../lib/select'
+import { Octicon } from '../octicons'
 
 export class PullRequestCompareBar extends React.Component<{}> {
   public render() {
     return (
-      <div className="pull-request-compare-bar">pull-request compare bar</div>
+      <div className="pull-request-compare-bar">
+        <Octicon symbol={OcticonSymbol.gitCompare} />
+
+        <div className="branch-box">
+          <Select value={'development'}>
+            <option key={'development'} value={'development'}>
+              {'development'}
+            </option>
+          </Select>
+        </div>
+
+        <Octicon symbol={OcticonSymbol.arrowLeft} />
+
+        <div className="branch-box">
+          <FancyTextBox
+            symbol={OcticonSymbol.gitBranch}
+            type="search"
+            value={'feature-branch'}
+            disabled={true}
+          />
+        </div>
+      </div>
     )
   }
 }
