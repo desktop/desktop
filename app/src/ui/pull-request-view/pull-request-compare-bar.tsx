9b3576b5038eb2fc4a9a93102583437c39a73f65
tidy-dev <tidy-dev@github.com>
2022-08-03 13:33:09 -0400

    Refactor compare bar input arrangements

--- app/src/ui/pull-request-view/pull-request-compare-bar.tsx
+++ app/src/ui/pull-request-view/pull-request-compare-bar.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react'
-import * as OcticonSymbol from '../octicons/octicons.generated'
-import { FancyTextBox } from '../lib/fancy-text-box'
 import { Select } from '../lib/select'
-import { Octicon } from '../octicons'
 import { Branch } from '../../models/branch'
 import { Button } from '../lib/button'
+import { Ref } from '../lib/ref'
 
 interface IPullRequestCompareBarProps {
   readonly branches: ReadonlyArray<Branch>
@@ -19,41 +17,29 @@ export class PullRequestCompareBar extends React.Component<IPullRequestCompareBa
   public render() {
     return (
       <div className="pull-request-compare-bar">
-        <div className="title">Comparing Changes</div>
-        <div className="subtitle">
-          Choose two branches to see what’s changed or to start a new pull
-          request.
-        </div>
-        <div className="controls">
-          <Octicon symbol={OcticonSymbol.gitCompare} />
-
-          <div className="branch-box">
-            <Select defaultValue={this.props.mergeBaseBranch.name}>
-              {this.props.branches.map(branch => (
-                <option key={branch.name} value={branch.name}>
-                  {branch.name}
-                </option>
-              ))}
-            </Select>
-          </div>
-
-          <Octicon symbol={OcticonSymbol.arrowLeft} />
-
-          <div className="branch-box">
-            <FancyTextBox
-              type="search"
-              value={this.props.currentBranch.name}
-              disabled={true}
-            />
-          </div>
-
+        <div className="header">
+          <div className="title">Comparing Changes</div>
           <div className="middle-box"></div>
-
           <Button type="submit" onClick={this.props.onCreatePullRequest}>
             Create Pull Request
           </Button>
-
           <Button onClick={this.props.onCancelPullRequest}>Cancel</Button>
+        </div>
+        <div className="controls">
+          <div>
+            Showing the changes made by merging commits into
+            <div className="branch-box">
+              <Select defaultValue={this.props.mergeBaseBranch.name}>
+                {this.props.branches.map(branch => (
+                  <option key={branch.name} value={branch.name}>
+                    {branch.name}
+                  </option>
+                ))}
+              </Select>
+            </div>
+            from
+            <Ref>{this.props.currentBranch.name}</Ref>
+          </div>
         </div>
       </div>
     )
