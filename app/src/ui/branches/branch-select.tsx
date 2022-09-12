b47f9675ccb54bfbaef9ccde6cc7a8ec57bf7e3b
tidy-dev <75402236+tidy-dev@users.noreply.github.com>
2022-09-12 15:44:43 -0400

    Add dropdown popover

--- app/src/ui/branches/branch-select.tsx
+++ app/src/ui/branches/branch-select.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Branch } from '../../models/branch'
+import { Button } from '../lib/button'
 import { Ref } from '../lib/ref'
 import { Octicon } from '../octicons'
 import * as OcticonSymbol from '../octicons/octicons.generated'
@@ -12,17 +13,49 @@ interface IBranchSelectProps {
   readonly onChange?: (branch: Branch) => void
 }
 
+interface IBranchSelectState {
+  readonly showBranchDropdown: boolean
+  readonly selectedBranch: Branch
+}
+
 /**
  * A branch select element for filter and selecting a branch.
  */
-export class BranchSelect extends React.Component<IBranchSelectProps, {}> {
+export class BranchSelect extends React.Component<
+  IBranchSelectProps,
+  IBranchSelectState
+> {
+  public constructor(props: IBranchSelectProps) {
+    super(props)
+
+    this.state = {
+      showBranchDropdown: false,
+      selectedBranch: props.branch,
+    }
+  }
+
+  private toggleBranchDropdown = () => {
+    this.setState({ showBranchDropdown: !this.state.showBranchDropdown })
+  }
+
+  public renderBranchDropdown() {
+    if (!this.state.showBranchDropdown) {
+      return
+    }
+
+    return <div className="branch-select-dropdown">List of branches here!</div>
+  }
+
   public render() {
     return (
       <div className="branch-select-component">
-        <Ref>
-          {this.props.selectedBranch.name}{' '}
-          <Octicon symbol={OcticonSymbol.triangleDown} />
-        </Ref>
+        <Button onClick={this.toggleBranchDropdown}>
+          <Ref>
+            {this.state.selectedBranch.name}{' '}
+            <Octicon symbol={OcticonSymbol.triangleDown} />
+          </Ref>
+        </Button>
+        {this.renderBranchDropdown()}
       </div>
     )
   }
