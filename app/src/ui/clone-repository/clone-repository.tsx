1ea55b46bc2ea544f51b8502e54a0ef67a0f587b
djdefi <djdefi@gmail.com>
2022-09-21 11:17:52 -0700

    Specify Enterprise *Server*

--- app/src/ui/clone-repository/clone-repository.tsx
+++ app/src/ui/clone-repository/clone-repository.tsx
@@ -476,7 +476,7 @@ export class CloneRepository extends React.Component<
             onAction={this.signInEnterprise}
           >
             <div>
-              If you have a GitHub Enterprise or AE account at work, sign in to
+              If you have a GitHub Enterprise Server or AE account at work, sign in to
               it to get access to your repositories.
             </div>
           </CallToAction>
