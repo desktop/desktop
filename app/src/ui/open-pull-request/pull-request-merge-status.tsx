1c7623648e647218f02f2000402ff08c5e4b9f19
tidy-dev <75402236+tidy-dev@users.noreply.github.com>
2022-10-04 13:38:20 -0400

    Add messages for each merge status kind
    Copying dotcom verbiage

--- app/src/ui/open-pull-request/pull-request-merge-status.tsx
+++ app/src/ui/open-pull-request/pull-request-merge-status.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react'
+import { assertNever } from '../../lib/fatal-error'
+import { ComputedAction } from '../../models/computed-action'
 import { MergeTreeResult } from '../../models/merge'
+import { Octicon } from '../octicons'
+import * as OcticonSymbol from '../octicons/octicons.generated'
 
 interface IPullRequestMergeStatusProps {
   /** The result of merging the pull request branch into the base branch */
@@ -9,7 +13,56 @@ interface IPullRequestMergeStatusProps {
 /** The component to display message about the result of merging the pull
  * request. */
 export class PullRequestMergeStatus extends React.Component<IPullRequestMergeStatusProps> {
+  private getMergeStatusDescription = () => {
+    const { mergeStatus } = this.props
+    if (mergeStatus === null) {
+      return ''
+    }
+
+    const { kind } = mergeStatus
+    switch (kind) {
+      case ComputedAction.Loading:
+        return (
+          <span className="pr-merge-status-loading">
+            <strong>Checking mergeability&hellip;</strong> Don’t worry, you can
+            still create the pull request.
+          </span>
+        )
+      case ComputedAction.Invalid:
+        return (
+          <span className="pr-merge-status-invalid">
+            <strong>Error checking merge status.</strong> Don’t worry, you can
+            still create the pull request.
+          </span>
+        )
+      case ComputedAction.Clean:
+        return (
+          <span className="pr-merge-status-clean">
+            <strong>
+              <Octicon symbol={OcticonSymbol.check} /> Able to merge.
+            </strong>{' '}
+            These branches can be automatically merged.
+          </span>
+        )
+      case ComputedAction.Conflicts:
+        return (
+          <span className="pr-merge-status-conflicts">
+            <strong>
+              <Octicon symbol={OcticonSymbol.x} /> Can't automatically merge.
+            </strong>{' '}
+            Don’t worry, you can still create the pull request.
+          </span>
+        )
+      default:
+        return assertNever(kind, `Unknown merge status kind of ${kind}.`)
+    }
+  }
+
   public render() {
-    return <div className="pull-request-merge-status">Merge Status</div>
+    return (
+      <div className="pull-request-merge-status">
+        {this.getMergeStatusDescription()}
+      </div>
+    )
   }
 }
