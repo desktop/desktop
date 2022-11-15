desktop/desktop

commit e11038a04fec292649bb1f9a0b427fd7fca8b64d

:art: includes
Co-Authored-By: icosamuel <begs1307@gmail.com>

app/src/ui/lib/list/list.tsx
@@ -397,7 +397,7 @@ export class List extends React.Component<IListProps, IListState> {
   private onRowMouseOver = (row: number, event: React.MouseEvent<any>) => {
     if (this.props.selectOnHover && this.canSelectRow(row)) {
       if (
-        this.props.selectedRows.indexOf(row) === -1 &&
+        this.props.selectedRows.includes(row) &&
         this.props.onSelectionChanged
       ) {
         this.props.onSelectionChanged([row], { kind: 'hover', event })

commit 0b711717fdff0a295080a567885a215a7c2ba883

Fix logic error in onRowMouseOver
Turns out https://github.com/desktop/desktop/commit/e11038a04fec292649bb1f9a0b427fd7fca8b64d introduced a regression where the intention was changed from "selection not included" to "selection included"

app/src/ui/lib/list/list.tsx
@@ -588,11 +588,8 @@ export class List extends React.Component<IListProps, IListState> {
 
   private onRowMouseOver = (row: number, event: React.MouseEvent<any>) => {
     if (this.props.selectOnHover && this.canSelectRow(row)) {
-      if (
-        this.props.selectedRows.includes(row) &&
-        this.props.onSelectionChanged
-      ) {
-        this.props.onSelectionChanged([row], { kind: 'hover', event })
+      if (!this.props.selectedRows.includes(row)) {
+        this.props.onSelectionChanged?.([row], { kind: 'hover', event })
         // By calling scrollRowToVisible we ensure that hovering over a partially
         // visible item at the top or bottom of the list scrolls it into view but
         // more importantly `scrollRowToVisible` automatically manages focus so