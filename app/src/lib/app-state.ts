9a990d19b9999fe0b8a346da4951a6e08fb5e917
tidy-dev <tidy-dev@github.com>
2022-11-22 10:32:58 -0500

    Add allPopups to the app state

--- app/src/lib/app-state.ts
+++ app/src/lib/app-state.ts
@@ -119,6 +119,7 @@ export interface IAppState {
   readonly showWelcomeFlow: boolean
   readonly focusCommitMessage: boolean
   readonly currentPopup: Popup | null
+  readonly allPopups: ReadonlyArray<Popup>
   readonly currentFoldout: Foldout | null
   readonly currentBanner: Banner | null
 
