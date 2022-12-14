7d65115d0553e78fd4019806aa24e6b2e233caf0
tidy-dev <tidy-dev@github.com>
2022-12-14 08:35:34 -0500

    Doc
    Co-Authored-By: Markus Olsson <634063+niik@users.noreply.github.com>

--- app/src/ui/dialog/is-top-most.tsx
+++ app/src/ui/dialog/is-top-most.tsx
@@ -1,5 +1,8 @@
 import memoizeOne from 'memoize-one'
 
+/** This method is a memoizedOne for a consistent means of handling when the
+ * isTopMost property of the `DialogStackContext` changes in the various popups
+ * that consume it.  */
 export function isTopMostDialog(
   onDialogIsTopMost: () => void,
   onDialogIsNotTopMost: () => void
