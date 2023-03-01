9cfd9097067353684f9ed5a9a8459e6a8d961278
tidy-dev <75402236+tidy-dev@users.noreply.github.com>
2023-03-01 07:40:59 -0500

    Update dispatcher.ts

--- app/src/ui/dispatcher/dispatcher.ts
+++ app/src/ui/dispatcher/dispatcher.ts
@@ -4053,8 +4053,6 @@ export class Dispatcher {
   }
 
   public launchTheAwesomeness(options: IAwesomeness) {
-    debugger
-    console.log(options)
     return this.appStore._launchTheAwesomeness(options)
   }
 }
