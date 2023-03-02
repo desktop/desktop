43da32d0a06e72a2a6d9c49b68d0b308ac14e3c6
tidy-dev <75402236+tidy-dev@users.noreply.github.com>
2023-03-02 09:19:30 -0500

    Add email label to misattributed warning select

--- app/src/ui/changes/commit-message-avatar.tsx
+++ app/src/ui/changes/commit-message-avatar.tsx
@@ -155,6 +155,7 @@ export class CommitMessageAvatar extends React.Component<
         </Row>
         <Row>
           <Select
+            label="Email"
             value={this.state.accountEmail}
             onChange={this.onSelectedGitHubEmailChange}
           >
