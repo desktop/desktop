a0cd65b0bb47fd479385cb81d00dbb9075ce26ec
Le Minh Tri <ansidev@gmail.com>
2023-05-11 01:57:38 +0700

    refactor: convert if-else to switch-case

--- app/src/lib/shells/darwin.ts
+++ app/src/lib/shells/darwin.ts
@@ -144,37 +144,36 @@ export function launch(
   foundShell: IFoundShell<Shell>,
   path: string
 ): ChildProcess {
-  if (foundShell.shell === Shell.Kitty) {
-    // kitty does not handle arguments as expected when using `open` with
-    // an existing session but closed window (it reverts to the previous
-    // directory rather than using the new directory directory).
-    //
-    // This workaround launches the internal `kitty` executable which
-    // will open a new window to the desired path.
-    return spawn(foundShell.path, ['--single-instance', '--directory', path])
-  } else if (
-    foundShell.shell === Shell.Alacritty ||
-    foundShell.shell === Shell.AlacrittyLegacy
-  ) {
-    // Alacritty cannot open files in the folder format.
-    //
-    // It uses --working-directory command to start the shell
-    // in the specified working directory.
-    return spawn(foundShell.path, ['--working-directory', path])
-  } else if (foundShell.shell === Shell.Tabby) {
-    // Tabby cannot open files in the folder format.
-    //
-    // It uses open command to start the shell
-    // in the specified working directory.
-    return spawn(foundShell.path, ['open', path])
-  } else if (foundShell.shell === Shell.WezTerm) {
-    // WezTerm, like Alacritty, "cannot open files in the 'folder' format."
-    //
-    // It uses the subcommand `start`, followed by the option `--cwd` to set
-    // the working directory, followed by the path.
-    return spawn(foundShell.path, ['start', '--cwd', path])
-  } else {
-    const bundleID = getBundleID(foundShell.shell)
-    return spawn('open', ['-b', bundleID, path])
+  switch (foundShell.shell) {
+    case Shell.Kitty:
+      // kitty does not handle arguments as expected when using `open` with
+      // an existing session but closed window (it reverts to the previous
+      // directory rather than using the new directory directory).
+      //
+      // This workaround launches the internal `kitty` executable which
+      // will open a new window to the desired path.
+      return spawn(foundShell.path, ['--single-instance', '--directory', path])
+    case Shell.Alacritty:
+    case Shell.AlacrittyLegacy:
+      // Alacritty cannot open files in the folder format.
+      //
+      // It uses --working-directory command to start the shell
+      // in the specified working directory.
+      return spawn(foundShell.path, ['--working-directory', path])
+    case Shell.Tabby:
+      // Tabby cannot open files in the folder format.
+      //
+      // It uses open command to start the shell
+      // in the specified working directory.
+      return spawn(foundShell.path, ['open', path])
+    case Shell.WezTerm:
+      // WezTerm, like Alacritty, "cannot open files in the 'folder' format."
+      //
+      // It uses the subcommand `start`, followed by the option `--cwd` to set
+      // the working directory, followed by the path.
+      return spawn(foundShell.path, ['start', '--cwd', path])
+    default:
+      const bundleID = getBundleID(foundShell.shell)
+      return spawn('open', ['-b', bundleID, path])
   }
 }
