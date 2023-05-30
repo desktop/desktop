e6f2095a805a23306d34a284364ace342a07db47
tidy-dev <75402236+tidy-dev@users.noreply.github.com>
2023-05-30 07:18:36 -0400

    Merge pull request #16566 from desktop/one-windows-version-ought-to-be-enough-for-anybody
    Disable automatic updates on Windows 7 / 8.1

---
+++ app/src/ui/banners/windows-version-no-longer-supported-banner.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import { Octicon } from '../octicons'
+import * as OcticonSymbol from '../octicons/octicons.generated'
+import { Banner } from './banner'
+import { LinkButton } from '../lib/link-button'
+import { setNumber } from '../../lib/local-storage'
+
+export const UnsupportedOSBannerDismissedAtKey =
+  'unsupported-os-banner-dismissed-at'
+
+export class WindowsVersionNoLongerSupportedBanner extends React.Component<{
+  onDismissed: () => void
+}> {
+  private onDismissed = () => {
+    setNumber(UnsupportedOSBannerDismissedAtKey, Date.now())
+    this.props.onDismissed()
+  }
+
+  public render() {
+    return (
+      <Banner
+        id="conflicts-found-banner"
+        dismissable={true}
+        onDismissed={this.onDismissed}
+      >
+        <Octicon className="alert-icon" symbol={OcticonSymbol.alert} />
+        <div className="banner-message">
+          <span>
+            This operating system is no longer supported. Software updates have
+            been disabled.
+          </span>
+          <LinkButton uri="https://docs.github.com/en/desktop/installing-and-configuring-github-desktop/overview/supported-operating-systems">
+            Support details
+          </LinkButton>
+        </div>
+      </Banner>
+    )
+  }
+}
