ca8f20e259747c45892a2a3625f77b2f9975563e
Markus Olsson <j.markus.olsson@gmail.com>
2023-06-20 16:22:43 +0200

    Grab windows pfx from secrets

--- script/setup-windows-certificate.ps1
+++ script/setup-windows-certificate.ps1
@@ -1,9 +1,2 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", "token $env:DESKTOPBOT_TOKEN")
-$headers.Add("Accept", 'application/vnd.github.v3.raw')
-
-Invoke-WebRequest 'https://api.github.com/repos/desktop/desktop-secrets/contents/windows-certificate.pfx' `
-              -Headers $headers `
-              -OutFile "$scriptPath\windows-certificate.pfx"
+[Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($env:DESKTOPBOT_TOKEN)) | Out-File "$scriptPath\windows-certificate.pfx"
