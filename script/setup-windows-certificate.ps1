ff0f8d167dbbcc6e65ffded0797f849ce181b9de
Markus Olsson <j.markus.olsson@gmail.com>
2023-06-20 16:37:07 +0200

    Pass the windows cert

--- script/setup-windows-certificate.ps1
+++ script/setup-windows-certificate.ps1
@@ -1,2 +1,2 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-[Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($env:DESKTOPBOT_TOKEN)) | Out-File "$scriptPath\windows-certificate.pfx"
+[Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($env:WINDOWS_CERT_PFX)) | Out-File "$scriptPath\windows-certificate.pfx"
