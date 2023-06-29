cfb092011bc8cc572558d3a7cc1ea364f86620e8
Markus Olsson <j.markus.olsson@gmail.com>
2023-06-29 19:20:50 +0200

    Encoding?

--- script/setup-windows-certificate.ps1
+++ script/setup-windows-certificate.ps1
@@ -1,2 +1,3 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-[Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($env:WINDOWS_CERT_PFX)) | Out-File "$scriptPath\windows-certificate.pfx"
+[Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($env:WINDOWS_CERT_PFX)) | Out-File "$scriptPath\windows-certificate.pfx" -encoding UTF8
+Get-Item "$scriptPath\windows-certificate.pfx" | Write-Out
