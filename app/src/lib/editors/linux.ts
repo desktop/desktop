6241472655c62e88693618494ca5de7bb6ab4cd5
Brendan Forster <github@brendanforster.com>
2023-07-03 17:04:21 -0300

    Remove geany as supported editor because it doesn't support opening a directory

--- app/src/lib/editors/linux.ts
+++ app/src/lib/editors/linux.ts
@@ -148,10 +148,6 @@ const editors: ILinuxExternalEditor[] = [
     paths: ['/usr/bin/notepadqq'],
   },
   {
-    name: 'Geany',
-    paths: ['/usr/bin/geany'],
-  },
-  {
     name: 'Mousepad',
     paths: ['/usr/bin/mousepad'],
   },
