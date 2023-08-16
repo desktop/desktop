2a5d637e7b83505a5614e181f519d6e7dcbf4c70
Sergio Padrino <sergio.padrino@gmail.com>
2023-08-16 18:23:06 +0200

    Add test for -git suffix in repo URLs

--- app/test/unit/remote-parsing-test.ts
+++ app/test/unit/remote-parsing-test.ts
@@ -9,6 +9,14 @@ describe('URL remote parsing', () => {
     expect(remote!.name).toBe('repo')
   })
 
+  it('parses HTTPS URLs with a trailing -git suffix', () => {
+    const remote = parseRemote('https://github.com/hubot/repo-git')
+    expect(remote).not.toBeNull()
+    expect(remote!.hostname).toBe('github.com')
+    expect(remote!.owner).toBe('hubot')
+    expect(remote!.name).toBe('repo-git')
+  })
+
   it('parses HTTPS URLs without a trailing git suffix', () => {
     const remote = parseRemote('https://github.com/hubot/repo')
     expect(remote).not.toBeNull()
