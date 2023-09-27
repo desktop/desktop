937a55e5dd2248a577042a5643ddb47d549447ee
Sergio Padrino <sergio.padrino@gmail.com>
2023-09-27 14:03:22 +0200

    Fix required Electron version for prod builds (24.8.3)

--- script/validate-electron-version.ts
+++ script/validate-electron-version.ts
@@ -15,7 +15,7 @@ type ChannelToValidate = 'production' | 'beta'
  * to a previous version of GitHub Desktop without losing all settings.
  */
 const ValidElectronVersions: Record<ChannelToValidate, string> = {
-  production: '24.4.0',
+  production: '24.8.3',
   beta: '26.2.1',
 }
 
