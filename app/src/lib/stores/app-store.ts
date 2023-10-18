3ed52eb84e32afa195b083e2e306dec711cd5e45
Jorge Pedroso <jpedroso@metaclassy.com>
2023-10-18 20:18:13 +0300

    Use a constant for the push/fetch button width
    Replace the inline hardcoded value with a constant defined closer the other toolbar constants.

--- app/src/lib/stores/app-store.ts
+++ app/src/lib/stores/app-store.ts
@@ -361,6 +361,8 @@ const pullRequestFileListConfigKey: string = 'pull-request-files-width'
 const defaultBranchDropdownWidth: number = 230
 const branchDropdownWidthConfigKey: string = 'branch-dropdown-width'
 
+const defaultPushFetchButtonWidth: number = 230
+
 const askToMoveToApplicationsFolderDefault: boolean = true
 const confirmRepoRemovalDefault: boolean = true
 const showCommitLengthWarningDefault: boolean = false
@@ -2257,7 +2259,8 @@ export class AppStore extends TypedBaseStore<IAppState> {
     // Since the repository list toolbar button width is tied to the width of
     // the sidebar we can't let it push the branch, and push/pull/fetch buttons
     // off screen.
-    const toolbarButtonsMinWidth = 460
+    const toolbarButtonsMinWidth =
+      defaultPushFetchButtonWidth + defaultBranchDropdownWidth
 
     // Start with all the available width
     let available = window.innerWidth
@@ -2288,7 +2291,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
     // Update the maximum width available for the branch dropdown resizable.
     // The branch dropdown can only be as wide as the available space
     // after taking the sidebar and pull fetch button widths.
-    const branchDropdownMax = available - 230
+    const branchDropdownMax = available - defaultPushFetchButtonWidth
     this.branchDropdownWidth = constrain(
       this.branchDropdownWidth,
       defaultBranchDropdownWidth,
