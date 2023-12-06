38c2d5855a6314293916c64e7f78e58eff072d7e
tidy-dev <75402236+tidy-dev@users.noreply.github.com>
2023-12-06 13:22:41 -0500

    Remove testing shortcut

--- app/src/lib/stores/helpers/tutorial-assessor.ts
+++ app/src/lib/stores/helpers/tutorial-assessor.ts
@@ -22,8 +22,10 @@ export class OnboardingTutorialAssessor {
     false
   )
   /** Has the user opted to skip the create pull request step? */
-  private prStepComplete: boolean =
-    1 === 1 ? false : getBoolean(pullRequestStepCompleteKey, false)
+  private prStepComplete: boolean = getBoolean(
+    pullRequestStepCompleteKey,
+    false
+  )
   /** Is the tutorial currently paused? */
   private tutorialPaused: boolean = getBoolean(tutorialPausedKey, false)
 
