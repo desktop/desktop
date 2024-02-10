72c10becb8f6ea7fb4207bdde465d45f0c204126
Jose Cortinas <jacortinas@gmail.com>
2024-02-09 20:50:29 -0600

    Update Octicon component for new data struct

--- app/src/ui/octicons/octicon.tsx
+++ app/src/ui/octicons/octicon.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react'
-import { OcticonSymbolType } from './octicons.generated'
+import {
+  OcticonSymbolType,
+  OcticonSymbolSize,
+  CustomOcticonSymbolType,
+  OcticonSymbolName,
+} from './octicons.generated'
 import classNames from 'classnames'
 import ReactDOM from 'react-dom'
 import { createObservableRef } from '../lib/observable-ref'
@@ -7,11 +12,11 @@ import { Tooltip, TooltipDirection } from '../lib/tooltip'
 
 interface IOcticonProps {
   /**
-   * An instance of an object conforming to the OcticonSymbol
-   * type. Supports custom paths as well as those provided
-   * through the static properties of the OcticonSymbol class.
+   * An instance of an object conforming to the OcticonSymbolType
+   * or CustomOcticonSymbolType type. Supports custom paths as well as
+   * those provided through the static properties of the OcticonSymbol class.
    */
-  readonly symbol: OcticonSymbolType
+  readonly symbol: OcticonSymbolType | CustomOcticonSymbolType
 
   /**
    * An optional classname that will be appended to the default
@@ -25,6 +30,8 @@ interface IOcticonProps {
   readonly title?: string
 
   readonly tooltipDirection?: TooltipDirection
+
+  readonly height?: OcticonSymbolSize
 }
 
 /**
@@ -34,15 +41,57 @@ interface IOcticonProps {
  * which is why the width and height properties specify the maximum and
  * not the minimum size.
  *
- * Usage: `<Octicon symbol={OcticonSymbol.mark_github} />`
+ * Usage: `<Octicon symbol={OcticonSymbol.markGithub} />`
  */
 export class Octicon extends React.Component<IOcticonProps, {}> {
   private svgRef = createObservableRef<SVGSVGElement>()
 
   public render() {
-    const { symbol, title, tooltipDirection } = this.props
-    const viewBox = `0 0 ${symbol.w} ${symbol.h}`
-    const className = classNames('octicon', this.props.className)
+    if ((this.props.symbol as CustomOcticonSymbolType).p !== undefined) {
+      return this.renderCustomIcon()
+    } else {
+      return this.renderOcticon()
+    }
+  }
+
+  private renderOcticon() {
+    const height = this.props.height ?? 16
+    const symbol = this.props.symbol as OcticonSymbolType
+
+    const naturalHeight = this.closestNaturalHeight(
+      Object.keys(symbol.h).map(h =>
+        parseInt(h, 10)
+      ) as Array<OcticonSymbolSize>,
+      height
+    )
+
+    const scaledSymbol = symbol.h[naturalHeight]!
+
+    const naturalWidth = scaledSymbol.w
+
+    const width = height * (naturalWidth / naturalHeight)
+
+    return this.renderIcon(symbol.s, height, width, scaledSymbol.p)
+  }
+
+  private renderCustomIcon() {
+    const symbol = this.props.symbol as CustomOcticonSymbolType
+    return this.renderIcon(symbol.s, symbol.h, symbol.w, symbol.p)
+  }
+
+  private renderIcon(
+    name: OcticonSymbolName | string,
+    height: OcticonSymbolSize | number,
+    width: number,
+    paths: string[]
+  ) {
+    const { title, tooltipDirection } = this.props
+    const viewBox = `0 0 ${width} ${height}`
+    const className = classNames(
+      'octicon',
+      `octicon-${name}`,
+      this.props.className
+    )
 
     // Hide the octicon from screen readers when it's only being used
     // as a visual without any attached meaning applicable to users
@@ -62,16 +111,33 @@ export class Octicon extends React.Component<IOcticonProps, {}> {
         viewBox={viewBox}
         ref={this.svgRef}
         tabIndex={-1}
+        height={height}
+        width={width}
       >
         {title !== undefined && (
           <Tooltip target={this.svgRef} direction={direction}>
             {title}
           </Tooltip>
         )}
-        {symbol.p.map((d, i) => (
+
+        {paths.map((d, i) => (
           <path key={i} d={d} />
         ))}
       </svg>
+    )
+  }
+
+  private closestNaturalHeight(
+    naturalHeights: Array<OcticonSymbolSize>,
+    height?: OcticonSymbolSize
+  ) {
+    if (height === undefined) {
+      return naturalHeights[0]
+    }
+
+    return naturalHeights.reduce(
+      (acc, naturalHeight) => (naturalHeight <= height ? naturalHeight : acc),
+      naturalHeights[0]
     )
   }
 }
