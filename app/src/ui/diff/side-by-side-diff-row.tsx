8d1d41321111a477bce6a7070302bdcef7c5dc28
tidy-dev <75402236+tidy-dev@users.noreply.github.com>
2024-02-14 07:21:14 -0500

    Let the hunk-handle take up space

--- app/src/ui/diff/side-by-side-diff-row.tsx
+++ app/src/ui/diff/side-by-side-diff-row.tsx
@@ -517,12 +517,6 @@ export class SideBySideDiffRow extends React.Component<
       return null
     }
 
-    // In unified mode, the hunk handle left position depends on the line gutter
-    // width.
-    const style: React.CSSProperties = this.props.showSideBySideDiff
-      ? {}
-      : { left: this.lineGutterWidth }
-
     return (
       // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
       <div
@@ -531,7 +525,6 @@ export class SideBySideDiffRow extends React.Component<
         onMouseLeave={this.onMouseLeaveHunk}
         onClick={this.onClickHunk}
         onContextMenu={this.onContextMenuHunk}
-        style={style}
       ></div>
     )
   }
