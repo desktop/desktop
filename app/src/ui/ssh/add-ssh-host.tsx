8245f2ed934b3a544c1f778b84100f1f4dbeb63e
Sergio Padrino <sergio.padrino@gmail.com>
2024-03-21 16:14:49 +0100

    Update AddSSHHost dialog to use backdropDismissable instead

--- app/src/ui/ssh/add-ssh-host.tsx
+++ app/src/ui/ssh/add-ssh-host.tsx
@@ -22,9 +22,9 @@ export class AddSSHHost extends React.Component<IAddSSHHostProps> {
         id="add-ssh-host"
         type="normal"
         title="SSH Host"
-        dismissable={false}
+        backdropDismissable={false}
         onSubmit={this.onSubmit}
-        onDismissed={this.props.onDismissed}
+        onDismissed={this.onCancel}
       >
         <DialogContent>
           <Row>
