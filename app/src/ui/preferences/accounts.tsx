08a51bca197c6d293dde915482e5455eb213406d
tidy-dev <75402236+tidy-dev@users.noreply.github.com>
2024-04-04 11:54:20 -0400

    Better manage accounts tab in settings when zoomed

--- app/src/ui/preferences/accounts.tsx
+++ app/src/ui/preferences/accounts.tsx
@@ -61,10 +61,12 @@ export class Accounts extends React.Component<IAccountsProps, {}> {
 
     return (
       <Row className="account-info">
-        <Avatar accounts={accounts} user={avatarUser} />
-        <div className="user-info">
-          <div className="name">{account.name}</div>
-          <div className="login">@{account.login}</div>
+        <div className="user-info-container">
+          <Avatar accounts={accounts} user={avatarUser} />
+          <div className="user-info">
+            <div className="name">{account.name}</div>
+            <div className="login">@{account.login}</div>
+          </div>
         </div>
         <Button onClick={this.logout(account)}>
           {__DARWIN__ ? 'Sign Out of' : 'Sign out of'} {accountTypeLabel}
