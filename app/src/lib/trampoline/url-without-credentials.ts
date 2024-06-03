d3ccb3ce6a04bcfb67d3bbba35992e606b6ded41
Markus Olsson <j.markus.olsson@gmail.com>
2024-06-03 17:29:09 +0200

    String or url, it's all the same

--- app/src/lib/trampoline/url-without-credentials.ts
+++ app/src/lib/trampoline/url-without-credentials.ts
@@ -1,4 +1,4 @@
-export function urlWithoutCredentials(remoteUrl: string): string {
+export function urlWithoutCredentials(remoteUrl: string | URL): string {
   const url = new URL(remoteUrl)
   url.username = ''
   url.password = ''
