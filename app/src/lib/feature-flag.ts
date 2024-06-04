f58f2ca2d82474e8acaf7a43bc4484ffcd08a173
Markus Olsson <j.markus.olsson@gmail.com>
2024-06-04 11:36:24 +0200

    Add flag for internal credential helper mode

--- app/src/lib/feature-flag.ts
+++ app/src/lib/feature-flag.ts
@@ -106,3 +106,4 @@ export const enableGroupDiffCheckmarks = enableDiffCheckMarksAndLinkUnderlines
 export const enableLinkUnderlines = enableDiffCheckMarksAndLinkUnderlines
 
 export const enableExternalCredentialHelper = enableBetaFeatures
+export const enableCredentialHelperTrampoline = enableBetaFeatures
