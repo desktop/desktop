ef8b3304922808a41545fc21e342c51cea24d811
Markus Olsson <j.markus.olsson@gmail.com>
2024-06-10 13:02:31 +0200

    Write new id if format is incorrect

--- app/src/lib/get-updater-guid.ts
+++ app/src/lib/get-updater-guid.ts
@@ -14,7 +14,7 @@ export const getUpdaterGUID = async () => {
     cachedGUID ??
     readFile(getUpdateGUIDPath(), 'utf8')
       .then(id => id.trim())
-      .then(id => (id.length === 36 ? id : undefined))
+      .then(id => (id.length === 36 ? id : writeUpdateGUID(uuid())))
       .catch(() => writeUpdateGUID(uuid()))
       .catch(e => {
         log.error(`Could not read update id`, e)
