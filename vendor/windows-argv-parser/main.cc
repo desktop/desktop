6cdf1e337da6e7f37ea58eca3677ff7c65a63aa0
Sergio Padrino <sergio.padrino@gmail.com>
2024-06-25 10:35:28 +0200

    Make the module work!

--- vendor/windows-argv-parser/main.cc
+++ vendor/windows-argv-parser/main.cc
@@ -1,114 +1,125 @@
 #include <node.h>
 #include <node_api.h>
 #include <nan.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "napi.h"
 
-#ifdef _WIN32
-#include <windows.h>
+using node::AddEnvironmentCleanupHook;
+using v8::HandleScope;
+using v8::Isolate;
+using v8::Local;
+using v8::Object;
 
-char **split_commandline(const char *cmdline, int *argc)
-{
-    int i;
-    char **argv = NULL;
-    assert(argc);
+// #ifdef _WIN32
+// #include <windows.h>
 
-    if (!cmdline)
-    {
-        return NULL;
-    }
+// char **split_commandline(const char *cmdline, int *argc)
+// {
+//     int i;
+//     char **argv = NULL;
+//     assert(argc);
 
-    wchar_t **wargs = NULL;
-    size_t needed = 0;
-    wchar_t *cmdlinew = NULL;
-    size_t len = strlen(cmdline) + 1;
+//     if (!cmdline)
+//     {
+//         return NULL;
+//     }
 
-    if (!(cmdlinew = (wchar_t *)calloc(len, sizeof(wchar_t))))
-        goto fail;
+//     wchar_t **wargs = NULL;
+//     size_t needed = 0;
+//     wchar_t *cmdlinew = NULL;
+//     size_t len = strlen(cmdline) + 1;
 
-    if (!MultiByteToWideChar(CP_ACP, 0, cmdline, -1, cmdlinew, len))
-        goto fail;
+//     if (!(cmdlinew = (wchar_t *)calloc(len, sizeof(wchar_t))))
+//         goto fail;
 
-    if (!(wargs = CommandLineToArgvW(cmdlinew, argc)))
-        goto fail;
+//     if (!MultiByteToWideChar(CP_ACP, 0, cmdline, -1, cmdlinew, len))
+//         goto fail;
 
-    if (!(argv = (char **)calloc(*argc, sizeof(char *))))
-        goto fail;
+//     if (!(wargs = CommandLineToArgvW(cmdlinew, argc)))
+//         goto fail;
 
-    // Convert from wchar_t * to ANSI char *
-    for (i = 0; i < *argc; i++)
-    {
-        // Get the size needed for the target buffer.
-        // CP_ACP = Ansi Codepage.
-        needed = WideCharToMultiByte(CP_ACP, 0, wargs[i], -1,
-                                    NULL, 0, NULL, NULL);
+//     if (!(argv = (char **)calloc(*argc, sizeof(char *))))
+//         goto fail;
 
-        if (!(argv[i] = (char *)malloc(needed)))
-            goto fail;
+//     // Convert from wchar_t * to ANSI char *
+//     for (i = 0; i < *argc; i++)
+//     {
+//         // Get the size needed for the target buffer.
+//         // CP_ACP = Ansi Codepage.
+//         needed = WideCharToMultiByte(CP_ACP, 0, wargs[i], -1,
+//                                     NULL, 0, NULL, NULL);
 
-        // Do the conversion.
-        needed = WideCharToMultiByte(CP_ACP, 0, wargs[i], -1,
-                                    argv[i], needed, NULL, NULL);
-    }
+//         if (!(argv[i] = (char *)malloc(needed)))
+//             goto fail;
 
-    if (wargs) LocalFree(wargs);
-    if (cmdlinew) free(cmdlinew);
-    return argv;
+//         // Do the conversion.
+//         needed = WideCharToMultiByte(CP_ACP, 0, wargs[i], -1,
+//                                     argv[i], needed, NULL, NULL);
+//     }
 
-fail:
-    if (wargs) LocalFree(wargs);
-    if (cmdlinew) free(cmdlinew);
+//     if (wargs) LocalFree(wargs);
+//     if (cmdlinew) free(cmdlinew);
+//     return argv;
 
-    if (argv)
-    {
-        for (i = 0; i < *argc; i++)
-        {
-            if (argv[i])
-            {
-                free(argv[i]);
-            }
-        }
+// fail:
+//     if (wargs) LocalFree(wargs);
+//     if (cmdlinew) free(cmdlinew);
 
-        free(argv);
-    }
+//     if (argv)
+//     {
+//         for (i = 0; i < *argc; i++)
+//         {
+//             if (argv[i])
+//             {
+//                 free(argv[i]);
+//             }
+//         }
 
-    return NULL;
+//         free(argv);
+//     }
+
+//     return NULL;
+// }
+
+// NAN_METHOD(ParseCommandLineArgv) {
+//   Nan::Utf8String commandLineNan(info[0]);
+//   std::string commandLine(*commandLineNan, commandLineNan.length());
+
+//   // Call split_commandline
+//   int argc;
+//   char **argv = split_commandline(commandLine.c_str(), &argc);
+
+//   // Create an array of strings from argv
+//   auto argvArray = Nan::New<v8::Array>(argc);
+//   for (int i = 0; i < argc; i++) {
+//     Nan::Set(argvArray, i, Nan::New(argv[i]).ToLocalChecked());
+//   }
+
+//   // Free memory
+//   for (int i = 0; i < argc; i++) {
+//     free(argv[i]);
+//   }
+//   free(argv);
+
+//   info.GetReturnValue().Set(argvArray);
+// }
+
+// #else
+
+Napi::Value ParseCommandLineArgv(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+
+    Napi::TypeError::New(env, "Not supported in this platform").
+      ThrowAsJavaScriptException();
+    return env.Undefined();
 }
 
-NAN_METHOD(ParseCommandLineArgv) {
-  Nan::Utf8String commandLineNan(info[0]);
-  std::string commandLine(*commandLineNan, commandLineNan.length());
+// #endif
 
-  // Call split_commandline
-  int argc;
-  char **argv = split_commandline(commandLine.c_str(), &argc);
-
-  // Create an array of strings from argv
-  auto argvArray = Nan::New<v8::Array>(argc);
-  for (int i = 0; i < argc; i++) {
-    Nan::Set(argvArray, i, Nan::New(argv[i]).ToLocalChecked());
-  }
-
-  // Free memory
-  for (int i = 0; i < argc; i++) {
-    free(argv[i]);
-  }
-  free(argv);
-
-  info.GetReturnValue().Set(argvArray);
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
+  exports.Set("parseCommandLineArgv", Napi::Function::New(env, ParseCommandLineArgv));
+  return exports;
 }
 
-#else
-
-NAN_METHOD(ParseCommandLineArgv) {
-  napi_value result;
-  napi_get_undefined(env, &result);
-  napi_throw_error(env, "ENOTSUP", "Not supported on this platform");
-  return result;
-}
-
-#endif
-
-NAN_MODULE_INIT(Init) {
-  Nan::SetMethod(target, "parseCommandLineArgv", ParseCommandLineArgv);
-}
-
-NAN_MODULE_WORKER_ENABLED("windows-argv-parser", Init)
+NODE_API_MODULE(windowsArgvParser, Init)
