392e600a6abe9eda9264a405287f48050e2b963b
Sergio Padrino <sergio.padrino@gmail.com>
2024-07-31 10:52:54 +0200

    More secret -> credential renaming

--- app/src/lib/ssh/ssh-credential-storage.ts
+++ app/src/lib/ssh/ssh-credential-storage.ts
@@ -10,12 +10,12 @@ type SSHCredentialEntry = {
   /** Store where this entry is stored. */
   store: string
 
-  /** Key used to identify the secret in the store (e.g. username or hash). */
+  /** Key used to identify the credential in the store (e.g. username or hash). */
   key: string
 }
 
 /**
- * This map contains the SSH secrets that are pending to be stored. What this
+ * This map contains the SSH credentials that are pending to be stored. What this
  * means is that a git operation is currently in progress, and the user wanted
  * to store the passphrase for the SSH key, however we don't want to store it
  * until we know the git operation finished successfully.
@@ -27,11 +27,11 @@ const mostRecentSSHCredentials = new Map<string, SSHCredentialEntry>()
  * the ongoing git operation fails to authenticate.
  *
  * @param operationGUID A unique identifier for the ongoing git operation. In
- *                      practice, it will always be the trampoline secret for the
+ *                      practice, it will always be the trampoline token for the
  *                      ongoing git operation.
- * @param store         Store where the SSH secret is stored.
- * @param key           Key that identifies the SSH secret (e.g. username or key
- *                      hash).
+ * @param store         Store where the SSH credential is stored.
+ * @param key           Key that identifies the SSH credential (e.g. username or
+ *                      key hash).
  * @param password      Password for the SSH key / user.
  */
 export async function setSSHCredential(
@@ -49,11 +49,11 @@ export async function setSSHCredential(
  * git operation fails to authenticate.
  *
  * @param operationGUID A unique identifier for the ongoing git operation. In
- *                      practice, it will always be the trampoline secret for the
+ *                      practice, it will always be the trampoline token for the
  *                      ongoing git operation.
- * @param store         Store where the SSH secret is stored.
- * @param key           Key that identifies the SSH secret (e.g. username or key
- *                      hash).
+ * @param store         Store where the SSH credential is stored.
+ * @param key           Key that identifies the SSH credential (e.g. username or
+ *                      key hash).
  */
 export function setMostRecentSSHCredential(
   operationGUID: string,
