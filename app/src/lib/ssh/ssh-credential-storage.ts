48886938fb60b0a73090201202361022751269f8
Sergio Padrino <sergio.padrino@gmail.com>
2024-07-31 10:07:33 +0200

    Improve documentation

--- app/src/lib/ssh/ssh-credential-storage.ts
+++ app/src/lib/ssh/ssh-credential-storage.ts
@@ -23,12 +23,35 @@ type SSHCredentialEntry = {
 const mostRecentSSHCredentials = new Map<string, SSHCredentialEntry>()
 
 /**
- * Keeps the SSH secret in memory to be deleted later if the ongoing git operation
- * fails to authenticate.
+ * Stores an SSH credential and also keeps it in memory to be deleted later if
+ * the ongoing git operation fails to authenticate.
  *
  * @param operationGUID A unique identifier for the ongoing git operation. In
  *                      practice, it will always be the trampoline secret for the
  *                      ongoing git operation.
+ * @param store         Store where the SSH secret is stored.
+ * @param key           Key that identifies the SSH secret (e.g. username or key
+ *                      hash).
+ * @param password      Password for the SSH key / user.
+ */
+export async function setSSHCredential(
+  operationGUID: string,
+  store: string,
+  key: string,
+  password: string) {
+
+  setMostRecentSSHCredential(operationGUID, store, key)
+  await TokenStore.setItem(store, key, password)
+}
+
+/**
+ * Keeps the SSH credential details in memory to be deleted later if the ongoing
+ * git operation fails to authenticate.
+ *
+ * @param operationGUID A unique identifier for the ongoing git operation. In
+ *                      practice, it will always be the trampoline secret for the
+ *                      ongoing git operation.
+ * @param store         Store where the SSH secret is stored.
  * @param key           Key that identifies the SSH secret (e.g. username or key
  *                      hash).
  */
@@ -40,11 +63,18 @@ export async function setMostRecentSSHCredential(
   mostRecentSSHCredentials.set(operationGUID, { store, key })
 }
 
-/** Removes the SSH key passphrase from memory. */
+/**
+ * Removes the SSH credential from memory. This must be used after a git
+ * operation finished, regardless the result.
+ */
 export function removeMostRecentSSHCredential(operationGUID: string) {
   mostRecentSSHCredentials.delete(operationGUID)
 }
 
+/**
+ * Deletes the SSH credential from the TokenStore. Used when the git operation
+ * fails to authenticate.
+ */
 export async function deleteMostRecentSSHCredential(operationGUID: string) {
   const entry = mostRecentSSHCredentials.get(operationGUID)
   if (entry) {
