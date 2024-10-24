e1cc57c0f202937e983fe2e3595b93ac55d90f9e
Markus Olsson <j.markus.olsson@gmail.com>
2024-10-24 13:59:50 +0200

    Infinity is the default now

--- app/src/lib/git/diff.ts
+++ app/src/lib/git/diff.ts
@@ -173,9 +173,7 @@ export async function getBranchMergeBaseDiff(
     args.push(file.status.oldPath)
   }
 
-  const result = await git(args, repository.path, 'getBranchMergeBaseDiff', {
-    maxBuffer: Infinity,
-  })
+  const result = await git(args, repository.path, 'getBranchMergeBaseDiff')
 
   return buildDiff(Buffer.from(result.stdout), repository, file, latestCommit)
 }
@@ -217,7 +215,6 @@ export async function getCommitRangeDiff(
   }
 
   const result = await git(args, repository.path, 'getCommitsDiff', {
-    maxBuffer: Infinity,
     expectedErrors: new Set([GitError.BadRevision]),
   })
 
