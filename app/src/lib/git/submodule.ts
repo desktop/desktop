66acf74dd03bef0523cebe7f93b97a568ee54aab
rschluep-posalux <rschluep@posalux.ch>
2026-08-13 14:08:04 +0200

    Try to test without forcing the repository initialization

--- app/src/lib/git/submodule.ts
+++ app/src/lib/git/submodule.ts
@@ -46,7 +46,7 @@ export async function updateSubmodulesAfterOperation<T extends Progress>(
     ...(allowFileProtocol ? ['-c', 'protocol.file.allow=always'] : []),
     'submodule',
     'update',
-    '--init',
+  //  '--init',
     '--recursive',
   ]
 
